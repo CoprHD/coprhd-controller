990ad6a087b4c042583f15eaf24c8daa4b939b45
Surendra <surendra.harikantra@emc.com>
2015-07-16 15:45:26 +0530

    Incorpoating Ben's review comments on synchronization object

--- vipr-portal/vipr-client/src/java/com/emc/vipr/client/impl/SSLUtil.java
+++ vipr-portal/vipr-client/src/java/com/emc/vipr/client/impl/SSLUtil.java
@@ -32,41 +32,53 @@ public class SSLUtil {
         setSSLSocketFactory(getTrustAllSslSocketFactory());
         trustAllEnabled = true;
     }
-    @SuppressWarnings("squid:S2444")//Suppressing sonar violation for the Lazy initialization of "static" field nullHostnameVerifier. If the static field is null then the object is instantiated.
+
     public static void trustAllHostnames() {
         if (nullHostnameVerifier == null) {
-            nullHostnameVerifier = getNullHostnameVerifier();
-            HttpsURLConnection.setDefaultHostnameVerifier(nullHostnameVerifier);
+            synchronized (SSLUtil.class) {
+                if (nullHostnameVerifier == null) {
+                    nullHostnameVerifier = getNullHostnameVerifier();
+                    HttpsURLConnection.setDefaultHostnameVerifier(nullHostnameVerifier);
+                }
+            }
         }
     }
 
-    @SuppressWarnings("squid:S2444")//Suppressing sonar violation for the Lazy initialization of "static" field nullHostnameVerifier. If the static field is null then the object is instantiated.
     public static NullHostNameVerifier getNullHostnameVerifier() {
         if (nullHostnameVerifier == null) {
-            nullHostnameVerifier = new NullHostNameVerifier();
+            synchronized (SSLUtil.class) {
+                if (nullHostnameVerifier == null) {
+                    nullHostnameVerifier = new NullHostNameVerifier();
+                }
+            }
         }
         return nullHostnameVerifier;
     }
 
-    @SuppressWarnings("squid:S2444")//Suppressing sonar violation for the Lazy initialization of "static" field trustAllSslSocketFactory. If the static field is null then the object is instantiated.
     public static SSLSocketFactory getTrustAllSslSocketFactory() {
         if (trustAllSslSocketFactory == null) {
-            SSLContext sc = getTrustAllContext();
-            trustAllSslSocketFactory = sc.getSocketFactory();
+            synchronized (SSLUtil.class) {
+                if (trustAllSslSocketFactory == null) {
+                    SSLContext sc = getTrustAllContext();
+                    trustAllSslSocketFactory = sc.getSocketFactory();
+                }
+            }
         }
         return trustAllSslSocketFactory;
     }
 
-    @SuppressWarnings("squid:S2444")//Suppressing sonar violation for the Lazy initialization of "static" field trustAllContext. If the static field is null then the object is instantiated.
     public static SSLContext getTrustAllContext() {
         if (trustAllContext == null) {
-            try {
-                SSLContext sc = SSLContext.getInstance("SSL");
-                sc.init(null, newTrustManagers(), new SecureRandom());
-                trustAllContext = sc;
-            }
-            catch (Exception e) {
-                log.error("Unable to register SSL TrustManager to trust all SSL Certificates", e);
+            synchronized (SSLUtil.class) {
+                if (trustAllContext == null) {
+                   try {
+                        SSLContext sc = SSLContext.getInstance("SSL");
+                        sc.init(null, newTrustManagers(), new SecureRandom());
+                        trustAllContext = sc;
+                   } catch (Exception e) {
+                        log.error("Unable to register SSL TrustManager to trust all SSL Certificates", e);
+                   }
+               }
             }
         }
         return trustAllContext;

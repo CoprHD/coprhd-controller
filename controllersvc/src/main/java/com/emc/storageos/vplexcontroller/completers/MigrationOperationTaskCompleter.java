ae1a6e229e06058a45e0976271ef1ba9ee6c6829
Tong <Tong.Yu@emc.com>
2015-07-21 10:42:56 -0400

    added new completer for migration operations

---
+++ controllersvc/src/main/java/com/emc/storageos/vplexcontroller/completers/MigrationOperationTaskCompleter.java
@@ -0,0 +1,40 @@
+package com.emc.storageos.vplexcontroller.completers;
+
+import java.net.URI;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.emc.storageos.db.client.DbClient;
+import com.emc.storageos.db.client.model.Operation;
+import com.emc.storageos.db.client.model.Volume;
+import com.emc.storageos.exceptions.DeviceControllerException;
+import com.emc.storageos.svcs.errorhandling.model.ServiceCoded;
+import com.emc.storageos.volumecontroller.TaskCompleter;
+
+public class MigrationOperationTaskCompleter extends TaskCompleter{
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+    private static final Logger s_logger = LoggerFactory.getLogger(MigrationOperationTaskCompleter.class);
+    
+    public MigrationOperationTaskCompleter(URI volURI, String opId) {
+        super(Volume.class, volURI, opId);
+    }
+    
+    @Override
+    protected void complete(DbClient dbClient, Operation.Status status, ServiceCoded coded) throws DeviceControllerException {
+
+        URI migrationURI = getId();
+        String opId = getOpId();
+        s_logger.info(String.format(
+            "Migration %s for task %s completed with status %s", migrationURI,
+            opId, status.name()));
+
+        // Update the task status.
+        setStatus(dbClient, status, coded);
+        s_logger.info("Updated status for migration task");
+    }
+
+}

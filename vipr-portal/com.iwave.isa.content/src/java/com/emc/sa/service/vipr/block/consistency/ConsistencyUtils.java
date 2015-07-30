bfd8593f472d5943353905cd70933b9c3497c918
Jay Logelin <Jay.Logelin@emc.com>
2015-07-30 13:33:57 -0300

    Added Consistency Group full copy services

--- vipr-portal/com.iwave.isa.content/src/java/com/emc/sa/service/vipr/block/consistency/ConsistencyUtils.java
+++ vipr-portal/com.iwave.isa.content/src/java/com/emc/sa/service/vipr/block/consistency/ConsistencyUtils.java
@@ -2,14 +2,17 @@ package com.emc.sa.service.vipr.block.consistency;
 
 import static com.emc.sa.service.vipr.ViPRExecutionUtils.addAffectedResources;
 import static com.emc.sa.service.vipr.ViPRExecutionUtils.execute;
-import static com.emc.sa.service.vipr.block.BlockStorageUtils.*;
+import static com.emc.sa.service.vipr.block.BlockStorageUtils.removeBlockResources;
 
 import java.net.URI;
 import java.util.Collections;
 
 import com.emc.sa.service.vipr.block.consistency.tasks.ActivateConsistencyGroupFullCopy;
 import com.emc.sa.service.vipr.block.consistency.tasks.CreateConsistencyGroupFullCopy;
+import com.emc.sa.service.vipr.block.consistency.tasks.DeactivateConsistencyGroupFullCopy;
 import com.emc.sa.service.vipr.block.consistency.tasks.DetachConsistencyGroupFullCopy;
+import com.emc.sa.service.vipr.block.consistency.tasks.RestoreConsistencyGroupFullCopy;
+import com.emc.sa.service.vipr.block.consistency.tasks.ResynchronizeConsistencyGroupFullCopy;
 import com.emc.storageos.model.block.BlockConsistencyGroupRestRep;
 import com.emc.storageos.model.block.VolumeDeleteTypeEnum;
 import com.emc.vipr.client.Tasks;
@@ -29,10 +32,24 @@ final class ConsistencyUtils {
 		return tasks;
 	}
 	
-	public static Tasks<BlockConsistencyGroupRestRep> activateConsistencyGroupFullCopy(URI consistencyGroupId, URI fullCopyId){
-		Tasks<BlockConsistencyGroupRestRep> copies = execute( new ActivateConsistencyGroupFullCopy(consistencyGroupId, fullCopyId) );
-		addAffectedResources(copies);
-        return copies;
+	public static Tasks<BlockConsistencyGroupRestRep> restoreFullCopy(URI consistencyGroupId, URI fullCopyId) {
+		return execute( new RestoreConsistencyGroupFullCopy(consistencyGroupId, fullCopyId) );
+	}
+	
+	public static Tasks<BlockConsistencyGroupRestRep> detachFullCopy(URI consistencyGroupId, URI fullCopyId) {
+		return execute( new DetachConsistencyGroupFullCopy(consistencyGroupId, fullCopyId) );
+	}
+	
+	public static Tasks<BlockConsistencyGroupRestRep> resynchronizeFullCopy(URI consistencyGroupId, URI fullCopyId) {
+		return execute( new ResynchronizeConsistencyGroupFullCopy(consistencyGroupId, fullCopyId) );
+	}
+	
+	public static Tasks<BlockConsistencyGroupRestRep> activateFullCopy(URI consistencyGroupId, URI fullCopyId){
+		return execute( new ActivateConsistencyGroupFullCopy(consistencyGroupId, fullCopyId) );
+	}
+	
+	public static Tasks<BlockConsistencyGroupRestRep> deactivateFullCopy(URI consistencyGroupId, URI fullCopyId){
+		return execute( new DeactivateConsistencyGroupFullCopy(consistencyGroupId, fullCopyId) );
 	}
 	
 	
---
+++ vipr-portal/com.iwave.isa.content/src/java/com/emc/sa/service/vipr/block/consistency/ResynchronizeFullCopyService.java
@@ -0,0 +1,29 @@
+package com.emc.sa.service.vipr.block.consistency;
+
+import static com.emc.sa.service.ServiceParams.CONSISTENCY_GROUP;
+import static com.emc.sa.service.ServiceParams.COPIES;
+import static com.emc.sa.service.vipr.block.consistency.ConsistencyUtils.resynchronizeFullCopy;
+
+import java.net.URI;
+import java.util.List;
+
+import com.emc.sa.engine.bind.Param;
+import com.emc.sa.engine.service.Service;
+import com.emc.sa.service.vipr.ViPRService;
+
+@Service("ConsistencyGroupResynchronizeFullCopy")
+public class ResynchronizeFullCopyService extends ViPRService {
+
+	@Param(CONSISTENCY_GROUP)
+	protected URI consistencyGroupId;
+
+	@Param(COPIES)
+	protected List<String> copyIds;
+
+	@Override
+	public void execute() throws Exception {
+		for (URI copyId : uris(copyIds)) {
+			resynchronizeFullCopy(consistencyGroupId, copyId);
+		}
+	}
+}

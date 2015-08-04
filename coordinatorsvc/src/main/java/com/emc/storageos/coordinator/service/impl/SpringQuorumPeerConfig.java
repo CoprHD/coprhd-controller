1d21a40b91d6f0cadf39b5086f38df013ea20872
Yao Yuan <yuan.yao@emc.com>
2015-08-04 10:34:44 +0800

    Preprocess ZK's server properties before send them to ZK. Parse server IP and port by seperator ',' instead of ':'

--- coordinatorsvc/src/main/java/com/emc/storageos/coordinator/service/impl/SpringQuorumPeerConfig.java
+++ coordinatorsvc/src/main/java/com/emc/storageos/coordinator/service/impl/SpringQuorumPeerConfig.java
@@ -8,13 +8,21 @@ package com.emc.storageos.coordinator.service.impl;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.Iterator;
+import java.util.Map.Entry;
 import java.util.Properties;
 
+import org.apache.zookeeper.server.quorum.QuorumPeer.LearnerType;
+import org.apache.zookeeper.server.quorum.QuorumPeer.QuorumServer;
 import org.apache.zookeeper.server.quorum.QuorumPeerConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.emc.storageos.coordinator.exceptions.CoordinatorException;
 
 public class SpringQuorumPeerConfig extends QuorumPeerConfig {
+    private static final Logger log = LoggerFactory.getLogger(SpringQuorumPeerConfig.class);
     private static final String SERVER_ID_FILE = "myid";
 
     private int _id;
@@ -47,6 +55,63 @@ public class SpringQuorumPeerConfig extends QuorumPeerConfig {
             writer.close();
         }
 
+        // Pre-process the properties to parse and remove server.* key/value
+        // In future, if _properties is still used somewhere else after init, we need to clone it keep original values
+        preprocessQuorumServers(_properties);
         parseProperties(_properties);
+    }
+
+    private void preprocessQuorumServers(Properties zkProp) throws ConfigException {
+        log.info("Preprocess quorum server from properties before send to ZK");
+
+        Iterator<Entry<Object, Object>> iterator = _properties.entrySet().iterator();
+        while (iterator.hasNext()) {
+            Entry<Object, Object> entry = iterator.next();
+            String key = entry.getKey().toString().trim();
+            if (key.startsWith("server.")) {
+                String value = entry.getValue().toString().trim();
+                createQuorumServer(key, value);
+                iterator.remove();
+            }
+        }
+    }
+
+    /**
+     * This logic is same as ZK 3.4.6 except splitting values with "," instead of ":"
+     */
+    private void createQuorumServer(String key, String value) throws ConfigException {
+        int dot = key.indexOf('.');
+        long sid = Long.parseLong(key.substring(dot + 1));
+        String parts[] = value.split(",");
+        if ((parts.length != 2) && (parts.length != 3) && (parts.length != 4)) {
+            log.error(value
+                    + " does not have the form host,port or host,port,port " +
+                    " or host,port,port,type");
+        }
+        InetSocketAddress addr = new InetSocketAddress(parts[0],
+                Integer.parseInt(parts[1]));
+        if (parts.length == 2) {
+            servers.put(Long.valueOf(sid), new QuorumServer(sid, addr));
+        } else if (parts.length == 3) {
+            InetSocketAddress electionAddr = new InetSocketAddress(
+                    parts[0], Integer.parseInt(parts[2]));
+            servers.put(Long.valueOf(sid), new QuorumServer(sid, addr,
+                    electionAddr));
+        } else if (parts.length == 4) {
+            InetSocketAddress electionAddr = new InetSocketAddress(
+                    parts[0], Integer.parseInt(parts[2]));
+            LearnerType type = LearnerType.PARTICIPANT;
+            if (parts[3].toLowerCase().equals("observer")) {
+                type = LearnerType.OBSERVER;
+                observers.put(Long.valueOf(sid), new QuorumServer(sid, addr,
+                        electionAddr, type));
+            } else if (parts[3].toLowerCase().equals("participant")) {
+                type = LearnerType.PARTICIPANT;
+                servers.put(Long.valueOf(sid), new QuorumServer(sid, addr,
+                        electionAddr, type));
+            } else {
+                throw new ConfigException("Unrecognised peertype: " + value);
+            }
+        }
     }
 }

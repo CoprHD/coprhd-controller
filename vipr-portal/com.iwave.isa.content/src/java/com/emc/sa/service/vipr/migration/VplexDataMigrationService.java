046026423ff24f49db092f47554f29473228ca2c
Jeremie Cormier <Jeremie.Cormier@emc.com>
2015-08-10 16:06:56 -0300

    First iteration for new Service Category 'Migration Services' with service 'VPLEX Data Migration'.

---
+++ vipr-portal/com.iwave.isa.content/src/java/com/emc/sa/service/vipr/migration/VplexDataMigrationService.java
@@ -0,0 +1,32 @@
+package com.emc.sa.service.vipr.migration;
+
+import java.net.URI;
+import java.util.List;
+
+import com.emc.sa.engine.bind.Param;
+import com.emc.sa.service.ServiceParams;
+import com.emc.sa.service.vipr.ViPRService;
+import com.emc.sa.service.vipr.block.tasks.ChangeBlockVolumeVirtualPool;
+import com.emc.storageos.model.block.VolumeRestRep;
+import com.emc.vipr.client.Tasks;
+
+public class VplexDataMigrationService extends ViPRService {
+
+    @Param(ServiceParams.PROJECT)
+    private URI projectId;
+
+    @Param(ServiceParams.VOLUME)
+    private List<String> volumeIds;
+
+    @Param(ServiceParams.TARGET_VIRTUAL_POOL)
+    private URI targetVirtualPool;
+
+    @Param(value=ServiceParams.CONSISTENCY_GROUP, required=false)
+    private URI consistencyGroup;
+
+    @Override
+    public void execute() throws Exception {
+        Tasks<VolumeRestRep> tasks = execute(new ChangeBlockVolumeVirtualPool(uris(volumeIds), targetVirtualPool, consistencyGroup));
+        addAffectedResources(tasks);
+    }
+}

6ea6be8ae54923363f4d3b4bad72f8a705c42aef
Nicholas Aquino <nicholas.aquino@emc.com>
2015-08-12 11:22:14 -0400

    fixing ServiceImpl.java to use _nodeName as NODE_ID_KEY because this cannot be changed

--- coordinatorsvc/src/main/java/com/emc/storageos/coordinator/common/impl/ServiceImpl.java
+++ coordinatorsvc/src/main/java/com/emc/storageos/coordinator/common/impl/ServiceImpl.java
@@ -29,7 +29,7 @@ public class ServiceImpl implements Service {
     private static final String ID_KEY = "_id";
     // Node id used for external display/query purpose.
     // EX: vipr1, vipr2, dataservice-10_247_100_15
-    private static final String NODE_ID_KEY= "_nodeId";
+    private static final String NODE_ID_KEY= "_nodeName";
     private static final String ENDPOINT_KEY = "_endpoint";
 
     private Properties _map = new Properties();

fc7c2056c7450bc91b11efef813ae2fa25898f0c
Ganesh, Om <om.ganesh@emc.com>
2015-08-12 17:55:16 +0530

    Checking in the stuff for Nas Representation

---
+++ internalLibraries/models/src/main/java/com/emc/storageos/model/vnas/VirtualNASList.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2015 EMC Corporation
+ * All Rights Reserved
+ */
+package com.emc.storageos.model.vnas;
+
+import com.emc.storageos.model.NamedRelatedResourceRep;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Class represents a return type that returns the id and self link for a
+ * list of storage ports.
+ */
+@XmlRootElement(name = "vnas_servers")
+public class VirtualNASList {
+    private List<NamedRelatedResourceRep> vnasservers;
+
+    public VirtualNASList() {
+    }
+
+    public VirtualNASList(List<NamedRelatedResourceRep> vnasservers) {
+        this.vnasservers = vnasservers;
+    }
+
+    /**o
+     * List of Storage ports. A Storage port represents a
+     * port of a storage device.
+     * 
+     * @valid none
+     */
+    @XmlElement(name = "vnas_server")
+    public List<NamedRelatedResourceRep> getPorts() {
+        if (vnasservers == null) {
+            vnasservers = new ArrayList<NamedRelatedResourceRep>();
+        }
+        return vnasservers;
+    }
+
+    public void setPorts(List<NamedRelatedResourceRep> vnasservers) {
+        this.vnasservers = vnasservers;
+    }
+}

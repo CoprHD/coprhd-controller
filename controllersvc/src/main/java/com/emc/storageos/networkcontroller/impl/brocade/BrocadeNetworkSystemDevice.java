9c87ab523c38604933760974b9c83b7377ef53b7
Nithya <Nithya.Ganesh@emc.com>
2015-08-12 20:58:21 -0400

    Reverted changes related to excluding alias for zone operations other than GET

--- controllersvc/src/main/java/com/emc/storageos/networkcontroller/impl/brocade/BrocadeNetworkSystemDevice.java
+++ controllersvc/src/main/java/com/emc/storageos/networkcontroller/impl/brocade/BrocadeNetworkSystemDevice.java
@@ -473,7 +473,7 @@ public class BrocadeNetworkSystemDevice extends NetworkSystemDeviceImpl
         boolean added = false;
         _log.info("Starting create zone with name " + zone.getName());
         // check if an active zone with the same name exists
-        Zone zoneInFabric = _smisHelper.getZone(client, zone.getName(), fabricWwn, true, true, false);
+        Zone zoneInFabric = _smisHelper.getZone(client, zone.getName(), fabricWwn, true, true, true);
         if (zoneInFabric != null) {
             _log.info("Found an active zone with the name " + zone.getName());
             // I have a active zone with the same name but it does not have all the members we
@@ -484,7 +484,7 @@ public class BrocadeNetworkSystemDevice extends NetworkSystemDeviceImpl
             }
         } else {
             // check if an inactive zone with the same name exists
-            zoneInFabric = _smisHelper.getZone(client, zone.getName(), fabricWwn, false, true, false);
+            zoneInFabric = _smisHelper.getZone(client, zone.getName(), fabricWwn, false, true, true);
             if (zoneInFabric != null) {
                 _log.info("Found an inactive zone with the name " + zone.getName());
                 if (activateZones) {
@@ -628,7 +628,7 @@ public class BrocadeNetworkSystemDevice extends NetworkSystemDeviceImpl
 
             // The actual work should be done on an inactive
             shadowZonsetPath = _smisHelper.getShadowZonesetPath(client, fabricId, fabricWwn, activeZonesetIns);
-            Map<String, Zone> zonesInFabric = _smisHelper.getZones(client, getZoneNames(zones), fabricWwn, false, true, false);
+            Map<String, Zone> zonesInFabric = _smisHelper.getZones(client, getZoneNames(zones), fabricWwn, false, true, true);
 
             // Find the set of zones to be actually deleted.
             // We don't attempt to delete zones that are already gone.
@@ -822,7 +822,7 @@ public class BrocadeNetworkSystemDevice extends NetworkSystemDeviceImpl
                 zonesetPath = _smisHelper.getShadowZonesetPath(client, fabricId, fabricWwn, activeZonesetIns);
             }
 
-            Map<String, Zone> zonesInFabric = _smisHelper.getZones(client, getZoneNames(zones), fabricWwn, false, true, false);
+            Map<String, Zone> zonesInFabric = _smisHelper.getZones(client, getZoneNames(zones), fabricWwn, false, true, true);
             for (ZoneUpdate zone : zones) {
                 try {
                     if (checkAndUpdateZone(client, zoneServiceIns, fabricId, fabricWwn, zonesetPath, zonesInFabric, zone)) {
@@ -999,7 +999,7 @@ public class BrocadeNetworkSystemDevice extends NetworkSystemDeviceImpl
             String name, CIMObjectPath path) throws WBEMException {
         Map<String, ZoneMember> map = new HashMap<String, ZoneMember>();
         try {
-            List<ZoneMember> members = _smisHelper.getZoneMembers(client, path, false);
+            List<ZoneMember> members = _smisHelper.getZoneMembers(client, path, true);
             for (ZoneMember member : members) {
                 if (member.hasAlias()) {
                     map.put(member.getAlias(), member);

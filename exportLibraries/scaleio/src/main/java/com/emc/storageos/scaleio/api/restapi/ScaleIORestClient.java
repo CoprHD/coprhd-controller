7caa93d5532c673c754196398a6f60b0420d3736
Tong <Tong.Yu@emc.com>
2015-08-14 16:16:08 -0400

    fixed the copyright comments

--- exportLibraries/scaleio/src/main/java/com/emc/storageos/scaleio/api/restapi/ScaleIORestClient.java
+++ exportLibraries/scaleio/src/main/java/com/emc/storageos/scaleio/api/restapi/ScaleIORestClient.java
@@ -1,12 +1,6 @@
 /**
  * Copyright (c) 2015 EMC Corporation
  * All Rights Reserved
- *
- * This software contains the intellectual property of EMC Corporation
- * or is licensed to EMC Corporation from third parties.  Use of this
- * software and the intellectual property contained therein is expressly
- * limited to the terms and conditions of the License Agreement under which
- * it is provided by or on behalf of EMC.
  */
 package com.emc.storageos.scaleio.api.restapi;
 
@@ -63,7 +57,6 @@ public class ScaleIORestClient extends StandardRestClient {
 
     private static Logger log = LoggerFactory.getLogger(ScaleIORestClient.class);
     ScaleIORestClientFactory factory;
-    private Map<String, String> protectionDomainMap = new HashMap<String, String>();
 
     /**
      * Constructor
--- exportLibraries/scaleio/src/main/test/com/emc/storageos/scaleio/api/ScaleIORestClientTest.java
+++ exportLibraries/scaleio/src/main/test/com/emc/storageos/scaleio/api/ScaleIORestClientTest.java
@@ -1,31 +1,20 @@
 /**
  * Copyright (c) 2015 EMC Corporation
  * All Rights Reserved
- *
- * This software contains the intellectual property of EMC Corporation
- * or is licensed to EMC Corporation from third parties.  Use of this
- * software and the intellectual property contained therein is expressly
- * limited to the terms and conditions of the License Agreement under which
- * it is provided by or on behalf of EMC.
  */
 package com.emc.storageos.scaleio.api;
 
 import java.net.URI;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
-
 import org.junit.BeforeClass;
-import org.junit.Test;
 
 import com.emc.storageos.scaleio.api.restapi.ScaleIORestClient;
 import com.emc.storageos.scaleio.api.restapi.ScaleIORestClientFactory;
 import com.emc.storageos.scaleio.api.restapi.response.ScaleIOSDS;
 import com.emc.storageos.scaleio.api.restapi.response.ScaleIOScsiInitiator;
-import com.emc.storageos.scaleio.api.restapi.response.ScaleIOSnapshotVolumeResponse;
 import com.emc.storageos.scaleio.api.restapi.response.ScaleIOVolume;
 import com.emc.storageos.services.util.EnvConfig;
 

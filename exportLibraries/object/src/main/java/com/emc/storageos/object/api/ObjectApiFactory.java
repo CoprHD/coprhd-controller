7a3f0505bf2a796a9a0d2023a0919495fe5f202e
iyangars <sreenidhi.iyangar@emc.com>
2015-08-20 15:32:02 +0530

    configuration xml changes

--- exportLibraries/object/src/main/java/com/emc/storageos/object/api/ObjectApiFactory.java
+++ exportLibraries/object/src/main/java/com/emc/storageos/object/api/ObjectApiFactory.java
@@ -1,5 +1,17 @@
 package com.emc.storageos.object.api;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 public class ObjectApiFactory {
+	private Logger _log = LoggerFactory.getLogger(ObjectApiFactory.class);
+
+    /**
+     * Initialize
+     */
+    public void init() {
+        _log.info(" ObjectApi factory initialized");
+    }
 
 }

b62ff0f3a28d6dfd2bd382e886a5eea9d7a981f2
iyangars <sreenidhi.iyangar@emc.com>
2015-08-23 01:10:33 +0530

    conf updates6

--- controllersvc/src/main/java/com/emc/storageos/volumecontroller/impl/ecs/ECSObjectStorageDevice.java
+++ controllersvc/src/main/java/com/emc/storageos/volumecontroller/impl/ecs/ECSObjectStorageDevice.java
@@ -13,28 +13,34 @@ import org.apache.commons.httpclient.protocol.Protocol;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.emc.storageos.db.client.DbClient;
 import com.emc.storageos.ecs.api.ECSApiFactory;
 import com.emc.storageos.volumecontroller.ObjectStorageDevice;
 import com.sun.jersey.client.apache.ApacheHttpClientHandler;
 
 public class ECSObjectStorageDevice implements ObjectStorageDevice {
-//	  private Logger _log = LoggerFactory.getLogger(ECSApiFactory.class);
-//	  private ECSApiFactory _factory;
-//
-//	    /**
-//	     * Set ECS API factory
-//	     * 
-//	     * @param factory
-//	     */
-//	    public void setECSApiFactory(ECSApiFactory factory) {
-//	        _factory = factory;
-//	    }
-//
-//	    /**
-//	     * Initialize HTTP client
-//	     */
-//	    public void init() {
-//	    	_log.info("From ECSObjectStorageDevice:init");
-//
-//	    }
+	  private Logger _log = LoggerFactory.getLogger(ECSObjectStorageDevice.class);
+	  private ECSApiFactory _factory;
+	  private DbClient _dbClient;
+
+	    /**
+	     * Set ECS API factory
+	     * 
+	     * @param factory
+	     */
+	    public void setECSApiFactory(ECSApiFactory factory) {
+	        _factory = factory;
+	    }
+
+	    public void setDbClient(DbClient dbc) {
+	        _dbClient = dbc;
+	    }
+	    
+	    /**
+	     * Initialize HTTP client
+	     */
+	    public void init() {
+	    	_log.info("From ECSObjectStorageDevice:init");
+
+	    }
 }

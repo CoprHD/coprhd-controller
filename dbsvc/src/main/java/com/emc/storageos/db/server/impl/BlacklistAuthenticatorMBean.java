96a6a1925d9b072edae6be2fd87ac556f8818aef
Liu Hui <hui.liu@emc.com>
2015-09-03 12:23:24 -0400

    Adjust dbconf.yaml

---
+++ dbsvc/src/main/java/com/emc/storageos/db/server/impl/BlacklistAuthenticatorMBean.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2014 EMC Corporation
+ * All Rights Reserved
+ */
+
+package com.emc.storageos.db.server.impl;
+
+import java.util.List;
+
+/**
+ * The API for blacklist
+ */
+interface BlacklistAuthenticatorMBean {
+    static final String MBEAN_NAME = "com.emc.storageos.db.server.impl:type=BlacklistAuthenticator";
+
+    /**
+     * Add node IPs to blacklist
+     * 
+     * @param newBlacklist
+     */
+    void addToBlacklist(List<String> newBlacklist);
+
+    /**
+     * Remove give list of node IP from blacklist
+     * 
+     * @param nodeList
+     */
+    void removeFromBlacklist(List<String> nodeList);
+
+    /**
+     * Get list of node IP in blacklist
+     * 
+     * @return
+     */
+    List<String> getBlacklist();
+
+}

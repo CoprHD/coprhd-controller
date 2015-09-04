3c9f1e0bcd71418cdacacdb7efb77b3bd09f7e43
Bharath Sreekanth <bharath.sreekanth@emc.com>
2015-09-04 13:50:09 -0400

    CTRL12501: fix review comments from Mike H

--- controllersvc/src/main/java/com/emc/storageos/volumecontroller/Recommendation.java
+++ controllersvc/src/main/java/com/emc/storageos/volumecontroller/Recommendation.java
@@ -17,7 +17,8 @@ public class Recommendation implements Serializable {
 	
 	// The virtual array for the recommendation.
     private URI virtualArray;
-    // The vpool used to get the recommendation
+    
+    // The virtual pool used to get the recommendation
     private VirtualPool virtualPool;
     
     private URI sourceStorageSystem;
@@ -25,54 +26,101 @@ public class Recommendation implements Serializable {
     private String deviceType;
     private int resourceCount;
     
+    /**
+     * Getter for recommended Virtual Array 
+     * @return Recommended Virtual Array
+     */
     public URI getVirtualArray() {
 		return virtualArray;
 	}
 
+	/**
+	 * Setter for recommended Virtual Array 
+	 * @param virtualArray Recommended Virtual Array 
+	 */
 	public void setVirtualArray(URI virtualArray) {
 		this.virtualArray = virtualArray;
 	}
 
+	/** Getter for recommended Virtual Pool
+	 * @return Recommended Virtual Pool
+	 */
 	public VirtualPool getVirtualPool() {
 		return virtualPool;
 	}
 
+	/**
+	 *  Setter for recommended Virtual Pool
+	 * @param virtualPool Recommended Virtual Pool
+	 */
 	public void setVirtualPool(VirtualPool virtualPool) {
 		this.virtualPool = virtualPool;
 	}
     
+    /**
+     * Getter for Resource count
+     * @return Resource count 
+     */
     public int getResourceCount() {
         return resourceCount;
     }
 
+    /**
+     * Setter for Resource count
+     * @param resourceCount Resource Count
+     */
     public void setResourceCount(int resourceCount) {
         this.resourceCount = resourceCount;
     }
 
+    /**
+     * Getter for recommended Storage Pool
+     * @return Recommended Storage Pool
+     */
     public URI getSourceStoragePool() {
         return sourceStoragePool;
     }
 
-    public void setSourceStoragePool(URI _sourceStoragePool) {
-        this.sourceStoragePool = _sourceStoragePool;
+    /**
+     * Setter for recommended storage pool
+     * @param sourceStoragePool
+     */
+    public void setSourceStoragePool(URI sourceStoragePool) {
+        this.sourceStoragePool = sourceStoragePool;
     }
 
+    /**
+     * Getter for recommended storage system
+     * @return Recommended storage system
+     */
     public URI getSourceStorageSystem() {
         return sourceStorageSystem;
     }
 
-    public void setSourceStorageSystem(URI _sourceStorageSystem) {
-        this.sourceStorageSystem = _sourceStorageSystem;
+    /**
+     * Setter for recommended storage system
+     * @param sourceStorageSystem Recommended Storage System
+     */
+    public void setSourceStorageSystem(URI sourceStorageSystem) {
+        this.sourceStorageSystem = sourceStorageSystem;
     }
 
+	/**
+	 * Getter for recommended device type
+	 * @return Recommended device type
+	 */
 	public String getDeviceType() {
 		return deviceType;
 	}
 
-	public void setDeviceType(String _deviceType) {
-		this.deviceType = _deviceType;
+	/**
+	 * Setter for recommended device type
+	 * @param deviceType Recommended device type
+	 */
+	public void setDeviceType(String deviceType) {
+		this.deviceType = deviceType;
 	}
-	
+		
 	public String toString() {
 		StringBuffer buffer = new StringBuffer();
 		buffer.append("Recommendation results: \n");
--- controllersvc/src/main/java/com/emc/storageos/volumecontroller/VPlexRecommendation.java
+++ controllersvc/src/main/java/com/emc/storageos/volumecontroller/VPlexRecommendation.java
@@ -2,16 +2,7 @@
  * Copyright 2015 EMC Corporation
  * All Rights Reserved
  */
-/**
- * Copyright (c) 2013 EMC Corporation
- * All Rights Reserved
- *
- * This software contains the intellectual property of EMC Corporation
- * or is licensed to EMC Corporation from third parties.  Use of this
- * software and the intellectual property contained therein is expressly
- * limited to the terms and conditions of the License Agreement under which
- * it is provided by or on behalf of EMC.
- */
+
 package com.emc.storageos.volumecontroller;
 
 import java.net.URI;
@@ -25,17 +16,15 @@ import java.net.URI;
 public class VPlexRecommendation extends Recommendation {
     
     // The VPlex storage system.
-    private URI _vplexStorageSystem;
+    private URI vplexStorageSystem;
     
     @Override
 	public String toString() {
     	StringBuffer ret = new StringBuffer();
  
-    	ret.append("VPlexRecommendation [_vplexStorageSystem=" + getVPlexStorageSystem()
-						+ ", _varray=" + getVirtualArray()
-						+ ", _vpool=" + getVirtualPool()
-						+ "]");
-    	
+    	ret.append(String.format("%s %n", super.toString()));
+    	ret.append(String.format("Vplex Storage System : %s %n", this.getVPlexStorageSystem().toString()));
+    
 		return ret.toString();
 	}
     
@@ -45,9 +34,8 @@ public class VPlexRecommendation extends Recommendation {
      * @return The VPlex storage system for the recommendation.
      */
        
-     public URI getVPlexStorageSystem() {
-     
-        return _vplexStorageSystem;
+     public URI getVPlexStorageSystem() {     
+        return this.vplexStorageSystem;
     } 
     
     /**
@@ -56,8 +44,7 @@ public class VPlexRecommendation extends Recommendation {
      * @param vplexStorageSystem The VPlex storage system for the recommendation.
      */
    
-     public void setVPlexStorageSystem(URI vplexStorageSystem) {
-     
-        _vplexStorageSystem = vplexStorageSystem;
+     public void setVPlexStorageSystem(URI vplexStorageSystem) {     
+        this.vplexStorageSystem = vplexStorageSystem;
     }
 }

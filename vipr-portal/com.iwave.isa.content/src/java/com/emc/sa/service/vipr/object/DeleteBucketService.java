3593f2a4af3c34f80c7b32a3cec383c6baf80033
Jeremie Cormier <Jeremie.Cormier@emc.com>
2015-09-08 14:21:22 -0300

    Initial setup for new 'Object Storage Services'

---
+++ vipr-portal/com.iwave.isa.content/src/java/com/emc/sa/service/vipr/object/DeleteBucketService.java
@@ -0,0 +1,13 @@
+package com.emc.sa.service.vipr.object;
+
+import com.emc.sa.engine.service.Service;
+import com.emc.sa.service.vipr.ViPRService;
+
+@Service("DeleteBucket")
+public class DeleteBucketService extends ViPRService  {
+    
+    @Override
+    public void execute() throws Exception {
+        
+    }
+}
---
+++ vipr-portal/com.iwave.isa.content/src/java/com/emc/sa/service/vipr/object/EditBucketService.java
@@ -0,0 +1,13 @@
+package com.emc.sa.service.vipr.object;
+
+import com.emc.sa.engine.service.Service;
+import com.emc.sa.service.vipr.ViPRService;
+
+@Service("EditBucket")
+public class EditBucketService extends ViPRService {
+    
+    @Override
+    public void execute() throws Exception {
+        
+    }
+}

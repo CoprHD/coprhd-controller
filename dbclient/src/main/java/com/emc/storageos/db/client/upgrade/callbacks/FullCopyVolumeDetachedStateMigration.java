62aa1348b06baf7405742e6183d59f257a777d37
mhegeric <mike.hegerich@emc.com>
2015-09-10 14:07:45 -0400

    Fix upgrade callback

--- dbclient/src/main/java/com/emc/storageos/db/client/upgrade/callbacks/FullCopyVolumeDetachedStateMigration.java
+++ dbclient/src/main/java/com/emc/storageos/db/client/upgrade/callbacks/FullCopyVolumeDetachedStateMigration.java
@@ -5,6 +5,7 @@
 package com.emc.storageos.db.client.upgrade.callbacks;
 
 import java.net.URI;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 
@@ -12,7 +13,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.emc.storageos.db.client.DbClient;
-import com.emc.storageos.db.client.URIUtil;
 import com.emc.storageos.db.client.model.StringSet;
 import com.emc.storageos.db.client.model.Volume;
 import com.emc.storageos.db.client.model.Volume.ReplicationState;
@@ -35,40 +35,27 @@ public class FullCopyVolumeDetachedStateMigration extends BaseCustomMigrationCal
 
     /**
      * For all full copy volume that is in the detached state, make sure
-     * that it's associated source volume field is null and that it it
+     * that it's associated source volume field is null and that it is
      * removed from the full copies list of the source.
      */
     private void initializeVolumeFields() {
         s_logger.info("Updating detached full copy volumes.");
         DbClient dbClient = this.getDbClient();
-        List<URI> volumeURIs = dbClient.queryByType(Volume.class, false);
+        List<URI> volumeURIs = dbClient.queryByType(Volume.class, true);
 
         Iterator<Volume> volumes =
                 dbClient.queryIterativeObjects(Volume.class, volumeURIs);
         while (volumes.hasNext()) {
             Volume volume = volumes.next();
+            boolean volumeUpdated = false;
 
             s_logger.info("Examining Volume (id={}) for upgrade", volume.getId().toString());
             String replicaState = volume.getReplicaState();
             // Check if the replicate state is detached.
-            if ((NullColumnValueGetter.isNotNullValue(replicaState)) && (ReplicationState.DETACHED.name().equals(replicaState))) {
+            if ((NullColumnValueGetter.isNotNullValue(replicaState)) &&
+                    (ReplicationState.DETACHED.name().equals(replicaState))) {
                 URI sourceURI = volume.getAssociatedSourceVolume();
                 if (!NullColumnValueGetter.isNullURI(sourceURI)) {
-                    if (URIUtil.isType(sourceURI, Volume.class)) {
-                        Volume sourceVolume = dbClient.queryObject(Volume.class, sourceURI);
-                        StringSet fullCopies = sourceVolume.getFullCopies();
-                        String volumeId = volume.getId().toString();
-                        if ((fullCopies != null) && (fullCopies.contains(volumeId))) {
-                            // Existing, detached full copies in the database should be
-                            // removed from the full copies list of their source volume.
-                            // This is the change for Jira 12766 (COP-13552) which is
-                            // made in the Darth (2.4) ViPR release.
-                            s_logger.info("Removing from full copies list of source volume {}", sourceVolume.getId());
-                            fullCopies.remove(volumeId);
-                            dbClient.persistObject(sourceVolume);
-                        }
-                    }
-
                     // We make sure the associated source volume is null.
                     // This change was made in ViPR 2.3 for Jira 12659, but
                     // the 2.3 upgrade callback never marked the associated
@@ -77,8 +64,41 @@ public class FullCopyVolumeDetachedStateMigration extends BaseCustomMigrationCal
                     // See class FullCopyVolumeReplicaStateMigration.
                     s_logger.info("Setting associated source volume to null");
                     volume.setAssociatedSourceVolume(NullColumnValueGetter.getNullURI());
-                    dbClient.persistObject(volume);
+                    volumeUpdated = true;
                 }
+            }
+
+            // For any volume that has full copies, make sure none of those full copies
+            // are in a detached state.
+            HashSet<String> fullCopiesToRemove = new HashSet<String>();
+            StringSet fullCopies = volume.getFullCopies();
+            if (fullCopies != null) {
+                for (String fullCopyId : fullCopies) {
+                    Volume fullCopy = dbClient.queryObject(Volume.class, URI.create(fullCopyId));
+                    if (fullCopy != null) {
+                        replicaState = fullCopy.getReplicaState();
+                        // Check if the replicate state is detached.
+                        if ((NullColumnValueGetter.isNotNullValue(replicaState)) &&
+                                (ReplicationState.DETACHED.name().equals(replicaState))) {
+                            fullCopiesToRemove.add(fullCopyId);
+                        }
+                    } else {
+                        fullCopiesToRemove.add(fullCopyId);
+                    }
+                }
+
+                // Existing, detached full copies in the database should be
+                // removed from the full copies list of their source volume.
+                // This is the change for Jira 12766 (COP-13552) which is
+                // made in the Darth (2.4) ViPR release.
+                s_logger.info("Removing {} from full copies list of source volume {}:{}", fullCopiesToRemove, volume.getId());
+                fullCopies.removeAll(fullCopiesToRemove);
+                volumeUpdated = true;
+            }
+
+            // Persist the changes if necessary.
+            if (volumeUpdated) {
+                dbClient.persistObject(volume);
             }
         }
     }

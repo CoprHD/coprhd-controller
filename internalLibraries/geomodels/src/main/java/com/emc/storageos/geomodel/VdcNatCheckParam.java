43c598db21832f3d781d7ef5914a14ebad28472f
Yao Yuan <yuan.yao@emc.com>
2015-09-16 10:43:15 +0800

    add comments for why has empty marker class VdcNatCheckResponse and VdcNatCheckParam

--- internalLibraries/geomodels/src/main/java/com/emc/storageos/geomodel/VdcNatCheckParam.java
+++ internalLibraries/geomodels/src/main/java/com/emc/storageos/geomodel/VdcNatCheckParam.java
@@ -9,6 +9,11 @@ import javax.xml.bind.annotation.XmlRootElement;
 
 import com.emc.vipr.model.sys.NatCheckParam;
 
+/**
+ * We hold this empty marker class because:
+ *  1. VDC NAT check may send more extra VDS-specified fields
+ *  2. Back-compatible, make sure XML has same root tag as previous version
+ */
 @XmlRootElement(name = "vdc-nat-check")
 public class VdcNatCheckParam extends NatCheckParam{
 }
--- internalLibraries/geomodels/src/main/java/com/emc/storageos/geomodel/VdcNatCheckResponse.java
+++ internalLibraries/geomodels/src/main/java/com/emc/storageos/geomodel/VdcNatCheckResponse.java
@@ -9,6 +9,11 @@ import javax.xml.bind.annotation.XmlRootElement;
 
 import com.emc.vipr.model.sys.NatCheckResponse;
 
+/**
+ * We hold this empty marker class because:
+ *  1. VDC NAT check may return more extra VDS-specified fields
+ *  2. Back-compatible, make sure return XML has same root tag as previous version
+ */
 @XmlRootElement
 public class VdcNatCheckResponse extends NatCheckResponse {
     

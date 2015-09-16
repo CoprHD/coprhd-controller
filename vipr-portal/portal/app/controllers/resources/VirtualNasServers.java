71e8d974aabcd576d420e6a1eebac936e67951e0
Surendra <surendra.harikantra@emc.com>
2015-09-16 14:19:52 +0530

    fix for COP-16920

--- vipr-portal/portal/app/controllers/resources/VirtualNasServers.java
+++ vipr-portal/portal/app/controllers/resources/VirtualNasServers.java
@@ -4,8 +4,11 @@ import static com.emc.vipr.client.core.util.ResourceUtils.uri;
 import static util.BourneUtil.getViprClient;
 
 import java.net.URI;
+import java.util.Collections;
 import java.util.List;
 import java.util.Set;
+
+import org.apache.commons.lang.StringUtils;
 
 import models.datatable.VirtualNasServerDataTable;
 import play.mvc.With;
@@ -28,24 +31,35 @@ public class VirtualNasServers extends ResourceController{
         render();
     }
     
-    public static void virtualNasServersJson(String projectId){
-        
-        ProjectRestRep projRestRep = getViprClient().projects().get(uri(projectId));
-        List<VirtualNASRestRep> vNasServers = Lists.newArrayList();
-        Set<String> vNasIds = projRestRep.getAssignedVNasServers();
-        List<URI> vNasUris = Lists.newArrayList();
-        if (vNasIds!=null) {
-           for (String id:vNasIds) {
-             vNasUris.add(uri(id));
-           }
-           if(!vNasUris.isEmpty()){
-           	vNasServers = getViprClient().virtualNasServers().getByIds(vNasUris);
-           }
+    public static void virtualNasServersJson(String projectId) {
+        if (StringUtils.isNotBlank(projectId)) {
+            setActiveProjectId(projectId);
+        } else {
+            projectId = getActiveProjectId();
         }
-        
+        List<VirtualNASRestRep> vNasServers = getVirtualNasServers(projectId);
         renderJSON(DataTablesSupport.createJSON(vNasServers, params));
     }
-    
+
+    private static List<VirtualNASRestRep> getVirtualNasServers(String projectId) {
+        if (projectId == null) {
+            return Collections.EMPTY_LIST;
+        }
+        List<VirtualNASRestRep> vNasServers = Lists.newArrayList();
+        ProjectRestRep projRestRep = getViprClient().projects().get(uri(projectId));
+        Set<String> vNasIds = projRestRep.getAssignedVNasServers();
+        List<URI> vNasUris = Lists.newArrayList();
+        if (vNasIds != null) {
+            for (String id : vNasIds) {
+                vNasUris.add(uri(id));
+            }
+            if (!vNasUris.isEmpty()) {
+                vNasServers = getViprClient().virtualNasServers().getByIds(vNasUris);
+            }
+        }
+        return vNasServers;
+    }
+
     public static class VirtualNasForProjectDataTable extends VirtualNasServerDataTable{
     
         public VirtualNasForProjectDataTable(){

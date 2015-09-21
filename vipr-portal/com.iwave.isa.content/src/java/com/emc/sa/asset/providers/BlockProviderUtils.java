3e58c8e92b3bf7de0011cbc52744fb4a935da919
Jeremie Cormier <Jeremie.Cormier@emc.com>
2015-09-21 11:48:22 -0300

    Added check to avoid NPE if volume does not belong to a Datastore.

--- vipr-portal/com.iwave.isa.content/src/java/com/emc/sa/asset/providers/BlockProviderUtils.java
+++ vipr-portal/com.iwave.isa.content/src/java/com/emc/sa/asset/providers/BlockProviderUtils.java
@@ -79,13 +79,15 @@ public class BlockProviderUtils {
         for (VolumeRestRep volume : getExportedBlockVolumes(viprClient, tenantId, hostOrClusterId)) {
             for (URI hostId : hostIds) {
                 String ds = KnownMachineTags.getBlockVolumeVMFSDatastore(hostId, volume);
-                if (ds.equals(datastore)) {
-                    boolean isMounted = isMounted(hostId, volume);
-                    if (onlyMounted && isMounted) {
-                        volumes.add(volume);
-                    }
-                    else if (!onlyMounted && !isMounted) {
-                        volumes.add(volume);
+                if (StringUtils.isNotBlank(ds)) {
+                    if (ds.equals(datastore)) {
+                        boolean isMounted = isMounted(hostId, volume);
+                        if (onlyMounted && isMounted) {
+                            volumes.add(volume);
+                        }
+                        else if (!onlyMounted && !isMounted) {
+                            volumes.add(volume);
+                        }
                     }
                 }
             }

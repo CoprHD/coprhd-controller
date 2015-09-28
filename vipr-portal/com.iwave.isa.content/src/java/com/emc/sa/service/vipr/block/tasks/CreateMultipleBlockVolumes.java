08addcc97deff026d8c9f2f37da999e919fd95be
Trevor Dawe <trevor.dawe@emc.com>
2015-09-28 14:42:53 -0300

    COP-16633: Added task for creating multiple block volumes in parallel

---
+++ vipr-portal/com.iwave.isa.content/src/java/com/emc/sa/service/vipr/block/tasks/CreateMultipleBlockVolumes.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2015 EMC Corporation
+ * All Rights Reserved
+ */
+package com.emc.sa.service.vipr.block.tasks;
+
+import java.util.List;
+
+import com.emc.sa.service.vipr.block.BlockStorageUtils;
+import com.emc.sa.service.vipr.block.CreateBlockVolumeHelper;
+import com.emc.sa.service.vipr.tasks.WaitForTasks;
+import com.emc.storageos.model.block.VolumeCreate;
+import com.emc.storageos.model.block.VolumeRestRep;
+import com.emc.vipr.client.Tasks;
+
+public class CreateMultipleBlockVolumes extends WaitForTasks<VolumeRestRep> {
+    private final List<CreateBlockVolumeHelper> helpers;
+
+    public CreateMultipleBlockVolumes(List<CreateBlockVolumeHelper> helpers) {
+        this.helpers = helpers;
+    }
+
+    @Override
+    public Tasks<VolumeRestRep> doExecute() throws Exception {
+        Tasks<VolumeRestRep> tasks = null;
+        for (CreateBlockVolumeHelper param : helpers) {
+            String volumeSize = BlockStorageUtils.gbToVolumeSize(param.getSizeInGb());
+            VolumeCreate create = new VolumeCreate();
+            create.setVpool(param.getVirtualPool());
+            create.setVarray(param.getVirtualArray());
+            create.setProject(param.getProject());
+            create.setName(param.getName());
+            create.setSize(volumeSize);
+            int numberOfVolumes = 1;
+            if ((param.getCount() != null) && (param.getCount() > 1)) {
+                numberOfVolumes = param.getCount();
+            }
+            create.setCount(numberOfVolumes);
+            create.setConsistencyGroup(param.getConsistencyGroup());
+
+            if (tasks == null) {
+                getClient().blockVolumes().create(create);
+            } else {
+                tasks.getTasks().addAll(getClient().blockVolumes().create(create).getTasks());
+            }
+        }
+        return tasks;
+    }
+}

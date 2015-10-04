03998b1f2017f8db47a1c4879c8dbee863681eca
Nathan Beach <nathan.beach@emc.com>
2015-10-04 14:15:14 -0500

    COP-17173 fixing up error message for replicas missing

--- apisvc/src/main/java/com/emc/storageos/api/service/impl/resource/blockingestorchestration/BlockIngestOrchestrator.java
+++ apisvc/src/main/java/com/emc/storageos/api/service/impl/resource/blockingestorchestration/BlockIngestOrchestrator.java
@@ -963,8 +963,8 @@ public abstract class BlockIngestOrchestrator {
                 taskStatus = new StringBuffer();
                 taskStatusMap.put(currentUnManagedVolume.getNativeGuid(), taskStatus);
             }
-            taskStatus.append(String.format("The umanaged volume %s has been partially ingested, but all replicas "
-                    + "have not yet been ingested. Uningested replicas: %s.", currentUnManagedVolume.getLabel(), 
+            taskStatus.append(String.format("The umanaged volume %s has been partially ingested, but not all replicas "
+                    + "have been ingested. Uningested replicas: %s.", currentUnManagedVolume.getLabel(), 
                     Joiner.on("\t").join(unIngestedReplicas)));
             // clear the map and stop traversing
             parentReplicaMap.clear();

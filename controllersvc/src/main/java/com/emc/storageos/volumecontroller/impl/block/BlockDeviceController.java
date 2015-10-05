ce79f2ed60276f97624f24fb584b9e42e04a11e6
ravi edpuganti <ravi.edpuganti@emc.com>
2015-10-05 13:36:08 +0000

    review comment implemented

--- controllersvc/src/main/java/com/emc/storageos/volumecontroller/impl/block/BlockDeviceController.java
+++ controllersvc/src/main/java/com/emc/storageos/volumecontroller/impl/block/BlockDeviceController.java
@@ -2828,7 +2828,7 @@ public class BlockDeviceController implements BlockController, BlockOrchestratio
                         Volume cloneVol = _dbClient.queryObject(Volume.class, uri);
                         BlockObject sourceObj = BlockObject.fetch(_dbClient, cloneVol.getAssociatedSourceVolume());
                         // detach if source is snapshot, or storage system is not vmax/vnx/hds
-                    	if(storageSystem.deviceIsType(Type.openstack)){
+                    	if (storageSystem.deviceIsType(Type.openstack)){
                     		setCloneReplicaStateStep(workflow, storageSystem, asList(uri), waitForSyncStep, ReplicationState.SYNCHRONIZED);
                     	}
                         else if (sourceObj instanceof BlockSnapshot

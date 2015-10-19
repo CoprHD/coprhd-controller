37257919708026a21b98a07a1cf2e465ea8a7b0e
Trevor Dawe <trevor.dawe@emc.com>
2015-10-19 20:49:16 -0300

    COP-17623: Retry find datastore if it can't be found

--- vipr-portal/com.iwave.isa.content/src/java/com/emc/sa/service/vmware/tasks/FindDatastore.java
+++ vipr-portal/com.iwave.isa.content/src/java/com/emc/sa/service/vmware/tasks/FindDatastore.java
@@ -13,8 +13,13 @@ import com.vmware.vim25.mo.Datastore;
 public class FindDatastore extends ExecutionTask<Datastore> {
     @Inject
     private VCenterAPI vcenter;
-    private String datacenterName;
-    private String datastoreName;
+    private final String datacenterName;
+    private final String datastoreName;
+
+    private static long SECONDS = 1000;
+    private static long MINUTES = 60 * SECONDS;
+    private static long FIND_DATASTORE_TIMEOUT = 5 * MINUTES;
+    private static long DELAY = 5 * SECONDS;
 
     public FindDatastore(String datacenterName, String datastoreName) {
         this.datacenterName = datacenterName;
@@ -22,15 +27,35 @@ public class FindDatastore extends ExecutionTask<Datastore> {
         provideDetailArgs(datastoreName, datacenterName);
     }
 
+    private boolean canRetry(long start, long length) {
+        long timeout = start + length;
+        return System.currentTimeMillis() < timeout;
+    }
+
     @Override
     public Datastore executeTask() throws Exception {
         debug("Executing: %s", getDetail());
-        Datastore datastore = vcenter.findDatastore(datacenterName, datastoreName);
+        Datastore datastore = null;
+        long startTime = System.currentTimeMillis();
+
+        while ((datastore == null) && canRetry(startTime, FIND_DATASTORE_TIMEOUT)) {
+            pause(DELAY);
+            datastore = vcenter.findDatastore(datacenterName, datastoreName);
+        }
+
         if (datastore == null) {
-            // TODO: remove the datastore tags?
             throw stateException("FindDatastore.illegalState.noDatastore",
                     datacenterName, vcenter.getAboutInfo().getFullName(), datastoreName);
         }
         return datastore;
+    }
+
+    private void pause(long delay) {
+        try {
+            Thread.sleep(delay);
+        } catch (InterruptedException e) {
+            warn(e);
+            Thread.currentThread().interrupt();
+        }
     }
 }

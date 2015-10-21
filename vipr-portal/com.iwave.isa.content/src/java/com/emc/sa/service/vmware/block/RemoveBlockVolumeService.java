dc0094af4b3f0eeadb88b16ad9e7569ba98a7104
Trevor Dawe <trevor.dawe@emc.com>
2015-10-21 13:41:55 -0300

    COP-17569: For delete VMware volume, also unmount and detach before unexporting

--- vipr-portal/com.iwave.isa.content/src/java/com/emc/sa/service/vmware/block/RemoveBlockVolumeService.java
+++ vipr-portal/com.iwave.isa.content/src/java/com/emc/sa/service/vmware/block/RemoveBlockVolumeService.java
@@ -4,10 +4,66 @@
  */
 package com.emc.sa.service.vmware.block;
 
+import static com.emc.sa.service.ServiceParams.DELETION_TYPE;
+import static com.emc.sa.service.ServiceParams.HOST;
+import static com.emc.sa.service.ServiceParams.VOLUMES;
+
+import java.net.URI;
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
+
+import com.emc.sa.engine.ExecutionUtils;
+import com.emc.sa.engine.bind.Param;
 import com.emc.sa.engine.service.Service;
-import com.emc.sa.service.vipr.block.RemoveBlockStorageForHostService;
+import com.emc.sa.machinetags.KnownMachineTags;
+import com.emc.sa.service.vipr.block.BlockStorageUtils;
+import com.emc.sa.service.vmware.VMwareHostService;
+import com.emc.storageos.model.block.BlockObjectRestRep;
+import com.emc.storageos.model.block.VolumeDeleteTypeEnum;
+import com.vmware.vim25.mo.Datastore;
 
 @Service("VMware-RemoveBlockVolume")
-public class RemoveBlockVolumeService extends RemoveBlockStorageForHostService {
+public class RemoveBlockVolumeService extends VMwareHostService {
+
+    @Param(HOST)
+    protected URI hostId;
+
+    @Param(VOLUMES)
+    protected List<String> volumeIds;
+
+    @Param(DELETION_TYPE)
+    protected VolumeDeleteTypeEnum deletionType;
+
+    List<BlockObjectRestRep> volumes;
+
+    @Override
+    public void precheck() {
+        volumes = BlockStorageUtils.getBlockResources(uris(volumeIds));
+    }
+
+    @Override
+    public void execute() {
+        for (BlockObjectRestRep volume : volumes) {
+            String datastoreName = KnownMachineTags.getBlockVolumeVMFSDatastore(hostId, volume);
+            if (!StringUtils.isEmpty(datastoreName)) {
+                Datastore datastore = vmware.getDatastore(datacenter.getLabel(), datastoreName);
+                if (datastore != null) {
+                    vmware.unmountVmfsDatastore(host, cluster, datastore);
+                }
+            }
+        }
+
+        for (BlockObjectRestRep volume : volumes) {
+            vmware.detachLuns(host, cluster, volume);
+        }
+
+        BlockStorageUtils.removeBlockResources(uris(volumeIds), deletionType);
+        // form is always passing hostId, never clusterId - need to figure out which it is.
+        String hostOrClusterId = BlockStorageUtils.getHostOrClusterId(hostId);
+        if (hostOrClusterId != null) {
+            ExecutionUtils.addAffectedResource(hostOrClusterId.toString());
+        }
+    }
 
 }

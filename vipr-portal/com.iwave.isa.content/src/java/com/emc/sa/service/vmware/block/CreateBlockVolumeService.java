931f00e6cc771a92dd1c8e71b96797fb910fb20e
Jeremie Cormier <Jeremie.Cormier@emc.com>
2015-10-22 11:44:45 -0300

    Changed vmware CreateBlockVolumeService to extend CreateBlockStorageForHostService. 'Create Volume for VMWare' service has the exact same functionality as CreateBlockStorageForHostService, this change is to avoid code duplication.

--- vipr-portal/com.iwave.isa.content/src/java/com/emc/sa/service/vmware/block/CreateBlockVolumeService.java
+++ vipr-portal/com.iwave.isa.content/src/java/com/emc/sa/service/vmware/block/CreateBlockVolumeService.java
@@ -4,58 +4,10 @@
  */
 package com.emc.sa.service.vmware.block;
 
-import java.net.URI;
-import java.util.List;
-
-import com.emc.sa.engine.bind.Bindable;
-import com.emc.sa.engine.bind.BindingUtils;
 import com.emc.sa.engine.service.Service;
-import com.emc.sa.service.vipr.ViPRService;
-import com.emc.sa.service.vipr.block.BlockStorageUtils;
-import com.emc.sa.service.vipr.block.BlockStorageUtils.HostVolumeParams;
-import com.emc.sa.service.vipr.block.BlockStorageUtils.VolumeTable;
-import com.emc.sa.service.vipr.block.CreateBlockVolumeForHostHelper;
-import com.google.common.collect.Lists;
+import com.emc.sa.service.vipr.block.CreateBlockStorageForHostService;
 
 @Service("VMware-CreateBlockVolume")
-public class CreateBlockVolumeService extends ViPRService {
+public class CreateBlockVolumeService extends CreateBlockStorageForHostService {
 
-    @Bindable(itemType = VolumeTable.class)
-    protected VolumeTable[] volumeTable;
-
-    @Bindable
-    protected HostVolumeParams volumeParams = new HostVolumeParams();
-
-    List<String> datastoreNames = null;
-    List<String> volumeNames = null;
-
-    protected List<CreateBlockVolumeForHostHelper> createBlockVolumeHelpers = Lists.newArrayList();
-
-    @Override
-    public void init() throws Exception {
-        super.init();
-
-        // for each pair of volume name and size, create a createBlockVolumeHelper
-        for (VolumeTable volumes : volumeTable) {
-            CreateBlockVolumeForHostHelper createBlockVolumeHelper = new CreateBlockVolumeForHostHelper();
-            BindingUtils.bind(createBlockVolumeHelper, BlockStorageUtils.createParam(volumes, volumeParams));
-            createBlockVolumeHelpers.add(createBlockVolumeHelper);
-        }
-    }
-
-    @Override
-    public void precheck() throws Exception {
-        for (CreateBlockVolumeForHostHelper helper : createBlockVolumeHelpers) {
-            helper.precheck();
-        }
-    }
-
-    @Override
-    public void execute() throws Exception {
-        if (!createBlockVolumeHelpers.isEmpty()) {
-            List<URI> volumeIds = Lists.newArrayList();
-            volumeIds.addAll(BlockStorageUtils.createMultipleVolumes(createBlockVolumeHelpers));
-            createBlockVolumeHelpers.get(0).exportVolumes(volumeIds);
-        }
-    }
 }

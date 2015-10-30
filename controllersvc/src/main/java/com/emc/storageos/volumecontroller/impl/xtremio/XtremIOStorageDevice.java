2631637f931e77c36670db20b2705fca4c7c3e98
Bradley Hughes <bradley.hughes@emc.com>
2015-10-30 15:31:45 -0300

    COP-17871: Added same check for RP+VPLEX targets and journals to add to cg operation for XIO.

--- controllersvc/src/main/java/com/emc/storageos/volumecontroller/impl/xtremio/XtremIOStorageDevice.java
+++ controllersvc/src/main/java/com/emc/storageos/volumecontroller/impl/xtremio/XtremIOStorageDevice.java
@@ -631,9 +631,14 @@ public class XtremIOStorageDevice extends DefaultBlockStorageDevice {
             for (URI uri : blockObjects) {
                 BlockObject blockObject = BlockObject.fetch(dbClient, uri);
                 if (blockObject != null) {
-                    client.addVolumeToConsistencyGroup(blockObject.getLabel(), consistencyGroup.getLabel(), clusterName);
-                    blockObject.setConsistencyGroup(consistencyGroupId);
-                    updatedBlockObjects.add(blockObject);
+                    // Do not add RP+VPlex journal or target backing volumes to consistency groups.
+                    // This causes issues with local array snapshots of RP+VPlex volumes.
+                    if (!RPHelper.isAssociatedToRpVplexType((Volume)blockObject, dbClient,
+                                    PersonalityTypes.METADATA, PersonalityTypes.TARGET)) {
+                        client.addVolumeToConsistencyGroup(blockObject.getLabel(), consistencyGroup.getLabel(), clusterName);
+                        blockObject.setConsistencyGroup(consistencyGroupId);
+                        updatedBlockObjects.add(blockObject);
+                    }
                 }
             }
             dbClient.updateAndReindexObject(updatedBlockObjects);

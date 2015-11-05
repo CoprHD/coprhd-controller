f3438af4458a21c7345e2cc9193b3e2e31a56d88
elliott, bill <bill.elliott@emc.com>
2015-11-05 11:21:14 -0500

    Remove storage port creation for RP.

--- controllersvc/src/main/java/com/emc/storageos/volumecontroller/impl/plugins/RPCommunicationInterface.java
+++ controllersvc/src/main/java/com/emc/storageos/volumecontroller/impl/plugins/RPCommunicationInterface.java
@@ -25,9 +25,7 @@ import com.emc.storageos.db.client.model.AbstractChangeTrackingSet;
 import com.emc.storageos.db.client.model.BlockSnapshot;
 import com.emc.storageos.db.client.model.DataObject.Flag;
 import com.emc.storageos.db.client.model.DiscoveredDataObject;
-import com.emc.storageos.db.client.model.DiscoveredDataObject.CompatibilityStatus;
 import com.emc.storageos.db.client.model.DiscoveredDataObject.DataCollectionJobStatus;
-import com.emc.storageos.db.client.model.DiscoveredDataObject.DiscoveryStatus;
 import com.emc.storageos.db.client.model.DiscoveredDataObject.RegistrationStatus;
 import com.emc.storageos.db.client.model.DiscoveredDataObject.Type;
 import com.emc.storageos.db.client.model.Initiator;
@@ -40,8 +38,6 @@ import com.emc.storageos.db.client.model.ProtectionSystem;
 import com.emc.storageos.db.client.model.RPSiteArray;
 import com.emc.storageos.db.client.model.StoragePool;
 import com.emc.storageos.db.client.model.StoragePort;
-import com.emc.storageos.db.client.model.StoragePort.OperationalStatus;
-import com.emc.storageos.db.client.model.StoragePort.PortType;
 import com.emc.storageos.db.client.model.StorageSystem;
 import com.emc.storageos.db.client.model.StringMap;
 import com.emc.storageos.db.client.model.StringSet;
@@ -673,37 +669,6 @@ public class RPCommunicationInterface extends ExtendedCommunicationInterfaceImpl
                                 initiator.setProtocol("FC");
                                 initiator.setIsManualCreation(false);
                                 initiator = getOrCreateNewInitiator(initiator);
-
-                                // Create the storage port object (if it doesn't exist)
-                                String nativeGuid = NativeGUIDGenerator.generateNativeGuid(system,
-                                        wwn, NativeGUIDGenerator.PORT);
-                                StoragePort port = checkPortExistsInDB(nativeGuid);
-                                boolean isNew = false;
-                                if (null == port) {
-                                    isNew = true;
-                                    port = new StoragePort();
-                                    port.setId(URIUtil.createId(StoragePort.class));
-                                    port.setStorageDevice(system.getId());
-                                    port.setStorageHADomain(null);
-                                    port.setNativeGuid(nativeGuid);
-                                    port.setRegistrationStatus(DiscoveredDataObject.RegistrationStatus.REGISTERED
-                                            .toString());
-                                }
-                                port.setPortSpeed(Long.MAX_VALUE);
-                                port.setPortNetworkId(wwn);
-                                port.setPortType(PortType.Unknown.name());
-                                port.setOperationalStatus(OperationalStatus.OK.toString());
-                                port.setTransportType(StoragePort.TransportType.FC.name());
-                                port.setPortGroup(null);
-                                port.setLabel(site + "+" + wwn);
-                                port.setPortName(port.getLabel());
-                                port.setCompatibilityStatus(CompatibilityStatus.COMPATIBLE.name());
-                                port.setDiscoveryStatus(DiscoveryStatus.VISIBLE.name());
-                                if (isNew) {
-                                    _dbClient.createObject(port);
-                                } else {
-                                    _dbClient.persistObject(port);
-                                }
 
                                 isNetworkSystemConfigured = true; // Set this to true as we found the RP initiators in a Network on the Network
                                 // System

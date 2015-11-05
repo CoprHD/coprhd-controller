1c7097fe181ce6e4b7049797113f784985e908ed
elliott, bill <bill.elliott@emc.com>
2015-11-05 14:24:37 -0500

    remove unused param to duplicate label message; confusing error messages

--- apisvc/src/main/java/com/emc/storageos/api/service/impl/resource/ComputeImageService.java
+++ apisvc/src/main/java/com/emc/storageos/api/service/impl/resource/ComputeImageService.java
@@ -154,7 +154,7 @@ public class ComputeImageService extends TaskResourceService {
         log.info("createComputeImage");
         // unique name required
         ArgValidator.checkFieldNotEmpty(param.getName(), "name");
-        checkDuplicateLabel(ComputeImage.class, param.getName(), param.getName());
+        checkDuplicateLabel(ComputeImage.class, param.getName());
 
         ArgValidator.checkFieldNotEmpty(param.getImageUrl(), "image_url");
         ArgValidator.checkUrl(param.getImageUrl(), "image_url");
@@ -238,7 +238,7 @@ public class ComputeImageService extends TaskResourceService {
         ArgValidator.checkEntity(ci, id, isIdEmbeddedInURL(id));
 
         if (!ci.getLabel().equals(param.getName())) {
-            checkDuplicateLabel(ComputeImage.class, param.getName(), param.getName());
+            checkDuplicateLabel(ComputeImage.class, param.getName());
             ci.setLabel(param.getName());
         }
 

88042f28318fb13ad71377e0d1638fd25b9a1e83
Simon Wang <simon.wang@emc.com>
2015-11-09 16:27:11 -0500

    merge with failure handling branch

--- syssvc/src/main/java/com/emc/storageos/systemservices/impl/Main.java
+++ syssvc/src/main/java/com/emc/storageos/systemservices/impl/Main.java
@@ -4,7 +4,6 @@
  */
 package com.emc.storageos.systemservices.impl;
 
-import com.emc.storageos.systemservices.impl.security.IPSecMonitor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.bridge.SLF4JBridgeHandler;
@@ -14,7 +13,6 @@ public class Main {
     private static final Logger _log = LoggerFactory.getLogger(Main.class);
     private static final String BUILD_TYPE = "buildType";
     private static final String SERVICE_BEAN = "syssvcserver";
-    private static final String IPSEC_MONITOR_BEAN = "ipsecMonitor";
 
     public static void main(String[] args) {
         try {
@@ -22,14 +20,7 @@ public class Main {
             GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
             ctx.getEnvironment().setActiveProfiles(System.getProperty(BUILD_TYPE));
             ctx.load(args);
-
-            // start ipsec monitor
-            IPSecMonitor ipsecMonitor = (IPSecMonitor) ctx.getBean(IPSEC_MONITOR_BEAN);
-            ipsecMonitor.start();
-
             ctx.refresh();
-
-            // start syssvc
             SysSvcImpl sysservice = (SysSvcImpl) ctx.getBean(SERVICE_BEAN);
             sysservice.start();
         } catch (Exception e) {

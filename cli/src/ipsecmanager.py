9cd410d4fdd166b14f4e81c6b6f9af557e1a3075
Eswaramoorthy Saminathan <Eswaramoorthy.Saminathan@emc.com>
2015-11-13 14:19:43 -0500

    COP-17909 - IPsec key rotation CLI changes.

--- cli/src/ipsecmanager.py
+++ cli/src/ipsecmanager.py
@@ -31,6 +31,20 @@ class IPsecManager(object):
                                     IPsecManager.URI_IPSEC,
                                     None)
         return s
+
+    def get_ipsec_status(self, xml=False):
+        if(xml == False):
+            (s, h) = common.service_json_request(self.__ipAddr, self.__port,
+                                                 "GET",
+                                                 IPsecManager.URI_IPSEC,
+                                                 None)
+            return common.json_decode(s)
+        else:
+            (s, h) = common.service_json_request(self.__ipAddr, self.__port,
+                                                 "GET",
+                                                 IPsecManager.URI_IPSEC,
+                                                 None, None, xml)
+            return s
         
 			
 def rotate_ipsec_key_parser(subcommand_parsers, common_parser):
@@ -48,12 +62,42 @@ def rotate_ipsec_key(args):
     try:
         res = IPsecManager(args.ip, args.port).rotate_ipsec_key()
         if(not res or res == ''):
-            print 'Failed to rotated the IPsec key. Reason : ' + res
+            print 'Failed to rotate the IPsec key. Reason : ' + res
         else:
             print 'Successfully rotated the IPsec key. New IPsec configuration version is ' + res
             
     except SOSError as e:
         common.format_err_msg_and_raise("rotate", "IPsec key",
+                                        e.err_text, e.err_code)
+
+def get_ipsec_status_parser(subcommand_parsers, common_parser):
+    # add command parser
+    ipsec_status_parser = subcommand_parsers.add_parser('status',
+                    description='ViPR IPsec status CLI usage',
+                    parents=[common_parser],
+                    conflict_handler='resolve',
+                    help='Gets the IPsec status and current configuration version.')
+
+    ipsec_status_parser.add_argument('-xml',
+                             dest='xml',
+                             action='store_true',
+                             help='XML response')
+
+    ipsec_status_parser.set_defaults(func=get_ipsec_status)
+
+
+def get_ipsec_status(args):
+    try:
+        res = IPsecManager(args.ip, args.port).get_ipsec_status(args.xml)
+        if(not res or res == ''):
+            print 'Failed to get the IPsec status. Reason : ' + res
+        else:
+            if(args.xml == True):
+                return common.format_xml(res)
+            else:
+                return common.format_json_object(res)
+    except SOSError as e:
+        common.format_err_msg_and_raise("status", "IPsec",
                                         e.err_text, e.err_code)
 										
 def ipsec_parser(parent_subparser, common_parser):
@@ -68,3 +112,6 @@ def ipsec_parser(parent_subparser, common_parser):
 		
     # rotate IPsec key command parser
     rotate_ipsec_key_parser(subcommand_parsers, common_parser)
+
+    # get IPsec status command parser
+    get_ipsec_status_parser(subcommand_parsers, common_parser)

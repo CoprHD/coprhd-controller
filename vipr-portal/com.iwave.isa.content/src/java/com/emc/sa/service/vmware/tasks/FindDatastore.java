b738c0148a9cfa5151658f7e16865324d618f149
Trevor Dawe <trevor.dawe@emc.com>
2015-11-13 10:54:10 -0400

    COP-17623: If unable to find datastore, retry the operation

--- vipr-portal/com.iwave.isa.content/src/java/com/emc/sa/service/vmware/tasks/FindDatastore.java
+++ vipr-portal/com.iwave.isa.content/src/java/com/emc/sa/service/vmware/tasks/FindDatastore.java
@@ -6,11 +6,11 @@ package com.emc.sa.service.vmware.tasks;
 
 import javax.inject.Inject;
 
-import com.emc.sa.engine.ExecutionTask;
 import com.iwave.ext.vmware.VCenterAPI;
+import com.iwave.ext.vmware.VMWareException;
 import com.vmware.vim25.mo.Datastore;
 
-public class FindDatastore extends ExecutionTask<Datastore> {
+public class FindDatastore extends RetryableTask<Datastore> {
     @Inject
     private VCenterAPI vcenter;
     private String datacenterName;
@@ -23,14 +23,17 @@ public class FindDatastore extends ExecutionTask<Datastore> {
     }
 
     @Override
-    public Datastore executeTask() throws Exception {
+    protected Datastore tryExecute() {
         debug("Executing: %s", getDetail());
         Datastore datastore = vcenter.findDatastore(datacenterName, datastoreName);
         if (datastore == null) {
-            // TODO: remove the datastore tags?
-            throw stateException("FindDatastore.illegalState.noDatastore",
-                    datacenterName, vcenter.getAboutInfo().getFullName(), datastoreName);
+            throw new VMWareException("Unable to find datastore");
         }
         return datastore;
+    }
+
+    @Override
+    protected boolean canRetry(VMWareException e) {
+        return true;
     }
 }

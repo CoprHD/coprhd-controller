32d2b58a970624a2b58dbf7b5119e65dc2200cda
nujira <ranjith.nuji@emc.com>
2015-11-16 18:28:39 +0530

    remove log info details

---
+++ exportLibraries/cinder/src/main/java/com/emc/storageos/cinder/model/CinderDeafaultQuotaDetails.java
@@ -0,0 +1,20 @@
+package com.emc.storageos.cinder.model;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import org.codehaus.jackson.map.annotate.JsonRootName;
+
+@JsonRootName(value="quota_set")
+@XmlRootElement(name="quota_set")
+public class CinderDeafaultQuotaDetails{
+	
+	@XmlElement(name="quota_set")
+	public Map<String, String> quota_set = new HashMap<String, String>();
+
+	public Map<String, String> getQuota_set() {
+		return quota_set;
+	}
+	
+}

35fc1ab8df5ded49253c915453d2005cba42d04b
SuzyWu2014 <shujinwu@onid.oregonstate.edu>
2015-11-17 15:37:28 -0800

    init set up fixed

---
+++ exportLibraries/storagedrivers/scaleiodriver/src/test/java/com/emc/storageos/driver/scaleio/ScaleIOStorageDriverTest.java
@@ -0,0 +1,166 @@
+package com.emc.storageos.driver.scaleio;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by shujinwu on 11/17/15.
+ */
+public class ScaleIOStorageDriverTest {
+
+    @Test
+    public void testCreateVolumes() throws Exception {
+
+    }
+
+    @Test
+    public void testExpandVolume() throws Exception {
+
+    }
+
+    @Test
+    public void testDeleteVolumes() throws Exception {
+
+    }
+
+    @Test
+    public void testCreateVolumeSnapshot() throws Exception {
+
+    }
+
+    @Test
+    public void testRestoreSnapshot() throws Exception {
+
+    }
+
+    @Test
+    public void testDeleteVolumeSnapshot() throws Exception {
+
+    }
+
+    @Test
+    public void testCreateVolumeClone() throws Exception {
+
+    }
+
+    @Test
+    public void testDetachVolumeClone() throws Exception {
+
+    }
+
+    @Test
+    public void testRestoreFromClone() throws Exception {
+
+    }
+
+    @Test
+    public void testDeleteVolumeClone() throws Exception {
+
+    }
+
+    @Test
+    public void testCreateVolumeMirror() throws Exception {
+
+    }
+
+    @Test
+    public void testDeleteVolumeMirror() throws Exception {
+
+    }
+
+    @Test
+    public void testSplitVolumeMirror() throws Exception {
+
+    }
+
+    @Test
+    public void testResumeVolumeMirror() throws Exception {
+
+    }
+
+    @Test
+    public void testRestoreVolumeMirror() throws Exception {
+
+    }
+
+    @Test
+    public void testGetITL() throws Exception {
+
+    }
+
+    @Test
+    public void testExportVolumesToInitiators() throws Exception {
+
+    }
+
+    @Test
+    public void testUnexportVolumesFromInitiators() throws Exception {
+
+    }
+
+    @Test
+    public void testCreateConsistencyGroup() throws Exception {
+
+    }
+
+    @Test
+    public void testCreateConsistencyGroupSnapshot() throws Exception {
+
+    }
+
+    @Test
+    public void testDeleteConsistencyGroupSnapshot() throws Exception {
+
+    }
+
+    @Test
+    public void testCreateConsistencyGroupClone() throws Exception {
+
+    }
+
+    @Test
+    public void testDeleteConsistencyGroupClone() throws Exception {
+
+    }
+
+    @Test
+    public void testGetRegistrationData() throws Exception {
+
+    }
+
+    @Test
+    public void testDiscoverStorageSystem() throws Exception {
+
+    }
+
+    @Test
+    public void testDiscoverStoragePools() throws Exception {
+
+    }
+
+    @Test
+    public void testDiscoverStoragePorts() throws Exception {
+
+    }
+
+    @Test
+    public void testGetStorageVolumes() throws Exception {
+
+    }
+
+    @Test
+    public void testGetSystemTypes() throws Exception {
+
+    }
+
+    @Test
+    public void testGetTask() throws Exception {
+
+    }
+
+    @Test
+    public void testGetStorageObject() throws Exception {
+
+    }
+}

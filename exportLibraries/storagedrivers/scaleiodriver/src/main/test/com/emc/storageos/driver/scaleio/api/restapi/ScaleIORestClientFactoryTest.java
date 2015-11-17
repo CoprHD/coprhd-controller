04fb9eeeadb963b16ff3fbd30855136b78d413bd
SuzyWu2014 <shujinwu@onid.oregonstate.edu>
2015-11-16 21:02:17 -0800

    initial factory

---
+++ exportLibraries/storagedrivers/scaleiodriver/src/main/test/com/emc/storageos/driver/scaleio/api/restapi/ScaleIORestClientFactoryTest.java
@@ -0,0 +1,16 @@
+package com.emc.storageos.driver.scaleio.api.restapi;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by shujinwu on 11/16/15.
+ */
+public class ScaleIORestClientFactoryTest {
+
+    @Test
+    public void testCreateNewRestClient() throws Exception {
+
+    }
+}

3f867dcf47fc9cf7fa58113aa64cba527ba28ce3
Sathish Sampath <sathish.sampath@emc.com>
2015-11-18 16:21:15 -0500

    Feature development

---
+++ exportLibraries/xtremio/src/main/java/com/emc/storageos/xtremio/restapi/model/response/XtremIOConsistencyGroupVolume.java
@@ -0,0 +1,28 @@
+package com.emc.storageos.xtremio.restapi.model.response;
+
+import java.util.List;
+
+import org.codehaus.jackson.annotate.JsonProperty;
+import org.codehaus.jackson.map.annotate.JsonRootName;
+
+import com.google.gson.annotations.SerializedName;
+
+@JsonRootName(value = "xtremio_consistency_groups_volumes")
+
+public class XtremIOConsistencyGroupVolume {
+
+    @SerializedName("consistency-groups")
+    @JsonProperty(value = "consistency-groups")
+    private XtremIOObjectInfo[] consistencyGroups;
+
+    public XtremIOObjectInfo[] getConsitencyGroups() {
+        return consistencyGroups != null ? consistencyGroups.clone() : consistencyGroups;
+    }
+
+    public void setConsistencyGroups(XtremIOObjectInfo[] consistencyGroups) {
+        if (consistencyGroups != null) {
+            this.consistencyGroups = consistencyGroups.clone();
+        }
+    
+    }
+}

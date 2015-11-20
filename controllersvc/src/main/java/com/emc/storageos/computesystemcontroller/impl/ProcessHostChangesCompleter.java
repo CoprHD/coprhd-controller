89c026075bfa47b2744cf8f65bb8456d34682fe9
root <trevor.dawe@emc.com>
2015-11-20 20:22:46 +0000

    COP-18370: Fixed log message to include cluster name

--- controllersvc/src/main/java/com/emc/storageos/computesystemcontroller/impl/ProcessHostChangesCompleter.java
+++ controllersvc/src/main/java/com/emc/storageos/computesystemcontroller/impl/ProcessHostChangesCompleter.java
@@ -77,7 +77,7 @@ public class ProcessHostChangesCompleter extends TaskCompleter {
                 List<URI> clusterHosts = ComputeSystemHelper.getChildrenUris(dbClient, clusterId, Host.class, "cluster");
                 // don't delete cluster if auto-exports are disabled or all hosts weren't deleted (ex: hosts provisioned by ViPR)
                 if (!cluster.getAutoExportEnabled()) {
-                    _logger.info("do not delete cluster {} - auto exports are disabled");
+                    _logger.info("do not delete cluster {} - auto exports are disabled", cluster.getLabel());
                 } else if (!clusterHosts.isEmpty()) {
                     _logger.info("do not delete cluster {} - it still has hosts - disassociate it from vcenter", cluster.getLabel());
                     cluster.setVcenterDataCenter(NullColumnValueGetter.getNullURI());

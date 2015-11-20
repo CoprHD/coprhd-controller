0f934772bbb9675294980c4569d796476d2e967c
Simon Wang <simon.wang@emc.com>
2015-11-20 17:44:58 -0500

    make curator barrier with fix temporarily

--- syssvc/src/main/java/com/emc/storageos/systemservices/impl/property/VdcSiteManager.java
+++ syssvc/src/main/java/com/emc/storageos/systemservices/impl/property/VdcSiteManager.java
@@ -13,7 +13,7 @@ import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
-import org.apache.curator.framework.recipes.barriers.DistributedDoubleBarrier;
+import com.emc.storageos.coordinator.client.service.DistributedDoubleBarrier;
 import org.apache.curator.framework.recipes.locks.InterProcessLock;
 import org.apache.zookeeper.ZooKeeper.States;
 import org.slf4j.Logger;

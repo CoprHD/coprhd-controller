145dfe0b23be26ff85ff50094e79b565cddc1c87
Amit Saurabh <amit.saurabh@emc.com>
2015-11-23 15:12:13 +0530

    adding full control permission for edit nfs4 acl page.

--- vipr-portal/portal/app/controllers/resources/FileSystems.java
+++ vipr-portal/portal/app/controllers/resources/FileSystems.java
@@ -244,7 +244,7 @@ public class FileSystems extends ResourceController {
         nfsACL.permissions = new HashSet<String>(Arrays.asList(strPerm));
         nfsACL.permissionType = permissionType;
         renderArgs.put("permissionOptions", StringOption.options(new String[] {
-                "read", "write", "execute" }));
+                "read", "write", "execute", "fullControl" }));
         renderArgs.put("permissionTypeOptions", StringOption.options(new String[] {
                 "allow", "deny" }));
         renderArgs.put("fileSystemId", uri(fileSystem));

4b3e9e100b1b5fc7fa0c408b79e3eb148191aa6a
SuzyWu2014 <shujinwu@onid.oregonstate.edu>
2015-12-01 14:53:16 -0800

    COP-18463:remove previous registry related code

--- exportLibraries/storagedrivers/scaleiodriver/src/main/java/com/emc/storageos/driver/scaleio/ScaleIORestHandleFactory.java
+++ exportLibraries/storagedrivers/scaleiodriver/src/main/java/com/emc/storageos/driver/scaleio/ScaleIORestHandleFactory.java
@@ -4,12 +4,10 @@ package com.emc.storageos.driver.scaleio;
 import com.emc.storageos.driver.scaleio.api.ScaleIOConstants;
 import com.emc.storageos.driver.scaleio.api.restapi.ScaleIORestClient;
 import com.emc.storageos.driver.scaleio.api.restapi.ScaleIORestClientFactory;
-import com.emc.storageos.storagedriver.Registry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.net.URI;
-import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -59,33 +57,7 @@ public class ScaleIORestHandleFactory {
         return handle;
     }
 
-    /*
-    * Get Rest client handle for a scaleIO storage system
-    * @param systemNativeId
-    */
-    public ScaleIORestClient getClientHandle(String systemNativeId, Registry registry) throws Exception {
 
-        ScaleIORestClient handle = null;
-        Map<String, List<String>> attributes;
-
-        synchronized (syncObject) {
-            handle = ScaleIORestClientMap.get(systemNativeId);
-            if (handle == null) {
-                attributes = registry.getDriverAttributesForKey(ScaleIOConstants.DRIVER_NAME, systemNativeId);
-                if (attributes != null) {
-                    URI baseURI = URI.create(ScaleIOConstants.getAPIBaseURI(attributes.get(ScaleIOConstants.IP_ADDRESS).get(0),Integer.parseInt(attributes.get(ScaleIOConstants.PORT_NUMBER).get(0))));
-
-                    handle = (ScaleIORestClient) scaleIORestClientFactory.getRESTClient(baseURI, attributes.get(ScaleIOConstants.USER_NAME).get(0),
-                            attributes.get(ScaleIOConstants.PASSWORD).get(0), true);
-                    ScaleIORestClientMap.put(systemNativeId, handle);
-                } else {
-                    log.info("no connection INFO found in Registry");
-                    handle=null;
-                }
-            }
-            return handle;
-        }
-    }
 
 
 }

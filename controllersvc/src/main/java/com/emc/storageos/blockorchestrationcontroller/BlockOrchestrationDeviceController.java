9abf371a4c9df27ab05c8f88ebca381b8f40b714
Tong <Tong.Yu@emc.com>
2015-12-07 11:38:04 -0500

    revert back the change for COP-18929

--- controllersvc/src/main/java/com/emc/storageos/blockorchestrationcontroller/BlockOrchestrationDeviceController.java
+++ controllersvc/src/main/java/com/emc/storageos/blockorchestrationcontroller/BlockOrchestrationDeviceController.java
@@ -320,6 +320,11 @@ public class BlockOrchestrationDeviceController implements BlockOrchestrationCon
             waitFor = _rpDeviceController.addStepsForChangeVirtualPool(
                     workflow, waitFor, volumes, taskId);
 
+            // This step is currently used to ensure that any existing resources get added to native
+            // CGs. Mainly used for VPLEX->RP+VPLEX change vpool. The existing VPLEX volume would not be
+            // in any CG and we now need it's backing volume(s) to be added to their local array CG.
+            waitFor = postRPChangeVpoolSteps(workflow, waitFor, volumes, taskId);
+            
             // Finish up and execute the plan.
             // The Workflow will handle the TaskCompleter
             String successMessage = "Change Virtual Pool suceeded for volumes: " + volURIs.toString();

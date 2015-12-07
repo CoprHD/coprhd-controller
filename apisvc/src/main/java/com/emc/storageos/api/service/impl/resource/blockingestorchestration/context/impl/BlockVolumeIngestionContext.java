4ea76b5338280fddede80be6869cbf7c4c78446f
Nathan Beach <nathan.beach@emc.com>
2015-12-07 13:21:31 -0600

    javadoc for BlockVolumeIngestionContext

--- apisvc/src/main/java/com/emc/storageos/api/service/impl/resource/blockingestorchestration/context/impl/BlockVolumeIngestionContext.java
+++ apisvc/src/main/java/com/emc/storageos/api/service/impl/resource/blockingestorchestration/context/impl/BlockVolumeIngestionContext.java
@@ -10,30 +10,36 @@ import java.util.List;
 import com.emc.storageos.api.service.impl.resource.blockingestorchestration.context.VolumeIngestionContext;
 import com.emc.storageos.api.service.impl.resource.utils.VolumeIngestionUtil;
 import com.emc.storageos.db.client.DbClient;
-import com.emc.storageos.db.client.model.BlockObject;
 import com.emc.storageos.db.client.model.UnManagedDiscoveredObjects.UnManagedVolume;
 
+/**
+ * A VolumeIngestionContext implementation for general Block Volume ingestion.
+ */
 public class BlockVolumeIngestionContext implements VolumeIngestionContext {
 
     protected DbClient _dbClient;
 
-    private UnManagedVolume unManagedVolume;
-    private List<String> errorMessages;
+    private UnManagedVolume _unManagedVolume;
+    private List<String> _errorMessages;
 
     public BlockVolumeIngestionContext(UnManagedVolume unManagedVolume, DbClient dbClient) {
-        this.unManagedVolume = unManagedVolume;
+        this._unManagedVolume = unManagedVolume;
         this._dbClient = dbClient;
     }
 
-    /* (non-Javadoc)
+    /*
+     * (non-Javadoc)
+     * 
      * @see com.emc.storageos.api.service.impl.resource.blockingestorchestration.context.VolumeIngestionContext#getUnmanagedVolume()
      */
     @Override
     public UnManagedVolume getUnmanagedVolume() {
-        return unManagedVolume;
+        return _unManagedVolume;
     }
 
-    /* (non-Javadoc)
+    /*
+     * (non-Javadoc)
+     * 
      * @see com.emc.storageos.api.service.impl.resource.blockingestorchestration.context.VolumeIngestionContext#isVolumeExported()
      */
     @Override
@@ -41,33 +47,39 @@ public class BlockVolumeIngestionContext implements VolumeIngestionContext {
         return VolumeIngestionUtil.checkUnManagedResourceAlreadyExported(getUnmanagedVolume());
     }
 
-    /* (non-Javadoc)
+    /*
+     * (non-Javadoc)
+     * 
      * @see com.emc.storageos.api.service.impl.resource.blockingestorchestration.context.VolumeIngestionContext#commit()
      */
     @Override
     public void commit() {
-        // TODO Auto-generated method stub
-        
+        // basic block volume ingestion doesn't need to commit anything
+        // as all database saves are handled at the end of the ingestion process
     }
 
-    /* (non-Javadoc)
+    /*
+     * (non-Javadoc)
+     * 
      * @see com.emc.storageos.api.service.impl.resource.blockingestorchestration.context.VolumeIngestionContext#rollback()
      */
     @Override
     public void rollback() {
-        // TODO Auto-generated method stub
-        
+        // basic block volume ingestion doesn't need to roll back anything
+        // as all database saves are handled at the end of the ingestion process
     }
 
-    /* (non-Javadoc)
+    /*
+     * (non-Javadoc)
+     * 
      * @see com.emc.storageos.api.service.impl.resource.blockingestorchestration.context.VolumeIngestionContext#getErrorMessages()
      */
     @Override
     public List<String> getErrorMessages() {
-        if (null == errorMessages) {
-            errorMessages = new ArrayList<String>();
+        if (null == _errorMessages) {
+            _errorMessages = new ArrayList<String>();
         }
-        
-        return errorMessages;
+
+        return _errorMessages;
     }
 }

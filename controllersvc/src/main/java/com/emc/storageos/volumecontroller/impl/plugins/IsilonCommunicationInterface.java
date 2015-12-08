be8db8c188d08f8b3f18ed8aaf73744394517998
Ganesh, Om <om.ganesh@emc.com>
2015-12-08 17:40:57 +0530

    Adding the StringUtils  import statment

--- controllersvc/src/main/java/com/emc/storageos/volumecontroller/impl/plugins/IsilonCommunicationInterface.java
+++ controllersvc/src/main/java/com/emc/storageos/volumecontroller/impl/plugins/IsilonCommunicationInterface.java
@@ -16,6 +16,7 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 

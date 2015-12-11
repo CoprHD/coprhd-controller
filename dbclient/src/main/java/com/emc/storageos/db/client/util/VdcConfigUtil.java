0c02561d03d150fd634aeb297d1f8cded2bd83cb
Hui Liu <hui.liu@emc.com>
2015-12-11 12:18:14 -0500

    fix typo in previous commit

--- dbclient/src/main/java/com/emc/storageos/db/client/util/VdcConfigUtil.java
+++ dbclient/src/main/java/com/emc/storageos/db/client/util/VdcConfigUtil.java
@@ -200,7 +200,7 @@ public class VdcConfigUtil {
             // right now we assume that SITE_IDS and SITE_IS_STANDBY only makes sense for local VDC
             // moving forward this may or may not be the case.
             vdcConfig.put(SITE_IDS, StringUtils.join(shortIds, ','));
-            vdcConfig.put(SITE_IS_STANDBY, String.valueOf(drUtil.isStandby())));
+            vdcConfig.put(SITE_IS_STANDBY, String.valueOf(drUtil.isStandby()));
         }
     }
 

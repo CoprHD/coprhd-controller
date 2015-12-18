85394d5af94844ea8baa130ac6b50241e19c3f85
Rodrigo Oshiro <rodrigo.oshiro@emc.com>
2015-12-18 12:59:55 -0500

    Initial creation of ubuntudevkit appliance version 2.5

---
+++ packaging/appliance-images/ubuntu/15.10/UbuntuDevKit/templates/config.sh
@@ -0,0 +1,193 @@
+#!/bin/bash
+
+workspace=$( pwd )
+cd /tmp/templates
+tar -xzvf nginx-1.6.2.tar.gz
+tar -xzvf v0.3.0.tar.gz
+tar -xzvf v0.25.tar.gz
+cd nginx-1.6.2
+patch -p1 < ../nginx_upstream_check_module-0.3.0/check_1.5.12+.patch
+./configure --add-module=../nginx_upstream_check_module-0.3.0 --add-module=../headers-more-nginx-module-0.25 --with-http_ssl_module --prefix=/usr --conf-path=/etc/nginx/nginx.conf
+make
+make install
+cd $workspace
+
+cat > /etc/systemd/system/multi-user.target.wants/ovf-network.service << EOF
+[Unit]
+Description=ADG Automatic network configuration
+DefaultDependencies=no
+After=network-pre.target
+Before=network-online.target
+
+[Service]
+Type=oneshot
+ExecStart=/opt/ADG/scripts/ovf-network.sh
+
+[Install]
+WantedBy=multi-user.target
+EOF
+ln -fs /etc/systemd/system/multi-user.target.wants/ovf-network.service /etc/systemd/system/ovf-network.service
+
+mkdir -p /opt/ADG/scripts
+cat > /opt/ADG/scripts/ovf-network.sh << EOF
+#!/bin/bash
+CDROM_DEVICE="/dev/sr0"
+CDROM_MOUNT="/tmp/cdromOvfEnv"
+
+export PATH=/bin:/usr/bin:/sbin:/usr/sbin
+
+mountCDROM()
+{
+  mkdir -p \${CDROM_MOUNT}
+  if [ ! -f /tmp/cdromOvfEnv/ovf-env.xml ]; then
+    mount \${CDROM_DEVICE} \${CDROM_MOUNT}
+  fi
+}
+
+umountCDROM()
+{
+  if [ -f /tmp/cdromOvfEnv/ovf-env.xml ]; then
+    umount \${CDROM_MOUNT}
+  fi
+  rm -fr \${CDROM_MOUNT}
+}
+
+parseOVF()
+{
+  hostname=\$( more /tmp/cdromOvfEnv/ovf-env.xml | grep network.hostname.SetupVM | grep -oP 'oe:value="\K[^"]*' )
+  DOM=\$( more /tmp/cdromOvfEnv/ovf-env.xml | grep network.DOM.SetupVM | grep -oP 'oe:value="\K[^"]*' )
+  ipv40=\$( more /tmp/cdromOvfEnv/ovf-env.xml | grep network.ipv40.SetupVM | grep -oP 'oe:value="\K[^"]*' )
+  ipv4dns=\$( more /tmp/cdromOvfEnv/ovf-env.xml | grep network.ipv4dns.SetupVM | grep -oP 'oe:value="\K[^"]*' | tr ',' ' ' )
+  ipv4gateway=\$( more /tmp/cdromOvfEnv/ovf-env.xml | grep network.ipv4gateway.SetupVM | grep -oP 'oe:value="\K[^"]*' )
+  ipv4netmask0=\$( more /tmp/cdromOvfEnv/ovf-env.xml | grep network.ipv4netmask0.SetupVM | grep -oP 'oe:value="\K[^"]*' )
+  vip=\$( more /tmp/cdromOvfEnv/ovf-env.xml | grep network.vip.SetupVM | grep -oP 'oe:value="\K[^"]*' )
+  interface=\$( ip addr | grep BROADCAST,MULTICAST | head -n 1 | tail -n 1 | cut -d ':' -f 2 | tr -d ' ' )
+
+  if [ ! -z "\$hostname" ]; then
+    hostname "\$hostname"
+  fi
+
+  if [ ! -f /etc/ovfenv.properties ]; then
+    echo "network_1_ipaddr6=::0" >> /etc/ovfenv.properties
+    echo "network_1_ipaddr=\$ipv40" >> /etc/ovfenv.properties
+    echo "network_gateway6=::0" >> /etc/ovfenv.properties
+    echo "network_gateway=\$ipv4gateway" >> /etc/ovfenv.properties
+    echo "network_netmask=\$ipv4netmask0" >> /etc/ovfenv.properties
+    echo "network_prefix_length=64" >> /etc/ovfenv.properties
+    echo "network_vip6=::0" >> /etc/ovfenv.properties
+    echo "network_vip=\$vip" >> /etc/ovfenv.properties
+    echo "node_count=1" >> /etc/ovfenv.properties
+    echo "node_id=vipr1" >> /etc/ovfenv.properties
+  fi
+
+  if [ ! -f /etc/network/interfaces ]; then
+    echo "# This file describes the network interfaces available on your system" >> /etc/network/interfaces
+    echo "# and how to activate them. For more information, see interfaces(5)." >> /etc/network/interfaces
+    echo "source /etc/network/interfaces.d/*" >> /etc/network/interfaces
+
+    echo "" >> /etc/network/interfaces
+    echo "# The loopback network interface" >> /etc/network/interfaces
+    echo "auto lo" >> /etc/network/interfaces
+    echo "iface lo inet loopback" >> /etc/network/interfaces
+
+    echo "" >> /etc/network/interfaces
+    echo "# The \$interface network interface" >> /etc/network/interfaces
+    echo "auto \$interface" >> /etc/network/interfaces
+    if [ -z "\$ipv40" ]; then
+      echo "iface \$interface inet dhcp" >> /etc/network/interfaces
+    elif [ "\$ipv40" = "0.0.0.0" ]; then
+      echo "iface \$interface inet dhcp" >> /etc/network/interfaces
+    else
+      echo "iface \$interface inet static" >> /etc/network/interfaces
+      echo "  address \$ipv40" >> /etc/network/interfaces
+    fi
+    if [ ! -z "\$ipv4netmask0" ]; then
+      echo "  netmask \$ipv4netmask0" >> /etc/network/interfaces
+    fi
+    if [ ! -z "\$ipv4gateway" ]; then
+      echo "  gateway \$ipv4gateway" >> /etc/network/interfaces
+    fi
+    if [ ! -z "\$DOM" ]; then
+      echo "  dns-search \$DOM" >> /etc/network/interfaces
+    fi
+    if [ ! -z "\$ipv4netmask0" ]; then
+      echo "  dns-nameservers \$ipv4dns" >> /etc/network/interfaces
+    fi
+  fi
+}
+
+mountCDROM
+parseOVF
+umountCDROM
+EOF
+chmod u+x /opt/ADG/scripts/ovf-network.sh
+rm /etc/network/interfaces
+
+groupadd -g 444 storageos
+useradd -r -d /opt/storageos -c "StorageOS" -g 444 -u 444 -s /bin/bash storageos
+
+# COPRHD environment build workarounds:
+cat > /opt/ADG/README.coprhd << EOF
+## /bluemoon share (Brazil only)
+mkdir -p /bluemoon
+mount -o nolock bluemoon.tpa-eld.localdomain:/VAPP /bluemoon
+
+## The following commands are to setup the environment like SLES:
+ln -fs /bin/sed /usr/bin/sed
+ln -fs /bin/systemctl /usr/bin/systemctl
+ln -fs /bin/bash /bin/sh
+
+cat > /etc/rc.status << EOS
+#!/bin/bash
+function rc_reset {
+  /bin/true
+}
+function rc_failed {
+  /bin/true
+}
+function rc_status {
+  /bin/true
+}
+function rc_exit {
+  /bin/true
+}
+EOS
+chmod u+x /etc/rc.status
+
+## The following command converts an RPM to a DEB:
+alien --scripts storageos*.rpm
+
+## The following command installs a DEB:
+export DO_NOT_START=yes
+dpkg -i storageos*.deb
+
+sed -i s/'localfs.service'/'local-fs.target'/g /etc/systemd/system/boot-ovfenv.service
+sed -i s/'network.service'/'network-online.target'/g /etc/systemd/system/boot-ovfenv.service
+sed -i s/'SuSEfirewall2_init.service'//g /etc/systemd/system/boot-ovfenv.service
+sed -i s/'SuSEfirewall2.service'//g /etc/systemd/system/boot-ovfenv.service
+sed -i s/'network.service'/'network-online.target'/g /etc/systemd/system/nginx.service
+sed -i s/'ntpd.service'/'ntp.service'/g /etc/systemd/system/nginx.service
+sed -i s/'SuSEfirewall2.service'//g /etc/systemd/system/storageos-installer.service
+sed -i s/'network.service'/'network-online.target'/g /etc/systemd/system/storageos-*.service
+sed -i s/'ntpd.service'/'ntp.service'/g /etc/systemd/system/storageos-*.service
+sed -i s/'network.service'/'network-online.target'/g /etc/systemd/system/keepalived.service
+sed -i s/'ntpd.service'/'ntp.service'/g /etc/systemd/system/keepalived.service
+sed -i s/'network.service'/'network-online.target'/g /etc/systemd/system/ipchecktool.service
+sed -i s/'SuSEfirewall2.service'//g /etc/systemd/system/ipchecktool.service
+sed -i s/'network.service'/'network-online.target'/g /etc/systemd/system/syncntp.service
+sed -i s/'ntpd.service'/'ntp.service'/g /etc/systemd/system/syncntp.service
+sed -i s/'network.service'/'network-online.target'/g /etc/systemd/system/connectemc.service
+
+## To check services, run
+touch /var/run/storageos/bootmode_normal
+systemctl daemon-reload
+systemctl -t service --all
+systemctl -t target --all
+dpkg -L storageos | grep service$
+
+## Reboot!
+## Edit "vi /etv/ovfenv.properties" and change last number of the first IP to 255 (it won't change the host's network...), then reboot
+
+EOF
+
+exit 0

66fc894810be0189f27e6d72ce6f70df97be4c4e
Jay Logelin <Jay.Logelin@emc.com>
2015-12-23 18:30:01 -1000

    Fixed unmount block volume bugs for HPUX

--- vipr-portal/com.iwave.isa.content/src/java/com/emc/sa/service/hpux/UnmountBlockVolumeHelper.java
+++ vipr-portal/com.iwave.isa.content/src/java/com/emc/sa/service/hpux/UnmountBlockVolumeHelper.java
@@ -8,20 +8,13 @@ import java.net.URI;
 import java.util.List;
 import java.util.Set;
 
-import org.apache.commons.lang.StringUtils;
-
-import com.emc.aix.model.MultiPathDevice;
 import com.emc.hpux.HpuxSystem;
 import com.emc.sa.engine.ExecutionUtils;
 import com.emc.sa.engine.bind.BindingUtils;
-import com.emc.sa.service.vipr.block.BlockStorageUtils;
-import com.emc.sa.util.VolumeWWNUtils;
 import com.emc.storageos.db.client.model.Initiator;
 import com.emc.storageos.model.block.BlockObjectRestRep;
-import com.emc.storageos.model.block.VolumeRestRep;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
-import com.iwave.ext.linux.model.PowerPathDevice;
 
 public class UnmountBlockVolumeHelper {
 
@@ -40,9 +33,6 @@ public class UnmountBlockVolumeHelper {
     /** The list of VolumeSpec objects which represents the volumes to unmount and their associated metadata. */
     private List<VolumeSpec> volumes;
 
-    /** The flag which indicates whether we're using EMC PowerPath for multipathing or not. */
-    private boolean usePowerPath;
-
     public void setVolumes(List<? extends BlockObjectRestRep> volumes) {
         this.volumes = Lists.newArrayList();
         for (BlockObjectRestRep volume : volumes) {
@@ -50,82 +40,20 @@ public class UnmountBlockVolumeHelper {
         }
     }
 
-    /** search through the volumes list to find the {@link VolumeSpec} which has the given wwn. */
-    private VolumeSpec getVolumeSpecByWwn(String wwn) {
-        for (VolumeSpec volume : volumes) {
-            if (VolumeWWNUtils.wwnMatches(wwn, volume.viprVolume)) {
-                return volume;
-            }
-        }
-        return null;
-    }
-
-    /**
-     * search through the volumes list to find the {@link VolumeSpec} which has the given wwn and return it's
-     * 'viprVolume' which is a {@link VolumeRestRep}
-     */
-    private BlockObjectRestRep findVolumeRestRepByWwn(String relatedWwn) {
-        VolumeSpec relatedVolume = getVolumeSpecByWwn(relatedWwn);
-        if (relatedVolume != null) {
-            return relatedVolume.viprVolume;
-        }
-        return null;
-    }
-
     public void precheck() {
-
         hpuxSupport.findMountPoints(volumes);
-
-        usePowerPath = hpuxSupport.checkForPowerPath();
-
-        /*
-         * if (usePowerPath) {
-         * hpuxSupport.findPowerPathDevices(volumes);
-         * } else {
-         * hpuxSupport.findMultipathDevices(volumes);
-         * }
-         */
-
-        findRelatedVolumes();
-
-    }
-
-    /**
-     * Finds the volumes related to the mount points selected. When a mount point is removed, all related volumes have
-     * to have their mount point tags removed.
-     */
-    private void findRelatedVolumes() {
-        for (VolumeSpec volume : volumes) {
-            volume.relatedVolumes = Lists.newArrayList();
-            Set<String> volumeWwns = getVolumeWwns(volume);
-            for (String relatedWwn : volumeWwns) {
-                BlockObjectRestRep related = findVolumeRestRepByWwn(relatedWwn);
-                if (related == null) {
-                    related = BlockStorageUtils.getVolumeByWWN(relatedWwn);
-                }
-                if (related != null) {
-                    volume.relatedVolumes.add(related);
-                }
-            }
-        }
     }
 
     public void unmountVolumes() {
+
+        hpuxSupport.rescan();
 
         Set<URI> untaggedVolumeIds = Sets.newHashSet();
         for (VolumeSpec volume : volumes) {
 
             hpuxSupport.unmount(volume.mountPoint.getPath());
 
-            // remove mount point tag from all volumes for this mount point
-            for (BlockObjectRestRep mountedVolume : volume.relatedVolumes) {
-                hpuxSupport.removeVolumeMountPointTag(mountedVolume);
-                untaggedVolumeIds.add(mountedVolume.getId());
-            }
-
-            if (usePowerPath) {
-                // hpuxSupport.removePowerPathDevices(volume.powerpathDevices);
-            }
+            hpuxSupport.removeVolumeMountPointTag(volume.viprVolume);
 
             // delete the directory entry if it's empty
             if (hpuxSupport.isDirectoryEmpty(volume.mountPoint.getPath())) {
@@ -142,29 +70,4 @@ public class UnmountBlockVolumeHelper {
         }
 
     }
-
-    private Set<String> getVolumeWwns(VolumeSpec volume) {
-        System.out.println(volume);
-        Set<String> volumeWwns = Sets.newHashSet();
-        if (usePowerPath) {
-            for (PowerPathDevice device : volume.powerpathDevices) {
-                volumeWwns.add(getVolumeWwn(device));
-            }
-        }
-        else {
-            for (MultiPathDevice entry : volume.multipathEntries) {
-                volumeWwns.add(getVolumeWwn(entry));
-            }
-        }
-        return volumeWwns;
-    }
-
-    private String getVolumeWwn(PowerPathDevice device) {
-        return StringUtils.upperCase(device.getWwn());
-    }
-
-    private String getVolumeWwn(MultiPathDevice entry) {
-        return StringUtils.upperCase(entry.getWwn());
-    }
-
 }

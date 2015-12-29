19583f7cb6f90ec2a7d3bf64e280e1a3566d5573
Boying Lu <boying.lu@emc.com>
2015-12-29 12:14:41 +0800

    Add copyrigt

--- internalLibraries/backup/src/scripts/restore-internal.sh
+++ internalLibraries/backup/src/scripts/restore-internal.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+# Copyright (c) 2016 EMC Corporation
+# All Rights Reserved
+#
+# This software contains the intellectual property of EMC Corporation
+# or is licensed to EMC Corporation from third parties.  Use of this
+# software and the intellectual property contained therein is expressly
+# limited to the terms and conditions of the License Agreement under which
+# it is provided by or on behalf of EMC.
+#
+
 # This script is to be used by restore or 
 # syssvc
 
--- internalLibraries/backup/src/scripts/restore-libs.sh
+++ internalLibraries/backup/src/scripts/restore-libs.sh
@@ -1,4 +1,13 @@
 #!/bin/echo "This is only used as a library"
+# Copyright (c) 2016 EMC Corporation
+# All Rights Reserved
+#
+# This software contains the intellectual property of EMC Corporation
+# or is licensed to EMC Corporation from third parties.  Use of this
+# software and the intellectual property contained therein is expressly
+# limited to the terms and conditions of the License Agreement under which
+# it is provided by or on behalf of EMC.
+#
 
 # $1=command
 # $2=include local (a boolean flag)

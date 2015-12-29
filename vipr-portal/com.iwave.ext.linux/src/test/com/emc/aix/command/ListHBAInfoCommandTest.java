dfa32d90f285d62f03381970a845d4ed253e9f89
Jay Logelin <Jay.Logelin@emc.com>
2015-12-29 11:15:55 -0400

    fixed warnings in AIX test cases

--- vipr-portal/com.iwave.ext.linux/src/test/com/emc/aix/command/ListHBAInfoCommandTest.java
+++ vipr-portal/com.iwave.ext.linux/src/test/com/emc/aix/command/ListHBAInfoCommandTest.java
@@ -8,9 +8,8 @@ import static org.easymock.EasyMock.createMockBuilder;
 
 import java.util.List;
 
-import junit.framework.Assert;
-
 import org.easymock.EasyMock;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 

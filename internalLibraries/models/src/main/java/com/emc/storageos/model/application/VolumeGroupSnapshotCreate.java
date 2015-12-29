c6610362e0b21d160b766c38c1bcb25ccb8f393e
Santhosh <Santhosh.Lakshmanan@emc.com>
2015-12-28 19:43:35 -0500

    adding missed out file in commit

---
+++ internalLibraries/models/src/main/java/com/emc/storageos/model/application/VolumeGroupSnapshotCreate.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) 2015 EMC Corporation
+ * All Rights Reserved
+ */
+package com.emc.storageos.model.application;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * Consistency Group Snapshot creation parameters
+ */
+@XmlRootElement(name = "volume_group_snapshot_create")
+public class VolumeGroupSnapshotCreate {
+
+    private String name;
+    private Boolean createInactive;
+    private Boolean readOnly;
+
+    // TODO provide set of array groups?
+
+    public VolumeGroupSnapshotCreate() {
+    }
+
+    public VolumeGroupSnapshotCreate(String name,
+            Boolean createInactive, Boolean readOnly) {
+        this.name = name;
+        this.createInactive = createInactive;
+        this.readOnly = readOnly;
+    }
+
+    /**
+     * Snapshot name
+     * 
+     * @valid none
+     */
+    @XmlElement
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * If create_inactive is set to true, then the
+     * operation will create the snapshot, but not
+     * activate the synchronization between source
+     * and target volumes. The activation would have
+     * to be done using the block snapshot activate
+     * operation.
+     * 
+     * The default value for the parameter is false.
+     * That is, the operation will create and activate
+     * the synchronization for the snapshot.
+     * 
+     * @valid true
+     * @valid false
+     */
+    @XmlElement(name = "create_inactive", required = false, defaultValue = "false")
+    public Boolean getCreateInactive() {
+        return createInactive;
+    }
+
+    public void setCreateInactive(Boolean createInactive) {
+        this.createInactive = createInactive;
+    }
+
+    /**
+     * If read_only is set to true, then the snapshot will be created
+     * as read only, i.e., it will not be possible to write into the snapshot.
+     * 
+     * The default value is false. That is, the snapshot will be created as writable.
+     * 
+     * @valid true
+     * @valid false
+     */
+    @XmlElement(name = "read_only", required = false, defaultValue = "false")
+    public Boolean getReadOnly() {
+        return readOnly;
+    }
+
+    public void setReadOnly(Boolean readOnly) {
+        this.readOnly = readOnly;
+    }
+}

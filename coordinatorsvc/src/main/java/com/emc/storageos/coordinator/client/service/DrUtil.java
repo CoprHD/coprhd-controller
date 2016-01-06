6820c84f549074635914ebe3b1981047d8689c0b
Yun Qin <yun.qin@emc.com>
2016-01-06 17:07:14 -0500

    COP-19601: automatically set standby site to PAUSED when the active site becomes unreachable.

--- coordinatorsvc/src/main/java/com/emc/storageos/coordinator/client/service/DrUtil.java
+++ coordinatorsvc/src/main/java/com/emc/storageos/coordinator/client/service/DrUtil.java
@@ -44,6 +44,8 @@ public class DrUtil {
     private static final int CONNECTION_TIMEOUT = 30*1000;
     public static final String ZOOKEEPER_MODE_OBSERVER = "observer";
     public static final String ZOOKEEPER_MODE_READONLY = "read-only";
+    public static final String ZOOKEEPER_MODE_LEADER = "leader";
+    public static final String ZOOKEEPER_MODE_FOLLOWER = "follower";
 
     private static final String DR_CONFIG_KIND = "disasterRecoveryConfig";
     private static final String DR_CONFIG_ID = "global";

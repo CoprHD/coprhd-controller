c354a6f3aabd1f61715be22ae712d8effff513da
mhegeric <mike.hegerich@emc.com>
2016-01-08 14:29:24 -0500

    Use CG id

--- controllersvc/src/main/java/com/emc/storageos/volumecontroller/impl/smis/vmax/VmaxSnapshotOperations.java
+++ controllersvc/src/main/java/com/emc/storageos/volumecontroller/impl/smis/vmax/VmaxSnapshotOperations.java
@@ -1890,7 +1890,7 @@ public class VmaxSnapshotOperations extends AbstractSnapshotOperations {
 
                     BlockObject sourceObj = null;
                     if (snapSession.hasConsistencyGroup()) {
-                        BlockConsistencyGroup cg = _dbClient.queryObject(BlockConsistencyGroup.class, snapSession.getId());
+                        BlockConsistencyGroup cg = _dbClient.queryObject(BlockConsistencyGroup.class, snapSession.getConsistencyGroup());
                         List<Volume> nativeVolumes = BlockConsistencyGroupUtils.getActiveNativeVolumesInCG(cg, _dbClient);
                         sourceObj = nativeVolumes.get(0);
                     } else {

c4d1ec39d674baadbfc9d48000969b0e9d701c97
Guoliang Yang <guoliang.yang@emc.com>
2016-01-11 18:26:58 +0800

    address review comments

--- apisvc/src/main/java/com/emc/storageos/api/service/impl/resource/utils/JSONAuditLogMarshaller.java
+++ apisvc/src/main/java/com/emc/storageos/api/service/impl/resource/utils/JSONAuditLogMarshaller.java
@@ -104,7 +104,7 @@ public class JSONAuditLogMarshaller implements AuditLogMarshaller {
     private boolean matchAndwriteOneAuditLog(BufferedWriter writer, AuditLog auditlog,String keyword) throws MarshallingExcetion {
         try {
             AuditLogUtils.resetDesc(auditlog, resb);
-            if ( keyword == null || keyword.isEmpty() || auditlog.getDescription().contains(keyword)) {
+            if (AuditLogUtils.isKeywordContained(auditlog,keyword)) {
                 if (_count.getAndIncrement() > 0) {
                     while (!_firstWritten.get()) {
                         // wait until the thread which writes the first auditlog is done

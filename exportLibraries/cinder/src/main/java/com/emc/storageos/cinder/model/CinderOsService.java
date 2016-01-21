ba527862036972d4268c0e79627435373e46f788
nujira <ranjith.nuji@emc.com>
2016-01-21 14:49:33 +0530

    code review comment for sonar fix performed

--- exportLibraries/cinder/src/main/java/com/emc/storageos/cinder/model/CinderOsService.java
+++ exportLibraries/cinder/src/main/java/com/emc/storageos/cinder/model/CinderOsService.java
@@ -18,4 +18,60 @@ public class CinderOsService {
 	public String updated_at;
 	public String host;
 	public String disabled_reason;
+	
+public String getStatus() {
+    return status;
+}
+public void setStatus(String status) {
+    this.status = status;
+}
+
+public String getBinary() {
+    return binary;
+}
+
+public void setBinary(String binary) {
+    this.binary = binary;
+}
+
+public String getZone() {
+    return zone;
+}
+
+public void setZone(String zone) {
+    this.zone = zone;
+}
+
+public String getState() {
+    return state;
+}
+
+public void setState(String state) {
+    this.state = state;
+}
+
+public String getUpdatedAt() {
+    return updated_at;
+}
+
+public void setUpdatedAt(String updatedAt) {
+    this.updated_at = updatedAt;
+}
+
+public String getHost() {
+    return host;
+}
+
+public void setHost(String host) {
+    this.host = host;
+}
+
+public String getDisabledReason() {
+    return disabled_reason;
+}
+
+public void setDisabledReason(String disabledReason) {
+    this.disabled_reason = disabledReason;
+}
+
 }

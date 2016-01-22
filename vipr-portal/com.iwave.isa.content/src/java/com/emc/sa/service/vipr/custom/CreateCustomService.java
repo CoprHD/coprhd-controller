4f93e4b3a737d16bf342ca60cfc0c3c7aea62189
Roshni Mridha <roshni.mridha@rsa.com>
2016-01-22 16:44:48 +0530

    changes for adding a custom link in Service catalog

---
+++ vipr-portal/com.iwave.isa.content/src/java/com/emc/sa/service/vipr/custom/CreateCustomService.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2012-2015 EMC
+ * All Rights Reserved
+ */
+package com.emc.sa.service.vipr.custom;
+
+import static com.emc.sa.service.ServiceParams.NAME;
+
+import com.emc.sa.engine.bind.Param;
+import com.emc.sa.engine.service.Service;
+import com.emc.sa.service.vipr.ViPRService;
+
+@Service("CreateBucket")
+public class CreateCustomService extends ViPRService {
+
+    @Param(NAME)
+    protected String name;
+
+	@Override
+	public void execute() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+  
+  
+}

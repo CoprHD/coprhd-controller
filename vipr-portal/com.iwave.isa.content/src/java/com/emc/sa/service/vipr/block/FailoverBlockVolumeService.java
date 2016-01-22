b423e44b6fc5ee7550ab315fc77b29f4bb8148af
Sean Gallacher <sean.gallacher@emc.com>
2016-01-22 11:19:31 -0400

    COP-17924: Updates to include new RP failover fields for image to access and point in time

--- vipr-portal/com.iwave.isa.content/src/java/com/emc/sa/service/vipr/block/FailoverBlockVolumeService.java
+++ vipr-portal/com.iwave.isa.content/src/java/com/emc/sa/service/vipr/block/FailoverBlockVolumeService.java
@@ -5,12 +5,16 @@
 package com.emc.sa.service.vipr.block;
 
 import static com.emc.sa.service.ServiceParams.FAILOVER_TARGET;
+import static com.emc.sa.service.ServiceParams.IMAGE_DATE;
+import static com.emc.sa.service.ServiceParams.IMAGE_TIME;
+import static com.emc.sa.service.ServiceParams.IMAGE_TO_ACCESS;
 import static com.emc.sa.service.ServiceParams.STORAGE_TYPE;
 import static com.emc.sa.service.ServiceParams.VOLUMES;
 import static com.emc.vipr.client.core.util.ResourceUtils.stringId;
 
 import java.net.URI;
 
+import com.emc.sa.asset.providers.BlockProvider;
 import com.emc.sa.engine.ExecutionUtils;
 import com.emc.sa.engine.bind.Param;
 import com.emc.sa.engine.service.Service;
@@ -33,6 +37,15 @@ public class FailoverBlockVolumeService extends ViPRService {
 
     @Param(FAILOVER_TARGET)
     protected URI protectionTarget;
+
+    @Param(value = IMAGE_TO_ACCESS, required = false)
+    protected String imageToAccess;
+
+    @Param(value = IMAGE_DATE, required = false)
+    protected String imageDate;
+
+    @Param(value = IMAGE_TIME, required = false)
+    protected String imageTime;
 
     private String type;
 
@@ -67,16 +80,28 @@ public class FailoverBlockVolumeService extends ViPRService {
             ExecutionUtils.fail("failTask.FailoverBlockVolumeService", args(sourceId, targetId), args());
         }
 
+        // TODO: Add new fields
         logInfo("fail.over.block.volume.service", type.toUpperCase(), sourceName, targetName);
     }
 
     @Override
     public void execute() {
         Tasks<? extends DataObjectRestRep> tasks;
+        String pointInTime = null;
+        if (imageDate != null && imageTime != null) {
+            pointInTime = imageDate + "_" + imageTime;
+        }
 
         if (ConsistencyUtils.isVolumeStorageType(storageType)) {
             // The type selected is volume
-            tasks = execute(new FailoverBlockVolume(protectionSource, protectionTarget, type));
+            if (type != null && type.equals("rp") && (!BlockProvider.LATEST_IMAGE_OPTION_KEY.equals(imageToAccess))) {
+                // This is a RP failover request so we need to pass along the copyName and pointInTime values.
+                // We only want to do this if the image selected is NOT the latest image (this is handled by
+                // the default case) but a specific snapshot or point in time.
+                tasks = execute(new FailoverBlockVolume(protectionSource, protectionTarget, type, imageToAccess, pointInTime));
+            } else {
+                tasks = execute(new FailoverBlockVolume(protectionSource, protectionTarget, type));
+            }
         } else {
             // The type selected is consistency group
             tasks = execute(new FailoverBlockConsistencyGroup(protectionSource, protectionTarget, type));

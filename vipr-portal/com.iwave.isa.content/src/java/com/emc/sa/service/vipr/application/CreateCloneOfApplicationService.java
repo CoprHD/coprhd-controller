7c211e445a748bca4a64eccc63237c73fb851cd8
Cara Garber <cara.garber@emc.com>
2016-01-25 21:59:30 -0500

    add create application clone service

---
+++ vipr-portal/com.iwave.isa.content/src/java/com/emc/sa/service/vipr/application/CreateCloneOfApplicationService.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2016 EMC
+ * All Rights Reserved
+ */
+package com.emc.sa.service.vipr.application;
+
+import java.net.URI;
+
+import com.emc.sa.engine.ExecutionUtils;
+import com.emc.sa.engine.bind.Param;
+import com.emc.sa.engine.service.Service;
+import com.emc.sa.service.ServiceParams;
+import com.emc.sa.service.vipr.ViPRService;
+import com.emc.storageos.model.DataObjectRestRep;
+import com.emc.storageos.model.block.NamedVolumesList;
+import com.emc.vipr.client.Tasks;
+
+@Service("CreateCloneOfApplication")
+public class CreateCloneOfApplicationService extends ViPRService {
+
+    @Param(ServiceParams.APPLICATION)
+    private URI applicationId;
+
+    @Param(ServiceParams.NAME)
+    protected String name;
+
+    @Param(ServiceParams.COUNT)
+    protected Integer count;
+    
+    protected URI volumeId;
+
+    @Override
+    public void execute() throws Exception {
+        Tasks<? extends DataObjectRestRep> tasks = execute(new CreateCloneOfApplication(applicationId, volumeId, name, count));
+        addAffectedResources(tasks);
+    }
+    
+    @Override
+    public void precheck() throws Exception {
+        NamedVolumesList volList = getClient().application().getVolumeByApplication(applicationId);
+        if (volList != null && volList.getVolumes() != null && !volList.getVolumes().isEmpty()) {
+            volumeId = volList.getVolumes().iterator().next().getId();
+        } else {
+            ExecutionUtils.fail("failTask.CreateCloneOfApplicationService.volumeId.precheck", new Object[] {});
+        }
+    }
+}

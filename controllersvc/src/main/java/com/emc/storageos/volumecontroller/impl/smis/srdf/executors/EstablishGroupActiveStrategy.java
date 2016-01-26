fe7fa6652708ece19b8380c3af7074c6340396ce
Seema Tahaliyani <seema.tahaliyani@emc.com>
2016-01-26 17:06:34 +0000

    Changes to add waitForCopyState for suspend and resume.

---
+++ controllersvc/src/main/java/com/emc/storageos/volumecontroller/impl/smis/srdf/executors/EstablishGroupActiveStrategy.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2015 EMC Corporation
+ * All Rights Reserved
+ */
+package com.emc.storageos.volumecontroller.impl.smis.srdf.executors;
+
+import com.emc.storageos.db.client.model.StorageSystem;
+import com.emc.storageos.volumecontroller.impl.smis.SmisCommandHelper;
+
+import javax.cim.CIMArgument;
+import javax.cim.CIMObjectPath;
+import javax.wbem.WBEMException;
+import java.util.Collection;
+
+public class EstablishGroupActiveStrategy implements ExecutorStrategy {
+    private SmisCommandHelper helper;
+
+    public EstablishGroupActiveStrategy(SmisCommandHelper helper) {
+        this.helper = helper;
+    }
+
+    @Override
+    public void execute(Collection<CIMObjectPath> objectPaths, StorageSystem provider) throws WBEMException {
+        CIMArgument[] args = helper.getActivePairResumeInputArguments(objectPaths.iterator().next());
+        helper.callModifyReplica(provider, args);
+    }
+}

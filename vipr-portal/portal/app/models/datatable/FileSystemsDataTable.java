f25025529bd451694e030fb845ed0f6bddfd0165
Ganesh, Om <om.ganesh@emc.com>
2016-01-29 16:55:07 +0530

    Checking in interm changes

--- vipr-portal/portal/app/models/datatable/FileSystemsDataTable.java
+++ vipr-portal/portal/app/models/datatable/FileSystemsDataTable.java
@@ -27,6 +27,10 @@ public class FileSystemsDataTable extends DataTable {
         addColumn("varray");
         addColumn("vpool");
         addColumn("protocols");
+        addColumn("status").setSearchable(false).setRenderFunction("render.uploadProgress");
+//        addColumn("status").setSearchable(false).setRenderFunction(
+//                "render.uploadProgress");
+        
         sortAll();
         setDefaultSort("name", "asc");
         setRowCallback("createRowLink");

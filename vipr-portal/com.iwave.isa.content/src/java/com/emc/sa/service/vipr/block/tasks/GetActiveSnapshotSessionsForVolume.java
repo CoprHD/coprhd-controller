0efda3dfba4ecb5c11489aad7433b80720e27060
mhegeric <mike.hegerich@emc.com>
2016-01-29 11:21:40 -0500

    Add new file

---
+++ vipr-portal/com.iwave.isa.content/src/java/com/emc/sa/service/vipr/block/tasks/GetActiveSnapshotSessionsForVolume.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2012-2015 iWave Software LLC
+ * All Rights Reserved
+ */
+package com.emc.sa.service.vipr.block.tasks;
+
+import java.net.URI;
+import java.util.List;
+
+import com.emc.sa.service.vipr.tasks.ViPRExecutionTask;
+import com.emc.storageos.model.block.BlockSnapshotSessionRestRep;
+
+public class GetActiveSnapshotSessionsForVolume extends ViPRExecutionTask<List<BlockSnapshotSessionRestRep>> {
+    private final URI volumeId;
+
+    public GetActiveSnapshotSessionsForVolume(String volumeId) {
+        this(uri(volumeId));
+    }
+
+    public GetActiveSnapshotSessionsForVolume(URI volumeId) {
+        this.volumeId = volumeId;
+        provideDetailArgs(volumeId);
+    }
+
+    @Override
+    public List<BlockSnapshotSessionRestRep> executeTask() throws Exception {
+        return getClient().blockSnapshotSessions().getByVolume(volumeId);
+    }
+}

5bf78c6a240ed8f7820102bcf126ef4e010372d3
Bill Elliott IV <bill.elliott@emc.com>
2016-02-04 20:57:47 -0500

    rewrote decorator methods due to ConcurrentModificationExceptions.  Also, CG info was being written out even for volumes not in a CG.

--- apisvc/src/main/java/com/emc/storageos/api/service/impl/resource/blockingestorchestration/cg/BlockVplexCGIngestDecorator.java
+++ apisvc/src/main/java/com/emc/storageos/api/service/impl/resource/blockingestorchestration/cg/BlockVplexCGIngestDecorator.java
@@ -29,37 +29,49 @@ public class BlockVplexCGIngestDecorator extends BlockCGIngestDecorator {
     private static final Logger logger = LoggerFactory.getLogger(BlockVplexCGIngestDecorator.class);
 
     @Override
-    public void decorateCG(BlockConsistencyGroup consistencyGroup, UnManagedVolume umv, Collection<BlockObject> associatedObjects,
+    public void decorateCG(BlockConsistencyGroup cg, UnManagedVolume umv, Collection<BlockObject> associatedObjects,
             IngestionRequestContext requestContext)
             throws Exception {
-        if (null != associatedObjects && !associatedObjects.isEmpty()) {
-            for (BlockObject blockObject : associatedObjects) {
-                StringSetMap systemCGs = consistencyGroup.getSystemConsistencyGroups();
-                if (null != systemCGs && !systemCGs.isEmpty()) {
-                    for (Entry<String, AbstractChangeTrackingSet<String>> systemCGEntry : systemCGs.entrySet()) {
-                        if (systemCGEntry.getKey().equalsIgnoreCase(blockObject.getStorageController().toString())) {
-                            if (systemCGEntry.getValue().contains(blockObject.getReplicationGroupInstance())) {
-                                logger.info(String.format("Found BlockObject %s,%s system details in cg %s", blockObject.getNativeGuid(),
-                                        blockObject.getReplicationGroupInstance(), consistencyGroup.getLabel()));
-                                continue;
-                            } else {
-                                logger.info(String.format("Adding BlockObject %s/%s in CG %s", blockObject.getNativeGuid(),
-                                        blockObject.getReplicationGroupInstance(), consistencyGroup.getLabel()));
-                                consistencyGroup.addSystemConsistencyGroup(blockObject.getStorageController().toString(),
-                                        blockObject.getReplicationGroupInstance());
-                            }
-                        } else {
-                            logger.info(String.format("BlockObject %s system %s not found in CG %s. Hence adding.",
-                                    blockObject.getNativeGuid(),
-                                    blockObject.getStorageController(), consistencyGroup.getLabel()));
-                            consistencyGroup.addSystemConsistencyGroup(blockObject.getStorageController().toString(),
-                                    blockObject.getReplicationGroupInstance());
-                        }
-                        if (!consistencyGroup.getTypes().contains(Types.VPLEX.toString())) {
-                            consistencyGroup.getTypes().add(Types.VPLEX.toString());
-                        }
+        if (null == associatedObjects || associatedObjects.isEmpty()) {
+            return;
+        }
+
+        for (BlockObject blockObj : associatedObjects) {
+            StringSetMap systemCGs = cg.getSystemConsistencyGroups();
+     
+            // No entries yet in the system consistency groups list.  That's OK, we'll create it.
+            if (null == systemCGs || systemCGs.isEmpty()) {
+                cg.setSystemConsistencyGroups(new StringSetMap());
+            }
+
+            // This volume is not in a CG of this type
+            if (blockObj.getReplicationGroupInstance() == null) {
+                continue;
+            }
+            
+            boolean found = false;
+            // Look through the existing entries in the CG and see if we find a match.
+            for (Entry<String, AbstractChangeTrackingSet<String>> systemCGEntry : systemCGs.entrySet()) {
+                if (systemCGEntry.getKey().equalsIgnoreCase(blockObj.getStorageController().toString())) {
+                    if (systemCGEntry.getValue().contains(blockObj.getReplicationGroupInstance())) {
+                        logger.info(String.format("Found BlockObject %s,%s system details in cg %s", blockObj.getNativeGuid(),
+                                blockObj.getReplicationGroupInstance(), cg.getLabel()));
+                        found = true;
+                        break;
                     }
                 }
+            }
+            
+            // If we didn't find this storage:cg combo, let's add it.
+            if (!found) {
+                logger.info(String.format("Adding BlockObject %s/%s in CG %s", blockObj.getNativeGuid(),
+                        blockObj.getReplicationGroupInstance(), cg.getLabel()));
+                            cg.addSystemConsistencyGroup(blockObj.getStorageController().toString(),
+                                    blockObj.getReplicationGroupInstance());
+            }
+            
+            if (!cg.getTypes().contains(Types.VPLEX.toString())) {
+                cg.getTypes().add(Types.VPLEX.toString());
             }
         }
     }

7b1d53eff274c9bcd04ca9e241cd0c5f54990fe6
Bill Elliott IV <bill.elliott@emc.com>
2016-02-08 11:08:13 -0500

    re-pushed changes to fix concurrentmodificationexception.

--- apisvc/src/main/java/com/emc/storageos/api/service/impl/resource/blockingestorchestration/cg/BlockVolumeCGIngestDecorator.java
+++ apisvc/src/main/java/com/emc/storageos/api/service/impl/resource/blockingestorchestration/cg/BlockVolumeCGIngestDecorator.java
@@ -36,37 +36,49 @@ public class BlockVolumeCGIngestDecorator extends BlockCGIngestDecorator {
     public void decorateCG(BlockConsistencyGroup cg, Collection<BlockObject> associatedObjects,
             IngestionRequestContext requestContext)
             throws Exception {
-        if (null != associatedObjects && !associatedObjects.isEmpty()) {
-            for (BlockObject blockObj : associatedObjects) {
-                StringSetMap systemCGs = cg.getSystemConsistencyGroups();
-                if (null != systemCGs && !systemCGs.isEmpty()) {
-                    for (Entry<String, AbstractChangeTrackingSet<String>> systemCGEntry : systemCGs.entrySet()) {
-                        if (systemCGEntry.getKey().equalsIgnoreCase(blockObj.getStorageController().toString())) {
-                            if (systemCGEntry.getValue().contains(blockObj.getReplicationGroupInstance())) {
-                                logger.info(String.format("Found BlockObject %s,%s system details in cg %s", blockObj.getNativeGuid(),
-                                        blockObj.getReplicationGroupInstance(), cg.getLabel()));
-                                continue;
-                            } else {
-                                logger.info(String.format("Adding BlockObject %s/%s in CG %s", blockObj.getNativeGuid(),
-                                        blockObj.getReplicationGroupInstance(), cg.getLabel()));
-                                cg.addSystemConsistencyGroup(blockObj.getStorageController().toString(),
-                                        blockObj.getReplicationGroupInstance());
-                            }
-                        } else {
-                            logger.info(String.format("BlockObject %s system %s not found in CG %s. Hence adding.",
-                                    blockObj.getNativeGuid(),
-                                    blockObj.getStorageController(), cg.getLabel()));
-                            cg.addSystemConsistencyGroup(blockObj.getStorageController().toString(), blockObj.getReplicationGroupInstance());
-                        }
-                        if (!cg.getTypes().contains(Types.LOCAL.toString())) {
-                            cg.getTypes().add(Types.LOCAL.toString());
-                        }
+
+        if (null == associatedObjects || associatedObjects.isEmpty()) {
+            logger.info("No associated BlockObject's found to decorate for cg {}", cg.getLabel());
+            return;
+        }
+
+        for (BlockObject blockObj : associatedObjects) {
+            StringSetMap systemCGs = cg.getSystemConsistencyGroups();
+     
+            // No entries yet in the system consistency groups list.  That's OK, we'll create it.
+            if (null == systemCGs || systemCGs.isEmpty()) {
+                cg.setSystemConsistencyGroups(new StringSetMap());
+            }
+
+            // This volume is not in a CG of this type
+            if (blockObj.getReplicationGroupInstance() == null) {
+                continue;
+            }
+            
+            boolean found = false;
+            // Look through the existing entries in the CG and see if we find a match.
+            for (Entry<String, AbstractChangeTrackingSet<String>> systemCGEntry : systemCGs.entrySet()) {
+                if (systemCGEntry.getKey().equalsIgnoreCase(blockObj.getStorageController().toString())) {
+                    if (systemCGEntry.getValue().contains(blockObj.getReplicationGroupInstance())) {
+                        logger.info(String.format("Found BlockObject %s,%s system details in cg %s", blockObj.getNativeGuid(),
+                                blockObj.getReplicationGroupInstance(), cg.getLabel()));
+                        found = true;
+                        break;
                     }
                 }
-
             }
-        } else {
-            logger.info("No associated BlockObject's found to decorate for cg {}", cg.getLabel());
+            
+            // If we didn't find this storage:cg combo, let's add it.
+            if (!found) {
+                logger.info(String.format("Adding BlockObject %s/%s in CG %s", blockObj.getNativeGuid(),
+                        blockObj.getReplicationGroupInstance(), cg.getLabel()));
+                            cg.addSystemConsistencyGroup(blockObj.getStorageController().toString(),
+                                    blockObj.getReplicationGroupInstance());
+            }
+            
+            if (!cg.getTypes().contains(Types.LOCAL.toString())) {
+                cg.getTypes().add(Types.LOCAL.toString());
+            }
         }
     }
 

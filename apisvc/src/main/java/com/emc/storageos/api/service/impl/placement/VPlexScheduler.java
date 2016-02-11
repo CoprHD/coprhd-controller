7fd8545ee19ae9172c2c7b425c23c14c06ec2f7a
Tong <Tong.Yu@emc.com>
2016-02-10 19:58:05 -0500

    clean up

--- apisvc/src/main/java/com/emc/storageos/api/service/impl/placement/VPlexScheduler.java
+++ apisvc/src/main/java/com/emc/storageos/api/service/impl/placement/VPlexScheduler.java
@@ -692,10 +692,9 @@ public class VPlexScheduler implements Scheduler {
         // If only specified VPlexes are desired, filter the vplexPoolMapForSrcNH
         // to only use pools from the requestedVPlexSystems.
         if (requestedVPlexSystems != null && requestedVPlexSystems.isEmpty() == false) {
-            Set<String> keys = vplexPoolMapForSrcVarray.keySet();
-            Iterator<String> it = keys.iterator();
+            Iterator<Map.Entry<String, List<StoragePool>>> it = vplexPoolMapForSrcVarray.entrySet().iterator();
             while (it.hasNext()) {
-                String vplexKey = it.next();
+                String vplexKey = it.next().getKey();
                 URI vplexURI = null;
                 try {
                     vplexURI = new URI(vplexKey);

59b368e59213dfb202099df31bffc154c87448b7
Cara Garber <cara.garber@emc.com>
2016-02-12 17:02:05 -0500

    add replication groups to application restore

--- vipr-portal/com.iwave.isa.content/src/java/com/emc/sa/service/vipr/application/RestoreApplicationFullCopyService.java
+++ vipr-portal/com.iwave.isa.content/src/java/com/emc/sa/service/vipr/application/RestoreApplicationFullCopyService.java
@@ -5,16 +5,13 @@
 package com.emc.sa.service.vipr.application;
 
 import java.net.URI;
+import java.util.List;
 
-import com.emc.sa.engine.ExecutionUtils;
 import com.emc.sa.engine.bind.Param;
 import com.emc.sa.engine.service.Service;
 import com.emc.sa.service.ServiceParams;
 import com.emc.sa.service.vipr.ViPRService;
 import com.emc.storageos.model.DataObjectRestRep;
-import com.emc.storageos.model.NamedRelatedResourceRep;
-import com.emc.storageos.model.block.NamedVolumesList;
-import com.emc.storageos.model.block.VolumeRestRep;
 import com.emc.vipr.client.Tasks;
 
 @Service("RestoreApplicationFullCopy")
@@ -24,30 +21,14 @@ public class RestoreApplicationFullCopyService extends ViPRService {
     private URI applicationId;
 
     @Param(ServiceParams.COPY_NAME)
-    protected String name;
-    
-    protected URI volumeId;
+    private String copyName;
+
+    @Param(value = ServiceParams.APPLICATION_SUB_GROUP, required = false)
+    private List<String> applicationSubGroup;
 
     @Override
     public void execute() throws Exception {
-        Tasks<? extends DataObjectRestRep> tasks = execute(new RestoreApplicationFullCopy(applicationId, volumeId, name));
+        Tasks<? extends DataObjectRestRep> tasks = execute(new RestoreApplicationFullCopy(applicationId, applicationSubGroup, copyName));
         addAffectedResources(tasks);
-    }
-    
-    @Override
-    public void precheck() throws Exception {
-        NamedVolumesList volList = getClient().application().getFullCopiesByApplication(applicationId);
-        if (volList != null && volList.getVolumes() != null && !volList.getVolumes().isEmpty()) {
-            for (NamedRelatedResourceRep volId : volList.getVolumes()) {
-                VolumeRestRep vol = getClient().blockVolumes().get(volId.getId());
-                if (vol != null && vol.getProtection() != null && vol.getProtection().getFullCopyRep() != null && 
-                        name.equals(vol.getProtection().getFullCopyRep().getFullCopySetName())) {
-                    volumeId = vol.getId();
-                }
-            }
-        }
-        if (volumeId == null) {
-            ExecutionUtils.fail("failTask.RestoreApplicationFullCopyService.volumeId.precheck", new Object[] {});
-        }
     }
 }

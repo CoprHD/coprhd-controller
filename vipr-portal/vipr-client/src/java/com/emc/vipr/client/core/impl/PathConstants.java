d10e88c5ec41779ceda4962ade28f1fb2c6a5e93
Trevor Dawe <trevor.dawe@emc.com>
2016-02-18 13:20:18 +0000

    Removed duplicate path

--- vipr-portal/vipr-client/src/java/com/emc/vipr/client/core/impl/PathConstants.java
+++ vipr-portal/vipr-client/src/java/com/emc/vipr/client/core/impl/PathConstants.java
@@ -195,7 +195,6 @@ public class PathConstants {
     public static final String APP_SUPPORT_VOLUME_URL = APP_SUPPORT_CREATE_APP_URL + "/{id}/volumes";
     public static final String APP_SUPPORT_CLONE_URL = APP_SUPPORT_CREATE_APP_URL + "/{id}/protection/full-copies";
     public static final String APP_SUPPORT_CLONE_SET_URL = APP_SUPPORT_CLONE_URL + "/copy-sets";
-    public static final String APP_SUPPORT_SNAPSHOT_URL = APP_SUPPORT_CREATE_APP_URL +"/{id}/protection/snapshots";
     public static final String APP_SUPPORT_SNAPSHOT_SET_URL = APP_SUPPORT_SNAPSHOT_URL + "/copy-sets";
     public static final String OBJECT_NAMESPACE_URL = "/vdc/object-namespaces";
 

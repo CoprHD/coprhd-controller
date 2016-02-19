3764d96f0b36d6d35ae570db39b58845c6a57851
Trevor Dawe <trevor.dawe@emc.com>
2016-02-18 21:11:30 -0400

    Fixed message

--- vipr-portal/com.iwave.isa.content/src/java/com/emc/sa/service/vipr/application/tasks/DeleteSnapshotForApplication.java
+++ vipr-portal/com.iwave.isa.content/src/java/com/emc/sa/service/vipr/application/tasks/DeleteSnapshotForApplication.java
@@ -20,7 +20,7 @@ public class DeleteSnapshotForApplication extends WaitForTasks<TaskResourceRep> 
     public DeleteSnapshotForApplication(URI applicationId, URI snapshot) {
         this.applicationId = applicationId;
         this.snapshot = snapshot;
-        provideDetailArgs(applicationId);
+        provideDetailArgs(applicationId, snapshot);
     }
 
     @Override

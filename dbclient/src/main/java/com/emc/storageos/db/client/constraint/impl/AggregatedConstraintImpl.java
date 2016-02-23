91733ba42eec65a027b8babe6f9d3f8d82815db4
zhangh40 <Henry.Zhang2@gmail.com>
2016-02-23 09:39:13 +0000

    remove unnecessary dependency

--- dbclient/src/main/java/com/emc/storageos/db/client/constraint/impl/AggregatedConstraintImpl.java
+++ dbclient/src/main/java/com/emc/storageos/db/client/constraint/impl/AggregatedConstraintImpl.java
@@ -18,8 +18,6 @@ import com.netflix.astyanax.query.RowQuery;
 
 import java.net.URI;
 
-import org.jsoup.helper.StringUtil;
-
 /**
  * Constrained query to get list of decommissioned object URIs of a given type
  */

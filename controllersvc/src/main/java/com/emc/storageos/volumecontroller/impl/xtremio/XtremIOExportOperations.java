3724bc29002204d07e84935740f3b51519d01570
Sean Gallacher <sean.gallacher@emc.com>
2016-02-25 14:39:52 -0400

    COP-20247: changed XIO export code to set volume wwn/nativeId using volInfo instead of wwn

--- controllersvc/src/main/java/com/emc/storageos/volumecontroller/impl/xtremio/XtremIOExportOperations.java
+++ controllersvc/src/main/java/com/emc/storageos/volumecontroller/impl/xtremio/XtremIOExportOperations.java
@@ -794,8 +794,8 @@ public class XtremIOExportOperations extends XtremIOOperations implements Export
                 if (xtremIOVolume != null) {
                     _log.info("Volume lunMap details Found {}", xtremIOVolume.getLunMaps().toString());
                     if (!xtremIOVolume.getWwn().isEmpty()) {
-                        blockObj.setWWN(xtremIOVolume.getWwn());
-                        blockObj.setNativeId(xtremIOVolume.getWwn());
+                        blockObj.setWWN(xtremIOVolume.getVolInfo().get(0));
+                        blockObj.setNativeId(xtremIOVolume.getVolInfo().get(0));
                         dbClient.updateObject(blockObj);
                     }
 

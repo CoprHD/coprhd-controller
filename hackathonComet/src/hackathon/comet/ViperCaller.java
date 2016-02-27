c1d38fd973bd35e4d2bfc35b390bc6e36212299b
Ganesh, Om <om.ganesh@emc.com>
2016-02-27 17:25:58 +0530

    Added Host caller

--- hackathonComet/src/hackathon/comet/ViperCaller.java
+++ hackathonComet/src/hackathon/comet/ViperCaller.java
@@ -8,8 +8,15 @@ import com.emc.vipr.client.ViPRCoreClient;
 public class ViperCaller {
 
     private ViPRCoreClient client;
-    public List<URI>  getVolumes(){
+    public ViperCaller(){
         client = MigrateClient.getViprClient();
+    }
+    public List<URI>  getVolumes(){
+        
        return client.blockVolumes().listBulkIds();
+    }
+    
+    public List<URI> getHosts(){
+        return client.hosts().listBulkIds();
     }
 }

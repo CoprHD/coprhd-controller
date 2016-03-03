b66af906a63eb586b947de5f21c210f0c3d1d0c5
Ganesh, Om <om.ganesh@emc.com>
2016-03-03 23:35:40 +0530

    Checking in the file -cleaned up migrated  file

--- viprrest/src/main/java/comet/Main.java
+++ viprrest/src/main/java/comet/Main.java
@@ -18,7 +18,9 @@ import java.net.URI;
  */
 public class Main {
     // Base URI the Grizzly HTTP server will listen on
-    public static final String BASE_URI = "http://localhost:9090/myapp/";
+//    public static final String BASE_URI = "http://localhost:9090/myapp/";
+    
+    public static final String BASE_URI = "http://0.0.0.0:9090/myapp/";
 
     /**
      * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.

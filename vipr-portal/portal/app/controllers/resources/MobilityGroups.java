b4130ff9697d54da4c6d11070cb0a1863d05d76d
Trevor Dawe <trevor.dawe@emc.com>
2016-03-04 15:44:30 -0400

    COP-21033: Removed creating/editing of mobility groups from UI because they are now in the catalog

--- vipr-portal/portal/app/controllers/resources/MobilityGroups.java
+++ vipr-portal/portal/app/controllers/resources/MobilityGroups.java
@@ -2,31 +2,17 @@
  * Copyright (c) 2015 EMC Corporation
  * All Rights Reserved
  */
-package controllers.arrays;
+package controllers.resources;
 
 import static com.emc.vipr.client.core.util.ResourceUtils.uri;
-import static controllers.Common.flashException;
 
 import java.net.URI;
-import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import models.datatable.MobilityGroupSupportDataTable;
-
-import org.apache.commons.lang.StringUtils;
-
-import play.data.binding.As;
-import play.data.validation.MaxSize;
-import play.data.validation.MinSize;
-import play.data.validation.Required;
-import play.data.validation.Validation;
 import play.mvc.With;
 import util.BourneUtil;
-import util.MessagesUtils;
 import util.MobilityGroupSupportUtil;
-import util.StringOption;
 import util.datatable.DataTable;
 import util.datatable.DataTablesSupport;
 
@@ -35,25 +21,12 @@ import com.emc.storageos.model.NamedRelatedResourceRep;
 import com.emc.storageos.model.application.VolumeGroupRestRep;
 import com.emc.storageos.model.block.VolumeRestRep;
 import com.emc.storageos.model.host.HostRestRep;
-import com.emc.vipr.client.exceptions.ViPRException;
 import com.google.common.collect.Lists;
 
 import controllers.Common;
-import controllers.deadbolt.Restrict;
-import controllers.deadbolt.Restrictions;
-import controllers.util.FlashException;
-import controllers.util.ViprResourceController;
 
 @With(Common.class)
-@Restrictions({ @Restrict("SYSTEM_ADMIN"), @Restrict("RESTRICTED_SYSTEM_ADMIN") })
-public class MobilityGroups extends ViprResourceController {
-
-    protected static final String SAVED_SUCCESS = "mobilityGroup.save.success";
-    protected static final String UNKNOWN = "MobilityGroups.unknown";
-    protected static final Set<String> ROLE = new HashSet(Arrays.asList(new String[] { "MOBILITY" }));
-    protected static final String[] GROUP_BY = { VolumeGroup.MigrationGroupBy.VOLUMES.name(), VolumeGroup.MigrationGroupBy.HOSTS.name(),
-            VolumeGroup.MigrationGroupBy.CLUSTERS.name() };
-    protected static final String[] MIGRATION_TYPE = { VolumeGroup.MigrationType.VPLEX.name() };
+public class MobilityGroups extends ResourceController {
 
     public static void list() {
         MobilityGroupSupportDataTable dataTable = new MobilityGroupSupportDataTable();
@@ -65,35 +38,10 @@ public class MobilityGroups extends ViprResourceController {
         renderJSON(DataTablesSupport.createJSON(mobilityGroups, params));
     }
 
-    public static void create() {
-        renderArgs.put("migrationTypes", StringOption.options(MIGRATION_TYPE));
-        renderArgs.put("migrationGroupBys", StringOption.options(GROUP_BY, false));
-        render();
-    }
-
-    public static void cancel() {
-        list();
-    }
-
     public static void mobilityGroupResourcesTable(String id) {
-        List<controllers.arrays.MobilityGroups.MobilityGroupResourcesTable.MobilityGroupResource> mobilityGroupResourcesTable = getTable(
+        List<controllers.resources.MobilityGroups.MobilityGroupResourcesTable.MobilityGroupResource> mobilityGroupResourcesTable = getTable(
                 uri(id)).fetch();
         renderJSON(DataTablesSupport.createJSON(mobilityGroupResourcesTable, params));
-    }
-
-    public static void edit(String id) {
-        VolumeGroupRestRep mobilityGroup = MobilityGroupSupportUtil.getMobilityGroup(id);
-        if (mobilityGroup != null) {
-            MobilityGroupForm mobilityGroupForm = new MobilityGroupForm(mobilityGroup);
-            renderArgs.put("migrationTypes", StringOption.options(MIGRATION_TYPE));
-            renderArgs.put("migrationGroupBys", StringOption.options(GROUP_BY, false));
-            renderArgs.put("mobilityGroupResourcesTable", getTable(mobilityGroup.getId()));
-            edit(mobilityGroupForm);
-        }
-        else {
-            flash.error(MessagesUtils.get(UNKNOWN, id));
-            list();
-        }
     }
 
     private static MobilityGroupResourcesTable getTable(URI uri) {
@@ -106,112 +54,6 @@ public class MobilityGroups extends ViprResourceController {
             return new MobilityGroupClustersResourcesTable(mobilityGroup.getId());
         } else {
             return null;
-        }
-    }
-
-    private static void edit(MobilityGroupForm mobilityGroupForm) {
-        render("@edit", mobilityGroupForm);
-    }
-
-    public static void delete(@As(",") String[] ids) {
-        try {
-            if (ids != null && ids.length > 0) {
-                boolean deleteExecuted = false;
-                for (String mobilityGroup : ids) {
-                    MobilityGroupSupportUtil.deleteMobilityGroup(uri(mobilityGroup));
-                    deleteExecuted = true;
-                }
-                if (deleteExecuted == true) {
-                    flash.success(MessagesUtils.get("mobilityGroups.deleted"));
-                }
-            }
-        } catch (ViPRException e) {
-            flashException(e);
-        }
-        list();
-    }
-
-    @FlashException(keep = true, referrer = { "create", "edit" })
-    public static void save(MobilityGroupForm mobilityGroupForm) {
-        mobilityGroupForm.validate("mobilityGroupForm");
-        if (Validation.hasErrors()) {
-            Common.handleError();
-        }
-        try {
-            mobilityGroupForm.save();
-            flash.success(MessagesUtils.get(SAVED_SUCCESS, mobilityGroupForm.name));
-            backToReferrer();
-        } catch (ViPRException e) {
-            flashException(e);
-            error(mobilityGroupForm);
-        }
-    }
-
-    private static void backToReferrer() {
-        String referrer = Common.getReferrer();
-        if (StringUtils.isNotBlank(referrer)) {
-            redirect(referrer);
-        } else {
-            list();
-        }
-    }
-
-    private static void error(MobilityGroupForm mobilityGroupForm) {
-        params.flash();
-        Validation.keep();
-        edit(mobilityGroupForm);
-    }
-
-    public static class MobilityGroupForm {
-
-        public String id;
-        @MaxSize(128)
-        @MinSize(2)
-        @Required
-        public String name;
-
-        public String description;
-
-        public String migrationType;
-
-        public String migrationGroupBy;
-
-        public Set<String> roles;
-
-        public MobilityGroupForm(VolumeGroupRestRep applicationForm) {
-            this.id = applicationForm.getId().toString();
-            this.name = applicationForm.getName();
-            this.description = applicationForm.getDescription();
-            this.roles = applicationForm.getRoles();
-            this.migrationType = applicationForm.getMigrationType();
-            this.migrationGroupBy = applicationForm.getMigrationGroupBy();
-        }
-
-        public boolean isNew() {
-            return StringUtils.isBlank(id);
-        }
-
-        public void validate(String fieldname) {
-            Validation.valid(fieldname, this);
-        }
-
-        public void save() throws ViPRException {
-            if (isNew()) {
-                MobilityGroupSupportUtil.createMobilityGroup(name, description, ROLE,
-                        migrationGroupBy, migrationType);
-            } else {
-                VolumeGroupRestRep oldApplication = MobilityGroupSupportUtil.getMobilityGroup(id);
-                this.migrationGroupBy = oldApplication.getMigrationGroupBy();
-
-                if (oldApplication.getName().equals(name)) {
-                    this.name = "";
-                }
-                if (oldApplication.getDescription().equals(description)) {
-                    this.description = "";
-                }
-
-                MobilityGroupSupportUtil.updateMobilityGroup(name, description, id);
-            }
         }
     }
 

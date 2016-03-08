3d4c1d49da6526c91824184dff8252ed85058c09
hr2 <ramya.h@emc.com>
2016-03-08 12:22:41 +0530

    Remove "Active" from replication mode in block virtual pool for RP. keep it only for SRDF

---
+++ vipr-portal/portal/app/models/SRDFCopyMode.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2015 EMC Corporation
+ * All Rights Reserved
+ */
+
+package models;
+import util.StringOption;
+
+public class SRDFCopyMode {
+	public static final String ASYNCHRONOUS = "ASYNCHRONOUS";
+	public static final String SYNCHRONOUS = "SYNCHRONOUS";
+	public static final String ACTIVE = "ACTIVE";
+
+	public static final String[] VALUES = { ASYNCHRONOUS, SYNCHRONOUS, ACTIVE };
+
+	public static final StringOption[] OPTIONS = StringOption.options(VALUES,
+			"SRDFCopyMode", false);
+}

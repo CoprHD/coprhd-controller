0fecc9ba6845c552b3f1c8b9fb90ab16d0634096
Hui Liu <hui.liu@emc.com>
2016-03-10 04:01:53 +0000

    exclude ACTIVE_FAILING_OVER from vdcconfig properties

--- dbclient/src/main/java/com/emc/storageos/db/client/util/VdcConfigUtil.java
+++ dbclient/src/main/java/com/emc/storageos/db/client/util/VdcConfigUtil.java
@@ -154,6 +154,7 @@ public class VdcConfigUtil {
                     && (site.getState().equals(SiteState.STANDBY_PAUSING)
                     || site.getState().equals(SiteState.STANDBY_PAUSED)
                     || site.getState().equals(SiteState.STANDBY_REMOVING)
+                    || site.getState().equals(SiteState.ACTIVE_FAILING_OVER)
                     || site.getState().equals(SiteState.ACTIVE_DEGRADED))) {
 
                 continue;

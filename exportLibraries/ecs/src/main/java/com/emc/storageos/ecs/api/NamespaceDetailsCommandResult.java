e316d32a5c9722e04d00ae6e71230714d543cd8c
Sreenidhi Iyangar <sreenidhi.iyangar@emc.com>
2016-03-10 17:33:37 +0530

    fix for namespace gson conversion and error handling with wrong tenant mapping

--- exportLibraries/ecs/src/main/java/com/emc/storageos/ecs/api/NamespaceDetailsCommandResult.java
+++ exportLibraries/ecs/src/main/java/com/emc/storageos/ecs/api/NamespaceDetailsCommandResult.java
@@ -9,120 +9,59 @@ import java.util.ArrayList;
 public class NamespaceDetailsCommandResult {
     private String name;
     private String id;
-    private ECSLink link;
-    private String inactive;
-    private String global;
-    private String remote;
-    private String vdc;
-    private ArrayList<String> tags;
     private String default_data_services_vpool;
     private ArrayList<String> allowed_vpools_list;
     private ArrayList<String> disallowed_vpools_list;
-    private String namespace_admins;
-    private ArrayList<String> user_mapping;
-
-    //get set methods
-    String getName() {
+    private String is_encryption_enabled;
+    private String is_stale_allowed;
+    private String is_compliance_enabled;
+    
+    public String getName() {
         return name;
     }
-    
-    void setName(String name) {
+    public void setName(String name) {
         this.name = name;
     }
-    
-    String getId() {
+    public String getId() {
         return id;
     }
-    
-    void setId(String id) {
+    public void setId(String id) {
         this.id = id;
     }
-    
-    ECSLink getLink() {
-        return link;
-    }
-
-    void setLink(ECSLink link) {
-        this.link = link;
-    }
-    
-    String getInactive() {
-        return inactive;
-    }
-    
-    void setInactive(String inactive) {
-        this.inactive = inactive;
-    }
-
-    String getGlobal() {
-        return global;
-    }
-    
-    void setGlobal(String global) {
-        this.global = global;
-    }
-    
-    String getRemote() {
-        return remote;
-    }
-    
-    void setRemote(String remote) {
-        this.remote = remote;
-    }
-    
-    String getVdc() {
-        return vdc;
-    }
-    
-    void setVdc(String vdc) {
-        this.vdc = name;
-    }
-    
-    ArrayList<String> getTags() {
-        return tags;
-    }
-    
-    void setTags(ArrayList<String> tags) {
-        this.tags = tags;
-    }
-    
-    String getDefault_data_services_vpool() {
+    public String getDefault_data_services_vpool() {
         return default_data_services_vpool;
     }
-    
-    void setDefault_data_services_vpool(String default_data_services_vpool) {
+    public void setDefault_data_services_vpool(String default_data_services_vpool) {
         this.default_data_services_vpool = default_data_services_vpool;
     }
-    
-    ArrayList<String> getAllowed_vpools_list() {
+    public ArrayList<String> getAllowed_vpools_list() {
         return allowed_vpools_list;
     }
-    
-    void setAllowed_vpools_list(ArrayList<String> allowed_vpools_list) {
+    public void setAllowed_vpools_list(ArrayList<String> allowed_vpools_list) {
         this.allowed_vpools_list = allowed_vpools_list;
     }
-
-    ArrayList<String> getDisallowed_vpools_list() {
+    public ArrayList<String> getDisallowed_vpools_list() {
         return disallowed_vpools_list;
     }
-    
-    void setDisallowed_vpools_list(ArrayList<String> disallowed_vpools_list) {
+    public void setDisallowed_vpools_list(ArrayList<String> disallowed_vpools_list) {
         this.disallowed_vpools_list = disallowed_vpools_list;
     }
-    
-    String getNamespace_admins() {
-        return namespace_admins;
+    public String getIs_encryption_enabled() {
+        return is_encryption_enabled;
     }
-    
-    void setNamespace_admins(String namespace_admins) {
-        this.namespace_admins = namespace_admins;
+    public void setIs_encryption_enabled(String is_encryption_enabled) {
+        this.is_encryption_enabled = is_encryption_enabled;
     }
-    
-    ArrayList<String> getUser_mapping() {
-        return user_mapping;
+    public String getIs_stale_allowed() {
+        return is_stale_allowed;
     }
-    
-    void setUser_mapping(ArrayList<String> user_mapping) {
-        this.user_mapping = user_mapping;
+    public void setIs_stale_allowed(String is_stale_allowed) {
+        this.is_stale_allowed = is_stale_allowed;
+    }
+    public String getIs_compliance_enabled() {
+        return is_compliance_enabled;
+    }
+    public void setIs_compliance_enabled(String is_compliance_enabled) {
+        this.is_compliance_enabled = is_compliance_enabled;
     }
 }

a103dc4cbc132112cc59279965f24afc60443fcf
zhangh40 <Henry.Zhang2@gmail.com>
2016-03-10 08:14:29 +0000

    perform compact for each db service

--- dbsvc/src/main/java/com/emc/storageos/db/server/impl/DbCompactWorker.java
+++ dbsvc/src/main/java/com/emc/storageos/db/server/impl/DbCompactWorker.java
@@ -6,6 +6,8 @@ package com.emc.storageos.db.server.impl;
 
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
+
+import org.apache.cassandra.config.DatabaseDescriptor;
 import org.apache.cassandra.service.StorageService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,7 +16,9 @@ import com.emc.storageos.services.util.NamedScheduledThreadPoolExecutor;
 public final class DbCompactWorker {
 	private static final Logger log = LoggerFactory.getLogger(DbCompactWorker.class);
 	private static final int RETRY_TIMES = 3;
-	
+	private static final int INITIAL_DELAY_IN_HOUR = 1; 
+	private static final int INTERVAL_IN_HOUR = 24;
+	private static final SchemaUtil schemaUtil = new SchemaUtil();
     private static ScheduledExecutorService executor = new NamedScheduledThreadPoolExecutor("DbCompact", 1);
 
     /*
@@ -28,19 +32,16 @@ public final class DbCompactWorker {
             public void run() {
             	runDbCompact();
             }
-        }, 0, 1, TimeUnit.DAYS);
+        }, INITIAL_DELAY_IN_HOUR, INTERVAL_IN_HOUR, TimeUnit.HOURS);
     }
 
-
 	private static void runDbCompact() {
-		for (String keyspace : StorageService.instance.getKeyspaces()) {
-			log.info("start db compact for {}", keyspace);
-			runDbCompactWithRetry(keyspace);
-		}
+		String keyspace = DatabaseDescriptor.getClusterName();
+		log.info("start db compact for {}", keyspace);
+		runDbCompactWithRetry(keyspace);
 	}
 	
 	private static void runDbCompactWithRetry(String keyspace) {
-
 		for (int i=0; i<RETRY_TIMES; i++) {
 			try {
 				StorageService.instance.forceKeyspaceCompaction(keyspace);

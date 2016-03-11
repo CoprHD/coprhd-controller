f20fc8f854665b98d5df0aa40a9197b16e3fc8ea
Thondukulam Kumar <thondukulam.kumar@emc.com>
2016-03-11 11:23:19 +0530

    Fixed traceback error

--- cli/src/authentication.py
+++ cli/src/authentication.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
-/*
+
+'''
  * Copyright 2012 EMC Corporation
  * Copyright 2016 Intel Corporation
  *
@@ -15,8 +16,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- */
+ '''
 
 import os
 import sys

9a12bfcb3f51361ea17b8f8920cca648fac6b4b2
zhangh40 <Henry.Zhang2@gmail.com>
2016-03-11 10:23:46 +0000

    address comment

--- dbsvc/src/main/java/com/emc/storageos/db/server/impl/DbCompactWorker.java
+++ dbsvc/src/main/java/com/emc/storageos/db/server/impl/DbCompactWorker.java
@@ -12,38 +12,24 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import com.emc.storageos.services.util.NamedScheduledThreadPoolExecutor;
 
-public final class DbCompactWorker {
+public class DbCompactWorker implements Runnable{
     private static final Logger log = LoggerFactory.getLogger(DbCompactWorker.class);
-    private static final int RETRY_TIMES = 3;
-    private static final int INITIAL_DELAY_IN_HOUR = 1; 
-    private static final int INTERVAL_IN_HOUR = 24;
-    private static ScheduledExecutorService executor = new NamedScheduledThreadPoolExecutor("DbCompact", 1);
+    private int retryTimes;
+    private int initialDelayInHour; 
+    private int intervalInHour;
+	private static ScheduledExecutorService executor = new NamedScheduledThreadPoolExecutor("DbCompact", 1);
 
-    private DbCompactWorker() {
-        
-    }
     /*
      * start the background db major compact job which will run every day, 
      * a major compaction consolidates all existing SSTables into a single SSTable.
      * it would clean tombstones.
      * */
-    public static void start() {
-        executor.scheduleWithFixedDelay(new Runnable() {
-            @Override
-            public void run() {
-                runDbCompact();
-            }
-        }, INITIAL_DELAY_IN_HOUR, INTERVAL_IN_HOUR, TimeUnit.HOURS);
+    public void start() {
+        executor.scheduleWithFixedDelay(this, initialDelayInHour, intervalInHour, TimeUnit.HOURS);
     }
 
-    private static void runDbCompact() {
-        String keyspace = DatabaseDescriptor.getClusterName();
-        log.info("start db compact for {}", keyspace);
-        runDbCompactWithRetry(keyspace);
-    }
-    
-    private static void runDbCompactWithRetry(String keyspace) {
-        for (int i=0; i<RETRY_TIMES; i++) {
+    private void runDbCompactWithRetry(String keyspace) {
+        for (int i=0; i<retryTimes; i++) {
             try {
                 StorageService.instance.forceKeyspaceCompaction(keyspace);
                 log.info("{} db compact successfully", keyspace);
@@ -54,4 +40,35 @@ public final class DbCompactWorker {
         }
         
     }
+
+    @Override
+	public void run() {
+        String keyspace = DatabaseDescriptor.getClusterName();
+        log.info("start db compact for {}", keyspace);
+        runDbCompactWithRetry(keyspace);
+	}
+	
+    public int getRetryTimes() {
+		return retryTimes;
+	}
+
+	public void setRetryTimes(int retryTimes) {
+		this.retryTimes = retryTimes;
+	}
+
+	public int getInitialDelayInHour() {
+		return initialDelayInHour;
+	}
+
+	public void setInitialDelayInHour(int initialDelayInHour) {
+		this.initialDelayInHour = initialDelayInHour;
+	}
+
+	public int getIntervalInHour() {
+		return intervalInHour;
+	}
+
+	public void setIntervalInHour(int intervalInHour) {
+		this.intervalInHour = intervalInHour;
+	}
 }

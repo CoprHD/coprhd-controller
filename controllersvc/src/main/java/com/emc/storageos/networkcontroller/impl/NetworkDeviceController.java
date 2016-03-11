63d1aece91d605c82dcfe757ee8afe3fb615a37c
DeLaTorre <Santiago.DeLaTorre@emc.com>
2016-03-11 09:40:44 -0500

    put NetworkDeviceController.java back to what it was

--- controllersvc/src/main/java/com/emc/storageos/networkcontroller/impl/NetworkDeviceController.java
+++ controllersvc/src/main/java/com/emc/storageos/networkcontroller/impl/NetworkDeviceController.java
@@ -949,13 +949,9 @@ public class NetworkDeviceController implements NetworkController {
     public boolean zoneExportMasksCreate(URI exportGroupURI,
             List<URI> exportMaskURIs, Collection<URI> volumeURIs, String token) {
         ExportGroup exportGroup = null;
-        List<URI> listExportGroupURI = new ArrayList<URI>();  
-        listExportGroupURI.add(exportGroupURI);             
         try {    	
-        	Iterator<ExportGroup> exportGroupIterator = _dbClient.queryIterativeObjects(ExportGroup.class, listExportGroupURI);
-        	if (exportGroupIterator.hasNext()){
-        		exportGroup = exportGroupIterator.next(); 
-        	}       	   	            
+        	exportGroup = _dbClient
+                    .queryObject(ExportGroup.class, exportGroupURI);   	            
         	_log.info(String.format("Entering zoneExportMasksCreate for ExportGroup: %s (%s)",
                     exportGroup.getLabel(), exportGroup.getId()));
             if (exportMaskURIs == null && exportGroup.getExportMasks() != null) {

8d62cbe1c405dd2059c1f5d64105890a9ea528ec
Steve Mendes <steve.mendes@emc.com>
2016-03-15 16:11:34 +0000

    refactoring.  GSON classes moved out of service.  Methods moved to Utils class from Service.

---
+++ vipr-portal/com.iwave.isa.content/src/java/com/emc/sa/service/vipr/rackhd/gson/FinishedTask.java
@@ -0,0 +1,11 @@
+package com.emc.sa.service.vipr.rackhd.gson;
+
+public class FinishedTask {
+    private Job job;
+    public Job getJob() {
+        return job;
+    }
+    public void setJob(Job job) {
+        this.job = job;
+    }     
+}

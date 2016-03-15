1d3d3c9fdad66cbfd5b2812c90fb2d3fedab2e44
Steve Mendes <steve.mendes@emc.com>
2016-03-15 16:11:34 +0000

    refactoring.  GSON classes moved out of service.  Methods moved to Utils class from Service.

--- vipr-portal/com.iwave.isa.content/src/java/com/emc/sa/service/vipr/rackhd/RackHdService.java
+++ vipr-portal/com.iwave.isa.content/src/java/com/emc/sa/service/vipr/rackhd/RackHdService.java
@@ -5,14 +5,14 @@
 package com.emc.sa.service.vipr.rackhd;
 
 import com.emc.sa.service.vipr.ViPRService;
-import com.emc.sa.util.DiskSizeConversionUtils;
-import com.emc.storageos.db.client.util.SizeUtil;
+import com.emc.sa.service.vipr.rackhd.gson.AffectedResource;
+import com.emc.sa.service.vipr.rackhd.gson.Context;
+import com.emc.sa.service.vipr.rackhd.gson.FinishedTask;
+import com.emc.sa.service.vipr.rackhd.gson.Job;
+import com.emc.sa.service.vipr.rackhd.gson.RackHdWorkflow;
 import com.emc.storageos.rackhd.api.restapi.RackHdRestClient;
 import com.emc.storageos.rackhd.api.restapi.RackHdRestClientFactory;
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 import com.google.gson.JsonSyntaxException;
 import com.sun.jersey.api.client.ClientResponse;
 
@@ -46,21 +46,10 @@ public class RackHdService extends ViPRService {
 
     // Name of parameter containing playbook name:
     private static final String PLAYBOOK_PARAM_NAME = "Playbook"; 
-
     private static final String RACKHD_API_NODES = "/api/1.1/nodes"; //include leading slash
-
     private static final String RACKHD_API_WORKFLOWS = "/api/1.1/workflows";
     private static final int RACKHD_WORKFLOW_CHECK_INTERVAL = 10; // secs
-    private static final int RACKHD_WORKFLOW_CHECK_TIMEOUT = 300; // secs
-
-    // TDOD: read these possible values from workflow response?
-    private static final String WORKFLOW_SUCCESS_STATE =  "succeeded";
-    private static final String WORKFLOW_FAILED_STATE =  "failed";
-    private static final String WORKFLOW_COMPLETE_STATE = "complete";
-    private static final String WORKFLOW_TIMEOUT_STATE = "timeout";
-    private static final String WORKFLOW_CANCELLED_STATE = "cancelled";
-    private static final String WORKFLOW_VALID_STATE = "valid";
-
+    
     // JSON converter
     private static Gson gson = null;
 
@@ -122,26 +111,25 @@ public class RackHdService extends ViPRService {
     @Override
     public void execute() throws Exception {
         String nodeListResponse = makeRestCall(RACKHD_API_NODES);
-        String nodeId = getAnyNode(nodeListResponse);         
+        String nodeId = RackHdUtils.getAnyNode(nodeListResponse);         
         info("MENDES: Will execute against node ID " + nodeId);
 
         String apiWorkflowUri = "/api/1.1/nodes/" + nodeId + "/workflows";
 
-
         String workflowResponse = 
-                makeRestCall(apiWorkflowUri,makePostBody());
+                makeRestCall(apiWorkflowUri,RackHdUtils.makePostBody(params, workflowName,playbookName));
 
         // Get results (wait for RackHD workflow to complete)
         int intervals = 0;
-        while ( !isWorkflowComplete(workflowResponse) ||  // does complete flag matter?
-                isWorkflowValid(workflowResponse) ) { // status not updated from 'valid' even when complete?!
-            sleep(RACKHD_WORKFLOW_CHECK_INTERVAL);
+        while ( !RackHdUtils.isWorkflowComplete(workflowResponse) ||  // does complete flag matter?
+                RackHdUtils.isWorkflowValid(workflowResponse) ) { // status not updated from 'valid' even when complete?!
+            RackHdUtils.sleep(RACKHD_WORKFLOW_CHECK_INTERVAL);
             workflowResponse = makeRestCall(RACKHD_API_WORKFLOWS + "/" + 
-                    getWorkflowTaskId(workflowResponse));
+                    RackHdUtils.getWorkflowTaskId(workflowResponse));
             updateAffectedResources(workflowResponse);
             //updateTaskStatus(workflowResponse);
-            if( isTimedOut(++intervals) ) {
-                error("RackHD workflow " + getWorkflowTaskId(workflowResponse) + " timed out.");
+            if( RackHdUtils.isTimedOut(++intervals) ) {
+                error("RackHD workflow " + RackHdUtils.getWorkflowTaskId(workflowResponse) + " timed out.");
                 return;
             }
             //TODO: get tasks completed/total and update UI
@@ -150,28 +138,13 @@ public class RackHdService extends ViPRService {
         updateAffectedResources(workflowResponse);
         //updateTaskStatus(workflowResponse);
 
-        if(isWorkflowFailed(workflowResponse)) { 
-            String errMsg = "Workflow failed.  Status is '" +
-                    getWorkflowObjFromJson(workflowResponse).get_status() +
-                    "'.  Message from RackHD was: '" +
-                    getWorkflowObjFromJson(workflowResponse).getContext().
-                    getAnsibleResultFile() + "'.";
-            if( StringUtils.isBlank(getWorkflowObjFromJson(workflowResponse).getContext().
-                    getAnsibleResultFile()) ) { 
-                JsonParser parser = new JsonParser();
-                JsonObject json = parser.parse(workflowResponse).getAsJsonObject();
-                Gson gson = new GsonBuilder().setPrettyPrinting().create();
-                String prettyJson = gson.toJson(json);
-                errMsg = errMsg + "  Workflow details: " + prettyJson;
-            }
-            throw new IllegalStateException(errMsg);
-        }
+        RackHdUtils.checkForWorkflowFailed(workflowResponse);  
     }
 
     private void updateAffectedResources(String workflowResponse) {
         info("MENDES: updateAffectedResources  wf Response=" + workflowResponse);
         RackHdWorkflow wf = 
-                getWorkflowObjFromJson(workflowResponse);  
+                RackHdUtils.getWorkflowObjFromJson(workflowResponse);  
         for(FinishedTask task : Arrays.asList(wf.getFinishedTasks())) {
             //String resultJson = 
             //        task.getJob().getContext().getAnsibleResultFile();
@@ -193,80 +166,6 @@ public class RackHdService extends ViPRService {
                         "It was: " + resultJson);
             }
         }
-    }
-
-    private boolean isWorkflowValid(String workflowResponse) {
-        RackHdWorkflow rackHdWorkflow = 
-                getWorkflowObjFromJson(workflowResponse);  
-        info("RackHD workflow status (isValid?) =" + rackHdWorkflow.get_status());
-        return rackHdWorkflow.get_status().
-                equalsIgnoreCase(WORKFLOW_VALID_STATE);
-    }
-
-    private String makePostBody() {
-        StringBuffer postBody = new StringBuffer();
-        postBody.append("{\"name\": \"" + workflowName + "\"");
-        if( !params.isEmpty() || (playbookName != null) ) {
-            postBody.append(",\"options\":{\"defaults\":{");
-            if(playbookName != null) {
-                postBody.append("\"playbook\":\"" + playbookName + "\"");
-            }
-            if(!params.isEmpty()) {
-                postBody.append(",\"vars\":{");
-                for(String varName : params.keySet()) {
-                    postBody.append("\"" + varName + "\":" +
-                            params.get(varName).toString() + ",");
-                }
-                postBody.deleteCharAt(postBody.length()-1); // remove last comma
-                postBody.append("}");
-            }
-            postBody.append("}}");
-        }
-        postBody.append("}");  
-
-        return postBody.toString();
-    }
-
-    private boolean isTimedOut(int intervals) {
-        return (intervals * RACKHD_WORKFLOW_CHECK_INTERVAL) >= 
-                RACKHD_WORKFLOW_CHECK_TIMEOUT;
-    }
-
-    private String getWorkflowTaskId(String workflowResponse) {
-        RackHdWorkflow rackHdWorkflow = getWorkflowObjFromJson(workflowResponse);  
-        return rackHdWorkflow.getId();
-    }
-
-    private RackHdWorkflow getWorkflowObjFromJson(String workflowResponse){
-        return gson.fromJson(workflowResponse,RackHdWorkflow.class);
-    }
-
-    private boolean isWorkflowFailed(String workflowResponse) {
-        RackHdWorkflow rackHdWorkflow = 
-                getWorkflowObjFromJson(workflowResponse);  
-        info("RackHD workflow status (isFailed?)=" + rackHdWorkflow.get_status());
-        String status = rackHdWorkflow.get_status();   
-        // TODO: get failure states from workflow response
-        return status.equalsIgnoreCase(WORKFLOW_FAILED_STATE) || 
-                status.equalsIgnoreCase(WORKFLOW_TIMEOUT_STATE) || 
-                status.equalsIgnoreCase(WORKFLOW_CANCELLED_STATE);
-    }
-
-    private boolean isWorkflowComplete(String workflowResponse) {
-        RackHdWorkflow rackHdWorkflow = 
-                getWorkflowObjFromJson(workflowResponse);  
-        info("RackHD workflow complete=" + rackHdWorkflow.getCompleteEventString());
-        return rackHdWorkflow.getCompleteEventString().
-                equalsIgnoreCase(WORKFLOW_COMPLETE_STATE);
-    }
-
-    private void sleep(int i) {
-        try {
-            Thread.sleep(i*1000);
-        }
-        catch (InterruptedException e) {
-            e.printStackTrace();
-        } 
     }
 
     private String makeRestCall(String uriString) {
@@ -295,124 +194,4 @@ public class RackHdService extends ViPRService {
         return responseString;
     }
 
-    private String getAnyNode(String responseString) {
-        RackHdNode[] rackHdNodeArray = gson.fromJson(responseString,RackHdNode[].class);    
-        return getComputeNodeId(rackHdNodeArray);  
-    }
-
-    private class RackHdNode {
-        private String id;
-        private String type;
-
-        private String getId() {
-            return id;
-        }
-        private void setId(String id) {
-            this.id = id;
-        }
-        private String getType() {
-            return type;
-        }
-        private void setType(String type) {
-            this.type = type;
-        }
-
-        private boolean isComputeNode() {
-            return (getType() != null) && getType().equals("compute");
-        }
-    }
-
-    private static String getComputeNodeId(RackHdNode[] nodeArray) {
-        for(RackHdNode rackHdNode : nodeArray)
-            if(rackHdNode.isComputeNode())
-                return rackHdNode.getId();  
-        return null;
-    }
-
-    private class RackHdWorkflow {
-        private String _status;
-        private String id;
-        private Context context; 
-        private String completeEventString;
-        private FinishedTask[] finishedTasks;
-
-        public FinishedTask[] getFinishedTasks() {
-            return finishedTasks;
-        }
-        public void setFinishedTasks(FinishedTask[] finishedTasks) {
-            this.finishedTasks = finishedTasks;
-        }
-        public String getCompleteEventString() {
-            return completeEventString;
-        }
-        public void setCompleteEventString(String completeEventString) {
-            this.completeEventString = completeEventString;
-        }
-        public Context getContext() {
-            return context;
-        }
-        public void setContext(Context context) {
-            this.context = context;
-        }
-        public String getId() {
-            return id;
-        }
-        public void setId(String id) {
-            this.id = id;
-        }
-        public String get_status() {
-            return _status;
-        }
-        public void set_status(String _status) {
-            this._status = _status;
-        }
-    }
-
-    private class Context {
-        private String ansibleResultFile;
-        public String getAnsibleResultFile() {
-            return ansibleResultFile;
-        }
-        public void setAnsibleResultFile(String ansibleResultFile) {
-            this.ansibleResultFile = ansibleResultFile;
-        }
-    }
-
-    private class FinishedTask {
-        private Job job;
-        public Job getJob() {
-            return job;
-        }
-        public void setJob(Job job) {
-            this.job = job;
-        }     
-    }
-
-    private class Job {
-        private Context context;
-        public Context getContext() {
-            return context;
-        }
-        public void setContext(Context context) {
-            this.context = context;
-        }
-    }
-
-    private class AffectedResource {
-        private String key;
-        private String value;
-
-        public String getKey() {
-            return key;
-        }
-        public void setKey(String key) {
-            this.key = key;
-        }
-        public String getValue() {
-            return value;
-        }
-        public void setValue(String value) {
-            this.value = value;
-        }
-    }
 } 
---
+++ vipr-portal/com.iwave.isa.content/src/java/com/emc/sa/service/vipr/rackhd/gson/Context.java
@@ -0,0 +1,11 @@
+package com.emc.sa.service.vipr.rackhd.gson;
+
+public class Context {
+    private String ansibleResultFile;
+    public String getAnsibleResultFile() {
+        return ansibleResultFile;
+    }
+    public void setAnsibleResultFile(String ansibleResultFile) {
+        this.ansibleResultFile = ansibleResultFile;
+    }
+}

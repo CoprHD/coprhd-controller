32ff826c16d14bc756427a9f3d4ff004fe6ef9be
Sean Gallacher <sean.gallacher@emc.com>
2016-03-16 09:13:01 -0300

    COP-14748: Removed import

--- exportLibraries/vplex/src/main/java/com/emc/storageos/vplex/api/VPlexApiVirtualVolumeManager.java
+++ exportLibraries/vplex/src/main/java/com/emc/storageos/vplex/api/VPlexApiVirtualVolumeManager.java
@@ -14,8 +14,6 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.StringTokenizer;
 
-import javafx.beans.binding.When;
-
 import org.codehaus.jettison.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;

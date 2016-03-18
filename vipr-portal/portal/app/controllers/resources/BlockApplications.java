d567236f47d2d4be8295b45a420a39c74726805e
hr2 <ramya.h@emc.com>
2016-03-18 15:05:11 +0530

    format

--- vipr-portal/portal/app/controllers/resources/BlockApplications.java
+++ vipr-portal/portal/app/controllers/resources/BlockApplications.java
@@ -56,14 +56,17 @@ public class BlockApplications extends ResourceController {
         renderJSON(DataTablesSupport.createJSON(applications, params));
     }
 
-    public static void blockApplicationDetails(String id) {
-        renderArgs.put("dataTable", new VolumeApplicationDataTable());
-        renderArgs.put("cloneDataTable", new ApplicationFullCopySetsDataTable());
-        renderArgs.put("snapshotDataTable", new ApplicationSnapshotSetDataTable());
-        renderArgs.put("snapsessionDataTable", new ApplicationSnapSetDataTable());
-        VolumeGroupRestRep application = AppSupportUtil.getApplication(id);
-        render(application);
-    }
+	public static void blockApplicationDetails(String id) {
+		renderArgs.put("dataTable", new VolumeApplicationDataTable());
+		renderArgs
+				.put("cloneDataTable", new ApplicationFullCopySetsDataTable());
+		renderArgs.put("snapshotDataTable",
+				new ApplicationSnapshotSetDataTable());
+		renderArgs.put("snapsessionDataTable",
+				new ApplicationSnapSetDataTable());
+		VolumeGroupRestRep application = AppSupportUtil.getApplication(id);
+		render(application);
+	}
 
 	public static void applicationCloneJson(String id) {
 		List<ApplicationFullCopySets> cloneDetails = Lists.newArrayList();

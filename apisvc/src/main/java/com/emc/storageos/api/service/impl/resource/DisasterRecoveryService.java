7efb5729c4f8c8d60240e750d09c2697fe68c0c0
Nick Aquino <nicholas.aquino@emc.com>
2016-03-25 16:27:22 +0000

    change Cluster State to Controller Status on DR page for consistency with the rest of the product, added info to retry precheck message about what operation has been run since STANDBY_ERROR

--- apisvc/src/main/java/com/emc/storageos/api/service/impl/resource/DisasterRecoveryService.java
+++ apisvc/src/main/java/com/emc/storageos/api/service/impl/resource/DisasterRecoveryService.java
@@ -984,8 +984,8 @@ public class DisasterRecoveryService {
         if (!drOperation.equals(standby.getLastState().getDRAction())) {
             log.error("Active site last operation was {}, retry is only supported if no other operations have been performed", drOperation);
             throw APIException.internalServerErrors.retryStandbyPrecheckFailed(standby.getName(), standby.getLastState().toString(),
-                    "Another DR operation has been run on Active site. Only the latest operation can be retried. " +
-                            "This is an unrecoverable Error, please remove site and deploy a new one.");
+                    String.format("Another DR operation %s has been run on Active site. Only the latest operation can be retried. " +
+                            "This is an unrecoverable Error, please remove site and deploy a new one.",drOperation));
         }
         
         InterProcessLock lock = drUtil.getDROperationLock();

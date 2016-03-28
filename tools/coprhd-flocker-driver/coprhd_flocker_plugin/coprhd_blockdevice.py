e95fd887e2bc55b0014bd364d3abc7df71ed49eb
hemanth makaraju <hemanth.makaraju@emc.com>
2016-03-28 19:07:29 +0530

    reverted iscsi changes

--- tools/coprhd-flocker-driver/coprhd_flocker_plugin/coprhd_blockdevice.py
+++ tools/coprhd-flocker-driver/coprhd_flocker_plugin/coprhd_blockdevice.py
@@ -618,8 +618,8 @@ class CoprHDBlockDeviceAPI(object):
             - Possibly creation of new volumes
         :return:none
         """
-        check_output([b"iscsiadm", "-m", "node", "--logout"])
-        check_output([b"iscsiadm", "-m", "node", "--login"])
+        #check_output([b"iscsiadm", "-m", "node", "--logout"])
+        #check_output([b"iscsiadm", "-m", "node", "--login"])
         check_output([b"rescan-scsi-bus", "-r", "-c"])
 
     def get_device_path(self, blockdevice_id):

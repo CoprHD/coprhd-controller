e04a1220a22c0b4b219b7216c2bb57a70af862cf
Nathan Beach <nathan.beach@emc.com>
2016-03-28 11:20:39 -0500

    adding check if backend volumes have already been processed by a previous front-end export ingestion

--- apisvc/src/main/java/com/emc/storageos/api/service/impl/resource/blockingestorchestration/BlockIngestOrchestrator.java
+++ apisvc/src/main/java/com/emc/storageos/api/service/impl/resource/blockingestorchestration/BlockIngestOrchestrator.java
@@ -922,9 +922,16 @@ public abstract class BlockIngestOrchestrator {
                 } else if (replica instanceof Volume) {
                     if (isSRDFTargetVolume(replica, processedUnManagedVolumes)) {
                         VolumeIngestionUtil.setupSRDFParentRelations(replica, parent, _dbClient);
-                    } else if (VolumeIngestionUtil.isVplexVolume(parent, _dbClient)
-                            && VolumeIngestionUtil.isVplexBackendVolume(replica, _dbClient)) {
-                        VolumeIngestionUtil.setupVplexParentRelations(replica, parent, _dbClient);
+                    } else if (VolumeIngestionUtil.isVplexVolume(parent, _dbClient)) {
+                        if (parent instanceof Volume) {
+                            StringSet associatedVolumes = ((Volume) parent).getAssociatedVolumes();
+                            if (associatedVolumes != null && associatedVolumes.contains(replica.getId().toString())) {
+                                _logger.info("associated volume {} of {} has already been ingested", 
+                                        replica.forDisplay(), parent.forDisplay());
+                            }
+                        } else if (VolumeIngestionUtil.isVplexBackendVolume(replica, _dbClient)) {
+                            VolumeIngestionUtil.setupVplexParentRelations(replica, parent, _dbClient);
+                       }
                     } else {
                         VolumeIngestionUtil.setupCloneParentRelations(replica, parent, _dbClient);
                     }

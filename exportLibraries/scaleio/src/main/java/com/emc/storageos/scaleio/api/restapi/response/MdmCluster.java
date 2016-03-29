62fc31f5dbb2ea625727c3623090a9a5eca6e39d
Ravi Edpuganti <ravi.edpuganti@emc.com>
2016-03-29 18:18:17 +0000

    added new pohos

---
+++ exportLibraries/scaleio/src/main/java/com/emc/storageos/scaleio/api/restapi/response/MdmCluster.java
@@ -0,0 +1,133 @@
+/**
+ * Copyright (c) 2016 EMC Corporation
+ * All Rights Reserved
+ */
+package com.emc.storageos.scaleio.api.restapi.response;
+
+import java.util.List;
+
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+
+import com.emc.storageos.scaleio.api.ParsePattern;
+import com.emc.storageos.scaleio.api.ScaleIOConstants;
+
+/**
+ * System attributes
+ * 
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class MdmCluster {
+	 private String clusterState;
+
+	    private String id;
+
+	    private String goodNodesNum;
+
+	    private TieBreakers[] tieBreakers;
+
+	    private String name;
+
+	    private Slaves[] slaves;
+
+	    private String goodReplicasNum;
+
+	    private Master master;
+
+	    private String clusterMode;
+
+	    public String getClusterState ()
+	    {
+	        return clusterState;
+	    }
+
+	    public void setClusterState (String clusterState)
+	    {
+	        this.clusterState = clusterState;
+	    }
+
+	    public String getId ()
+	    {
+	        return id;
+	    }
+
+	    public void setId (String id)
+	    {
+	        this.id = id;
+	    }
+
+	    public String getGoodNodesNum ()
+	    {
+	        return goodNodesNum;
+	    }
+
+	    public void setGoodNodesNum (String goodNodesNum)
+	    {
+	        this.goodNodesNum = goodNodesNum;
+	    }
+
+	    public TieBreakers[] getTieBreakers ()
+	    {
+	        return tieBreakers;
+	    }
+
+	    public void setTieBreakers (TieBreakers[] tieBreakers)
+	    {
+	        this.tieBreakers = tieBreakers;
+	    }
+
+	    public String getName ()
+	    {
+	        return name;
+	    }
+
+	    public void setName (String name)
+	    {
+	        this.name = name;
+	    }
+
+	    public Slaves[] getSlaves ()
+	    {
+	        return slaves;
+	    }
+
+	    public void setSlaves (Slaves[] slaves)
+	    {
+	        this.slaves = slaves;
+	    }
+
+	    public String getGoodReplicasNum ()
+	    {
+	        return goodReplicasNum;
+	    }
+
+	    public void setGoodReplicasNum (String goodReplicasNum)
+	    {
+	        this.goodReplicasNum = goodReplicasNum;
+	    }
+
+	    public Master getMaster ()
+	    {
+	        return master;
+	    }
+
+	    public void setMaster (Master master)
+	    {
+	        this.master = master;
+	    }
+
+	    public String getClusterMode ()
+	    {
+	        return clusterMode;
+	    }
+
+	    public void setClusterMode (String clusterMode)
+	    {
+	        this.clusterMode = clusterMode;
+	    }
+
+	    @Override
+	    public String toString()
+	    {
+	        return "ClassPojo [clusterState = "+clusterState+", id = "+id+", goodNodesNum = "+goodNodesNum+", tieBreakers = "+tieBreakers+", name = "+name+", slaves = "+slaves+", goodReplicasNum = "+goodReplicasNum+", master = "+master+", clusterMode = "+clusterMode+"]";
+	    }
+}
---
+++ exportLibraries/scaleio/src/main/java/com/emc/storageos/scaleio/api/restapi/response/Slaves.java
@@ -0,0 +1,114 @@
+/**
+ * Copyright (c) 2016 EMC Corporation
+ * All Rights Reserved
+ */
+package com.emc.storageos.scaleio.api.restapi.response;
+
+import java.util.List;
+
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+
+import com.emc.storageos.scaleio.api.ParsePattern;
+import com.emc.storageos.scaleio.api.ScaleIOConstants;
+
+/**
+ * System attributes
+ * 
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Slaves {
+	private String port;
+	private String id;
+	private String versionInfo;
+	private String status;
+	private String[] ips;
+	private String name;	
+	private String role;	
+	private String[] managementIPs;
+	
+	public String getPort ()
+	{
+	    return port;
+	}
+	
+	public void setPort (String port)
+	{
+	    this.port = port;
+	}
+	
+	public String getId ()
+	{
+	    return id;
+	}
+	
+	public void setId (String id)
+	{
+	    this.id = id;
+	}
+	
+	public String getVersionInfo ()
+	{
+	    return versionInfo;
+	}
+	
+	public void setVersionInfo (String versionInfo)
+	{
+	    this.versionInfo = versionInfo;
+	}
+	
+	public String getStatus ()
+	{
+	    return status;
+	}
+	
+	public void setStatus (String status)
+	{
+	    this.status = status;
+	}
+	
+	public String[] getIps ()
+	{
+	    return ips;
+	}
+	
+	public void setIps (String[] ips)
+	{
+	    this.ips = ips;
+	}
+	
+	public String getName ()
+	{
+	    return name;
+	}
+	
+	public void setName (String name)
+	{
+	    this.name = name;
+	}
+	
+	public String getRole ()
+	{
+	    return role;
+	}
+	
+	public void setRole (String role)
+	{
+	    this.role = role;
+	}
+	
+	public String[] getManagementIPs ()
+	{
+	    return managementIPs;
+	}
+	
+	public void setManagementIPs (String[] managementIPs)
+	{
+	    this.managementIPs = managementIPs;
+	}
+	
+	@Override
+	public String toString()
+	{
+	    return "ClassPojo [port = "+port+", id = "+id+", versionInfo = "+versionInfo+", status = "+status+", ips = "+ips+", name = "+name+", role = "+role+", managementIPs = "+managementIPs+"]";
+	}
+}

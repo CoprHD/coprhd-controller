8489410b087327ceba8f7dedefaaf9e76a92a4e7
Amit Herlekar <amitherlekar@gmail.com>
2016-03-29 13:13:27 +0530

    Increased the wait time to 30 seconds

--- controllersvc/src/main/java/com/emc/storageos/fileorchestrationcontroller/FileOrchestrationDeviceController.java
+++ controllersvc/src/main/java/com/emc/storageos/fileorchestrationcontroller/FileOrchestrationDeviceController.java
@@ -185,8 +185,8 @@ public class FileOrchestrationDeviceController implements FileOrchestrationContr
             waitFor = _fileReplicationDeviceController.addStepsForDeleteFileSystems(workflow,
                     waitFor, fileDescriptors, taskId);
 
-            s_logger.info("Waiting for 10 seconds before mirror pair detaches completely...");
-            TimeUnit.SECONDS.sleep(10);
+            s_logger.info("Waiting for 30 seconds before mirror pair detaches completely...");
+            TimeUnit.SECONDS.sleep(30);
 
             // Next, call the FileDeviceController to add its delete methods.
             waitFor = _fileDeviceController.addStepsForDeleteFileSystems(workflow, waitFor, fileDescriptors, taskId);

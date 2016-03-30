fb08754c2302fd8c79aabcfde90b846c2bc5267d
Surendra <surendra.harikantra@emc.com>
2016-03-30 12:06:30 +0530

    changing the error code

--- exportLibraries/ecs/src/main/java/com/emc/storageos/ecs/api/ECSExceptions.java
+++ exportLibraries/ecs/src/main/java/com/emc/storageos/ecs/api/ECSExceptions.java
@@ -83,7 +83,7 @@ public interface ECSExceptions {
     @DeclareServiceCode(ServiceCode.ECS_ADD_USER_SECRET_KEYS_ERROR)
     public ECSException addUserSecretKeysFailedExc(final String user, final Throwable e);
     
-    @DeclareServiceCode(ServiceCode.ECS_BUCKET_ACL_ERROR)
+    @DeclareServiceCode(ServiceCode.ECS_NODE_ERROR)
     public ECSException getECSVersionFailed(final String url, final String message);
 
 

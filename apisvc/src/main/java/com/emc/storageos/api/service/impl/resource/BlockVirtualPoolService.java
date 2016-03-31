37d4f5dc2c4cfa5b2e0f3a9370be26b120af5b35
Bill Elliott IV <bill.elliott@emc.com>
2016-03-31 09:33:14 -0400

    Add VPLEX filtering for new and updated virtual pools. Add RelationIndex support for Joiner.

--- apisvc/src/main/java/com/emc/storageos/api/service/impl/resource/BlockVirtualPoolService.java
+++ apisvc/src/main/java/com/emc/storageos/api/service/impl/resource/BlockVirtualPoolService.java
@@ -207,7 +207,7 @@ public class BlockVirtualPoolService extends VirtualPoolService {
         Set<URI> allSrdfTargetVPools = SRDFUtils.fetchSRDFTargetVirtualPools(_dbClient);
         Set<URI> allRpTargetVPools = RPHelper.fetchRPTargetVirtualPools(_dbClient);
         if (null != vpool.getMatchedStoragePools() || null != vpool.getInvalidMatchedPools()) {
-            ImplicitUnManagedObjectsMatcher.matchVirtualPoolsWithUnManagedVolumes(vpool, allSrdfTargetVPools, allRpTargetVPools, _dbClient);
+            ImplicitUnManagedObjectsMatcher.matchVirtualPoolsWithUnManagedVolumes(vpool, allSrdfTargetVPools, allRpTargetVPools, _dbClient, true);
         }
 
         _dbClient.createObject(vpool);
@@ -526,7 +526,7 @@ public class BlockVirtualPoolService extends VirtualPoolService {
         if (null != vpool.getMatchedStoragePools() || null != vpool.getInvalidMatchedPools()) {
             Set<URI> allSrdfTargetVPools = SRDFUtils.fetchSRDFTargetVirtualPools(_dbClient);
             Set<URI> allRpTargetVPools = RPHelper.fetchRPTargetVirtualPools(_dbClient);
-            ImplicitUnManagedObjectsMatcher.matchVirtualPoolsWithUnManagedVolumes(vpool, allSrdfTargetVPools, allRpTargetVPools, _dbClient);
+            ImplicitUnManagedObjectsMatcher.matchVirtualPoolsWithUnManagedVolumes(vpool, allSrdfTargetVPools, allRpTargetVPools, _dbClient, true);
         }
 
         // Validate Mirror Vpool

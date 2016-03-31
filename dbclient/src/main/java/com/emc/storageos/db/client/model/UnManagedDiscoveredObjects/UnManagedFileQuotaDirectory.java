4be7a2ea408614e89593116594357855b224165c
Avinash G A <avinashga23@gmail.com>
2016-03-31 12:35:35 +0530

    Added persistence code for unmanaged file quota directories

--- dbclient/src/main/java/com/emc/storageos/db/client/model/UnManagedDiscoveredObjects/UnManagedFileQuotaDirectory.java
+++ dbclient/src/main/java/com/emc/storageos/db/client/model/UnManagedDiscoveredObjects/UnManagedFileQuotaDirectory.java
@@ -15,6 +15,17 @@ public class UnManagedFileQuotaDirectory extends UnManagedFileObject {
     // GUID of parent FS
     protected String _parentFSNativeGuid;
     
+    protected Boolean _opLock;
+
+    protected Long _size;
+
+    protected Integer _softLimit;
+
+    protected Integer _notificationLimit;
+
+    protected Integer _softGrace;
+
+    
     /**
      * Get parent fs native guid
      * 
@@ -36,4 +47,53 @@ public class UnManagedFileQuotaDirectory extends UnManagedFileObject {
         setChanged("parentFsGuid");
     }
 
+    @Name("opLock")
+    public Boolean getOpLock() {
+        return _opLock;
+    }
+    
+    public void setOpLock(Boolean _opLock) {
+        this._opLock = _opLock;
+        setChanged("opLock");
+    }
+    
+    @Name("size")
+    public Long getSize() {
+        return _size;
+    }
+    
+    public void setSize(Long _size) {
+        this._size = _size;
+        setChanged("size");
+    }
+    
+    @Name("softLimit")
+    public Integer getSoftLimit() {
+        return _softLimit;
+    }
+    
+    public void setSoftLimit(Integer _softLimit) {
+        this._softLimit = _softLimit;
+        setChanged("softLimit");
+    }
+    
+    @Name("notificationLimit")
+    public Integer getNotificationLimit() {
+        return _notificationLimit;
+    }
+    
+    public void setNotificationLimit(Integer _notificationLimit) {
+        this._notificationLimit = _notificationLimit;
+        setChanged("notificationLimit");
+    }
+    
+    @Name("softGrace")
+    public Integer getSoftgrace() {
+        return _softGrace;
+    }
+    
+    public void setSoftGrace(Integer _softGrace) {
+        this._softGrace = _softGrace;
+        setChanged("softGrace");
+    }
 }

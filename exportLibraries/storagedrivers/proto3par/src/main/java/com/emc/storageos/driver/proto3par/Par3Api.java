8d0b4ecfe9222514ed1757495a022d51c5ef1a22
iyangars <sreenidhi.iyangar@emc.com>
2016-04-03 16:02:53 +0530

    3par proto ver1

---
+++ exportLibraries/storagedrivers/proto3par/src/main/java/com/emc/storageos/driver/proto3par/Par3Api.java
@@ -0,0 +1,239 @@
+package com.emc.storageos.driver.proto3par;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.lang.mutable.MutableBoolean;
+import org.apache.commons.lang.mutable.MutableInt;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.emc.storageos.storagedriver.AbstractStorageDriver;
+import com.emc.storageos.storagedriver.BlockStorageDriver;
+import com.emc.storageos.storagedriver.DriverTask;
+import com.emc.storageos.storagedriver.RegistrationData;
+import com.emc.storageos.storagedriver.model.ITL;
+import com.emc.storageos.storagedriver.model.Initiator;
+import com.emc.storageos.storagedriver.model.StorageHostComponent;
+import com.emc.storageos.storagedriver.model.StorageObject;
+import com.emc.storageos.storagedriver.model.StoragePool;
+import com.emc.storageos.storagedriver.model.StoragePort;
+import com.emc.storageos.storagedriver.model.StorageSystem;
+import com.emc.storageos.storagedriver.model.StorageVolume;
+import com.emc.storageos.storagedriver.model.VolumeClone;
+import com.emc.storageos.storagedriver.model.VolumeConsistencyGroup;
+import com.emc.storageos.storagedriver.model.VolumeMirror;
+import com.emc.storageos.storagedriver.model.VolumeSnapshot;
+import com.emc.storageos.storagedriver.storagecapabilities.CapabilityInstance;
+import com.emc.storageos.storagedriver.storagecapabilities.StorageCapabilities;
+
+public class Par3Api extends AbstractStorageDriver implements BlockStorageDriver {
+	
+	private static final Logger _log = LoggerFactory.getLogger(Par3Api.class);
+
+	@Override
+	public List<String> getSystemTypes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DriverTask getTask(String taskId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <T extends StorageObject> T getStorageObject(String storageSystemId, String objectId, Class<T> type) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public RegistrationData getRegistrationData() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DriverTask discoverStorageSystem(List<StorageSystem> storageSystems) {
+		// TODO Auto-generated method stub
+		 _log.info("3PAR proto driver");
+		return null;
+	}
+
+	@Override
+	public DriverTask discoverStoragePools(StorageSystem storageSystem, List<StoragePool> storagePools) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DriverTask discoverStoragePorts(StorageSystem storageSystem, List<StoragePort> storagePorts) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DriverTask discoverStorageHostComponents(StorageSystem storageSystem,
+			List<StorageHostComponent> embeddedStorageHostComponents) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DriverTask getStorageVolumes(StorageSystem storageSystem, List<StorageVolume> storageVolumes,
+			MutableInt token) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DriverTask createVolumes(List<StorageVolume> volumes, StorageCapabilities capabilities) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DriverTask expandVolume(StorageVolume volume, long newCapacity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DriverTask deleteVolumes(List<StorageVolume> volumes) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DriverTask createVolumeSnapshot(List<VolumeSnapshot> snapshots, StorageCapabilities capabilities) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DriverTask restoreSnapshot(StorageVolume volume, VolumeSnapshot snapshot) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DriverTask deleteVolumeSnapshot(List<VolumeSnapshot> snapshots) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DriverTask createVolumeClone(List<VolumeClone> clones, StorageCapabilities capabilities) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DriverTask detachVolumeClone(List<VolumeClone> clones) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DriverTask restoreFromClone(List<VolumeClone> clones) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DriverTask deleteVolumeClone(List<VolumeClone> clones) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DriverTask createVolumeMirror(List<VolumeMirror> mirrors, StorageCapabilities capabilities) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DriverTask deleteVolumeMirror(List<VolumeMirror> mirrors) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DriverTask splitVolumeMirror(List<VolumeMirror> mirrors) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DriverTask resumeVolumeMirror(List<VolumeMirror> mirrors) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DriverTask restoreVolumeMirror(StorageVolume volume, VolumeMirror mirror) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<ITL> getITL(StorageSystem storageSystem, List<Initiator> initiators) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DriverTask exportVolumesToInitiators(List<Initiator> initiators, List<StorageVolume> volumes,
+			Map<String, String> volumeToHLUMap, List<StoragePort> recommendedPorts, List<StoragePort> availablePorts,
+			StorageCapabilities capabilities, MutableBoolean usedRecommendedPorts, List<StoragePort> selectedPorts) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DriverTask unexportVolumesFromInitiators(List<Initiator> initiators, List<StorageVolume> volumes) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DriverTask createConsistencyGroup(VolumeConsistencyGroup consistencyGroup) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DriverTask deleteConsistencyGroup(VolumeConsistencyGroup consistencyGroup) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DriverTask createConsistencyGroupSnapshot(VolumeConsistencyGroup consistencyGroup,
+			List<VolumeSnapshot> snapshots, List<CapabilityInstance> capabilities) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DriverTask deleteConsistencyGroupSnapshot(List<VolumeSnapshot> snapshots) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DriverTask createConsistencyGroupClone(VolumeConsistencyGroup consistencyGroup, List<VolumeClone> clones,
+			List<CapabilityInstance> capabilities) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DriverTask deleteConsistencyGroupClone(List<VolumeClone> clones) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

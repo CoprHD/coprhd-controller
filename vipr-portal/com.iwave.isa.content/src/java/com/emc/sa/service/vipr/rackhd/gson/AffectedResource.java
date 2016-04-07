7badf2e25cd9dd8f05f469c620f70f3bcf9641d8
Steve Mendes <steve.mendes@emc.com>
2016-04-07 17:07:28 +0000

    resolving issues during cherry pick

--- vipr-portal/com.iwave.isa.content/src/java/com/emc/sa/service/vipr/rackhd/gson/AffectedResource.java
+++ vipr-portal/com.iwave.isa.content/src/java/com/emc/sa/service/vipr/rackhd/gson/AffectedResource.java
@@ -1,19 +1,19 @@
 package com.emc.sa.service.vipr.rackhd.gson;
 
-public class AffectedResource { 
-    private String key;
-    private String value;
+public class AffectedResource {
+    private String id;
+    private String name; 
 
-    public String getKey() {
-        return key;
+    public String getId() {
+        return id;
     }
-    public void setKey(String key) {
-        this.key = key;
+    public void setId(String id) {
+        this.id = id;
     }
-    public String getValue() {
-        return value;
-    }
-    public void setValue(String value) {
-        this.value = value; 
+    public String getName() {
+        return name;
+    } 
+    public void setName(String name) {
+        this.name = name; 
     }
 }

90c988bd532d405d4c3acb041beaea5e75682089
Steve Mendes <steve.mendes@emc.com>
2016-04-07 17:07:28 +0000

    Combined AffectedResource & AssetOptionPair, and key/value -> id/name.  Playbooks updated.  Added wait for ViPR tasks in RackHDService.

---
+++ vipr-portal/com.iwave.isa.content/src/java/com/emc/sa/service/vipr/rackhd/gson/ViprTask.java
@@ -0,0 +1,15 @@
+package com.emc.sa.service.vipr.rackhd.gson;
+
+public class ViprTask {
+
+    ViprOperation[] task;
+
+    public ViprOperation[] getTask() {
+        return task;
+    }
+
+    public void setTask(ViprOperation[] task) {
+        this.task = task;
+    }
+    
+}

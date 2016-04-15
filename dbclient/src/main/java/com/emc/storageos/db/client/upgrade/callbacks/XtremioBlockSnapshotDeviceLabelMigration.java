bea9a64274e7aa8ed75485944c83f82423cc7704
krisht <tejaswini.krishnamurthy@emc.com>
2016-04-15 15:42:38 -0700

    Addressed review comments and also throwing MigrationCallbackException on error.

--- dbclient/src/main/java/com/emc/storageos/db/client/upgrade/callbacks/XtremioBlockSnapshotDeviceLabelMigration.java
+++ dbclient/src/main/java/com/emc/storageos/db/client/upgrade/callbacks/XtremioBlockSnapshotDeviceLabelMigration.java
@@ -16,6 +16,7 @@ import com.emc.storageos.db.client.model.DiscoveredDataObject;
 import com.emc.storageos.db.client.model.StorageSystem;
 import com.emc.storageos.db.client.upgrade.BaseCustomMigrationCallback;
 import com.emc.storageos.db.client.util.NullColumnValueGetter;
+import com.emc.storageos.svcs.errorhandling.resources.MigrationCallbackException;
 
 /**
  * Migration handler to set the deviceLabel of XtremIO snapshots same as their labels.
@@ -27,34 +28,42 @@ public class XtremioBlockSnapshotDeviceLabelMigration extends BaseCustomMigratio
     private static final Logger log = LoggerFactory.getLogger(XtremioBlockSnapshotDeviceLabelMigration.class);
 
     /**
-     * Get all the BlockSnapshots associated with XtremIO storage systems and set the deviceLabel to be same as the label of the snapshots.
+     * Get all the BlockSnapshots associated with XtremIO storage systems and if the deviceLabel is not set, set it to the label of the
+     * snapshots.
      *
      */
     @Override
-    public void process() {
-        DbClient dbClient = getDbClient();
-        List<URI> storageSystemURIList = dbClient.queryByType(StorageSystem.class, true);
-        Iterator<StorageSystem> storageSystems = dbClient.queryIterativeObjects(StorageSystem.class, storageSystemURIList);
-        while (storageSystems.hasNext()) {
-            StorageSystem storageSystem = storageSystems.next();
-            if (DiscoveredDataObject.Type.xtremio.name().equalsIgnoreCase(storageSystem.getSystemType())) {
-                URIQueryResultList snapshotURIs = new URIQueryResultList();
-                dbClient.queryByConstraint(ContainmentConstraint.Factory.getStorageDeviceSnapshotConstraint(storageSystem.getId()),
-                        snapshotURIs);
-                Iterator<BlockSnapshot> xioSnapshots = dbClient.queryIterativeObjects(BlockSnapshot.class, snapshotURIs);
-                List<BlockSnapshot> updatedSnaps = new ArrayList<BlockSnapshot>();
-                while (xioSnapshots.hasNext()) {
-                    BlockSnapshot xioSnapshot = xioSnapshots.next();
-                    if (!xioSnapshot.getInactive() && NullColumnValueGetter.isNullValue(xioSnapshot.getDeviceLabel())) {
-                        String label = xioSnapshot.getLabel();
-                        log.info(String.format("Setting deviceLabel of snapshot %s : %s to %s", label,
-                                xioSnapshot.getNativeGuid(), label));
-                        xioSnapshot.setDeviceLabel(label);
-                        updatedSnaps.add(xioSnapshot);
+    public void process() throws MigrationCallbackException {
+        log.info("Started execution of XtremioBlockSnapshotDeviceLabelMigration migration callback.");
+        try {
+            DbClient dbClient = getDbClient();
+            List<URI> storageSystemURIList = dbClient.queryByType(StorageSystem.class, true);
+            Iterator<StorageSystem> storageSystems = dbClient.queryIterativeObjects(StorageSystem.class, storageSystemURIList);
+            while (storageSystems.hasNext()) {
+                StorageSystem storageSystem = storageSystems.next();
+                if (DiscoveredDataObject.Type.xtremio.name().equalsIgnoreCase(storageSystem.getSystemType())) {
+                    URIQueryResultList snapshotURIs = new URIQueryResultList();
+                    dbClient.queryByConstraint(ContainmentConstraint.Factory.getStorageDeviceSnapshotConstraint(storageSystem.getId()),
+                            snapshotURIs);
+                    Iterator<BlockSnapshot> xioSnapshots = dbClient.queryIterativeObjects(BlockSnapshot.class, snapshotURIs);
+                    List<BlockSnapshot> updatedSnaps = new ArrayList<BlockSnapshot>();
+                    while (xioSnapshots.hasNext()) {
+                        BlockSnapshot xioSnapshot = xioSnapshots.next();
+                        if (!xioSnapshot.getInactive() && NullColumnValueGetter.isNullValue(xioSnapshot.getDeviceLabel())) {
+                            String label = xioSnapshot.getLabel();
+                            log.info(String.format("Setting deviceLabel of snapshot %s : %s to %s", label,
+                                    xioSnapshot.getNativeGuid(), label));
+                            xioSnapshot.setDeviceLabel(label);
+                            updatedSnaps.add(xioSnapshot);
+                        }
                     }
+                    dbClient.updateObject(updatedSnaps);
                 }
-                dbClient.updateObject(updatedSnaps);
             }
+            log.info("Completed executing XtremioBlockSnapshotDeviceLabelMigration migration callback.");
+        } catch (Exception e) {
+            String errorMsg = String.format("%s encounter unexpected error %s", this.getName(), e.getMessage());
+            throw new MigrationCallbackException(errorMsg, e);
         }
     }
 

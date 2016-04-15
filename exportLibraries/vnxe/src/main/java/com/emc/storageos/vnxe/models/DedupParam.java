fade2320da5352efd5d29afbe1b5d4014891c994
Tong <Tong.Yu@emc.com>
2016-04-15 07:36:55 -0400

    Committed the new classes

---
+++ exportLibraries/vnxe/src/main/java/com/emc/storageos/vnxe/models/DedupParam.java
@@ -0,0 +1,16 @@
+package com.emc.storageos.vnxe.models;
+
+public class DedupParam {
+    private Boolean isDeduplicationEnabled = true;
+
+    public Boolean getIsDeduplicationEnabled() {
+                return isDeduplicationEnabled;
+            }
+
+    public void setIsDeduplicationEnabled(Boolean isDeduplicationEnabled) {
+                this.isDeduplicationEnabled = isDeduplicationEnabled;
+            }
+    
+    
+
+}

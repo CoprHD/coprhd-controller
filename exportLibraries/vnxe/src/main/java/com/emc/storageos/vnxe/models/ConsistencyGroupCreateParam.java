8bd0887e305c7b96d57c1270f17b7d684065e7dd
Tong <Tong.Yu@emc.com>
2016-04-15 07:29:19 -0400

    Committed the new classes

---
+++ exportLibraries/vnxe/src/main/java/com/emc/storageos/vnxe/models/ConsistencyGroupCreateParam.java
@@ -0,0 +1,22 @@
+package com.emc.storageos.vnxe.models;
+
+public class ConsistencyGroupCreateParam extends LunGroupCreateParam{
+    private ReplicationParam replicationParameters;
+    private DedupParam dedupParameters;
+    
+    public ReplicationParam getReplicationParameters() {
+                return replicationParameters;
+            }
+    public void setReplicationParameters(ReplicationParam replicationParameters) {
+                this.replicationParameters = replicationParameters;
+            }
+    public DedupParam getDedupParameters() {
+                return dedupParameters;
+            }
+    public void setDedupParameters(DedupParam dedupParameters) {
+                this.dedupParameters = dedupParameters;
+            }
+    
+    
+
+}
---
+++ exportLibraries/vnxe/src/main/java/com/emc/storageos/vnxe/requests/ConsistencyGroupRequests.java
@@ -0,0 +1,43 @@
+package com.emc.storageos.vnxe.requests;
+
+import com.emc.storageos.vnxe.models.ConsistencyGroupCreateParam;
+import com.emc.storageos.vnxe.models.LunGroupModifyParam;
+import com.emc.storageos.vnxe.models.StorageResource;
+import com.emc.storageos.vnxe.models.VNXeCommandJob;
+import com.emc.storageos.vnxe.models.VNXeCommandResult;
+
+public class ConsistencyGroupRequests extends KHRequests<StorageResource> {
+    private static String URL_CREATE = "/api/types/storageResource/action/createConsistencyGroup";
+    private static String URL_MODIFY1 = "/api/instances/storageResource/";
+    private static String URL_MODIFY2 = "/action/modifyConsistencyGroup";
+
+    public ConsistencyGroupRequests(KHClient client) {
+        super(client);
+    }
+
+    public VNXeCommandResult createConsistencyGroup(ConsistencyGroupCreateParam createParam) {
+        _url = URL_CREATE;
+        return postRequestSync(createParam);
+    }
+
+    public VNXeCommandJob modifyConsistencyGroupAsync(String id, LunGroupModifyParam param) {
+        StringBuilder urlBld = new StringBuilder(URL_MODIFY1);
+        urlBld.append(id);
+        urlBld.append(URL_MODIFY2);
+        _url = urlBld.toString();
+
+        return postRequestAsync(param);
+
+    }
+
+    public VNXeCommandResult modifyConsistencyGroupSync(String id, LunGroupModifyParam param) {
+        StringBuilder urlBld = new StringBuilder(URL_MODIFY1);
+        urlBld.append(id);
+        urlBld.append(URL_MODIFY2);
+        _url = urlBld.toString();
+
+        return postRequestSync(param);
+
+    }
+
+}

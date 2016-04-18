8fde1c9eec76668641edc9cc5b8bdb105e992f37
Tong <Tong.Yu@emc.com>
2016-04-18 09:00:06 -0400

    snapshot for cg

---
+++ controllersvc/src/main/java/com/emc/storageos/volumecontroller/impl/vnxunity/job/VNXUnityCreateCGSnapshotJob.java
@@ -0,0 +1,102 @@
+package com.emc.storageos.volumecontroller.impl.vnxunity.job;
+
+import java.net.URI;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.emc.storageos.db.client.DbClient;
+import com.emc.storageos.db.client.model.BlockSnapshot;
+import com.emc.storageos.db.client.model.StorageSystem;
+import com.emc.storageos.db.client.model.Volume;
+import com.emc.storageos.vnxe.VNXeApiClient;
+import com.emc.storageos.vnxe.models.ParametersOut;
+import com.emc.storageos.vnxe.models.Snap;
+import com.emc.storageos.vnxe.models.VNXeCommandJob;
+import com.emc.storageos.vnxe.models.VNXeLun;
+import com.emc.storageos.vnxe.models.VNXeLunGroupSnap;
+import com.emc.storageos.volumecontroller.JobContext;
+import com.emc.storageos.volumecontroller.TaskCompleter;
+import com.emc.storageos.volumecontroller.Job.JobStatus;
+import com.emc.storageos.volumecontroller.impl.NativeGUIDGenerator;
+import com.emc.storageos.volumecontroller.impl.block.taskcompleter.BlockSnapshotCreateCompleter;
+import com.emc.storageos.volumecontroller.impl.vnxe.job.VNXeBlockCreateCGSnapshotJob;
+import com.emc.storageos.volumecontroller.impl.vnxe.job.VNXeJob;
+
+public class VNXUnityCreateCGSnapshotJob extends VNXeJob{
+
+    private static final Logger _logger = LoggerFactory.getLogger(VNXUnityCreateCGSnapshotJob.class);
+
+    public VNXUnityCreateCGSnapshotJob(String jobId,
+            URI storageSystemUri, Boolean createInactive, TaskCompleter taskCompleter) {
+        super(jobId, storageSystemUri, taskCompleter, "createCGSnapshot");
+
+    }
+    
+    public void updateStatus(JobContext jobContext) throws Exception {
+        DbClient dbClient = jobContext.getDbClient();
+        try {
+            if (_status == JobStatus.IN_PROGRESS) {
+                return;
+            }
+            BlockSnapshotCreateCompleter completer = (BlockSnapshotCreateCompleter) getTaskCompleter();
+            List<BlockSnapshot> snapshots = dbClient.queryObject(BlockSnapshot.class, completer.getSnapshotURIs());
+            StorageSystem storage = dbClient.queryObject(StorageSystem.class, getStorageSystemUri());
+            if (_status == JobStatus.SUCCESS) {
+                VNXeApiClient vnxeApiClient = getVNXeClient(jobContext);
+                VNXeCommandJob vnxeJob = vnxeApiClient.getJob(getJobIds().get(0));
+                ParametersOut output = vnxeJob.getParametersOut();
+                // get the luns belonging to the lun group
+                String snapId = output.getId();
+                Snap snap = vnxeApiClient.getSnapshot(snapId);
+                String snapGroupId = snap.getSnapGroup().getId();
+                Snap snapGroup = vnxeApiClient.getSnapshot(snapGroupId);
+                List<VNXeLun> groupLuns = vnxeApiClient.getLunByStorageResourceId(snapGroup.getStorageResource().getId());
+                // Create mapping of volume.nativeDeviceId to BlockSnapshot object
+                Map<String, BlockSnapshot> volumeToSnapMap = new HashMap<String, BlockSnapshot>();
+                for (BlockSnapshot snapshot : snapshots) {
+                    Volume volume = dbClient.queryObject(Volume.class, snapshot.getParent());
+                    volumeToSnapMap.put(volume.getNativeId(), snapshot);
+                }
+
+                for (VNXeLun groupLun : groupLuns) {
+                    BlockSnapshot snapshot = volumeToSnapMap.get(groupLun.getId());
+                    if (snapshot == null) {
+                        _logger.info("No snapshot found for the vnxe lun - ", groupLun.getId());
+                        continue;
+                    }
+                    snapshot.setNativeId(output.getId());
+                    snapshot.setNativeGuid(NativeGUIDGenerator.generateNativeGuid(storage, snapshot));
+                    snapshot.setDeviceLabel(groupLun.getName());
+                    snapshot.setReplicationGroupInstance(snapGroupId);
+                    snapshot.setIsSyncActive(true);
+                    snapshot.setInactive(false);
+                    snapshot.setCreationTime(Calendar.getInstance());
+                    snapshot.setWWN(groupLun.getSnapWwn());
+                    snapshot.setAllocatedCapacity(snap.getSize());
+                    snapshot.setProvisionedCapacity(snap.getSize());
+                    _logger.info(String.format("Going to set blocksnapshot %1$s nativeId to %2$s (%3$s). Associated lun is %4$s (%5$s)",
+                            snapshot.getId().toString(), output.getId(), snapshot.getLabel(), groupLun.getId(), groupLun.getName()));
+                    dbClient.updateObject(snapshot);
+                }
+
+            } else if (_status == JobStatus.FAILED) {
+                _logger.info("Failed to create snapshot");
+                for (BlockSnapshot snapshot : snapshots) {
+                    snapshot.setInactive(true);
+                }
+                dbClient.updateObject(snapshots);
+            }
+        } catch (Exception e) {
+            _logger.error("Caught an exception while trying to updateStatus for VNXeBlockCreateCGSnapshotJob", e);
+            setErrorStatus("Encountered an internal error during group snapshot create job status processing : " + e.getMessage());
+        } finally {
+            super.updateStatus(jobContext);
+        }
+    }
+
+}

63288dc14f783d834ef6eac16e88d061da01ddbe
Alik Saring <alik.saring@emc.com>
2016-04-20 10:45:12 -0400

    Upload file changes for Southbound

--- apisvc/src/main/java/com/emc/storageos/api/service/impl/resource/StorageSystemTypeService.java
+++ apisvc/src/main/java/com/emc/storageos/api/service/impl/resource/StorageSystemTypeService.java
@@ -240,7 +240,7 @@ public class StorageSystemTypeService extends TaskResourceService {
 		saveFile(fileInputStream, filePath);
 		//String output = "File saved to server location : " + filePath;
 		log.info("Device driver file uploaded at " + filePath);
-		Response myhttpresponse = Response.status(Response.Status.OK).build();
+		Response myhttpresponse = Response.status(Response.Status.NO_CONTENT).build();
 		return myhttpresponse; //Response.status(Response.Status.OK).build();
 	}
 

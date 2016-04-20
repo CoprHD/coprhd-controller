4b19b71916cff6aa820aea17ac502354f9f66b20
Amit Saurabh <amit.saurabh@emc.com>
2016-04-20 14:36:32 +0530

    adding the file customization in the NAs tab

--- controllersvc/src/main/java/com/emc/storageos/customconfigcontroller/CustomConfigConstants.java
+++ controllersvc/src/main/java/com/emc/storageos/customconfigcontroller/CustomConfigConstants.java
@@ -76,6 +76,6 @@ public interface CustomConfigConstants {
     public String NAS_DYNAMIC_PERFORMANCE_PLACEMENT_ENABLED = "NasDynamicPerformancePlacementEnabled";
     public String USE_PHYSICAL_NAS_FOR_PROVISIONING = "NasUsePhysicalNASForProvisioning";
     public String SHARE_VNAS_WITH_MULTIPLE_PROJECTS = "NasShareVNASWithMultipleProjects";
-    public String ISILON_FILE_SHARE_CUSTOM_PATH = "IsilonFileShareCustomPath";
+    public String FILE_SHARE_PATH_CUSTOMIZATION = "nasSharePathCustomization";
 
 }

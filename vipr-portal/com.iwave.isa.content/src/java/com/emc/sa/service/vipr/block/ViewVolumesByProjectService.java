6506fc7fbfd546b794a06bd625713b1df8b5924a
hr2 <ramya.h@emc.com>
2016-04-27 14:42:46 +0530

    trail service

---
+++ vipr-portal/com.iwave.isa.content/src/java/com/emc/sa/service/vipr/block/ViewVolumesByProjectService.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2012-2015 iWave Software LLC
+ * All Rights Reserved
+ */
+package com.emc.sa.service.vipr.block;
+
+import com.emc.sa.engine.service.Service;
+import com.emc.sa.service.vipr.ViPRService;
+
+@Service("ViewVolumesByProject")
+public class ViewVolumesByProjectService extends ViPRService {
+	
+	
+	@Override
+    public void execute() {
+		
+	}
+}

46bcad7828fd4d6724c288e9a2aa5e955be92791
hr2 <ramya.h@emc.com>
2016-04-28 14:39:34 +0530

    Changes

--- vipr-portal/com.iwave.isa.content/src/java/com/emc/sa/service/vipr/block/ViewVolumesByProjectService.java
+++ vipr-portal/com.iwave.isa.content/src/java/com/emc/sa/service/vipr/block/ViewVolumesByProjectService.java
@@ -4,15 +4,19 @@
  */
 package com.emc.sa.service.vipr.block;
 
+import static com.emc.sa.service.ServiceParams.NAME;
+
+import com.emc.sa.engine.bind.Param;
 import com.emc.sa.engine.service.Service;
 import com.emc.sa.service.vipr.ViPRService;
 
 @Service("ViewVolumesByProject")
 public class ViewVolumesByProjectService extends ViPRService {
-	
+	@Param(NAME)
+    protected String name;
 	
 	@Override
     public void execute() {
-		
+		System.out.println(name + "myservice test");
 	}
 }

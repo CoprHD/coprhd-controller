ea8a7024bfeabc18d2d2c3e7586a1d189d8e01ad
krisht <tejaswini.krishnamurthy@emc.com>
2016-05-04 14:21:56 -0700

    Incorporated review comments.

--- dbsvc/src/main/test/com/emc/storageos/db/server/upgrade/impl/callback/XtremioBlockSnapshotDeviceLabelMigrationTest.java
+++ dbsvc/src/main/test/com/emc/storageos/db/server/upgrade/impl/callback/XtremioBlockSnapshotDeviceLabelMigrationTest.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2016 EMC Corporation
+ * All Rights Reserved
+ */
+
 package com.emc.storageos.db.server.upgrade.impl.callback;
 
 import java.io.IOException;
@@ -28,6 +33,9 @@ public class XtremioBlockSnapshotDeviceLabelMigrationTest extends DbSimpleMigrat
 
     private static final Logger log = LoggerFactory.getLogger(XtremioBlockSnapshotDeviceLabelMigrationTest.class);
     private static final String SNAPSHOT_LABEL = "snapshotLabel";
+    private static final String SNAPSHOT_DEVICE_LABEL = "snapshot-device-label";
+    private static final String SNAPSHOT_WITH_DEVICE_LABEL = "snap-with-device-label";
+    private static final String NON_XIO_SNAPSHOT_DEVICE_LABEL = "vnx-snapshot-device-label";
 
     @BeforeClass
     public static void setup() throws IOException {
@@ -54,6 +62,7 @@ public class XtremioBlockSnapshotDeviceLabelMigrationTest extends DbSimpleMigrat
     @Override
     protected void prepareData() throws Exception {
         createXIOSnapshots();
+        createNonXIOSnapshots();
     }
 
     @Override
@@ -69,9 +78,34 @@ public class XtremioBlockSnapshotDeviceLabelMigrationTest extends DbSimpleMigrat
                 Iterator<BlockSnapshot> xioSnapshots = _dbClient.queryIterativeObjects(BlockSnapshot.class, snapshotURIs);
                 while (xioSnapshots.hasNext()) {
                     BlockSnapshot xioSnapshot = xioSnapshots.next();
-                    Assert.assertNotNull("XtremIO snapshot should have deviceLabel set", xioSnapshot.getDeviceLabel());
-                    Assert.assertTrue("deviceLabel should be set to " + SNAPSHOT_LABEL,
-                            xioSnapshot.getDeviceLabel().equals(SNAPSHOT_LABEL));
+                    if (!xioSnapshot.getInactive()) {
+                        Assert.assertNotNull("XtremIO snapshot should have deviceLabel set", xioSnapshot.getDeviceLabel());
+                        if (SNAPSHOT_LABEL.equals(xioSnapshot.getLabel())) {
+                            // Verify that in case of snapshot without deviceLabel, we set it to the label
+                            Assert.assertTrue("deviceLabel should be set to " + SNAPSHOT_LABEL,
+                                    xioSnapshot.getDeviceLabel().equals(SNAPSHOT_LABEL));
+                        } else if (SNAPSHOT_WITH_DEVICE_LABEL.equals(xioSnapshot.getLabel())) {
+                            // Verify that in case of snapshot with deviceLabel, we do not change it
+                            Assert.assertTrue("deviceLabel should be set to " + SNAPSHOT_DEVICE_LABEL,
+                                    xioSnapshot.getDeviceLabel().equals(SNAPSHOT_DEVICE_LABEL));
+                        }
+                    }
+                }
+            }
+
+            if (DiscoveredDataObject.Type.vnxblock.name().equalsIgnoreCase(storageSystem.getSystemType())) {
+                URIQueryResultList snapshotURIs = new URIQueryResultList();
+                _dbClient.queryByConstraint(ContainmentConstraint.Factory.getStorageDeviceSnapshotConstraint(storageSystem.getId()),
+                        snapshotURIs);
+                Iterator<BlockSnapshot> vnxSnapshots = _dbClient.queryIterativeObjects(BlockSnapshot.class, snapshotURIs);
+                while (vnxSnapshots.hasNext()) {
+                    BlockSnapshot vnxSnapshot = vnxSnapshots.next();
+                    if (!vnxSnapshot.getInactive()) {
+                        Assert.assertNotNull("snapshot should have deviceLabel set", vnxSnapshot.getDeviceLabel());
+                        // Verify that in case of snapshot with deviceLabel, we do not change it
+                        Assert.assertTrue("deviceLabel should be set to " + NON_XIO_SNAPSHOT_DEVICE_LABEL,
+                                vnxSnapshot.getDeviceLabel().equals(NON_XIO_SNAPSHOT_DEVICE_LABEL));
+                    }
                 }
             }
         }
@@ -90,12 +124,48 @@ public class XtremioBlockSnapshotDeviceLabelMigrationTest extends DbSimpleMigrat
         parentVolume.setLabel("parentVolume");
         _dbClient.createObject(parentVolume);
 
+        NamedURI parentVolNamedUri = new NamedURI(parentVolume.getId(), parentVolume.getLabel());
+
+        BlockSnapshot snapshot = createSnapshot(SNAPSHOT_LABEL, xioStorageSystem.getId(), parentVolNamedUri);
+        _dbClient.createObject(snapshot);
+
+        BlockSnapshot snapshotWithDeviceLabel = createSnapshot("snap-with-device-label", xioStorageSystem.getId(), parentVolNamedUri);
+        snapshotWithDeviceLabel.setDeviceLabel("snapshot-device-label");
+        _dbClient.createObject(snapshotWithDeviceLabel);
+
+        BlockSnapshot inactiveSnapshot = createSnapshot("inactive-snapshot", xioStorageSystem.getId(), parentVolNamedUri);
+        _dbClient.createObject(inactiveSnapshot);
+        inactiveSnapshot.setInactive(true);
+        _dbClient.updateObject(inactiveSnapshot);
+    }
+
+    private BlockSnapshot createSnapshot(String label, URI storageController, NamedURI parentURI) {
         BlockSnapshot snapshot = new BlockSnapshot();
         snapshot.setId(URIUtil.createId(BlockSnapshot.class));
-        snapshot.setStorageController(xioStorageSystem.getId());
-        snapshot.setLabel(SNAPSHOT_LABEL);
+        snapshot.setStorageController(storageController);
+        snapshot.setLabel(label);
+        snapshot.setParent(parentURI);
+
+        return snapshot;
+    }
+
+    private void createNonXIOSnapshots() throws Exception {
+        log.info("Preparing BlockSnapshot for XtremioBlockSnapshotDeviceLabelMigrationTest");
+        StorageSystem storageSystem = new StorageSystem();
+        storageSystem.setId(URIUtil.createId(StorageSystem.class));
+        storageSystem.setSystemType(DiscoveredDataObject.Type.vnxblock.name());
+        _dbClient.createObject(storageSystem);
+
+        Volume parentVolume = new Volume();
+        URI volumeUri = URIUtil.createId(Volume.class);
+        parentVolume.setId(volumeUri);
+        parentVolume.setLabel("vnx-parentVolume");
+        _dbClient.createObject(parentVolume);
+
         NamedURI parentVolNamedUri = new NamedURI(parentVolume.getId(), parentVolume.getLabel());
-        snapshot.setParent(parentVolNamedUri);
+
+        BlockSnapshot snapshot = createSnapshot("vnx-snapshot", storageSystem.getId(), parentVolNamedUri);
+        snapshot.setDeviceLabel(NON_XIO_SNAPSHOT_DEVICE_LABEL);
         _dbClient.createObject(snapshot);
     }
 

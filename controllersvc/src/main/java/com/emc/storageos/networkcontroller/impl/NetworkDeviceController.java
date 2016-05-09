a373d8b3675cd358677c8848be288932bdb77c3f
DeLaTorre <Santiago.DeLaTorre@emc.com>
2016-05-09 14:42:35 -0400

    undo changes

--- controllersvc/src/main/java/com/emc/storageos/networkcontroller/impl/NetworkDeviceController.java
+++ controllersvc/src/main/java/com/emc/storageos/networkcontroller/impl/NetworkDeviceController.java
@@ -413,11 +413,8 @@ public class NetworkDeviceController implements NetworkController {
                                 _dbClient.markForDeletion(ref);
                                 _log.info(String.format("Remove FCZoneReference key: %s volume %s id %s",
                                         ref.getPwwnKey(), ref.getVolumeUri(), ref.getId().toString()));
-                                if(!zones.isEmpty()){
-                                	recordZoneEvent(ref, OperationTypeEnum.REMOVE_SAN_ZONE.name(),
-                                            OperationTypeEnum.REMOVE_SAN_ZONE.getDescription());
-                                }
-                                
+                                recordZoneEvent(ref, OperationTypeEnum.REMOVE_SAN_ZONE.name(),
+                                        OperationTypeEnum.REMOVE_SAN_ZONE.getDescription());
                             }
                         }
                     } catch (DatabaseException ex) {
@@ -437,11 +434,8 @@ public class NetworkDeviceController implements NetworkController {
                             _log.info(String.format(
                                     "%s FCZoneReference key: %s volume %s group %s",
                                     newOrExisting[0], ref.getPwwnKey(), ref.getVolumeUri(), exportGroupUri));
-                            if(!zones.isEmpty()){
-                            	recordZoneEvent(ref, OperationTypeEnum.ADD_SAN_ZONE.name(),
-                                        OperationTypeEnum.ADD_SAN_ZONE.getDescription());
-                            }
-                            
+                            recordZoneEvent(ref, OperationTypeEnum.ADD_SAN_ZONE.name(),
+                                    OperationTypeEnum.ADD_SAN_ZONE.getDescription());
                         }
                     } catch (DatabaseException ex) {
                         _log.error("Could not persist FCZoneReference: " + refKey);

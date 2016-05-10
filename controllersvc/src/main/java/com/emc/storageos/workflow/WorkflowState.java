4239a4b6a957ec88dbbf1cf938946f3c5ca6f70b
bill elliott <bill.elliott@emc.com>
2016-05-10 14:45:46 -0400

    add class/method property field.

--- controllersvc/src/main/java/com/emc/storageos/workflow/WorkflowState.java
+++ controllersvc/src/main/java/com/emc/storageos/workflow/WorkflowState.java
@@ -1,11 +1,11 @@
 package com.emc.storageos.workflow;
 
 public enum WorkflowState {
-	CREATED, 				// Initial state
-	RUNNING, 				// Running
-	SUCCESS, 				// Terminated with SUCCESS
-	ERROR,                   // Terminated with ERROR 
-	ROLLING_BACK,                 // Working on rolling back after error 
-	SUSPENDED_ERROR,          // Suspended 
+	CREATED, 			  // Initial state
+	RUNNING, 			  // Running
+	SUCCESS, 		      // Terminated with SUCCESS
+	ERROR,                // Terminated with ERROR 
+	ROLLING_BACK,         // Working on rolling back after error 
+	SUSPENDED_ERROR,      // Suspended 
 	SUSPENDED_NO_ERROR    // Suspended, but no error currently
 }

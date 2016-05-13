1b6a5018a1c22d47a1487c57f546c72d13c08513
Steve Mendes <steve.mendes@emc.com>
2016-05-13 17:04:41 +0000

    added support for ScaleIO MDM install and uninstall, multi-task operation handling, dependent assetOptions, specifying Node in catalog svc, override fields named Node_2 override Node

--- vipr-portal/com.iwave.isa.content/src/java/com/emc/sa/service/vipr/rackhd/gson/ViprTask.java
+++ vipr-portal/com.iwave.isa.content/src/java/com/emc/sa/service/vipr/rackhd/gson/ViprTask.java
@@ -2,14 +2,40 @@ package com.emc.sa.service.vipr.rackhd.gson;
 
 public class ViprTask {
 
-    ViprOperation[] task;
-
-    public ViprOperation[] getTask() {
-        return task;
-    }
-
-    public void setTask(ViprOperation[] task) {
-        this.task = task;
-    }
+    private String name;
+    private String id;
+    private ViprResource resource;
+    private String state;
+    private String op_id;
     
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getOp_id() {
+        return op_id;
+    }
+    public void setOp_id(String op_id) {
+        this.op_id = op_id;
+    }
+    public ViprResource getResource() {
+        return resource;
+    }
+    public void setResource(ViprResource resource) {
+        this.resource = resource;
+    }
+    public String getState() {
+        return state;
+    }
+    public void setState(String state) {
+        this.state = state;
+    }
 }

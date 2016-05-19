cbd3253b53ed70fac4b62f67a3953db74ce60fd3
Vineeth Totappanavar <vineeth.totappanavar@emc.com>
2016-05-19 14:51:25 +0530

    Parash code review comments fix - 3

---
+++ exportLibraries/xiv/src/main/java/com/emc/storageos/xiv/api/XIVRestClientFactory.java
@@ -0,0 +1,16 @@
+package com.emc.storageos.xiv.api;
+
+import java.net.URI;
+
+import com.emc.storageos.services.restutil.RestClientFactory;
+import com.emc.storageos.services.restutil.RestClientItf;
+import com.sun.jersey.api.client.Client;
+
+public class XIVRestClientFactory extends RestClientFactory{
+
+    @Override
+    protected RestClientItf createNewRestClient(URI endpoint, String username, String password, Client client) {
+        return new XIVRestClient(endpoint, username, password, client);
+    }
+
+}

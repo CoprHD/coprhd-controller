5fb5dd9aa3731be26f9f94f76f48bf24393af956
Sreenidhi Iyangar <sreenidhi.iyangar@emc.com>
2016-05-19 20:44:26 +0530

    volume details update

---
+++ exportLibraries/storagedrivers/3par/src/main/java/com/emc/storageos/hp3par/command/VolumeDetailsCommandResult.java
@@ -0,0 +1,26 @@
+package com.emc.storageos.hp3par.command;
+
+public class VolumeDetailsCommandResult {
+    private Long sizeMiB;
+    private String wwn;
+    private String uuid;
+    
+    public Long getSizeMiB() {
+        return sizeMiB;
+    }
+    public void setSizeMiB(Long sizeMiB) {
+        this.sizeMiB = sizeMiB;
+    }
+    public String getWwn() {
+        return wwn;
+    }
+    public void setWwn(String wwn) {
+        this.wwn = wwn;
+    }
+    public String getUuid() {
+        return uuid;
+    }
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+}

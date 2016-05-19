c0652521918704f7cdb7a05aa6d05fd86d8efe46
Lianlai Zhang <lianlai.zhang@gmail.com>
2016-05-19 15:15:11 +0000

    Minor change to avoid unnecessary placement on multiple arrays for single resource request

--- apisvc/src/main/java/com/emc/storageos/api/service/impl/placement/StorageScheduler.java
+++ apisvc/src/main/java/com/emc/storageos/api/service/impl/placement/StorageScheduler.java
@@ -876,7 +876,7 @@ public class StorageScheduler implements Scheduler {
         // Handle array affinity placement
         // If inCG is true, it is similar to the array affinity case.
         // Only difference is that resources will not be placed to more than one preferred systems if inCG is true
-        if (inCG ||capabilities.getHostArrayAffinity()) {
+        if (capabilities.getResourceCount() > 1 && inCG ||capabilities.getHostArrayAffinity()) {
             List<Recommendation> recommendations = performArrayAffinityPlacement(varrayId, capabilities, candidatePools, inCG);
             if (!recommendations.isEmpty()) {
                 return recommendations;

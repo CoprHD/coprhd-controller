f8f1ff8c31dab8579055aa74abc186505c3662bd
Lianlai Zhang <lianlai.zhang@gmail.com>
2016-05-23 22:21:29 +0000

    Refined array affinity discovery

--- controllersvc/src/main/java/com/emc/storageos/volumecontroller/impl/plugins/discovery/smis/processor/export/ArrayAffinityPostProcessor.java
+++ controllersvc/src/main/java/com/emc/storageos/volumecontroller/impl/plugins/discovery/smis/processor/export/ArrayAffinityPostProcessor.java
@@ -9,16 +9,14 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.emc.storageos.computesystemcontroller.impl.ComputeSystemHelper;
 import com.emc.storageos.db.client.DbClient;
-import com.emc.storageos.db.client.constraint.AlternateIdConstraint;
-import com.emc.storageos.db.client.constraint.URIQueryResultList;
 import com.emc.storageos.db.client.model.Host;
-import com.emc.storageos.db.client.model.Initiator;
 import com.emc.storageos.db.client.model.StringSet;
 import com.emc.storageos.db.client.model.UnManagedDiscoveredObjects.UnManagedExportMask;
 import com.emc.storageos.plugins.common.PartitionManager;
@@ -31,8 +29,6 @@ public class ArrayAffinityPostProcessor {
             .getLogger(ArrayAffinityPostProcessor.class);
     private static final List<String> HOST_PROPERTIES =
             Arrays.asList("preferredSystemIds", "label");
-    private static final List<String> UNMANAGED_EXPORT_MASK_PROPERTIES =
-            Arrays.asList("storageSystem", "knownVolumeUris", "hasUnknownVolume", "inactive");
 
     private static final String HOST = "Host";
     private static final int BATCH_SIZE = 100;
@@ -43,7 +39,7 @@ public class ArrayAffinityPostProcessor {
         _partitionManager = partitionManager;
     }
 
-    public void updatePreferredSystems(DbClient dbClient) {
+    public void updatePreferredSystems(Map<URI, Set<UnManagedExportMask>> hostUnManagedExportMasks, URI systemId, DbClient dbClient) {
         List<Host> hostsToUpdate = new ArrayList<Host>();
 
         try {
@@ -55,9 +51,21 @@ public class ArrayAffinityPostProcessor {
                 if (host != null) {
                     _logger.info("Processing host {}", host.getLabel());
                     StringSet existingPreferredSystems = host.getPreferredSystemIds();
-                    StringSet newPreferredSystems = findPreferredSystems(host.getId(), dbClient);
-                    if (!newPreferredSystems.equals(existingPreferredSystems)) {
-                        host.getPreferredSystemIds().replace(newPreferredSystems);
+                    boolean notPreferredSystem = true;
+                    String systemIdStr = systemId.toString();
+                    if (hostUnManagedExportMasks != null && !hostUnManagedExportMasks.isEmpty()) {
+                        Set<UnManagedExportMask> masks = hostUnManagedExportMasks.get(host.getId());
+                        if (masks != null && !masks.isEmpty() && allUnKnownVolumesInMasks(masks)) {
+                            if (!existingPreferredSystems.contains(systemIdStr)) {
+                                notPreferredSystem = false;
+                                existingPreferredSystems.add(systemIdStr);
+                                hostsToUpdate.add(host);
+                            }
+                        }
+                    }
+
+                    if (notPreferredSystem && existingPreferredSystems.contains(systemIdStr)) {
+                        existingPreferredSystems.remove(systemIdStr);
                         hostsToUpdate.add(host);
                     }
                 }
@@ -78,39 +86,22 @@ public class ArrayAffinityPostProcessor {
     }
 
     /**
-     * Returns a set of Ids of storage systems which have storage mapped to the given host.
+     * Check if all volumes in mask are unknown.
      *
-     * @param hostUri the Host URI to check
-     * @param dbClient a reference to the database client
-     * @return a set of Ids of storage systems
+     * @param masks UnManagedExportMasks to check
+     * @return true if all volumes are unknown in at least one of the masks
      */
-    private StringSet findPreferredSystems(URI hostURI, DbClient dbClient) {
-        _logger.info("Finding unmanaged export masks for host " + hostURI);
-        List<Initiator> initiators = ComputeSystemHelper.queryInitiators(dbClient, hostURI);
-
-        StringSet preferredSystems = new StringSet();
-        URIQueryResultList results = new URIQueryResultList();
-        for (Initiator initiator : initiators) {
-            _logger.info("Looking at initiator " + initiator.getInitiatorPort());
-            dbClient.queryByConstraint(
-                    AlternateIdConstraint.Factory.getUnManagedExportMaskKnownInitiatorConstraint(initiator.getInitiatorPort()), results);
-            if (results.iterator() != null) {
-                Iterator<UnManagedExportMask> masks =
-                        dbClient.queryIterativeObjectFields(UnManagedExportMask.class,
-                                UNMANAGED_EXPORT_MASK_PROPERTIES, results);
-                while (masks.hasNext()) {
-                    UnManagedExportMask mask = masks.next();
-                    _logger.info("Processing unmanaged export mask {}", mask.getMaskName());
-                    if (mask != null && !mask.getInactive()) {
-                        // all volumes in the mask are unknown to ViPR
-                        if (mask.getHasUnknownVolume() && mask.getKnownVolumeUris().isEmpty()) {
-                            preferredSystems.add(mask.getStorageSystemUri().toString());
-                        }
-                    }
+    private boolean allUnKnownVolumesInMasks(Set<UnManagedExportMask> masks) {
+        for (UnManagedExportMask mask : masks) {
+            _logger.info("Processing unmanaged export mask {}", mask.getMaskName());
+            if (mask != null && !mask.getInactive()) {
+                // all volumes in the mask are unknown to ViPR
+                if (mask.getHasUnknownVolume() && mask.getKnownVolumeUris().isEmpty()) {
+                    return true;
                 }
             }
         }
 
-        return preferredSystems;
+        return false;
     }
 }

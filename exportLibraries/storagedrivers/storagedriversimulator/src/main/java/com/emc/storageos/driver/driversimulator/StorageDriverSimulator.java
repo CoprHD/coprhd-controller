282756762956c74f425469ec679eae610bc14807
Evgeny <evgeny.roytman@emc.com>
2016-05-23 18:44:56 -0400

    Added DefaultStorageDriver implementation to improve usability of the SDK.

--- exportLibraries/storagedrivers/storagedriversimulator/src/main/java/com/emc/storageos/driver/driversimulator/StorageDriverSimulator.java
+++ exportLibraries/storagedrivers/storagedriversimulator/src/main/java/com/emc/storageos/driver/driversimulator/StorageDriverSimulator.java
@@ -15,6 +15,7 @@ import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
 
+import com.emc.storageos.storagedriver.DefaultStorageDriver;
 import org.apache.commons.lang.mutable.MutableBoolean;
 import org.apache.commons.lang.mutable.MutableInt;
 import org.slf4j.Logger;
@@ -42,7 +43,7 @@ import com.emc.storageos.storagedriver.storagecapabilities.CapabilityInstance;
 import com.emc.storageos.storagedriver.storagecapabilities.StorageCapabilities;
 
 
-public class StorageDriverSimulator extends AbstractStorageDriver implements BlockStorageDriver {
+public class StorageDriverSimulator extends DefaultStorageDriver implements BlockStorageDriver {
 
     private static final Logger _log = LoggerFactory.getLogger(StorageDriverSimulator.class);
     private static final String DRIVER_NAME = "SimulatorDriver";

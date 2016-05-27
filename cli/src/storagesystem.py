10bc26ddcd985ff4a9dcc750678ece26115581db
Thondukulam Kumar <thondukulam.kumar@emc.com>
2016-05-27 23:02:36 +0530

    Fixed the storagesystem bracket issue

--- cli/src/storagesystem.py
+++ cli/src/storagesystem.py
@@ -78,8 +78,8 @@ class StorageSystem(object):
         'xtremio',
         'ibmxiv',
         'vnxe',
-        'ceph']
-        'unity',
+        'ceph',
+        'unity']
 
     FILE_SYSTEM_LIST = [
         'isilon',

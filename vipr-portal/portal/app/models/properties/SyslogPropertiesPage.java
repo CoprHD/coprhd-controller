7766874f32f4b5203cd29b2b0905ecb570a8bdd9
Nick Aquino <nicholas.aquino@emc.com>
2016-06-09 14:37:39 -0400

    changes to UI to support logs and basic property selection

--- vipr-portal/portal/app/models/properties/SyslogPropertiesPage.java
+++ vipr-portal/portal/app/models/properties/SyslogPropertiesPage.java
@@ -6,17 +6,27 @@ package models.properties;
 
 import java.util.Map;
 import util.ConfigProperty;
+import util.StringOption;
 
 public class SyslogPropertiesPage extends CustomPropertyPage {
     private Property externalLocationUrl;
+    private Property maxManualCopies;
+    private StringOption logOptions;
 
     public SyslogPropertiesPage(Map<String, Property> properties) {
         super("Syslog");
         setRenderTemplate("syslogPage.html");
         externalLocationUrl = addCustomProperty(properties, ConfigProperty.BACKUP_EXTERNAL_URL);
+        maxManualCopies = addCustomProperty(properties, ConfigProperty.BACKUP_MAX_MANUAL_COPIES);
     }
 
     public Property getExternalLocationUrl() {
         return externalLocationUrl;
+    }
+    public Property getMaxManualCopies() {
+        return maxManualCopies;
+    }
+    public Property getLogOptions() {
+        return logOptions;
     }
 }

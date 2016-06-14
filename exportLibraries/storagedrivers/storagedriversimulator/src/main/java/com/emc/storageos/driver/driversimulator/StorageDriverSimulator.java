6c71ad92c8e76591eb5bd73bae172f5244101a88
DeLaTorre <Santiago.DeLaTorre@emc.com>
2016-06-14 14:56:14 -0400

    implementd function for adding volumes in StorageDriverSimulator

--- exportLibraries/storagedrivers/storagedriversimulator/src/main/java/com/emc/storageos/driver/driversimulator/StorageDriverSimulator.java
+++ exportLibraries/storagedrivers/storagedriversimulator/src/main/java/com/emc/storageos/driver/driversimulator/StorageDriverSimulator.java
@@ -382,6 +382,23 @@ public class StorageDriverSimulator extends DefaultStorageDriver implements Bloc
                 volumes.get(0).getStorageSystemId(), volumes.toString());
         return task;
     }
+    
+    @Override
+    public DriverTask addVolumesToConsistencyGroup (List<StorageVolume> volumes, StorageCapabilities capabilities){
+    	_log.info("Adding {} Volumes to Consistency Group", volumes.toString());
+        String taskType = "add-volumes-to-consistency-groupd";
+        String taskId = String.format("%s+%s+%s", DRIVER_NAME, taskType, UUID.randomUUID().toString());
+        DriverTask task = new DriverSimulatorTask(taskId);
+        task.setStatus(DriverTask.TaskStatus.READY);
+        
+        String msg = String.format("StorageDriver: doAddVolumesToConsistencyGroup information for storage system {}, volume nativeIds {}, Consistency Group - end",
+                volumes.get(0).getStorageSystemId(), volumes.toString());
+        _log.info(msg);
+        task.setMessage(msg);
+        
+        return task;
+    }
+
 
     @Override
     public DriverTask createVolumeSnapshot(List<VolumeSnapshot> snapshots, StorageCapabilities capabilities) {

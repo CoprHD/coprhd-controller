a5a2507124f7f2ac7c4aec2a84388829f31d6b4b
DeLaTorre <Santiago.DeLaTorre@emc.com>
2016-06-16 16:57:25 -0400

    changes in fucntions

--- exportLibraries/storagedrivers/storagedriversimulator/src/main/java/com/emc/storageos/driver/driversimulator/StorageDriverSimulator.java
+++ exportLibraries/storagedrivers/storagedriversimulator/src/main/java/com/emc/storageos/driver/driversimulator/StorageDriverSimulator.java
@@ -384,8 +384,8 @@ public class StorageDriverSimulator extends DefaultStorageDriver implements Bloc
     }
     
     @Override
-    public DriverTask addVolumesToConsistencyGroup (List<StorageVolume> volumes, StorageCapabilities capabilities){
-    	_log.info("Adding {} Volumes to Consistency Group {}", volumes.toString(), volumes.get(0).getConsistencyGroup());
+    public DriverTask addVolumesToConsistencyGroup (List<StorageVolume> volumes, String cgName, StorageCapabilities capabilities){
+    	_log.info("Adding {} Volumes to Consistency Group {}", volumes.toString(), cgName);
         String taskType = "add-volumes-to-consistency-groupd";
         String taskId = String.format("%s+%s+%s", DRIVER_NAME, taskType, UUID.randomUUID().toString());
         DriverTask task = new DriverSimulatorTask(taskId);
@@ -400,8 +400,8 @@ public class StorageDriverSimulator extends DefaultStorageDriver implements Bloc
     }
     
     @Override
-    public DriverTask removeVolumesFromConsistencyGroup(List<StorageVolume> volumes, StorageCapabilities capabilities){
-        _log.info("Remove {} Volumes from Consistency Group {}", volumes.toString(), volumes.get(0).getConsistencyGroup());
+    public DriverTask removeVolumesFromConsistencyGroup(List<StorageVolume> volumes, String cgName, StorageCapabilities capabilities){
+        _log.info("Remove {} Volumes from Consistency Group {}", volumes.toString(), cgName);
         String taskType = "remove-volumes-to-consistency-groupd";
         String taskId = String.format("%s+%s+%s", DRIVER_NAME, taskType, UUID.randomUUID().toString());
         DriverTask task = new DriverSimulatorTask(taskId);

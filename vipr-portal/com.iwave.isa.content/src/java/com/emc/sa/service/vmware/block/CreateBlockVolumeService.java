4fbb8c50838c231bfbce3fe806a317b353a855b3
Trevor Dawe <trevor.dawe@emc.com>
2016-06-22 10:15:52 -0300

    COP-23367: Rescan in VMware services for delete datastore and create volume

--- vipr-portal/com.iwave.isa.content/src/java/com/emc/sa/service/vmware/block/CreateBlockVolumeService.java
+++ vipr-portal/com.iwave.isa.content/src/java/com/emc/sa/service/vmware/block/CreateBlockVolumeService.java
@@ -4,10 +4,32 @@
  */
 package com.emc.sa.service.vmware.block;
 
+import com.emc.sa.engine.bind.Bindable;
 import com.emc.sa.engine.service.Service;
 import com.emc.sa.service.vipr.block.CreateBlockStorageForHostService;
+import com.emc.sa.service.vmware.VMwareHostService;
 
 @Service("VMware-CreateBlockVolume")
-public class CreateBlockVolumeService extends CreateBlockStorageForHostService {
+public class CreateBlockVolumeService extends VMwareHostService {
+
+    @Bindable
+    protected CreateBlockStorageForHostService hostService = new CreateBlockStorageForHostService();
+
+    @Override
+    public void init() throws Exception {
+        hostService.init();
+    }
+
+    @Override
+    public void precheck() throws Exception {
+        hostService.precheck();
+    }
+
+    @Override
+    public void execute() throws Exception {
+        hostService.execute();
+        super.connectAndInitializeHost();
+        vmware.refreshStorage(host, cluster);
+    }
 
 }

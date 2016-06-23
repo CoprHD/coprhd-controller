98a3f12748c0b165c84561d5cef2995e1c70c5b6
Max Huang <max.huang@emc.com>
2016-06-23 12:16:30 +0800

    Reformat the source file.

--- exportLibraries/storagedrivers/vmaxv3driver/src/test/java/com/emc/storageos/driver/vmaxv3driver/util/rest/RestClientTest.java
+++ exportLibraries/storagedrivers/vmaxv3driver/src/test/java/com/emc/storageos/driver/vmaxv3driver/util/rest/RestClientTest.java
@@ -29,28 +29,28 @@ public class RestClientTest extends BaseRestTest {
     @Test
     public void testCreateStorageGroup() {
         String body = "{\"srpId\": \"none\", \"storageGroupId\": \"test1\", " +
-            "\"sloBasedStorageGroupParam\": [{\"num_of_vols\": 2, \"volumeAttribute\": " +
-            "{\"volume_size\": \"10\", \"capacityUnit\": \"GB\"}}]}";
+                "\"sloBasedStorageGroupParam\": [{\"num_of_vols\": 2, \"volumeAttribute\": " +
+                "{\"volume_size\": \"10\", \"capacityUnit\": \"GB\"}}]}";
         logger.info("body = {}", body);
         String response = client.request("/univmax/restapi/sloprovisioning/symmetrix/" + symId + "/storagegroup",
-            RequestType.POST, body);
+                RequestType.POST, body);
         logger.info("response = {}", response);
     }
 
-    @Test(dependsOnMethods = {"testCreateStorageGroup"})
+    @Test(dependsOnMethods = { "testCreateStorageGroup" })
     public void testUpdateStorageGroup() {
         String body = "{\"editStorageGroupActionParam\": {\"expandStorageGroupParam\": {" +
-            "\"num_of_vols\": 1, \"volumeAttribute\": {\"volume_size\": \"10\", \"capacityUnit\": \"GB\"}, \"create_new_volumes\": true}}}";
+                "\"num_of_vols\": 1, \"volumeAttribute\": {\"volume_size\": \"10\", \"capacityUnit\": \"GB\"}, \"create_new_volumes\": true}}}";
         logger.info("body = {}", body);
         String response = client.request("/univmax/restapi/sloprovisioning/symmetrix/" + symId + "/storagegroup/test1",
-            RequestType.PUT, body);
+                RequestType.PUT, body);
         logger.info("response = {}", response);
     }
 
-    @Test(dependsOnMethods = {"testUpdateStorageGroup"})
+    @Test(dependsOnMethods = { "testUpdateStorageGroup" })
     public void testDeleteStorageGroup() {
         String response = client.request("/univmax/restapi/sloprovisioning/symmetrix/" + symId + "/storagegroup/test1",
-            RequestType.DELETE);
+                RequestType.DELETE);
         logger.info("response = {}", response);
     }
 }

1b062905543f0be8c0744cd58f83898fe5c0d3df
mhegeric <mike.hegerich@emc.com>
2016-06-27 11:19:29 -0400

    Add scripts to create/delete/query/list/show custom configurations

--- tools/tests/bourne.py
+++ tools/tests/bourne.py
@@ -472,6 +472,11 @@ URI_ZONE                        = URI_SERVICES_BASE + '/zone/{0}'
 URI_ZONES	                = URI_SERVICES_BASE + '/zone'
 URI_ZONE_CAPACITY               = URI_SERVICES_BASE + '/zone/capacity'
 
+URI_CUSTOMCONFIGS               = URI_SERVICES_BASE + '/config/controller'
+URI_CUSTOMCONFIG                = URI_CUSTOMCONFIGS + '/{0}'
+URI_CUSTOMCONFIG_DELETE         = URI_CUSTOMCONFIG + '/deactivate'
+
+
 URI_REPLICATION_GROUP           = URI_SERVICES_BASE + '/vdc/data-service/vpools/{0}'
 URI_REPLICATION_GROUPS          = URI_SERVICES_BASE + '/vdc/data-service/vpools'
 URI_REPLICATION_EXTEND          = URI_SERVICES_BASE + '/vdc/data-service/vpools/{0}/addvarrays'
@@ -8868,3 +8873,53 @@ class Bourne:
         # complete, then they are all complete.
         task = self.api_sync_2(session_uri, task_opid, self.block_snapshot_session_show_task)
         return (tasklist, task['state'], task['message'])
+
+
+    def customconfig_query(self, name, scopetype, scope, isdefault):
+        if (self.__is_uri(name)):
+            return name
+
+        cclist = self.customconfig_list()
+        for ccrel in cclist:
+            try:
+                if (ccrel['name'] == name):
+                    cc = self.api('GET', ccrel['link']['href'])
+                    ccscope = cc['scope']
+                    ccsysdflt = str(cc['system_default'])
+                    print ccsysdflt
+                    print isdefault
+                    if ((ccscope['type'] == scopetype) and (ccscope['value'] == scope) and (ccsysdflt == isdefault)):
+                        return ccrel['id']
+            except KeyError:
+                print 'No name key'
+        raise Exception('Bad custom configuration name: ' + name)
+
+    def customconfig_list(self):
+        cc_list = self.api('GET', URI_CUSTOMCONFIGS)
+        if (not cc_list):
+            return {}
+        ccs = cc_list['config']
+        if (type(ccs) != list):
+            return [ccs]
+        return ccs
+
+    def customconfig_show(self, uri):
+        return self.api('GET', URI_CUSTOMCONFIG.format(uri))
+
+    def customconfig_delete(self, uri):
+        return self.api('POST', URI_CUSTOMCONFIG_DELETE.format(uri))
+
+    def customconfig_create(self, type, value, scopetype, scope, register):
+        scope = {
+            'type' : scopetype,
+            'value' : scope,
+        }
+
+        parms = {
+            'config_type' : type,
+            'value' : value,
+            'scope' : scope,
+            'registered' : register,
+        }
+
+        return self.api('POST', URI_CUSTOMCONFIGS, parms, {})

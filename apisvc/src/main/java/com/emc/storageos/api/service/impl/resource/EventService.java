909820438414ae48c0d88732132ef7270b71a47e
Trevor Dawe <trevor.dawe@emc.com>
2016-07-05 12:24:08 -0300

    COP-22408: Added deactivate API call for Event

--- apisvc/src/main/java/com/emc/storageos/api/service/impl/resource/EventService.java
+++ apisvc/src/main/java/com/emc/storageos/api/service/impl/resource/EventService.java
@@ -16,6 +16,7 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
@@ -60,6 +61,17 @@ public class EventService extends TaskResourceService {
         // check the user permissions
         verifyAuthorizedInTenantOrg(event.getTenant(), getUserFromContext());
         return map(event);
+    }
+
+    @POST
+    @Path("/{id}/deactivate")
+    @Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+    public Response deleteEvent(@PathParam("id") URI id) throws DatabaseException {
+        ActionableEvent event = queryObject(ActionableEvent.class, id, false);
+        // check the user permissions
+        verifyAuthorizedInTenantOrg(event.getTenant(), getUserFromContext());
+        _dbClient.markForDeletion(event);
+        return Response.ok().build();
     }
 
     @POST

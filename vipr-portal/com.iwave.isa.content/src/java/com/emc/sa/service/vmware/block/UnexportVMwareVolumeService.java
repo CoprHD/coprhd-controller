8e62efdbc488a84a6dc467452090e63601dda747
Trevor Dawe <trevor.dawe@emc.com>
2016-07-11 09:25:41 -0300

    COP-23745: During unexport VMware volume, enable storage I/O if the datastore still exists on other hosts

--- vipr-portal/com.iwave.isa.content/src/java/com/emc/sa/service/vmware/block/UnexportVMwareVolumeService.java
+++ vipr-portal/com.iwave.isa.content/src/java/com/emc/sa/service/vmware/block/UnexportVMwareVolumeService.java
@@ -88,7 +88,12 @@ public class UnexportVMwareVolumeService extends VMwareHostService {
             if (!StringUtils.isEmpty(datastoreName)) {
                 Datastore datastore = vmware.getDatastore(datacenter.getLabel(), datastoreName);
                 if (datastore != null) {
+                    boolean storageIOControlEnabled = datastore.getIormConfiguration().isEnabled();
                     vmware.unmountVmfsDatastore(host, cluster, datastore);
+                    datastore = vmware.getDatastore(datacenter.getLabel(), datastoreName);
+                    if (datastore != null && storageIOControlEnabled) {
+                        vmware.setStorageIOControl(datastore, true);
+                    }
                 }
             }
         }

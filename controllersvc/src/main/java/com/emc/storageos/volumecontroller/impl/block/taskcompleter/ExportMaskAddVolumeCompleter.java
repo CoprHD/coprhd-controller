f725229ff882072fd605b8bbb5e1ad2ad4ea44f3
Ian Bibby <ian.bibby@emc.com>
2016-07-27 14:03:03 +0000

    Updates export add volume completer to allow test_13 to pass

--- controllersvc/src/main/java/com/emc/storageos/volumecontroller/impl/block/taskcompleter/ExportMaskAddVolumeCompleter.java
+++ controllersvc/src/main/java/com/emc/storageos/volumecontroller/impl/block/taskcompleter/ExportMaskAddVolumeCompleter.java
@@ -5,14 +5,6 @@
 
 package com.emc.storageos.volumecontroller.impl.block.taskcompleter;
 
-import java.net.URI;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import org.slf4j.LoggerFactory;
-
 import com.emc.storageos.db.client.DbClient;
 import com.emc.storageos.db.client.model.BlockObject;
 import com.emc.storageos.db.client.model.ExportGroup;
@@ -21,6 +13,17 @@ import com.emc.storageos.db.client.model.Operation;
 import com.emc.storageos.exceptions.DeviceControllerException;
 import com.emc.storageos.svcs.errorhandling.model.ServiceCoded;
 import com.emc.storageos.util.ExportUtils;
+import com.emc.storageos.volumecontroller.impl.utils.ExportOperationContext;
+import com.emc.storageos.workflow.WorkflowService;
+import org.slf4j.LoggerFactory;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static com.emc.storageos.volumecontroller.impl.smis.vmax.VmaxExportOperationContext.OPERATION_ADD_VOLUMES_TO_STORAGE_GROUP;
 
 @SuppressWarnings("serial")
 public class ExportMaskAddVolumeCompleter extends ExportTaskCompleter {
@@ -43,17 +46,20 @@ public class ExportMaskAddVolumeCompleter extends ExportTaskCompleter {
             ExportGroup exportGroup = dbClient.queryObject(ExportGroup.class, getId());
             ExportMask exportMask = (getMask() != null) ?
                     dbClient.queryObject(ExportMask.class, getMask()) : null;
-            for (URI volumeURI : _volumes) {
-                BlockObject volume = BlockObject.fetch(dbClient, volumeURI);
-                _log.info(String.format("Done ExportMaskAddVolume - Id: %s, OpId: %s, status: %s",
-                        getId().toString(), getOpId(), status.name()));
 
-                if (exportMask != null && status == Operation.Status.ready) {
-                    exportMask.addToUserCreatedVolumes(volume);
-                }
+            if (exportMask == null) {
+                _log.warn("Export mask was null for task {}", getOpId());
+                return;
             }
 
-            if (exportMask != null && status == Operation.Status.ready) {
+            if (shouldUpdateDatabase(status)) {
+                for (URI volumeURI : _volumes) {
+                    BlockObject volume = BlockObject.fetch(dbClient, volumeURI);
+                    _log.info(String.format("Done ExportMaskAddVolume - Id: %s, OpId: %s, status: %s",
+                            getId().toString(), getOpId(), status.name()));
+                    exportMask.addToUserCreatedVolumes(volume);
+                }
+
                 exportMask.addVolumes(_volumeMap);
                 exportGroup.addExportMask(exportMask.getId());
                 ExportUtils.reconcileHLUs(dbClient, exportGroup, exportMask, _volumeMap);
@@ -67,6 +73,35 @@ public class ExportMaskAddVolumeCompleter extends ExportTaskCompleter {
         } finally {
             super.complete(dbClient, status, coded);
         }
+    }
+
+    /**
+     * This completer may complete with a ready or error status.  In the case of an error status,
+     * we can check the {@link ExportOperationContext} to see if the volumes were added and perform
+     * the necessary database updates.
+     *
+     * @param status    Status of the operation.
+     * @return          true, if the status is ready or the volumes were added despite error status.
+     */
+    private boolean shouldUpdateDatabase(Operation.Status status) {
+        return wereVolumesAdded() || status == Operation.Status.ready;
+    }
+
+    private boolean wereVolumesAdded() {
+        ExportOperationContext context = (ExportOperationContext) WorkflowService.getInstance().loadStepData(getOpId());
+
+        if (context != null) {
+            List<ExportOperationContext.ExportOperationContextOperation> operations = context.getOperations();
+
+            for (ExportOperationContext.ExportOperationContextOperation operation : operations) {
+                if (OPERATION_ADD_VOLUMES_TO_STORAGE_GROUP.equalsIgnoreCase(operation.getOperation())) {
+                    // TODO Check arguments.
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
 
 }

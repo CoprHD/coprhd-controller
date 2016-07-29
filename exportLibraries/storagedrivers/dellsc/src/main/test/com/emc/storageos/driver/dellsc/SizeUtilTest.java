730cc62c21d1f11a57bcc026db9957baaa46c65d
Sean McGinnis <sean_mcginnis@dell.com>
2016-07-29 12:28:14 -0500

    COP-23024 Provision operations for Dell SC
    Add volume create, extend, and delete operations to Dell
    Storage Center driver.
    
    Signed-off-by: Sean McGinnis sean_mcginnis@dell.com

---
+++ exportLibraries/storagedrivers/dellsc/src/main/test/com/emc/storageos/driver/dellsc/SizeUtilTest.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2016 Dell Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package com.emc.storageos.driver.dellsc;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Tests fir the SizeUtil class.
+ */
+public class SizeUtilTest {
+
+    /**
+     * Test method for {@link com.emc.storageos.driver.dellsc.SizeUtil#byteToGig(long)}.
+     */
+    @Test
+    public void testByteToGig() {
+        int result = SizeUtil.byteToGig(1073741824L);
+        Assert.assertTrue(result == 1);
+    }
+
+    /**
+     * Test method for {@link com.emc.storageos.driver.dellsc.SizeUtil#byteToGig(long)}.
+     */
+    @Test
+    public void testByteToGigLarger() {
+        int result = SizeUtil.byteToGig(107374182400L);
+        Assert.assertTrue(result == 100);
+    }
+
+    /**
+     * Test method for {@link com.emc.storageos.driver.dellsc.SizeUtil#sizeStrToBytes(java.lang.String)}.
+     */
+    @Test
+    public void testSizeStrToBytes() {
+        long result = SizeUtil.sizeStrToBytes("100 GB");
+        Assert.assertTrue(result == 107374182400L);
+    }
+
+    /**
+     * Test method for {@link com.emc.storageos.driver.dellsc.SizeUtil#sizeStrToBytes(java.lang.String)}.
+     */
+    @Test
+    public void testSizeStrToBytesEngNotation() {
+        long result = SizeUtil.sizeStrToBytes("1.073741824E9");
+        Assert.assertTrue(result == 1073741824L);
+    }
+}

b704f63c38ca47a074ebbdafd230dab075d75366
Sean McGinnis <sean_mcginnis@dell.com>
2016-08-02 17:03:51 -0500

    COP-23023 Discover Dell SC arrays through Southbound SDK
    Refactored code to better manage functional areas.
    Added consistency group handling.
    
    Signed-off-by: Sean McGinnis sean_mcginnis@dell.com

--- exportLibraries/storagedrivers/dellsc/src/main/java/com/emc/storageos/driver/dellsc/helpers/DellSCProvisioning.java
+++ exportLibraries/storagedrivers/dellsc/src/main/java/com/emc/storageos/driver/dellsc/helpers/DellSCProvisioning.java
@@ -23,7 +23,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import org.apache.commons.lang.mutable.MutableBoolean;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 

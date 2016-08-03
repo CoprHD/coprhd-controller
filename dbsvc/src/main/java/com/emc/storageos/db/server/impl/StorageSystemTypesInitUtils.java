5f68aa34c847a03d3ec2387cfe0b30806344dadf
Cao, Shiliang <shiliang.cao@emc.com>
2016-08-03 14:51:29 +0800

    modify the port of unity to non-ssl type

--- dbsvc/src/main/java/com/emc/storageos/db/server/impl/StorageSystemTypesInitUtils.java
+++ dbsvc/src/main/java/com/emc/storageos/db/server/impl/StorageSystemTypesInitUtils.java
@@ -136,7 +136,6 @@ public class StorageSystemTypesInitUtils {
         SSL_PORT_MAP.put(ECS, "4443");
         SSL_PORT_MAP.put(VNXe, "443");
         SSL_PORT_MAP.put(VNXFILE_SMIS, "5989");
-        SSL_PORT_MAP.put(UNITY, "443");
 
         NON_SSL_PORT_MAP = new HashMap<String, String>();
         NON_SSL_PORT_MAP.put(HITACHI_PROVIDER, "2001");
@@ -161,6 +160,7 @@ public class StorageSystemTypesInitUtils {
         NON_SSL_PORT_MAP.put(VNXe, "443");
         NON_SSL_PORT_MAP.put(VNXFILE_SMIS, "5988");
         NON_SSL_PORT_MAP.put(HITACHI, "2001");
+        NON_SSL_PORT_MAP.put(UNITY, "443");
 
         STORAGE_PROVIDER_MAP = new HashMap<String, String>();
         STORAGE_PROVIDER_MAP.put(VMAX, "Storage Provider for EMC VMAX, VNX Block");
@@ -383,9 +383,12 @@ public class StorageSystemTypesInitUtils {
         unity.setStorageTypeDispName(DISPLAY_NAME_MAP.get(UNITY));
         unity.setMetaType(StorageSystemType.META_TYPE.BLOCK_AND_FILE.toString().toLowerCase());
         unity.setIsSmiProvider(false);
-        unity.setIsDefaultSsl(true);
+        unity.setIsDefaultSsl(false);
         if (SSL_PORT_MAP.get(UNITY) != null) {
             unity.setSslPort(SSL_PORT_MAP.get(UNITY));
+        }
+        if (NON_SSL_PORT_MAP.get(UNITY) != null) {
+            unity.setNonSslPort(NON_SSL_PORT_MAP.get(UNITY));
         }
         unity.setDriverClassName(StorageSystemType.META_TYPE.BLOCK_AND_FILE.toString().toLowerCase());
 

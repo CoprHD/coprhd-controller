909cf87e9f8fdb75db649a7d1a8fc7888cabd497
Trevor Dawe <trevor.dawe@emc.com>
2016-08-04 14:25:18 -0300

    COP-22408: Fixed cluster change logic for vcenter

--- controllersvc/src/main/java/com/emc/storageos/computesystemcontroller/impl/adapter/VcenterDiscoveryAdapter.java
+++ controllersvc/src/main/java/com/emc/storageos/computesystemcontroller/impl/adapter/VcenterDiscoveryAdapter.java
@@ -511,8 +511,8 @@ public class VcenterDiscoveryAdapter extends EsxHostDiscoveryAdapter {
 
                 URI targetCluster = cluster != null ? cluster.getId() : NullColumnValueGetter.getNullURI();
 
-                boolean isClusterChanged = !(NullColumnValueGetter.isNullURI(oldClusterURI) ? NullColumnValueGetter.isNullURI(targetCluster)
-                        : targetCluster != null && oldClusterURI.toString().equals(target.getCluster().toString()));
+                boolean isClusterChanged = NullColumnValueGetter.isNullURI(oldClusterURI) ? !NullColumnValueGetter.isNullURI(targetCluster)
+                        : targetCluster != null && !oldClusterURI.toString().equals(target.getCluster().toString());
 
                 if (!oldInitiators.isEmpty() || !addedInitiators.isEmpty() || isClusterChanged) {
                     changes.add(new HostStateChange(target, oldClusterURI, targetCluster, oldInitiators, addedInitiators));

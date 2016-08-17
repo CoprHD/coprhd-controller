bc28f6567f805adf8f535e148c85e3a87917dbaf
Trevor Dawe <trevor.dawe@emc.com>
2016-08-17 16:45:35 -0300

    COP-22198: Added rollback to mount database and attach disk if failure during Unexport VMware volume service

---
+++ vipr-portal/com.iwave.isa.content/src/java/com/emc/sa/service/vmware/block/tasks/AttachScsiDisk.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2016 EMC Corporation
+ * All Rights Reserved
+ */
+package com.emc.sa.service.vmware.block.tasks;
+
+import java.util.List;
+
+import com.emc.sa.service.vmware.tasks.RetryableTask;
+import com.iwave.ext.vmware.HostStorageAPI;
+import com.iwave.ext.vmware.VMWareException;
+import com.vmware.vim25.HostScsiDisk;
+import com.vmware.vim25.QuiesceDatastoreIOForHAFailed;
+import com.vmware.vim25.ResourceInUse;
+import com.vmware.vim25.mo.HostSystem;
+
+public class AttachScsiDisk extends RetryableTask<Void> {
+    private final HostSystem host;
+    private final List<HostScsiDisk> disks;
+
+    public AttachScsiDisk(HostSystem host, List<HostScsiDisk> disks) {
+        this.host = host;
+        this.disks = disks;
+        // TODO add provide details
+    }
+
+    @Override
+    protected Void tryExecute() {
+        for (HostScsiDisk disk : disks) {
+            new HostStorageAPI(host).attachScsiLun(disk);
+        }
+        return null;
+    }
+
+    @Override
+    protected boolean canRetry(VMWareException e) {
+        return e.getCause() instanceof QuiesceDatastoreIOForHAFailed || e.getCause() instanceof ResourceInUse;
+    }
+}

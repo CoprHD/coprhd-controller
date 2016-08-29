9d314729eea5d5ffe2330ecc378077af86973374
Santhosh <Santhosh.Lakshmanan@emc.com>
2016-08-29 18:11:56 -0400

    revert accidental check-in. commit 2fe7c025a0b

--- controllersvc/src/main/java/com/emc/storageos/volumecontroller/impl/smis/SmisStorageDevice.java
+++ controllersvc/src/main/java/com/emc/storageos/volumecontroller/impl/smis/SmisStorageDevice.java
@@ -652,8 +652,9 @@ public class SmisStorageDevice extends DefaultBlockStorageDevice {
                     // COP-16705, COP-21770 - Ingested non-exported Volume may be associated with SG outside of ViPR.
                     _helper.removeVolumeFromStorageGroupsIfVolumeIsNotInAnyMV(storageSystem, volume);
                 }
-
-                CIMInstance volumeInstance = _helper.checkExists(storageSystem,
+                StorageSystem forProvider = _helper.getStorageSystemForProvider(storageSystem,
+                        volumes.get(0));
+                CIMInstance volumeInstance = _helper.checkExists(forProvider,
                         _cimPath.getBlockObjectPath(storageSystem, volume), false, false);
                 _helper.doApplyRecoverPointTag(storageSystem, volume, false);
                 if (volumeInstance == null) {

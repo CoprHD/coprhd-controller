a00e3161b4bd257bc836b0e22929017e03bcbb06
Anbalagan <Selvamanickam.Anbalagan@emc.com>
2016-08-31 06:52:57 -0400

    Reveredted code chnages in vmax exportGroupRemoveInitiator.

--- controllersvc/src/main/java/com/emc/storageos/volumecontroller/impl/block/VmaxMaskingOrchestrator.java
+++ controllersvc/src/main/java/com/emc/storageos/volumecontroller/impl/block/VmaxMaskingOrchestrator.java
@@ -569,23 +569,20 @@ public class VmaxMaskingOrchestrator extends AbstractBasicMaskingOrchestrator {
                                 _log.info(String.format(
                                         "Initiator %s is in an ExportMask that is shared by ExportGroups %s, so we will not remove it",
                                         initiator.getInitiatorPort(), Joiner.on(',').join(exportGroupURIs)));
-                                errorMessage.append(String.format(
-                                        " Volumes in other export groups - {%s} will be affected by this operation.", exportGroupURIs));
+                                // TODO we need to decide the proper operation here.
+                            } else {
+                                _log.info(String.format("Initiator %s is in an ExportMask that is shared by ExportGroups %s, " +
+                                        "but the initiator is not in any of them. Will remove it from the ExportMask.",
+                                        initiator.getInitiatorPort(), Joiner.on(',').join(exportGroupURIs)));
+                                List<URI> initiators = existingMasksToRemoveInitiator.get(mask.getId());
+                                if (initiators == null) {
+                                    initiators = new ArrayList<URI>();
+                                    existingMasksToRemoveInitiator.put(mask.getId(), initiators);
+                                }
+                                if (!initiators.contains(initiator.getId())) {
+                                    initiators.add(initiator.getId());
+                                }
                             }
-                            
-
-                            _log.info(String.format("Initiator %s is in an ExportMask that is shared by ExportGroups %s, " +
-                                    "but the initiator is not in any of them. Will remove it from the ExportMask.",
-                                    initiator.getInitiatorPort(), Joiner.on(',').join(exportGroupURIs)));
-                            List<URI> initiators = existingMasksToRemoveInitiator.get(mask.getId());
-                            if (initiators == null) {
-                                initiators = new ArrayList<URI>();
-                                existingMasksToRemoveInitiator.put(mask.getId(), initiators);
-                            }
-                            if (!initiators.contains(initiator.getId())) {
-                                initiators.add(initiator.getId());
-                            }
-                        
                         } else {
                             _log.info(
                                     String.format("We can remove initiator %s from mask %s", initiator.getInitiatorPort(),

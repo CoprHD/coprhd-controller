924be6823886611bcd6d54fa28cb076d2da3483e
Nathan Beach <nathan.beach@emc.com>
2016-09-01 18:26:26 -0500

    removing unused import

--- apisvc/src/main/java/com/emc/storageos/api/service/impl/resource/blockingestorchestration/MultipleMaskPerHostIngestOrchestrator.java
+++ apisvc/src/main/java/com/emc/storageos/api/service/impl/resource/blockingestorchestration/MultipleMaskPerHostIngestOrchestrator.java
@@ -5,7 +5,6 @@
 package com.emc.storageos.api.service.impl.resource.blockingestorchestration;
 
 import java.net.URI;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.lang.mutable.MutableInt;
@@ -16,17 +15,11 @@ import com.emc.storageos.api.service.impl.resource.blockingestorchestration.cont
 import com.emc.storageos.db.client.DbClient;
 import com.emc.storageos.db.client.constraint.AlternateIdConstraint;
 import com.emc.storageos.db.client.model.BlockObject;
-import com.emc.storageos.db.client.model.ExportGroup;
 import com.emc.storageos.db.client.model.ExportMask;
-import com.emc.storageos.db.client.model.Initiator;
 import com.emc.storageos.db.client.model.StorageSystem;
 import com.emc.storageos.db.client.model.UnManagedDiscoveredObjects.UnManagedExportMask;
 import com.emc.storageos.db.client.model.UnManagedDiscoveredObjects.UnManagedVolume;
-import com.emc.storageos.db.client.util.CommonTransformerFunctions;
-import com.emc.storageos.db.server.impl.StorageSystemTypesInitUtils;
-import com.emc.storageos.model.block.VolumeExportIngestParam;
 import com.emc.storageos.util.ConnectivityUtil;
-import com.google.common.collect.Collections2;
 
 /*
  * MULTIPLE_MASK_PER_HOST :

4bea5693bf74012808bf3cfe9b48ac8bc465672c
Anbalagan <Selvamanickam.Anbalagan@emc.com>
2016-09-01 22:19:00 -0400

    COP-25142 Re-allow multiple hosts in Single ExportGroup.

--- apisvc/src/main/java/com/emc/storageos/api/service/impl/resource/ExportGroupService.java
+++ apisvc/src/main/java/com/emc/storageos/api/service/impl/resource/ExportGroupService.java
@@ -1358,16 +1358,24 @@ public class ExportGroupService extends TaskResourceService {
                 initiator = _dbClient.queryObject(Initiator.class, initiatorURI);
                 String name = null;
                 if (initiator != null && !initiator.getInactive()) {
-                    if (!initiator.getInactive() && !VPlexControllerUtils.isVplexInitiator(initiator, _dbClient)
+                    // Temporarily suppress validating the host initiators from ExportGroup to allow multiple host sharing the same EG.
+                    if (isCluster && !initiator.getInactive() && !VPlexControllerUtils.isVplexInitiator(initiator, _dbClient)
                             && !ExportUtils.checkIfInitiatorsForRP(Arrays.asList(initiator))) {
-                        if (isCluster && StringUtils.hasText(initiator.getClusterName())) {
+                        /*if (isCluster && StringUtils.hasText(initiator.getClusterName())) {
                             name = initiator.getClusterName();
                         } else if (StringUtils.hasText(initiator.getHostName())) {
                             name = initiator.getHostName();
                         } else {
                             _log.error("Initiator {} does not have cluster/host name", initiator.getId());
                             throw APIException.badRequests.invalidInitiatorName(initiator.getId(), exportGroup.getId());
+                        }*/
+                        if (StringUtils.hasText(initiator.getClusterName())) {
+                            name = initiator.getClusterName();
+                        } else {
+                            _log.error("Initiator {} does not have cluster/host name", initiator.getId());
+                            throw APIException.badRequests.invalidInitiatorName(initiator.getId(), exportGroup.getId());
                         }
+                        
                         Set<URI> set = null;
                         if (initiatorMap.get(name) == null) {
                             set = new HashSet<URI>();

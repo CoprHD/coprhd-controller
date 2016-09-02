b1837956681198151547e9acdfa6b71fb2fba8cf
Ganesh, Om <om.ganesh@emc.com>
2016-09-02 16:30:23 +0530

    Correced the QD ingestion code with proper Metrics

--- controllersvc/src/main/java/com/emc/storageos/volumecontroller/impl/vnx/VNXFileStorageDeviceXML.java
+++ controllersvc/src/main/java/com/emc/storageos/volumecontroller/impl/vnx/VNXFileStorageDeviceXML.java
@@ -32,7 +32,6 @@ import com.emc.storageos.db.client.model.Snapshot;
 import com.emc.storageos.db.client.model.StorageHADomain;
 import com.emc.storageos.db.client.model.StoragePort;
 import com.emc.storageos.db.client.model.StorageSystem;
-import com.emc.storageos.db.client.util.SizeUtil;
 import com.emc.storageos.exceptions.DeviceControllerErrors;
 import com.emc.storageos.exceptions.DeviceControllerException;
 import com.emc.storageos.model.file.ExportRule;
@@ -128,8 +127,7 @@ public class VNXFileStorageDeviceXML extends AbstractFileStorageDevice {
     public BiosCommandResult doCreateFS(StorageSystem storage, FileDeviceInputOutput args) throws ControllerException {
 
         Map<String, String> autoExtendAtts = getAutoExtendAttrs(args);
-        
-        Long fsSize = SizeUtil.translateSize(args.getFsCapacity(),"KB").longValue(); 
+        Long fsSize = args.getFsCapacity() / BYTESPERMB;
         if (fsSize < 1) {
             // Invalid size throw an error
             String errMsg = "doCreateFS failed : FileSystem size in bytes is not valid " + args.getFsCapacity();
@@ -1207,8 +1205,7 @@ public class VNXFileStorageDeviceXML extends AbstractFileStorageDevice {
                 throw VNXException.exceptions.communicationFailed(VNXCOMM_ERR_MSG);
             }
             // quota directory create/update takes size in MB as similar to FS create.
-            Long sizeMBs = SizeUtil.translateSize(size, "KB").longValue();
-            
+            Long sizeMBs = size / BYTESPERMB;
             apiResult = vnxComm.createQuotaDirectory(storage, args.getFsName(), quotaTreetreeName, securityStyle, sizeMBs, oplocks,
                     isMountRequired);
             _log.info("createQuotaDirectory call result : {}", apiResult.isCommandSuccess());

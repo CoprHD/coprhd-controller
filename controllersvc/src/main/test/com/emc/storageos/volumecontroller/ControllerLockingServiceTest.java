7b8a7a1686572598c44dc15fc1758818a9a9e7df
Hui Liu <hui.liu@emc.com>
2016-09-06 14:24:35 -0400

    enhance unit test to cover more cases

--- controllersvc/src/main/test/com/emc/storageos/volumecontroller/ControllerLockingServiceTest.java
+++ controllersvc/src/main/test/com/emc/storageos/volumecontroller/ControllerLockingServiceTest.java
@@ -4,6 +4,7 @@
  */
 package com.emc.storageos.volumecontroller;
 
+import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -15,7 +16,6 @@ import org.slf4j.LoggerFactory;
 import org.testng.Assert;
 
 import com.emc.storageos.coordinator.client.service.CoordinatorTestBase;
-import com.emc.storageos.coordinator.client.service.DistributedPersistentLock;
 import com.emc.storageos.volumecontroller.impl.ControllerLockingServiceImpl;
 
 public class ControllerLockingServiceTest extends CoordinatorTestBase{
@@ -38,29 +38,72 @@ public class ControllerLockingServiceTest extends CoordinatorTestBase{
     
     @Test
     public  void testAcquireReleaseLock() {
-        Assert.assertTrue(impl.acquireLock(LOCKNAME, 10));
-        impl.releaseLock(LOCKNAME);
+        String lockName = getLockName();
+        Assert.assertTrue(impl.acquireLock(lockName, 10));
+        impl.releaseLock(lockName);
         
         // Lock can be acquired again
-        Assert.assertTrue(impl.acquireLock(LOCKNAME, 10));
-        Assert.assertTrue(impl.releaseLock(LOCKNAME));
+        Assert.assertTrue(impl.acquireLock(lockName, 10));
+        Assert.assertTrue(impl.releaseLock(lockName));
         
         // Lock are re-entrant
-        Assert.assertTrue(impl.acquireLock(LOCKNAME, 10));
-        Assert.assertTrue(impl.acquireLock(LOCKNAME, 10));
-        Assert.assertTrue(impl.releaseLock(LOCKNAME));
-        Assert.assertTrue(impl.releaseLock(LOCKNAME));
+        Assert.assertTrue(impl.acquireLock(lockName, 10));
+        Assert.assertTrue(impl.acquireLock(lockName, 10));
+        Assert.assertTrue(impl.releaseLock(lockName));
+        Assert.assertTrue(impl.releaseLock(lockName));
     }
 
     @Test
+    public void testAcquireReleaseLockTimeout() throws InterruptedException {
+        final String lockName = getLockName();
+        ExecutorService clients = Executors.newFixedThreadPool(2);
+        // Thread 1 - hold the lock
+        clients.submit(new Runnable() {
+            @Override
+            public void run() {
+                    try {
+                        boolean bLockActionResult =  impl.acquireLock(lockName, 10);
+                        Assert.assertTrue(bLockActionResult);
+                        log.error("Thread 1 - acquire lock {}", bLockActionResult);
+                        Thread.sleep(30000);
+                    } catch (InterruptedException e) {
+                        // Ignore this.
+                    } catch (Exception e) {
+                        log.info(": {} transient error ...", e);
+                    }
+                }
+        });
+
+        // Thread 2 - acquire the lock - should fail
+        clients.submit(new Runnable() {
+            @Override
+            public void run() {
+                    try {
+                        Thread.sleep(10000); // yield so that thread 1 acquired the lock
+                        boolean bLockActionResult =  impl.acquireLock(lockName, 10);
+                        log.error("Thread 2 - acquire lock {}", bLockActionResult);
+                        Assert.assertFalse(bLockActionResult);
+                        Thread.sleep(30000);
+                    } catch (InterruptedException e) {
+                        // Ignore this.
+                    } catch (Exception e) {
+                        log.info(": {} transient error ...", e);
+                    }
+                }
+        });
+
+        clients.awaitTermination(30, TimeUnit.SECONDS);
+        log.info("*** testAcquireReleaseLockTimeout end");
+    }
+    
+    @Test
     public void testAcquireReleaseLockMultiThread() throws InterruptedException {
-        
+        final String lockName = getLockName();
         ExecutorService clients = Executors.newFixedThreadPool(NUMCLIENTS);
         for (int i = 0; i < NUMCLIENTS; i++) {
             clients.submit(new Runnable() {
                 @Override
                 public void run() {
-                    String lockName = "TestLock";
                     while (true) {
                         try {
                             log.info(": {} ------ client: starts loop ------");
@@ -74,6 +117,7 @@ public class ControllerLockingServiceTest extends CoordinatorTestBase{
                                 Thread.sleep(10);
                                 log.info(": work done. releasing lock");
                                 bLockActionResult = impl.releaseLock(lockName);
+                                Assert.assertTrue(bLockActionResult);
                                 log.info(": lock release status: {};released lock", bLockActionResult);
                                 Thread.sleep(10);
                             } else {
@@ -95,13 +139,18 @@ public class ControllerLockingServiceTest extends CoordinatorTestBase{
     
     @Test
     public void testAcquireReleasePersistentLock() {
-        Assert.assertTrue(impl.acquirePersistentLock(LOCKNAME, CLIENT_A, 10));
-        Assert.assertFalse(impl.releasePersistentLock(LOCKNAME, CLIENT_B));
-        Assert.assertTrue(impl.releasePersistentLock(LOCKNAME, CLIENT_A));
+        final String lockName = getLockName();
+        Assert.assertTrue(impl.acquirePersistentLock(lockName, CLIENT_A, 10));
+        Assert.assertFalse(impl.releasePersistentLock(lockName, CLIENT_B));
+        Assert.assertTrue(impl.releasePersistentLock(lockName, CLIENT_A));
         
         // Lock again
-        Assert.assertTrue(impl.acquirePersistentLock(LOCKNAME, CLIENT_A, 10));
-        Assert.assertFalse(impl.acquirePersistentLock(LOCKNAME, CLIENT_B, 10));
-        Assert.assertTrue(impl.releasePersistentLock(LOCKNAME, CLIENT_A));
+        Assert.assertTrue(impl.acquirePersistentLock(lockName, CLIENT_A, 10));
+        Assert.assertFalse(impl.acquirePersistentLock(lockName, CLIENT_B, 10));
+        Assert.assertTrue(impl.releasePersistentLock(lockName, CLIENT_A));
+    }
+    
+    private String getLockName() {
+        return LOCKNAME + new Random(System.currentTimeMillis()).nextInt();
     }
 }

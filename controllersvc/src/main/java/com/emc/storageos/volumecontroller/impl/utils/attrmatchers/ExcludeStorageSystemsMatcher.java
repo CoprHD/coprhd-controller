9b4c5b8f32bdee9b849f995068056217740e2387
mhegeric <mike.hegerich@emc.com>
2016-09-08 12:55:32 -0400

    Improve error

--- controllersvc/src/main/java/com/emc/storageos/volumecontroller/impl/utils/attrmatchers/ExcludeStorageSystemsMatcher.java
+++ controllersvc/src/main/java/com/emc/storageos/volumecontroller/impl/utils/attrmatchers/ExcludeStorageSystemsMatcher.java
@@ -4,6 +4,7 @@
  */
 package com.emc.storageos.volumecontroller.impl.utils.attrmatchers;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -15,6 +16,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.util.CollectionUtils;
 
 import com.emc.storageos.db.client.model.StoragePool;
+import com.emc.storageos.db.client.model.StorageSystem;
 import com.emc.storageos.volumecontroller.AttributeMatcher;
 
 /**
@@ -34,18 +36,19 @@ public class ExcludeStorageSystemsMatcher extends AttributeMatcher {
             Map<String, Object> attributeMap, StringBuffer errorMessage) {
         // We only allow pools that are not on the excluded systems.
         List<StoragePool> matchedPools = new ArrayList<StoragePool>();
-        Set<String> excludeSystems = (Set<String>) attributeMap.get(Attributes.exclude_storage_system.toString());
+        Set<String> excludedSystemIds = (Set<String>) attributeMap.get(Attributes.exclude_storage_system.toString());
         Iterator<StoragePool> poolIterator = pools.iterator();
         while (poolIterator.hasNext()) {
             StoragePool pool = poolIterator.next();
-            if (!excludeSystems.contains(pool.getStorageDevice().toString())) {
+            if (!excludedSystemIds.contains(pool.getStorageDevice().toString())) {
                 matchedPools.add(pool);
             }
         }
         _logger.info("{} Storage pools matching after filtering excluded storage systems.", matchedPools.size());
         
         if (CollectionUtils.isEmpty(matchedPools)) {
-            errorMessage.append(String.format("The only matching storage pools are on systems %s, which are not allowed for the request.", excludeSystems));
+            errorMessage.append(String.format("The only matching storage pools are on systems %s, which are not allowed for the request.", 
+                    getExcludedSystemInfo(excludedSystemIds)));
             _logger.error(errorMessage.toString());
         }
 
@@ -66,5 +69,29 @@ public class ExcludeStorageSystemsMatcher extends AttributeMatcher {
     @Override
     protected List<StoragePool> matchStoragePoolsWithAttributeOff(List<StoragePool> pools, Map<String, Object> attributeMap) {
         return pools;
+    }
+    
+    /**
+     * Generates the excluded system info for display in the error message when
+     * the matcher eliminates all passed storage pools.
+     * 
+     * @param excludedSystemIds The Ids of the excluded systems.
+     * 
+     * @return A string identifying the excluded systems.
+     */
+    private String getExcludedSystemInfo(Set<String> excludedSystemIds) {
+        StringBuilder builder = new StringBuilder();
+        for (String excludedSystemId : excludedSystemIds) {
+            if (builder.length() != 0) {
+                builder.append(", ");
+            }
+            StorageSystem excludedSystem = _objectCache.queryObject(StorageSystem.class, URI.create(excludedSystemId));
+            if (excludedSystem != null) {
+                builder.append(excludedSystem.forDisplay());
+            } else {
+                builder.append(excludedSystemId);
+            }
+        }
+        return builder.toString();
     }
 }

6650f51e3c4b44c3ab39939e657645453b097701
Nathan Beach <nathan.beach@emc.com>
2016-09-19 07:30:50 -0500

    improving performance of vplex thin migration

--- dbclient/src/main/java/com/emc/storageos/db/client/upgrade/callbacks/VplexVolumeThinlyProvisionedMigration.java
+++ dbclient/src/main/java/com/emc/storageos/db/client/upgrade/callbacks/VplexVolumeThinlyProvisionedMigration.java
@@ -5,6 +5,7 @@
 package com.emc.storageos.db.client.upgrade.callbacks;
 
 import java.net.URI;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
@@ -12,12 +13,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.emc.storageos.db.client.DbClient;
-import com.emc.storageos.db.client.model.DiscoveredDataObject;
 import com.emc.storageos.db.client.model.StorageSystem;
 import com.emc.storageos.db.client.model.Volume;
 import com.emc.storageos.db.client.upgrade.BaseCustomMigrationCallback;
 import com.emc.storageos.db.client.util.NullColumnValueGetter;
 import com.emc.storageos.svcs.errorhandling.resources.MigrationCallbackException;
+import com.emc.storageos.vplexcontroller.VPlexControllerUtils;
 
 /**
  * If we are upgrading from any version before 3.5, the thinlyProvisioned 
@@ -41,13 +42,22 @@ public class VplexVolumeThinlyProvisionedMigration extends BaseCustomMigrationCa
         List<URI> volumeURIs = dbClient.queryByType(Volume.class, true);
         Iterator<Volume> volumesIter = dbClient.queryIterativeObjects(Volume.class, volumeURIs);
         int volumeUpdatedCount = 0;
+
+        // cache a list of vplex URIs for performance reasons
+        List<URI> vplexUris = new ArrayList<URI>();
+        List<StorageSystem> vplexes = VPlexControllerUtils.getAllVplexStorageSystems(dbClient);
+        for (StorageSystem vplex : vplexes) {
+            if (null != vplex) {
+                vplexUris.add(vplex.getId());
+            }
+        }
+        logger.info("found {} vplex storage systems in the database", vplexUris.size());
+
         while (volumesIter.hasNext()) {
             Volume volume = volumesIter.next();
             URI systemURI = volume.getStorageController();
             if (!NullColumnValueGetter.isNullURI(systemURI)) {
-                StorageSystem system = dbClient.queryObject(StorageSystem.class, systemURI);
-                if ((system != null) &&
-                        (DiscoveredDataObject.Type.vplex.name().equals(system.getSystemType()))) {
+                if (vplexUris.contains(systemURI)) {
                     // This is a VPLEX volume. If we are upgrading from any version
                     // before 3.5, if the thinlyProvisioned property is true, it should 
                     // be set to false on any ViPR-managed volumes.

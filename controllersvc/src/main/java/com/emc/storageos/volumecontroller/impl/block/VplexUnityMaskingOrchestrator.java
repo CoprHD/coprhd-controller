9c08185f6c0527bc3a6ab680ef311e845c4642b6
mhegeric <mike.hegerich@emc.com>
2016-09-20 14:32:49 -0400

    remove unnecessary import

--- controllersvc/src/main/java/com/emc/storageos/volumecontroller/impl/block/VplexUnityMaskingOrchestrator.java
+++ controllersvc/src/main/java/com/emc/storageos/volumecontroller/impl/block/VplexUnityMaskingOrchestrator.java
@@ -8,7 +8,6 @@ import java.net.URI;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;

2a0124d46a247895418d2fb0c4a23c74b6c015e7
Amit Saurabh <amit.saurabh@emc.com>
2016-09-21 15:26:16 +0530

    added the call to add initiator to host with pair initiator as optional argument.

--- internalLibraries/models/src/main/java/com/emc/storageos/model/host/InitiatorUpdateParam.java
+++ internalLibraries/models/src/main/java/com/emc/storageos/model/host/InitiatorUpdateParam.java
@@ -4,6 +4,7 @@
  */
 package com.emc.storageos.model.host;
 
+import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
 /**
@@ -11,4 +12,15 @@ import javax.xml.bind.annotation.XmlRootElement;
  */
 @XmlRootElement(name = "initiator_update")
 public class InitiatorUpdateParam extends BaseInitiatorParam {
+
+    private Boolean disassociatePairedInitiator;
+
+    @XmlElement(name = "disassociate_paired_initiator")
+    public Boolean getDiscoverable() {
+        return disassociatePairedInitiator;
+    }
+
+    public void setDiscoverable(Boolean disassociatePairedInitiator) {
+        this.disassociatePairedInitiator = disassociatePairedInitiator;
+    }
 }

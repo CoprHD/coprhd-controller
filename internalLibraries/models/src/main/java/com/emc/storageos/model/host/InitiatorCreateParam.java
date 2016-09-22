b5457b329a9d21930028bfda66bb0d8274445873
Amit Saurabh <amit.saurabh@emc.com>
2016-09-22 18:47:37 +0530

    modified to keep old create initiator api as it is.

--- internalLibraries/models/src/main/java/com/emc/storageos/model/host/InitiatorCreateParam.java
+++ internalLibraries/models/src/main/java/com/emc/storageos/model/host/InitiatorCreateParam.java
@@ -4,9 +4,6 @@
  */
 package com.emc.storageos.model.host;
 
-import java.net.URI;
-
-import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
 /**
@@ -14,19 +11,4 @@ import javax.xml.bind.annotation.XmlRootElement;
  */
 @XmlRootElement(name = "initiator_create")
 public class InitiatorCreateParam extends BaseInitiatorParam {
-
-    private URI associatedInitiator;
-
-    /**
-     * The URI of the associated initiator
-     */
-    @XmlElement(name = "associated_initiator", required = false)
-    public URI getAssociatedInitiator() {
-        return associatedInitiator;
-    }
-
-    public void setAssociatedInitiator(URI associatedInitiator) {
-        this.associatedInitiator = associatedInitiator;
-    }
-
 }

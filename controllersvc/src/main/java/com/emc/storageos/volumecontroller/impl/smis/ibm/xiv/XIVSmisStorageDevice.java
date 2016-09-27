b7f5183e0fe8f8795142d50269d96ad04d245c78
chandan singh <chandan.singh@emc.com>
2016-09-27 15:31:08 +0530

    HSMHostNotFound

--- controllersvc/src/main/java/com/emc/storageos/volumecontroller/impl/smis/ibm/xiv/XIVSmisStorageDevice.java
+++ controllersvc/src/main/java/com/emc/storageos/volumecontroller/impl/smis/ibm/xiv/XIVSmisStorageDevice.java
@@ -1036,7 +1036,6 @@ public class XIVSmisStorageDevice extends DefaultBlockStorageDevice {
 
     private synchronized void addVolumesToCG(StorageSystem storageSystem, URI consistencyGroupId, List<URI> volumeURIs) throws Exception {
         BlockConsistencyGroup consistencyGroup = _dbClient.queryObject(BlockConsistencyGroup.class, consistencyGroupId);
-        //Checking
         if (null != consistencyGroup) {
             String groupName = _helper.getConsistencyGroupName(consistencyGroup, storageSystem);
             if (groupName.equals(EMPTY_CG_NAME)) {

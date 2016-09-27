c80ef3fcf7dd30d2bc88b072d7f13254d8853d1a
bao, liping <liping.bao@emc.com>
2016-09-27 12:08:46 -0400

    remove printStackTrace

--- vipr-portal/com.iwave.isa.content/src/java/com/emc/sa/service/vipr/application/CreateCloneOfApplicationService.java
+++ vipr-portal/com.iwave.isa.content/src/java/com/emc/sa/service/vipr/application/CreateCloneOfApplicationService.java
@@ -71,7 +71,7 @@ public class CreateCloneOfApplicationService extends ViPRService {
                 getModelClient().delete(replica);
             }
         } catch (Exception e) {
-            e.printStackTrace();
+            ;
         }
     }
 }

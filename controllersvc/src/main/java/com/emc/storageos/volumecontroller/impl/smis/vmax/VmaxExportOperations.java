e254a144dda7e8fcb29b594940f093f35c3165d3
Ian Bibby <ian.bibby@emc.com>
2016-09-28 14:56:24 +0000

    COP-25687 Updates to findExportMasks for VMAX

--- controllersvc/src/main/java/com/emc/storageos/volumecontroller/impl/smis/vmax/VmaxExportOperations.java
+++ controllersvc/src/main/java/com/emc/storageos/volumecontroller/impl/smis/vmax/VmaxExportOperations.java
@@ -1812,24 +1812,6 @@ public class VmaxExportOperations implements ExportMaskOperations {
 
                         // Update the tracking containers
                         Map<String, Integer> volumeWWNs = _helper.getVolumesFromLunMaskingInstance(client, instance);
-
-                        for (Entry<String, Integer> entry : volumeWWNs.entrySet()) {
-                            String normalizedWWN = BlockObject.normalizeWWN(entry.getKey());
-                            URIQueryResultList volumeList = new URIQueryResultList();
-                            _dbClient.queryByConstraint(
-                                    AlternateIdConstraint.Factory.getVolumeWwnConstraint(normalizedWWN), volumeList);
-                            Iterator<URI> volumes = volumeList.iterator();
-                            while (volumes.hasNext()) {
-                                URI volume = volumes.next();
-                                Volume obj = _dbClient.queryObject(Volume.class, volume);
-                                if (exportMask.getUserAddedVolumes() == null
-                                        || !exportMask.getUserAddedVolumes().containsKey(normalizedWWN)) {
-                                    exportMask.addToUserCreatedVolumes(obj);
-                                    exportMask.addVolume(volume, entry.getValue());
-                                    _log.info("Adding managed wwn:{} to mask:{}", obj.getWWN(), exportMask.getId());
-                                }
-                            }
-                        }
                         if (!CollectionUtils.isEmpty(exportMask.getExistingVolumes())) {
                             exportMask.getExistingVolumes().clear();
                         }

8ef3d0cd6997ac1c5b47c53d2b8eee4e5f13ff88
Steve Mendes <steve.mendes@gmail.com>
2016-10-07 09:24:46 -0400

    Added updated copyright to new files

--- exportLibraries/oe/src/main/java/com/emc/storageos/oe/api/restapi/OrchestrationEngineRestClient.java
+++ exportLibraries/oe/src/main/java/com/emc/storageos/oe/api/restapi/OrchestrationEngineRestClient.java
@@ -1,7 +1,20 @@
-/**
- * Copyright (c) 2015 EMC Corporation
- * All Rights Reserved
+/*
+ * Copyright 2016 Dell Inc. or its subsidiaries.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
  */
+
 package com.emc.storageos.oe.api.restapi;
 
 import java.net.URI;
@@ -24,6 +37,10 @@ import com.sun.jersey.api.client.filter.LoggingFilter;
  * This class implements methods for calling OE REST API to do operations.
  */
 
+/**
+ * @author root
+ *
+ */
 public class OrchestrationEngineRestClient extends StandardRestClient {
 
     private static Logger log = LoggerFactory.getLogger(OrchestrationEngineRestClient.class);

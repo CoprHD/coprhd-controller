dcf755027d83ac418125fdb872230f76f5d39f06
Steve Mendes <steve.mendes@gmail.com>
2016-10-07 13:13:12 -0400

    handled thread interruption by resetting interrupt flag (so vipr can handle it as it wants)

--- vipr-portal/com.iwave.isa.content/src/java/com/emc/sa/service/vipr/oe/OrchestrationUtils.java
+++ vipr-portal/com.iwave.isa.content/src/java/com/emc/sa/service/vipr/oe/OrchestrationUtils.java
@@ -77,7 +77,7 @@ public class OrchestrationUtils {
             Thread.sleep(i*1000);
         }
         catch (InterruptedException e) {
-            e.printStackTrace();
+            Thread.currentThread().interrupt();
         } 
     }
 

24a9c37715c5824fcaf63deda1a641d9abd4adf3
Trevor Dawe <trevor.dawe@emc.com>
2016-11-08 14:46:18 +0000

    COP-26171: Added test for vCenter discovery and event approval

--- tools/tests/export-tests/wftests.sh
+++ tools/tests/export-tests/wftests.sh
@@ -1714,6 +1714,76 @@ test_1() {
     done
 }
 
+add_host_to_cluster() {
+    host=$1
+    cluster=$2
+    args="addHostTo $cluster $host"
+    echo "Adding host $host to $cluster"
+    curl -ikL --header "Content-Type: application/json" --header "username:xx" --header "password: yy" --data '{"args": "'"$args"'"}' -X POST http://${HW_SIMULATOR_IP}:8235/vmware/modify &> /dev/null
+}
+
+remove_host_from_cluster() {
+    host=$1
+    cluster=$2
+    args="removeHostFrom $cluster $host"
+    echo "Removing host $host from $cluster"
+    curl -ikL --header "Content-Type: application/json" --header "username:xx" --header "password: yy" --data '{"args": "'"$args"'"}' -X POST http://${HW_SIMULATOR_IP}:8235/vmware/modify &> /dev/null
+}
+
+discover_vcenter() {
+    vcenter=$1
+    echo "Discovering vCenter $vcenter"
+    vcenter discover $vcenter
+    sleep 60
+}
+
+approve_pending_event() {
+    EVENT_ID=$(events list emcworld | grep pending | awk '{print $1}')
+    echo "Approving event $EVENT_ID"
+    events approve $EVENT_ID
+    sleep 30
+}
+
+vcenter_event_test() {
+    echot "vCenter Event Test: Export to cluster, move host into cluster, rediscover vCenter, approve event"
+    expname=${EXPORT_GROUP_NAME}t2
+    item=${RANDOM}
+    cfs="ExportGroup ExportMask"
+    mkdir -p results/${item}
+
+    verify_export ${expname}1 ${HOST1} gone
+
+    # Perform any DB validation in here
+    snap_db 1 ${cfs}
+
+    # Run the export group command
+    runcmd export_group create $PROJECT ${expname}1 $NH --type Cluster --volspec ${PROJECT}/${VOLNAME}-1 --clusters "emcworld/cluster-1"
+
+    # move hosts21 into cluster-1
+    remove_host_from_cluster "host21" "cluster-2"
+    add_host_to_cluster "host21" "cluster-1"
+    discover_vcenter "vcenter1"
+
+    approve_pending_event
+
+    # Remove the shared export
+    runcmd export_group delete ${PROJECT}/${expname}1
+
+    # Snap the DB again
+    snap_db 2 ${cfs}
+
+    # Validate nothing was left behind
+    validate_db 1 2 ${cfs}
+
+    verify_export ${expname}1 ${HOST1} gone
+
+    # Set vCenter back to previous state
+    remove_host_from_cluster "host21" "cluster-1"
+    add_host_to_cluster "host21" "cluster-2"
+    discover_vcenter "vcenter1"
+}
+
+
 # Basic export test for vcenter cluster
 #
 #

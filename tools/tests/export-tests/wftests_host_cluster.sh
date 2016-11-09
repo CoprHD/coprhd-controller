5ee37efc23e02f4865d8b1a31328697cc93a3460
Bradley Hughes <bradley.hughes@emc.com>
2016-11-09 15:30:02 -0400

    COP-26260: Happy path tuned and tweaked for test_host_remove_initiator()

--- tools/tests/export-tests/wftests_host_cluster.sh
+++ tools/tests/export-tests/wftests_host_cluster.sh
@@ -121,3 +121,146 @@ approve_pending_event() {
     events approve $EVENT_ID
     sleep 30
 }
+
+
+####
+# Remove initiator
+# 1. Add manual host using UI
+# 2. Add 4 initiators to the host using the UI
+# 3. Add these 4 initiators to the network assigned to your virtual array
+# 4. Create and export a volume to this host
+# 5. Verify that 2 initiators are in the export group for this host
+# 6. Remove 1 initiator from the host using the UI
+# 7. Monitor the export group update task and verify the export group contains only 1 initiator when complete
+test_host_remove_initiator() {
+    echot "Test host_remove_initiator Begins"
+
+    common_failure_injections="failure_004_final_step_in_workflow_complete \
+                               failure_005_BlockDeviceController.createVolumes_before_device_create \
+                               failure_006_BlockDeviceController.createVolumes_after_device_create \
+                               failure_004_final_step_in_workflow_complete:failure_013_BlockDeviceController.rollbackCreateVolumes_before_device_delete \
+                               failure_004_final_step_in_workflow_complete:failure_014_BlockDeviceController.rollbackCreateVolumes_after_device_delete"
+
+    if [ "${SS}" = "vplex" ]; then
+        storage_failure_injections="failure_007_NetworkDeviceController.zoneExportRemoveVolumes_before_unzone \
+                                    failure_008_NetworkDeviceController.zoneExportRemoveVolumes_after_unzone \
+                                    failure_009_VPlexVmaxMaskingOrchestrator.createOrAddVolumesToExportMask_before_operation \
+                                    failure_010_VPlexVmaxMaskingOrchestrator.createOrAddVolumesToExportMask_after_operation"
+    fi
+
+    if [ "${SS}" = "vnx" -o "${SS}" = "vmax2" -o "${SS}" = "vmax3" ]
+    then
+    storage_failure_injections="failure_015_SmisCommandHelper.invokeMethod_createVolume \
+                                    failure_011_VNXVMAX_Post_Placement_outside_trycatch \
+                                    failure_012_VNXVMAX_Post_Placement_inside_trycatch"
+    fi
+
+    failure_injections="${common_failure_injections} ${storage_failure_injections}"
+
+    # Placeholder when a specific failure case is being worked...
+    # failure_injections="failure_015"
+
+    #######
+
+    column_family="Volume ExportGroup ExportMask"
+    random_number=${RANDOM}
+    mkdir -p results/${random_number}
+    volume=${VOLNAME}-${random_number}
+    host=fakehost${random_number}
+    exportgroup=exportgroup${random_number}
+    
+    # Snap DB
+    snap_db 1 ${column_family}
+        
+    # Create new random WWNs for nodes and initiators
+    node1=`randwwn 20 C1`
+    node2=`randwwn 20 C2`
+    node3=`randwwn 20 C3`
+    node4=`randwwn 20 C4`
+    init1=`randwwn 10 C1`
+    init2=`randwwn 10 C2`
+    init3=`randwwn 10 C3`
+    init4=`randwwn 10 C4`
+    
+    # Add initator WWNs to the network
+    run transportzone add $NH/${FC_ZONE_A} ${init1}
+    run transportzone add $NH/${FC_ZONE_A} ${init2}
+    run transportzone add $NH/${FC_ZONE_A} ${init3}
+    run transportzone add $NH/${FC_ZONE_A} ${init4}
+        
+    # Create fake host
+    runcmd hosts create $host $TENANT Other ${host}.lss.emc.com --port 1
+    # runcmd hosts create $host $TENANT Other ${host}.lss.emc.com --port 8111 --username user --password 'password' --osversion 1.0 --cluster ${tenant}/${cluster}
+    
+    # Create new initators and add to fakehost
+    runcmd initiator create $host FC ${init1} --node ${node1}
+    runcmd initiator create $host FC ${init2} --node ${node2}
+    runcmd initiator create $host FC ${init3} --node ${node3}
+    runcmd initiator create $host FC ${init4} --node ${node4}
+
+    # Create volume
+    runcmd volume create ${volume} ${PROJECT} ${NH} ${VPOOL_BASE} 1GB
+    
+    # Zzzzzz
+    sleep 2
+    
+    # Export the volume to the fake host    
+    runcmd export_group create $PROJECT ${exportgroup} $NH --type Host --volspec ${PROJECT}/${volume} --hosts "${host}"
+    
+    # Double check the export group to ensure the initiators are present
+    foundinit1=`export_group show $PROJECT/${exportgroup} | grep ${init1}`
+    foundinit2=`export_group show $PROJECT/${exportgroup} | grep ${init2}`
+    foundinit3=`export_group show $PROJECT/${exportgroup} | grep ${init3}`
+    foundinit4=`export_group show $PROJECT/${exportgroup} | grep ${init4}`    
+    
+    if [[ "${foundinit1}" = ""  || "${foundinit2}" = "" || "${foundinit3}" = "" || "${foundinit4}" = "" ]]; then
+        # Fail, those initiators should have been added to the export group
+        echo "+++ FAIL - Some initiators were not found on the export group...fail."
+        exit 1
+    else
+        echo "+++ SUCCESS - All initiators from host present on export group"   
+    fi
+            
+    # Zzzzzz
+    sleep 2
+    
+    # Delete two initiators from the host...these should now be auto-removed from the export group 
+    runcmd initiator delete ${host}/${init1}
+    runcmd initiator delete ${host}/${init2}
+    
+    # Zzzzzz
+    sleep 5
+    
+    # Ensure that initiator 1 and 2 have been removed
+    foundinit1=`export_group show $PROJECT/${exportgroup} | grep ${init1}`
+    foundinit2=`export_group show $PROJECT/${exportgroup} | grep ${init2}`
+    foundinit3=`export_group show $PROJECT/${exportgroup} | grep ${init3}`
+    foundinit4=`export_group show $PROJECT/${exportgroup} | grep ${init4}`
+
+    if [[ "${foundinit1}" != "" || "${foundinit2}" != "" ]]; then
+        # Fail, those initiators 1 and 2 should be removed and initiators 3 and 4 should still be present
+        echo "+++ FAIL - Expected host initiators were not removed from the export group."
+        exit 1
+    else
+        echo "+++ SUCCESS - All expected host initiators removed from export group" 
+    fi
+    
+    # Cleanup    
+    # 1. Unexport the volume
+    # 2. Delete the volume
+    # 3. Delete the export group
+    # 4. Delete the host initiators
+    # 5. Delete the host
+    runcmd export_group update ${PROJECT}/${exportgroup} --remVols ${PROJECT}/${volume}
+    runcmd volume delete ${PROJECT}/${volume} --wait
+    runcmd export_group delete ${PROJECT}/${exportgroup}    
+    runcmd initiator delete ${host}/${init3}
+    runcmd initiator delete ${host}/${init4}
+    runcmd hosts delete ${host}
+    
+    # Snap DB
+    snap_db 2 ${column_family}
+    
+    # Validate DB
+    validate_db 1 2 ${column_family}
+}

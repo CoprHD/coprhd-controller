f113f3dcdc2c00da3e84b54a9e25ce84c7bc0e78
chandan <chandan.singh@emc.com>
2016-11-11 14:00:21 +0000

    ExportMaskAddInitiatorRollBack

--- controllersvc/src/main/java/com/emc/storageos/volumecontroller/impl/block/taskcompleter/ExportMaskRemoveInitiatorCompleter.java
+++ controllersvc/src/main/java/com/emc/storageos/volumecontroller/impl/block/taskcompleter/ExportMaskRemoveInitiatorCompleter.java
@@ -18,6 +18,7 @@ import com.emc.storageos.db.client.model.ExportMask;
 import com.emc.storageos.db.client.model.Initiator;
 import com.emc.storageos.db.client.model.Operation;
 import com.emc.storageos.exceptions.DeviceControllerException;
+import com.emc.storageos.svcs.errorhandling.model.ServiceCoded;
 import com.emc.storageos.util.ExportUtils;
 
 public class ExportMaskRemoveInitiatorCompleter extends ExportTaskCompleter {
@@ -67,6 +68,44 @@ public class ExportMaskRemoveInitiatorCompleter extends ExportTaskCompleter {
                     getId().toString(), getOpId()), e);
         } finally {
             super.ready(dbClient);
+        }
+    }
+    
+    @Override
+    protected void complete(DbClient dbClient, Operation.Status status, ServiceCoded coded) throws DeviceControllerException {
+    	try {
+            ExportGroup exportGroup = dbClient.queryObject(ExportGroup.class, getId());
+            ExportMask exportMask = (getMask() != null) ?
+                    dbClient.queryObject(ExportMask.class, getMask()) : null;
+            if (exportMask != null && Operation.isTerminalState(status)) {
+                List<Initiator> initiators =
+                        dbClient.queryObject(Initiator.class, _initiatorURIs);
+                exportMask.removeInitiators(initiators);
+                exportMask.removeFromUserCreatedInitiators(initiators);
+                if (exportMask.getInitiators() == null ||
+                        exportMask.getInitiators().isEmpty()) {
+                    exportGroup.removeExportMask(exportMask.getId());
+                    dbClient.markForDeletion(exportMask);
+                    dbClient.updateObject(exportGroup);
+                } else {
+                    List<URI> targetPorts = ExportUtils.getRemoveInitiatorStoragePorts(exportMask, initiators, dbClient);
+                    if (targetPorts != null && !targetPorts.isEmpty()) {
+                        for (URI targetPort : targetPorts) {
+                            exportMask.removeTarget(targetPort);
+                        }
+                    }
+                    dbClient.updateObject(exportMask);
+                }
+                _log.info(String.format(
+                        "Done ExportMaskRemoveInitiator - Id: %s, OpId: %s, status: %s",
+                        getId().toString(), getOpId(), status.name()));
+            }
+        } catch (Exception e) {
+            _log.error(String.format(
+                    "Failed updating status for ExportMaskRemoveInitiator - Id: %s, OpId: %s",
+                    getId().toString(), getOpId()), e);
+        } finally {
+        	super.complete(dbClient, status, coded);
         }
     }
 

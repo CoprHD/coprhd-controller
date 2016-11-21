7a5044ce2bf6fe2a3c36bc8e9c0c4e82aa1030c3
balak1 <keerthi.bala2@emc.com>
2016-11-20 21:49:50 -0500

    Adding OE client for calling primitive APIs

---
+++ vipr-portal/vipr-client/src/java/com/emc/vipr/client/OEClient.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2016 Dell Inc. or its subsidiaries.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package com.emc.vipr.client;
+
+import com.emc.storageos.model.orchestration.PrimitiveList;
+import com.emc.vipr.client.catalog.impl.PathConstants;
+import com.emc.vipr.client.impl.RestClient;
+
+import javax.ws.rs.core.UriBuilder;
+
+/**
+ * Client for OE APIs - primitives, workflows
+ */
+public class OEClient {
+    protected final ViPRCatalogClient2 parent;
+    protected final RestClient client;
+
+    public OEClient(ViPRCatalogClient2 parent, RestClient client) {
+        this.parent = parent;
+        this.client = client;
+    }
+
+    public PrimitiveList getPrimitives() {
+        UriBuilder builder = client.uriBuilder(PathConstants.OE_PRIMITIVES);
+        return client.getURI(PrimitiveList.class, builder.build());
+    }
+}

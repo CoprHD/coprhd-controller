e94fffd02a1432153ca78d93debcc8d531ce98e3
hr2 <ramya.h@emc.com>
2016-11-24 09:50:40 +0530

    Remove the unused exception which was added previously

--- exportLibraries/xtremio/src/main/java/com/emc/storageos/xtremio/restapi/errorhandling/XtremIOApiExceptions.java
+++ exportLibraries/xtremio/src/main/java/com/emc/storageos/xtremio/restapi/errorhandling/XtremIOApiExceptions.java
@@ -4,10 +4,6 @@
  */
 package com.emc.storageos.xtremio.restapi.errorhandling;
 
-import java.net.URI;
-import java.util.List;
-import java.util.Set;
-
 import com.emc.storageos.svcs.errorhandling.annotations.DeclareServiceCode;
 import com.emc.storageos.svcs.errorhandling.annotations.MessageBundle;
 import com.emc.storageos.svcs.errorhandling.resources.ServiceCode;
@@ -50,7 +46,4 @@ public interface XtremIOApiExceptions {
 
     @DeclareServiceCode(ServiceCode.XTREMIO_API_ERROR)
     XtremIOApiException hluRetrievalFailed(final String message, final Throwable cause);
-    
-    @DeclareServiceCode(ServiceCode.XTREMIO_API_ERROR)
-    XtremIOApiException hluViolation(Set<Integer> hlu, List<String> volumeIds);
 }

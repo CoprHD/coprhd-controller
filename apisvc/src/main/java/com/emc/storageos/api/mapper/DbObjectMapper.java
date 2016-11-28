e93c6d3d2f76654da13be416e91a862e35a4dc22
Surendra Harikantra <surendra.harikantra@emc.com>
2016-11-28 22:40:27 +0530

    more changes

--- apisvc/src/main/java/com/emc/storageos/api/mapper/DbObjectMapper.java
+++ apisvc/src/main/java/com/emc/storageos/api/mapper/DbObjectMapper.java
@@ -334,6 +334,7 @@ public class DbObjectMapper {
         
         to.setId(from.getId());
         to.setName(from.getLabel());
+        to.setDescription(from.getDescription());
         if(from.getMaxPaths() != null){
             to.setMaxPaths(from.getMaxPaths());
         }

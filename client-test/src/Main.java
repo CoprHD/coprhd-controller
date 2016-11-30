eae9cb2208664f347b25b30959983e3433ea0dbf
Simon Wang <simon.wang@emc.com>
2016-11-30 14:56:03 -0500

    test login

---
+++ client-test/src/Main.java
@@ -0,0 +1,15 @@
+import com.emc.vipr.client.ClientConfig;
+import com.emc.vipr.client.ViPRCoreClient;
+
+/**
+ * Created by wangs12 on 11/30/2016.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        ViPRCoreClient client = new ViPRCoreClient(new ClientConfig().withHost("10.247.78.60").withIgnoringCertificates(true));
+        client.auth().login("admin", "changeme");
+        System.out.println("Login successfully");
+    }
+
+}

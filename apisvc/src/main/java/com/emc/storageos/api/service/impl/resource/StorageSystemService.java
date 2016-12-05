b863d7f2ffe6a31ba7969556d565591b373903fc
Ravi Edpuganti <ravi.edpuganti@emc.com>
2016-12-05 18:40:43 +0530

    draft change 3

--- apisvc/src/main/java/com/emc/storageos/api/service/impl/resource/StorageSystemService.java
+++ apisvc/src/main/java/com/emc/storageos/api/service/impl/resource/StorageSystemService.java
@@ -1964,6 +1964,7 @@ public class StorageSystemService extends TaskResourceService {
     private boolean validateNameSpace(String nameSpace, StorageSystem storageSystem) {
         boolean validNameSpace = false;
 
+        _log.info("NAME SPACE INVOKED IS :" + nameSpace);
         
         if (Discovery_Namespaces.BLOCK_SNAPSHOTS.name().equalsIgnoreCase(nameSpace)) {
             if (Type.vmax.name().equalsIgnoreCase(storageSystem.getSystemType()) ||
@@ -1997,7 +1998,7 @@ public class StorageSystemService extends TaskResourceService {
 
         if (nameSpace.equalsIgnoreCase(Discovery_Namespaces.DETECT_EXTERNAL_CHANGES.toString()) &&
         		!Type.vplex.toString().equalsIgnoreCase(storageSystem.getSystemType()) ){
-        	return false;
+        	return true;
         }
         
         boolean isFileStorageSystem = storageSystem.storageSystemIsFile();

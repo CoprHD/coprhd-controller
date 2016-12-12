c0fa476d5ccca33b4924a7084b289c1a655672ef
Boying Lu <boying.lu@emc.com>
2016-12-12 11:51:45 +0800

    Add license header

--- dbclient/src/main/java/com/emc/storageos/db/client/impl/CompositeIndexColumnName.java
+++ dbclient/src/main/java/com/emc/storageos/db/client/impl/CompositeIndexColumnName.java
@@ -1,10 +1,11 @@
+/*
+ * Copyright (c) 2016 EMC Corporation
+ * All Rights Reserved
+ */
 package com.emc.storageos.db.client.impl;
 
 import java.util.UUID;
 
-/**
- * Created by brian on 10/28/16.
- */
 public interface CompositeIndexColumnName {
     String getOne();
 

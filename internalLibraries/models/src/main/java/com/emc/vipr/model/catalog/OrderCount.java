1bd4f117e6913b1970b7a3dc1e8a3d4e3f4439b1
Boying Lu <boying.lu@dell.com>
2016-12-26 13:44:02 +0800

    Fix the counting issue

--- internalLibraries/models/src/main/java/com/emc/vipr/model/catalog/OrderCount.java
+++ internalLibraries/models/src/main/java/com/emc/vipr/model/catalog/OrderCount.java
@@ -9,28 +9,26 @@ import java.util.HashMap;
 import java.util.Map;
 
 import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement(name = "order_count")
 public class OrderCount {
-    private Map<String, Long> countMap = new HashMap();
+    private Map<String, Long> counts = new HashMap();
 
     public OrderCount() {
     }
 
     public OrderCount(Map<String, Long> map) {
-        countMap = map;
+        counts = map;
     }
 
     public void put(String key, long count) {
-        countMap.put(key, count);
+        counts.put(key, count);
     }
 
-    @XmlElement(name = "counts")
+    @XmlElementWrapper(name = "counts")
     public Map<String, Long> getCounts() {
-        if (countMap == null) {
-            countMap = new HashMap<>();
-        }
-        return countMap;
+        return counts;
     }
 }

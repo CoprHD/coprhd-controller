8744b855b9b5c8bd7b98108333f0578e0d989ea0
lakhinana, vasudevu <vasudevu.lakhinana@emc.com>
2017-01-05 10:49:35 +0530

    Moved policies tab from tenants to virtual asserts

---
+++ vipr-portal/portal/app/controllers/arrays/FileReplicationTopology.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2015 EMC Corporation
+ * All Rights Reserved
+ */
+package controllers.arrays;
+
+import org.apache.commons.lang.StringUtils;
+
+import play.data.validation.Required;
+import play.data.validation.Validation;
+
+public class FileReplicationTopology {
+
+    @Required
+    public String sourceVArray;
+    public String targetVArray;
+
+    public boolean isEnabled() {
+        return StringUtils.isNotBlank(sourceVArray);
+    }
+
+    public void validate(String formName) {
+        Validation.valid(formName, this);
+    }
+
+    public void load(String srcVArray, String tgtVArray) {
+        sourceVArray = srcVArray;
+        targetVArray = tgtVArray;
+    }
+}

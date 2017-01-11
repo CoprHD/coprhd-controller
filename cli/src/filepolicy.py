87e53fbe3d20e3217c80fb54f1357c2e4a4754c5
Ganesh <Om.Ganesh@emc.com>
2017-01-11 15:56:08 +0530

    Updated Copyright comment

--- cli/src/filepolicy.py
+++ cli/src/filepolicy.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 
-# Copyright (c) 2012-13 EMC Corporation
+# Copyright (c) 2016-17 Dell EMC Technologies
 # All Rights Reserved
 #
-# This software contains the intellectual property of EMC Corporation
-# or is licensed to EMC Corporation from third parties.  Use of this
+# This software contains the intellectual property of Dell EMC Technologies
+# or is licensed to Dell EMC Technologies from third parties.  Use of this
 # software and the intellectual property contained therein is expressly
 # limited to the terms and conditions of the License Agreement under which
-# it is provided by or on behalf of EMC.
+# it is provided by or on behalf of Dell EMC Technologies.
 
 import common
 import tag

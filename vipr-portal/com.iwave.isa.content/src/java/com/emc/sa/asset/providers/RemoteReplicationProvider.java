9b08d718fef46ee9e6899d158100ae86790ea394
Amit Saurabh <amit.saurabh@emc.com>
2017-01-12 15:49:35 +0530

    some more changes.

--- vipr-portal/com.iwave.isa.content/src/java/com/emc/sa/asset/providers/RemoteReplicationProvider.java
+++ vipr-portal/com.iwave.isa.content/src/java/com/emc/sa/asset/providers/RemoteReplicationProvider.java
@@ -15,12 +15,13 @@ import com.emc.sa.asset.BaseAssetOptionsProvider;
 import com.emc.sa.asset.annotation.Asset;
 import com.emc.sa.asset.annotation.AssetDependencies;
 import com.emc.sa.asset.annotation.AssetNamespace;
+import com.emc.storageos.model.remotereplication.RemoteReplicationSetRestRep;
 import com.emc.storageos.model.vpool.BlockVirtualPoolRestRep;
 import com.emc.vipr.model.catalog.AssetOption;
 
 @Component
 @AssetNamespace("vipr")
-public class RemoteReplicationSetProvider extends BaseAssetOptionsProvider {
+public class RemoteReplicationProvider extends BaseAssetOptionsProvider {
 
     @Asset("remoteReplicationSet")
     @AssetDependencies({ "project", "blockVirtualPool" })
@@ -35,11 +36,32 @@ public class RemoteReplicationSetProvider extends BaseAssetOptionsProvider {
         }
     }
 
-    @Asset("remoteReplicationSet")
-    @AssetDependencies({ "project" })
-    public List<AssetOption> getRemoteReplicationSets(AssetOptionsContext ctx, URI projectId) {
-        return createBaseResourceOptions(api(ctx).remoteReplicationSets().search().byProject(projectId).run());
+    @Asset("remoteReplicationMode")
+    @AssetDependencies({ "project", "remoteReplicationSet" })
+    public List<AssetOption>
+            getRemoteReplicationModes(AssetOptionsContext ctx, URI projectId, URI remoteReplicationSetID) {
+
+        RemoteReplicationSetRestRep remoteReplicationSet = api(ctx).remoteReplicationSets().get(remoteReplicationSetID);
+        if (remoteReplicationSet != null)
+            return createStringOptions(remoteReplicationSet.getSupportedReplicationModes());
+        else {
+            return Collections.emptyList();
+        }
     }
+
+    @Asset("remoteReplicationGroup")
+    @AssetDependencies({ "project", "remoteReplicationSet" })
+    public List<AssetOption> getRemoteReplicationGroups(AssetOptionsContext ctx, URI projectId, URI remoteReplicationSetID) {
+
+        // Only provide remote Replication groups if the if user selected a  Set supports .
+        RemoteReplicationSetRestRep remoteReplicationSet = api(ctx).remoteReplicationSets().get(remoteReplicationSetID);
+        if (remoteReplicationSet != null) {
+            return createBaseResourceOptions(api(ctx).remoteReplicationGroups().search().byProject(projectId).run());
+        } else {
+            return Collections.emptyList();
+        }
+    }
+
 
     private boolean isSupportedVPool(BlockVirtualPoolRestRep vpool) {
         return vpool != null && vpool.getProtection().getRemoteReplicationParam() != null;

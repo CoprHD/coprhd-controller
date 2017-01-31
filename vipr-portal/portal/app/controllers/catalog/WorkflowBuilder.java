55ef045426340c56a2937739f15fdf5474d73f11
balak1 <keerthi.bala2@emc.com>
2017-01-31 11:47:46 -0500

    Adding wizard for creating local ansible primitive and its corresponding controller methods

--- vipr-portal/portal/app/controllers/catalog/WorkflowBuilder.java
+++ vipr-portal/portal/app/controllers/catalog/WorkflowBuilder.java
@@ -22,8 +22,10 @@ import com.emc.storageos.model.orchestration.OrchestrationWorkflowDocument;
 import com.emc.storageos.model.orchestration.OrchestrationWorkflowList;
 import com.emc.storageos.model.orchestration.OrchestrationWorkflowRestRep;
 import com.emc.storageos.model.orchestration.OrchestrationWorkflowUpdateParam;
+import com.emc.storageos.model.orchestration.PrimitiveCreateParam;
 import com.emc.storageos.model.orchestration.PrimitiveList;
 import com.emc.storageos.model.orchestration.PrimitiveRestRep;
+import com.emc.storageos.model.orchestration.PrimitiveResourceRestRep;
 import com.emc.storageos.primitives.Primitive;
 import com.emc.storageos.primitives.PrimitiveHelper;
 import com.emc.storageos.primitives.ViPRPrimitive;
@@ -38,11 +40,15 @@ import controllers.Common;
 import java.io.File;
 import java.net.URI;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import util.StringOption;
+
+import org.apache.commons.lang.StringUtils;
 import org.owasp.esapi.ESAPI;
 import play.Logger;
 import play.data.validation.Required;
@@ -57,12 +63,17 @@ import static util.BourneUtil.getCatalogClient;
  */
 @With(Common.class)
 public class WorkflowBuilder extends Controller {
+	private static final String NO_PARENT = "#";
     private static final String MY_LIBRARY_ROOT = "myLib";
     private static final String VIPR_LIBRARY_ROOT = "viprLib";
     private static final String VIPR_PRIMITIVE_ROOT = "viprrest";
     private static final String NODE_TYPE_FILE = "file";
+	private static final String MY_LIBRARY = "My Library";
+    private static final String VIPR_LIBRARY = "ViPR Library";
+    private static final String VIPR_PRIMITIVE_LIBRARY = "ViPR REST Primitives";
 
     public static void view() {
+        setAnsibleResources();
         render();
     }
 
@@ -113,6 +124,7 @@ public class WorkflowBuilder extends Controller {
         // get workflow directories and prepare nodes
         WFBulkRep wfBulkRep = getCatalogClient().wfDirectories().getAll();
         List<Node> topLevelNodes = new ArrayList<Node>();
+		prepareRootNodes(topLevelNodes);
         Node node;
         String nodeParent;
         for (WFDirectoryRestRep wfDirectoryRestRep : wfBulkRep
@@ -130,19 +142,29 @@ public class WorkflowBuilder extends Controller {
                 for (URI u : wfDirectoryRestRep.getWorkflows()) {
                     if (oeId2NameMap.containsKey(u)) {
                         topLevelNodes.add(new Node(u.toString(), oeId2NameMap
-                                .get(u), node.id, WFBuilderNodeTypes.WORKFLOW.toString()));
+                                .remove(u), node.id, WFBuilderNodeTypes.WORKFLOW.toString()));
                     }
                 }
             }
             topLevelNodes.add(node);
         }
 
+		//add remaining workflows with no parent to default root - My_Library_root
+        for(Map.Entry<URI,String> entry : oeId2NameMap.entrySet()) {
+            topLevelNodes.add(new Node(entry.getKey().toString(), entry.getValue(), MY_LIBRARY_ROOT, WFBuilderNodeTypes.WORKFLOW.toString()));
+        }
         // Get primitives data and prepare nodes
         addPrimitives(topLevelNodes);
 
         renderJSON(topLevelNodes);
     }
 
+	// Preparing top level nodes in workflow directory
+    private static void prepareRootNodes(List<Node> topLevelNodes) {
+        topLevelNodes.add(new Node(MY_LIBRARY_ROOT, MY_LIBRARY, NO_PARENT, WFBuilderNodeTypes.FOLDER.toString()));
+        topLevelNodes.add(new Node(VIPR_LIBRARY_ROOT, VIPR_LIBRARY, NO_PARENT, WFBuilderNodeTypes.FOLDER.toString()));
+        topLevelNodes.add(new Node(VIPR_PRIMITIVE_ROOT, VIPR_PRIMITIVE_LIBRARY, VIPR_LIBRARY_ROOT, WFBuilderNodeTypes.FOLDER.toString()));
+    }
     public static void editWFDirName(String id, String newName) {
         try {
             WFDirectoryUpdateParam param = new WFDirectoryUpdateParam();
@@ -212,7 +234,7 @@ public class WorkflowBuilder extends Controller {
                     .orchestrationPrimitives().createWorkflow(param);
 
             // Add this workflowid to directory
-            if (null != orchestrationWorkflowRestRep) {
+            if (!MY_LIBRARY_ROOT.equals(dirID) && null != orchestrationWorkflowRestRep) {
                 final WFDirectoryUpdateParam wfDirectoryParam = new WFDirectoryUpdateParam();
                 final Set<URI> addWorkflows = new HashSet<URI>();
                 addWorkflows.add(orchestrationWorkflowRestRep.getId());
@@ -414,5 +436,159 @@ public class WorkflowBuilder extends Controller {
         shellPrimitive.validate();
         //TODO : call APIs to load script and create this primitive
         view();
+    }
+	
+	public static class LocalAnsiblePrimitiveForm {
+        @Required
+        private String name;
+        public String description;
+
+        private boolean existing;
+        private String existingResource;
+        private File ansiblePackage;
+        private String ansiblePackageName;
+        private String ansiblePlaybook;
+        @Required
+        private String hostFilePath;
+        private String inputs; //comma separated list of inputs
+        private String outputs; // comma separated list of ouputs
+
+        //TODO
+        public void validate(){
+
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+
+        public String getExistingResource() {
+            return existingResource;
+        }
+
+        public void setExistingResource(String existingResource) {
+            this.existingResource = existingResource;
+        }
+
+        public File getAnsiblePackage() {
+            return ansiblePackage;
+        }
+
+        public void setAnsiblePackage(File ansiblePackage) {
+            this.ansiblePackage = ansiblePackage;
+        }
+
+        public String getAnsiblePackageName() {
+            return ansiblePackageName;
+        }
+
+        public void setAnsiblePackageName(String ansiblePackageName) {
+            this.ansiblePackageName = ansiblePackageName;
+        }
+
+        public String getHostFilePath() {
+            return hostFilePath;
+        }
+
+        public void setHostFilePath(String hostFilePath) {
+            this.hostFilePath = hostFilePath;
+        }
+
+        public String getInputs() {
+            return inputs;
+        }
+
+        public void setInputs(String inputs) {
+            this.inputs = inputs;
+        }
+
+        public String getOutputs() {
+            return outputs;
+        }
+
+        public void setOutputs(String outputs) {
+            this.outputs = outputs;
+        }
+
+        public String getAnsiblePlaybook() {
+            return ansiblePlaybook;
+        }
+
+        public void setAnsiblePlaybook(String ansiblePlaybook) {
+            this.ansiblePlaybook = ansiblePlaybook;
+        }
+
+        public boolean isExisting() {
+            return existing;
+        }
+
+        public void setExisting(boolean existing) {
+            this.existing = existing;
+        }
+    }
+
+    public static void createLocalAnsiblePrimitive(@Valid LocalAnsiblePrimitiveForm localAnsible){
+        localAnsible.validate();
+
+        try {
+            PrimitiveResourceRestRep primitiveResourceRestRep = null;
+            if(localAnsible.isExisting()) {
+                //TODO: waiting for resources GET
+            }
+            else if(null != localAnsible.ansiblePackage) {
+                //upload ansible package
+                primitiveResourceRestRep = getCatalogClient().orchestrationPrimitives().createPrimitiveResource("ANSIBLE", localAnsible.ansiblePackage, localAnsible.ansiblePackageName);
+            }
+
+            // Create Primitive
+            if (null != primitiveResourceRestRep) {
+                PrimitiveCreateParam primitiveCreateParam = new PrimitiveCreateParam();
+                //TODO - remove this hardcoded string once the enum is available
+                primitiveCreateParam.setType("ANSIBLE");
+                primitiveCreateParam.setName(localAnsible.getName());
+                primitiveCreateParam.setDescription(localAnsible.getDescription());
+                primitiveCreateParam.setResource(primitiveResourceRestRep.getId());
+                primitiveCreateParam.setAttributes(new HashMap<String, String>());
+                primitiveCreateParam.getAttributes().put("playbook", localAnsible.getAnsiblePlaybook());
+                if (StringUtils.isNotEmpty(localAnsible.getInputs())) {
+                    primitiveCreateParam.setInput(Arrays.asList(localAnsible.getInputs().split(",")));
+                }
+                if (StringUtils.isNotEmpty(localAnsible.getOutputs())) {
+                    primitiveCreateParam.setOutput(Arrays.asList(localAnsible.getOutputs().split(",")));
+                }
+
+                getCatalogClient().orchestrationPrimitives().createPrimitive(primitiveCreateParam);
+            }
+            else {
+                //TODO: throw error
+            }
+        }
+        catch (final Exception e) {
+            Logger.error(e.getMessage());
+        }
+
+        view();
+
+    }
+
+    private static void setAnsibleResources() {
+        //TODO - Get these resources using API and remove this temporary data
+        List<StringOption> ansibleResourceNames = new ArrayList<StringOption>();
+        ansibleResourceNames.add(new StringOption("1x","CreateHostPackage"));
+        ansibleResourceNames.add(new StringOption("2x","Create Project Package"));
+        renderArgs.put("ansibleResourceNames", ansibleResourceNames);
+
     }
 }

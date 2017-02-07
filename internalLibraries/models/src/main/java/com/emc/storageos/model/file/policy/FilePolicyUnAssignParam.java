1ab7eecdc141f611090e8c207512b88f61ea9d8f
lakhinana, vasudevu <vasudevu.lakhinana@emc.com>
2017-02-07 16:59:18 +0530

    Removed unwanted code

--- internalLibraries/models/src/main/java/com/emc/storageos/model/file/policy/FilePolicyUnAssignParam.java
+++ internalLibraries/models/src/main/java/com/emc/storageos/model/file/policy/FilePolicyUnAssignParam.java
@@ -5,7 +5,11 @@
 package com.emc.storageos.model.file.policy;
 
 import java.io.Serializable;
+import java.net.URI;
+import java.util.Set;
 
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
 import javax.xml.bind.annotation.XmlRootElement;
 
 /**
@@ -15,8 +19,19 @@ import javax.xml.bind.annotation.XmlRootElement;
 public class FilePolicyUnAssignParam implements Serializable {
 
     private static final long serialVersionUID = 1L;
+    private Set<URI> unassignfrom;
 
     public FilePolicyUnAssignParam() {
         super();
+    }
+
+    @XmlElementWrapper(name = "unassign_from")
+    @XmlElement(name = "URI")
+    public Set<URI> getUnassignfrom() {
+        return unassignfrom;
+    }
+
+    public void setUnassignfrom(Set<URI> unassignfrom) {
+        this.unassignfrom = unassignfrom;
     }
 }
--- internalLibraries/models/src/main/java/com/emc/storageos/model/file/policy/FilePolicyUpdateParam.java
+++ internalLibraries/models/src/main/java/com/emc/storageos/model/file/policy/FilePolicyUpdateParam.java
@@ -5,23 +5,10 @@
 package com.emc.storageos.model.file.policy;
 
 import java.io.Serializable;
-import java.net.URI;
 
-import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement(name = "file_policy_update")
 public class FilePolicyUpdateParam extends FilePolicyParam implements Serializable {
     private static final long serialVersionUID = 1L;
-    private URI policyStorageResource;
-
-    @XmlElement(name = "policy_storage_resource")
-    public URI getPolicyStorageResource() {
-        return policyStorageResource;
-    }
-
-    public void setPolicyStorageResource(URI policyStorageResource) {
-        this.policyStorageResource = policyStorageResource;
-    }
-
 }

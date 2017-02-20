b49019078ed748a172ad801cf29c34ae6ac6f563
Amit <amitherlekar@gmail.com>
2017-02-20 10:50:35 +0530

    Setting DR port for replication policy.

--- controllersvc/src/main/java/com/emc/storageos/fileorchestrationcontroller/FileOrchestrationUtils.java
+++ controllersvc/src/main/java/com/emc/storageos/fileorchestrationcontroller/FileOrchestrationUtils.java
@@ -930,11 +930,10 @@ public final class FileOrchestrationUtils {
         while (storagePortIter.hasNext()) {
             StoragePort port = dbClient.queryObject(StoragePort.class, storagePortIter.next());
             if (port != null && !port.getInactive()) {
-                targetHost = port.getPortNetworkId();
                 // iterate until dr port found!!
                 if (port.getTag() != null && port.getTag().contains("dr_port")) {
-                    _log.info("DR port {} found from storage system {} for replication", targetHost, targetSystem.getLabel());
-                    break;
+                    _log.info("DR port {} found from storage system {} for replication", port.getPortNetworkId(), targetSystem.getLabel());
+                    return port.getPortNetworkId();
                 }
             }
         }

15e6ba79f80f115849ac24277959f91d83fa539d
Boying Lu <boying.lu@dell.com>
2017-02-22 13:32:43 +0800

    Remove dead codes

--- dbclient/src/main/java/com/emc/storageos/db/client/upgrade/callbacks/AddClassNameTimeSeriesMigrationCallback.java
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright (c) 2017 EMC Corporation
- * All Rights Reserved
- */
-package com.emc.storageos.db.client.upgrade.callbacks;
-
-public class AddClassNameTimeSeriesMigrationCallback {
-}

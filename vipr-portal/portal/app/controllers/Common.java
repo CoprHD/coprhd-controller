4605fa83eb5111d3f26d154e70cb778effd05410
wangs12 <simon.wang@gmail.com>
2017-02-27 13:56:47 -0500

    Correct URI for control config and remove debug logs

--- vipr-portal/portal/app/controllers/Common.java
+++ vipr-portal/portal/app/controllers/Common.java
@@ -85,7 +85,7 @@ public class Common extends Controller {
             put("/usergroup/save","userGroup.name", PATH_SANITIZER);
 
             put("/config/passwords","user", PATH_SANITIZER);
-            put("/config/controller/preview", "value", PATH_SANITIZER);
+            put("/customConfigs/preview", "value", PATH_SANITIZER);
         }
     };
 
@@ -126,9 +126,7 @@ public class Common extends Controller {
 
     @Before(priority = 0)
     public static void xssCheck() {
-        Logger.info("--- xss check, path is %", request.path);
         for (String param : params.all().keySet()) {
-            Logger.info("--- param name is %s", param);
             // skip xss sanitation for fields which name contains password
             if (param.toLowerCase().contains("password") || param.toLowerCase().contains("username")) {
                 Logger.debug("skip sanitation for " + param);
@@ -138,14 +136,11 @@ public class Common extends Controller {
             String[] data = params.getAll(param);
             if ((data != null) && (data.length > 0)) {
                 String sanitizer = (String)XSS_SANITIZERS.get(request.path,param);
-                Logger.debug("---- Cleaning data for " + param);
                 String[] cleanValues = new String[data.length];
                 for (int i = 0; i < data.length; ++i) {
                     if (PATH_SANITIZER.equals(sanitizer)){
-                        Logger.info("---- using path sanitizer");
                         cleanValues[i] = SecurityUtils.stripPathXSS(data[i]);
                     } else {
-                        Logger.info("--- using normal sanitizer");
                         cleanValues[i] = SecurityUtils.stripXSS(data[i]);
                     }
                     params.put(param, cleanValues);

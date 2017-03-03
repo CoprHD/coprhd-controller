d8199a2063694066e6b46b1d8bee740e3bbfba86
jeremie.cormier <jeremie.cormier@emc.com>
2017-03-03 11:59:09 -0400

    Added wwns to datastore to list all volume underneat to see what volumes are being delete when removing a datastore.

--- vipr-portal/com.iwave.isa.content/src/java/com/emc/sa/asset/providers/VMWareProvider.java
+++ vipr-portal/com.iwave.isa.content/src/java/com/emc/sa/asset/providers/VMWareProvider.java
@@ -279,14 +279,20 @@ public class VMWareProvider extends BaseHostProvider {
     }
 
     protected static List<AssetOption> createDatastoreOptions(List<? extends BlockObjectRestRep> mountedVolumes, URI hostId) {
-        Set<String> datastores = Sets.newHashSet(); // There can be multiple volumes to a DS, so de-dupe in a Set
+        Map<String, List<String>> datastores = Maps.newHashMap(); // There can be multiple volumes to a DS, so de-dupe in a hash map
         for (BlockObjectRestRep volume : mountedVolumes) {
-            datastores.add(KnownMachineTags.getBlockVolumeVMFSDatastore(hostId, volume));
+            String key = KnownMachineTags.getBlockVolumeVMFSDatastore(hostId, volume);
+            List<String> values = Lists.newArrayList();
+            if (datastores.containsKey(key)) {
+                values = datastores.get(key);
+            }
+            values.add(volume.getWwn());
+            datastores.put(key, values);
         }
 
         List<AssetOption> options = Lists.newArrayList();
-        for (String datastore : datastores) {
-            options.add(new AssetOption(datastore, datastore));
+        for (Map.Entry<String, List<String>> datastore : datastores.entrySet()) {
+            options.add(new AssetOption(datastore.getKey(), getMessage("datastore.label", datastore.getKey(), String.join(",", datastore.getValue()))));
         }
 
         AssetOptionsUtils.sortOptionsByLabel(options);

59f76cf13446f3189c7b78189055a824e6de3085
Trevor Dawe <trevor.dawe@emc.com>
2017-03-03 12:02:07 -0400

    COP-28596: Set the host to discoverable during OS install

--- apisvc/src/main/java/com/emc/storageos/api/service/impl/resource/HostService.java
+++ apisvc/src/main/java/com/emc/storageos/api/service/impl/resource/HostService.java
@@ -2135,6 +2135,7 @@ public class HostService extends TaskResourceService {
         } else {
             host.setPassword(param.getRootPassword());
             host.setUsername("root");
+            host.setDiscoverable(true);
         }
 
         // check that CS has os install network

f2bffcd5a60b885806a951755b0035ad2227dac8
Bill Elliott IV <bill.elliott@emc.com>
2017-03-24 11:04:07 -0400

    Remove no-op rollback steps only on steps associated with remove/decomission/delete operations. They are causing confusion in the task list and are not necessary.

--- controllersvc/src/main/java/com/emc/storageos/computecontroller/impl/ComputeDeviceControllerImpl.java
+++ controllersvc/src/main/java/com/emc/storageos/computecontroller/impl/ComputeDeviceControllerImpl.java
@@ -10,9 +10,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
-import com.emc.storageos.db.client.model.OpStatusMap;
-import com.emc.storageos.db.client.model.Task;
-import com.emc.storageos.svcs.errorhandling.resources.ServiceCode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -31,9 +28,11 @@ import com.emc.storageos.db.client.model.ExportGroup;
 import com.emc.storageos.db.client.model.ExportMask;
 import com.emc.storageos.db.client.model.Host;
 import com.emc.storageos.db.client.model.Initiator;
+import com.emc.storageos.db.client.model.OpStatusMap;
 import com.emc.storageos.db.client.model.Operation;
 import com.emc.storageos.db.client.model.Operation.Status;
 import com.emc.storageos.db.client.model.ScopedLabel;
+import com.emc.storageos.db.client.model.Task;
 import com.emc.storageos.db.client.model.UCSServiceProfile;
 import com.emc.storageos.db.client.model.UCSServiceProfileTemplate;
 import com.emc.storageos.db.client.model.VcenterDataCenter;
@@ -47,6 +46,7 @@ import com.emc.storageos.model.ResourceOperationTypeEnum;
 import com.emc.storageos.services.OperationTypeEnum;
 import com.emc.storageos.svcs.errorhandling.model.ServiceCoded;
 import com.emc.storageos.svcs.errorhandling.resources.InternalException;
+import com.emc.storageos.svcs.errorhandling.resources.ServiceCode;
 import com.emc.storageos.util.ExportUtils;
 import com.emc.storageos.util.InvokeTestFailure;
 import com.emc.storageos.vcentercontroller.VcenterController;
@@ -801,20 +801,17 @@ public class ComputeDeviceControllerImpl implements ComputeDeviceController {
             waitFor = workflow.createStep(CHECK_HOST_INITIATORS,
                     "Check for host initiators", waitFor, cs.getId(),
                     cs.getSystemType(), this.getClass(), new Workflow.Method("checkHostInitiators", hostId),
-                    new Workflow.Method(ROLLBACK_NOTHING_METHOD),
-                    null);
+                    null, null);
 
             waitFor = workflow.createStep(DEACTIVATION_MAINTENANCE_MODE,
                     "If synced with vCenter, put the host in maintenance mode", waitFor, cs.getId(),
                     cs.getSystemType(), this.getClass(), new Workflow.Method("putHostInMaintenanceMode", hostId),
-                    new Workflow.Method(ROLLBACK_NOTHING_METHOD),
-                    null);
+                    null, null);
 
             waitFor = workflow.createStep(DEACTIVATION_REMOVE_HOST_VCENTER,
                     "If synced with vCenter, remove the host from the cluster", waitFor, cs.getId(),
                     cs.getSystemType(), this.getClass(), new Workflow.Method("removeHostFromVcenterCluster", hostId),
-                    new Workflow.Method(ROLLBACK_NOTHING_METHOD),
-                    null);
+                    null, null);
         }
 
         return waitFor;
@@ -885,19 +882,18 @@ public class ComputeDeviceControllerImpl implements ComputeDeviceController {
             //TODO: need to break this up into individual smaller steps so that we can try to recover using rollback if decommission failed
             waitFor = workflow.createStep(DEACTIVATION_COMPUTE_SYSTEM_HOST, "Unbind blade from service profile",
                     waitFor, cs.getId(), cs.getSystemType(), this.getClass(), new Workflow.Method(
-                            "deactiveComputeSystemHost", cs.getId(), hostId),
-                    new Workflow.Method(ROLLBACK_NOTHING_METHOD), null);
-
+                            "deactiveComputeSystemHost", cs.getId(), hostId), null, null);
+                            
             if (deactivateBootVolume && !NullColumnValueGetter.isNullURI(host.getBootVolumeId())) {
                 waitFor = workflow.createStep(DEACTIVATION_COMPUTE_SYSTEM_BOOT_VOLUME_UNTAG,
                         "Untag the boot volume for the host", waitFor, cs.getId(), cs.getSystemType(),
                         this.getClass(), new Workflow.Method("untagBlockBootVolume", hostId, volumeDescriptors),
-                        new Workflow.Method(ROLLBACK_NOTHING_METHOD), null);
+                        null, null);
 
                 waitFor = workflow.createStep(DEACTIVATION_COMPUTE_SYSTEM_BOOT_VOLUME,
                         "Delete the boot volume for the host", waitFor, cs.getId(), cs.getSystemType(),
                         this.getClass(), new Workflow.Method("deleteBlockBootVolume", hostId, volumeDescriptors),
-                        new Workflow.Method(ROLLBACK_NOTHING_METHOD), null);
+                        null, null);
             } else if (!deactivateBootVolume) {
                 log.info("flag deactivateBootVolume set to false");
             } else if (!NullColumnValueGetter.isNullURI(host.getBootVolumeId())){
@@ -1056,8 +1052,6 @@ public class ComputeDeviceControllerImpl implements ComputeDeviceController {
                 return;
             }
             
-            
-
             String task = UUID.randomUUID().toString();
 
             URI bootVolumeId = getBootVolumeIdFromDescriptors(volumeDescriptors, host);

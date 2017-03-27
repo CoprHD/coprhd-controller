800c15541e4de94ef5a7aabe3cfd2eaf0eccf495
krisht <tejaswini.krishnamurthy@emc.com>
2017-03-27 09:58:25 -0700

    Fix an issue seen during testing

--- controllersvc/src/main/java/com/emc/storageos/volumecontroller/impl/block/taskcompleter/ZoneReferencesRemoveCompleter.java
+++ controllersvc/src/main/java/com/emc/storageos/volumecontroller/impl/block/taskcompleter/ZoneReferencesRemoveCompleter.java
@@ -4,6 +4,7 @@
  */
 package com.emc.storageos.volumecontroller.impl.block.taskcompleter;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,6 +15,7 @@ import com.emc.storageos.db.client.DbClient;
 import com.emc.storageos.db.client.model.FCZoneReference;
 import com.emc.storageos.db.client.model.Operation;
 import com.emc.storageos.db.client.model.Operation.Status;
+import com.emc.storageos.db.client.util.NullColumnValueGetter;
 import com.emc.storageos.db.exceptions.DatabaseException;
 import com.emc.storageos.exceptions.DeviceControllerException;
 import com.emc.storageos.networkcontroller.NetworkFCZoneInfo;
@@ -48,6 +50,11 @@ public class ZoneReferencesRemoveCompleter extends TaskCompleter {
             String refKey = null;
             try {
                 for (NetworkFCZoneInfo fabricInfo : zoneInfoList) {
+                    URI fcZoneReferenceId = fabricInfo.getFcZoneReferenceId();
+                    if (NullColumnValueGetter.isNullURI(fcZoneReferenceId)) {
+                        _log.info("fcZoneReferenceId is null. Nothing to remove.");
+                        continue;
+                    }
                     FCZoneReference ref = dbClient.queryObject(FCZoneReference.class, fabricInfo.getFcZoneReferenceId());
                     if (ref != null) {
                         refKey = ref.getPwwnKey();

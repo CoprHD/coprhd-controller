463f82af9084ec5422360555a47a4e2c21d71b54
mhegeric <mike.hegerich@emc.com>
2017-04-06 14:02:08 -0400

    Add PerformanceParams column family

---
+++ dbclient/src/main/java/com/emc/storageos/db/client/model/PerformanceParams.java
@@ -0,0 +1,154 @@
+/*
+ * Copyright 2017 EMC Corporation
+ * All Rights Reserved
+ */
+package com.emc.storageos.db.client.model;
+
+
+/**
+ * This column family captures array performance parameters that are deprecated from
+ * the VirtualPool column family as part of the virtual pool simplification project.
+ * Like VirtualPools, PerformanceParam instances can be specified for volumes when they
+ * are provisioned.
+ */
+@Cf("PerformanceParams")
+@SuppressWarnings("serial")
+public class PerformanceParams extends DataObjectWithACLs implements GeoVisibleResource {
+    
+    // A user supplied description.
+    private String _description;
+    
+    // The FAST Policy Name.
+    private String _autoTierPolicyName;
+
+    // Compression setting for all Flash VMAX3 storage arrays.
+    private Boolean _compressionEnabled;
+
+    // VMAX Host I/O bandwidth limit. Unlimited when not specified or 0.
+    private Integer _hostIOLimitBandwidth;
+    
+    // VMAX Host I/O IOPS limit. Unlimited when not specified or 0.
+    private Integer _hostIOLimitIOPs;
+
+    // Percentage of the requested volume size that is allocated when provisioning thin volumes.
+    private Integer _thinVolumePreAllocationPercentage;
+
+    // Whether or not volumes should be provisioned in storage pools that support deduplication.
+    private Boolean _dedupCapable;
+
+    // Whether or not to use striped (false) or concatenated (true) meta-volumes when expanding volumes.
+    private Boolean _fastExpansion = false;
+    
+    /*
+     * Required member variable getters and setters.
+     */
+
+    @Name("description")
+    public String getDescription() {
+        return _description;
+    }
+
+    public void setDescription(final String description) {
+        _description = description;
+        setChanged("description");
+    }
+
+    @Name("autoTierPolicyName")
+    public String getAutoTierPolicyName() {
+        return _autoTierPolicyName;
+    }
+
+    public void setAutoTierPolicyName(final String autoTierPolicyName) {
+        _autoTierPolicyName = autoTierPolicyName;
+        setChanged("autoTierPolicyName");
+    }
+
+    @Name("compressionEnabled")
+    public Boolean getCompressionEnabled() {
+        return _compressionEnabled == null ? false : _compressionEnabled;
+    }
+
+    public void setCompressionEnabled(final Boolean compressionEnabled) {
+        _compressionEnabled = compressionEnabled;
+        setChanged("compressionEnabled");
+    }
+
+    @Name("hostIOLimitBandwidth")
+    public Integer getHostIOLimitBandwidth() {
+        return _hostIOLimitBandwidth;
+    }
+
+    public void setHostIOLimitBandwidth(final Integer limitHostBandwidth) {
+        _hostIOLimitBandwidth = limitHostBandwidth == null ? null : Math.max(0, limitHostBandwidth);
+        setChanged("hostIOLimitBandwidth");
+    }
+
+    @Name("hostIOLimitIOPs")
+    public Integer getHostIOLimitIOPs() {
+        return _hostIOLimitIOPs;
+    }
+
+    public void setHostIOLimitIOPs(final Integer limitHostIOPs) {
+        _hostIOLimitIOPs = limitHostIOPs == null ? null : Math.max(0, limitHostIOPs);
+        setChanged("hostIOLimitIOPs");
+    }
+
+    @Name("thinVolumePreAllocationPercentage")
+    public Integer getThinVolumePreAllocationPercentage() {
+        return _thinVolumePreAllocationPercentage;
+    }
+
+    public void setThinVolumePreAllocationPercentage(final Integer thinVolumePreAllocationPercentage) {
+        _thinVolumePreAllocationPercentage = thinVolumePreAllocationPercentage;
+        setChanged("thinVolumePreAllocationPercentage");
+    }
+
+    @Name("dedupCapable")
+    public Boolean getDedupCapable() {
+        if (null == _dedupCapable) {
+            return false;
+        }
+        return _dedupCapable;
+    }
+
+    public void setDedupCapable(final Boolean dedupCapable) {
+        if (null == dedupCapable) {
+            _dedupCapable = false;
+        } else {
+            _dedupCapable = dedupCapable;
+        }
+        setChanged("dedupCapable");
+    }
+    
+    @Name("fastExpansion")
+    public Boolean getFastExpansion() {
+        return _fastExpansion;
+    }
+
+    public void setFastExpansion(final Boolean fastExpansion) {
+        _fastExpansion = fastExpansion;
+        setChanged("fastExpansion");
+    }
+
+    /*
+     * Utility Methods
+     */
+
+    /**
+     * Returns whether or not a Host I/O IOPS limit is set.
+     * 
+     * @return true if a limit is set, false otherwise.
+     */
+    public boolean isHostIOLimitIOPsSet() {
+        return _hostIOLimitIOPs != null && _hostIOLimitIOPs > 0;
+    }
+
+    /**
+     * Returns whether or not a Host I/O bandwidth limit is set.
+     * 
+     * @return true if a limit is set, false otherwise.
+     */
+    public boolean isHostIOLimitBandwidthSet() {
+        return _hostIOLimitBandwidth != null && _hostIOLimitBandwidth > 0;
+    }
+}

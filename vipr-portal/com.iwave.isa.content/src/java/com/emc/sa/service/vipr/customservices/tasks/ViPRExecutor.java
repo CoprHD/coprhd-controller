150adafc661249c6b53105ce32ec721945489a92
Sonali <sonali.sahu@emc.com>
2017-04-06 14:58:42 -0400

    match the step type to primitive type name

--- vipr-portal/com.iwave.isa.content/src/java/com/emc/sa/service/vipr/customservices/tasks/ViPRExecutor.java
+++ vipr-portal/com.iwave.isa.content/src/java/com/emc/sa/service/vipr/customservices/tasks/ViPRExecutor.java
@@ -1,19 +1,20 @@
 package com.emc.sa.service.vipr.customservices.tasks;
 
-import com.emc.sa.catalog.primitives.CustomServicesPrimitiveDAOs;
-import com.emc.sa.service.vipr.tasks.ViPRExecutionTask;
-import com.emc.storageos.model.customservices.CustomServicesWorkflowDocument;
-import com.emc.vipr.client.impl.RestClient;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-
 import java.util.List;
 import java.util.Map;
 
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.emc.sa.catalog.primitives.CustomServicesPrimitiveDAOs;
+import com.emc.sa.service.vipr.tasks.ViPRExecutionTask;
+import com.emc.storageos.model.customservices.CustomServicesWorkflowDocument;
+import com.emc.storageos.primitives.CustomServicesConstants;
+import com.emc.vipr.client.impl.RestClient;
 
 public class ViPRExecutor implements MakeCustomServicesExecutor {
-    private final static String TYPE = "ViPR REST API";
-    private static final org.slf4j.Logger logger = LoggerFactory.getLogger(CustomServicesViprExecution.class);
+    private final static String TYPE = CustomServicesConstants.VIPR_PRIMITIVE_TYPE;
+    private static final org.slf4j.Logger logger = LoggerFactory.getLogger(ViPRExecutor.class);
     @Autowired
     private CustomServicesPrimitiveDAOs daos;
     private RestClient client;

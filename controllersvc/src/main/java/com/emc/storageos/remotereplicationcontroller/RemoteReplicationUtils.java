68a2f513738d1cad9a19efde492d993457c6770e
eroytman <evgeny.roytman@emc.com>
2017-04-07 10:53:12 -0400

    small fix.

--- controllersvc/src/main/java/com/emc/storageos/remotereplicationcontroller/RemoteReplicationUtils.java
+++ controllersvc/src/main/java/com/emc/storageos/remotereplicationcontroller/RemoteReplicationUtils.java
@@ -10,7 +10,6 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
-import com.emc.storageos.db.exceptions.DatabaseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 

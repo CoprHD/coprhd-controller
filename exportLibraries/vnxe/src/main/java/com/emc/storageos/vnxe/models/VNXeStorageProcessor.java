c76c190b9c7e96b98a823dca0dca02b508085d32
Tong <Tong.Yu@emc.com>
2017-04-20 15:15:58 -0400

    COP-29448 set slotnumber for Unity storageHADomain

--- exportLibraries/vnxe/src/main/java/com/emc/storageos/vnxe/models/VNXeStorageProcessor.java
+++ exportLibraries/vnxe/src/main/java/com/emc/storageos/vnxe/models/VNXeStorageProcessor.java
@@ -16,7 +16,7 @@ public class VNXeStorageProcessor extends VNXeBase {
     private boolean needsReplacement;
     private boolean isRescueMode;
     private String model;
-    private int slotNumber;
+    private Integer slotNumber;
     private String name;
     private String emcPartNumber;
     private String emcSerialNumber;
@@ -61,7 +61,7 @@ public class VNXeStorageProcessor extends VNXeBase {
         this.model = model;
     }
 
-    public int getSlotNumber() {
+    public Integer getSlotNumber() {
         return slotNumber;
     }
 

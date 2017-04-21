5ac31248f81bed0ea8df0fdd7b713a5c349d3169
krisht <tejaswini.krishnamurthy@emc.com>
2017-04-20 22:58:48 -0700

    Commit the new file

---
+++ vipr-portal/com.iwave.isa.content/src/java/com/emc/sa/service/ArtificialFailures.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2017 Dell EMC
+ * All Rights Reserved
+ */
+
+package com.emc.sa.service;
+
+public class ArtificialFailures {
+	public static final String ARTIFICIAL_FAILURE_LINUX_MOUNT_VOLUME = "mountLinuxVolume";
+	public static final String ARTIFICIAL_FAILURE_WINDOWS_EXTEND_DRIVE = "extendDrivesWindows";
+}

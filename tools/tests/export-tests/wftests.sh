e03954e9b88e2d31cba6b65d882b2b9e36f27187
Bill Elliott IV <bill.elliott@emc.com>
2017-04-25 23:01:59 +0000

    rename failure point to match what its doing.  remove failure_004 from case that doesn't do rollback

--- tools/tests/export-tests/wftests.sh
+++ tools/tests/export-tests/wftests.sh
@@ -1651,7 +1651,7 @@ test_4() {
     storage_failure_injections=""
     if [ "${SS}" = "vplex" ]
     then
-	storage_failure_injections="failure_004:failure_074_VPlexDeviceController.deleteStorageDevice_before_delete"
+	storage_failure_injections="failure_004:failure_074_VPlexDeviceController.deleteStorageView_before_delete"
     fi 
 
     if [ "${SS}" = "vnx" ]
@@ -1673,7 +1673,7 @@ test_4() {
     failure_injections="${common_failure_injections} ${storage_failure_injections} ${network_failure_injections}"
 
     # Placeholder when a specific failure case is being worked...
-    #failure_injections="failure_004:failure_074_VPlexDeviceController.deleteStorageDevice_before_delete"
+    #failure_injections="failure_004:failure_074_VPlexDeviceController.deleteStorageView_before_delete"
 
     for failure in ${failure_injections}
     do
@@ -1755,7 +1755,7 @@ test_5() {
     storage_failure_injections=""
     if [ "${SS}" = "vplex" ]
     then
-	storage_failure_injections="failure_004:failure_074_VPlexDeviceController.deleteStorageDevice_before_delete"
+	storage_failure_injections="failure_074_VPlexDeviceController.deleteStorageView_before_delete"
     fi
 
     if [ "${SS}" = "vmax2" ]

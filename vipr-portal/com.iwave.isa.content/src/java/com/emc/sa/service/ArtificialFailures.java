77c636d70a6694cc48ecc044e418b48dd314dbd5
Ian Bibby <ian.bibby@emc.com>
2017-04-28 05:04:53 +0000

    COP-29238 Move catalog failure string to ArtificialFailures

--- vipr-portal/com.iwave.isa.content/src/java/com/emc/sa/service/ArtificialFailures.java
+++ vipr-portal/com.iwave.isa.content/src/java/com/emc/sa/service/ArtificialFailures.java
@@ -8,4 +8,5 @@ package com.emc.sa.service;
 public class ArtificialFailures {
 	public static final String ARTIFICIAL_FAILURE_LINUX_MOUNT_VOLUME = "mountLinuxVolume";
 	public static final String ARTIFICIAL_FAILURE_WINDOWS_EXTEND_DRIVE = "extendDrivesWindows";
+	public static final String ARTIFICIAL_FAILURE_VMWARE_EXTEND_DATASTORE = "extend_vmfs_datastore";
 }

fb9c26cab24c967377b774534942bbb46792a8e9
Hemanth <hemanth.patil@emc.com>
2017-05-03 18:59:23 +0530

    Change for PrimitiveResource AsssetOptions

--- vipr-portal/vipr-client/src/java/com/emc/vipr/client/core/PrimitiveResources.java
+++ vipr-portal/vipr-client/src/java/com/emc/vipr/client/core/PrimitiveResources.java
@@ -4,14 +4,13 @@
  */
 package com.emc.vipr.client.core;
 
+import static com.emc.vipr.client.core.util.ResourceUtils.defaultList;
+
 import java.net.URI;
 import java.util.List;
 import java.util.Properties;
 
-import com.emc.storageos.model.BulkIdParam;
 import com.emc.storageos.model.NamedRelatedResourceRep;
-import com.emc.storageos.model.auth.ACLAssignmentChanges;
-import com.emc.storageos.model.auth.ACLEntry;
 import com.emc.storageos.model.customservices.CustomServicesPrimitiveResourceList;
 import com.emc.storageos.model.customservices.CustomServicesPrimitiveResourceRestRep;
 import com.emc.vipr.client.ViPRCoreClient;
@@ -20,13 +19,18 @@ import com.emc.vipr.client.core.impl.PathConstants;
 import com.emc.vipr.client.core.util.ResourceUtils;
 import com.emc.vipr.client.impl.RestClient;
 
-public class PrimitiveResources extends AbstractCoreBulkResources<CustomServicesPrimitiveResourceRestRep> implements
-        TopLevelResources<CustomServicesPrimitiveResourceRestRep>, ACLResources {
-	
+/**
+ * CustomServicePrimitiveResource resources.
+ * <p>
+ * Base URL: <tt>/primitive/resources</tt>
+ */
+public class PrimitiveResources extends AbstractCoreResources<CustomServicesPrimitiveResourceRestRep>  implements
+	TopLevelResources<CustomServicesPrimitiveResourceRestRep> {
+
 	private static final String ANSIBLE_PACKAGE_RESOURCE_TYPE = "ANSIBLE";
 	private static final String INVENTORY_FILE_RESOURCE_TYPE = "ANSIBLE_inventory";
 	
-	public PrimitiveResources(ViPRCoreClient parent, RestClient client) {
+    public PrimitiveResources(ViPRCoreClient parent, RestClient client) {
         super(parent, client, CustomServicesPrimitiveResourceRestRep.class, PathConstants.PRIMITIVE_RESOURCE_GET_URL);
     }
 
@@ -40,65 +44,35 @@ public class PrimitiveResources extends AbstractCoreBulkResources<CustomServices
         return (PrimitiveResources) super.withInternal(internal);
     }
 
-    @Override
-    protected List<CustomServicesPrimitiveResourceRestRep> getBulkResources(BulkIdParam input) {
-        return null;
-    }
-
-    /**
-     * Gets the list of all inventory files. This is a convenience method for: <tt>getByRefs(list())</tt>.
-     * 
-     * @return the list of inventory files.
-     */
-    @Override
-    public List<CustomServicesPrimitiveResourceRestRep> getAll() {
-        return getAll(null);
-    }
-
-    /**
-     * Gets the list of all inventory files, optionally filtering the results. This is a convenience method for:
-     * <tt>getByRefs(list(), filter)</tt>.
-     * 
-     * @param filter
-     *            the resource filter to apply to the results as they are returned (optional).
-     * @return the list of inventory files.
-     */
 	@Override
-	public List<CustomServicesPrimitiveResourceRestRep> getAll(ResourceFilter<CustomServicesPrimitiveResourceRestRep> filter) {
-		return getByRefs(list(), filter);
+	public List<? extends NamedRelatedResourceRep> list() {
+    	Properties prop = new Properties();
+    	prop.setProperty("type", ANSIBLE_PACKAGE_RESOURCE_TYPE);
+		CustomServicesPrimitiveResourceList response = client.get(CustomServicesPrimitiveResourceList.class, baseUrl, prop);
+        return ResourceUtils.defaultList(response.getResources());
 	}
 
-    /**
-     * Lists all primitive resource ansible packages.
-     * <p>
-     * API Call: <tt>GET /primitives/resource?type="ANSIBLE"</tt>
-     * 
-     * @return the list of inventory files references.
-     */
-    @Override
-    public List<NamedRelatedResourceRep> list() {
-    	CustomServicesPrimitiveResourceList response = client.get(CustomServicesPrimitiveResourceList.class, baseUrl, ANSIBLE_PACKAGE_RESOURCE_TYPE);
-        return ResourceUtils.defaultList(response.getResources());
-    }
+	@Override
+	public List<CustomServicesPrimitiveResourceRestRep> getAll() {
+		return getAll(null);
+	}
 
     @Override
-    public List<ACLEntry> getACLs(URI id) {
-        return doGetACLs(id);
+    public List<CustomServicesPrimitiveResourceRestRep> getAll(ResourceFilter<CustomServicesPrimitiveResourceRestRep> filter) {
+        List<NamedRelatedResourceRep> refs = listAnsiblePackages();
+        return getByRefs(refs);
     }
-
-    @Override
-    public List<ACLEntry> updateACLs(URI id, ACLAssignmentChanges aclChanges) {
-        return doUpdateACLs(id, aclChanges);
-    }
-
+	
     /**
      * Get all primitive resource ansible packages
      * 
      * @param filter
+     * @brief Get all primitive resource ansible packages
      * @return
      */
     public List<CustomServicesPrimitiveResourceRestRep> getAnsiblePackages(ResourceFilter<CustomServicesPrimitiveResourceRestRep> filter) {
-    	return getByRefs(listAnsiblePackages(), filter);
+        List<NamedRelatedResourceRep> refs = listAnsiblePackages();
+        return getByRefs(refs);
     }
     
     public List<NamedRelatedResourceRep> listAnsiblePackages() {
@@ -106,30 +80,33 @@ public class PrimitiveResources extends AbstractCoreBulkResources<CustomServices
     	prop.setProperty("type", ANSIBLE_PACKAGE_RESOURCE_TYPE);
     	CustomServicesPrimitiveResourceList response = client.get(CustomServicesPrimitiveResourceList.class, baseUrl, prop);
         return ResourceUtils.defaultList(response.getResources());
-    }
-    	
+    }	
+    
     /**
-     * Get all primitive resource inventory files.
+     * Get all primitive resource inventory files
      * 
      * @param filter
+     * @brief Get all primitive resource inventory files
      * @return
      */
     public List<CustomServicesPrimitiveResourceRestRep> getInventoryFiles(ResourceFilter<CustomServicesPrimitiveResourceRestRep> filter) {
-    	return getByRefs(listInventoryFiles(), filter);
+        List<NamedRelatedResourceRep> refs = listInventoryFiles();
+        return super.getByRefs(refs);
     }
-    
+
     public List<NamedRelatedResourceRep> listInventoryFiles() {
     	Properties prop = new Properties();
     	prop.setProperty("type", INVENTORY_FILE_RESOURCE_TYPE);
     	CustomServicesPrimitiveResourceList response = client.get(CustomServicesPrimitiveResourceList.class, baseUrl, prop);
-        return ResourceUtils.defaultList(response.getResources());
+        return defaultList(response.getResources());
     }
-
+    
     /**
      * Get all primitive resource inventory files by ansible package
      * 
      * @param filter
      * @param ansiblePackageId
+     * @brief Get all primitive resource inventory files by ansible package
      * @return
      */
     public List<CustomServicesPrimitiveResourceRestRep> getInventoryFilesByAnsiblePackage(ResourceFilter<CustomServicesPrimitiveResourceRestRep> filter, URI ansiblePackageId) {
@@ -143,5 +120,5 @@ public class PrimitiveResources extends AbstractCoreBulkResources<CustomServices
     	CustomServicesPrimitiveResourceList response = client.get(CustomServicesPrimitiveResourceList.class, baseUrl, prop);
         return ResourceUtils.defaultList(response.getResources());
     }
-
+    
 }

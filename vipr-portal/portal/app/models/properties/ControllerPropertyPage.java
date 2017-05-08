02cfee768800006123c1588d9a6f15bbd046de44
Santhosh <Santhosh.Lakshmanan@emc.com>
2017-05-08 12:23:41 -0400

    COP-30255 Make persisting metering Stat objects into DB  a configurable property

--- vipr-portal/portal/app/models/properties/ControllerPropertyPage.java
+++ vipr-portal/portal/app/models/properties/ControllerPropertyPage.java
@@ -9,6 +9,7 @@ import java.util.Map;
 public class ControllerPropertyPage extends CustomPropertyPage {
     private Property meteringEnabled;
     private Property meteringInterval;
+    private Property persistMeteringStats;
     private Property monitoringEnabled;
     private Property mdsCloneZoneset;
     private Property mdsAllowZonesetCommit;
@@ -18,6 +19,7 @@ public class ControllerPropertyPage extends CustomPropertyPage {
         setRenderTemplate("controllerPage.html");
         meteringEnabled = addCustomProperty(properties, "controller_enable_metering");
         meteringInterval = addCustomProperty(properties, "controller_metering_interval");
+        persistMeteringStats = addCustomProperty(properties, "controller_metering_persist_stat_records");
         monitoringEnabled = addCustomProperty(properties, "controller_enable_monitoring");
         mdsCloneZoneset = addCustomProperty(properties, "controller_mds_clone_zoneset");
         mdsAllowZonesetCommit = addCustomProperty(properties, "controller_mds_allow_zoneset_commit");
@@ -30,6 +32,10 @@ public class ControllerPropertyPage extends CustomPropertyPage {
 
     public Property getMeteringInterval() {
         return meteringInterval;
+    }
+
+    public Property getPersistMeteringStats() {
+        return persistMeteringStats;
     }
 
     public Property getMonitoringEnabled() {

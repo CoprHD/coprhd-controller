effd9ec19f54ba5473e2673aca9b2b19db87eae8
bonduj <joseph.bondu@emc.com>
2017-05-10 09:01:02 +0530

    fixed cli command for reduce quota

--- cli/src/fileshare.py
+++ cli/src/fileshare.py
@@ -83,7 +83,7 @@ class Fileshare(object):
 
     isTimeout = False
     timeout = 300
-    gb_to_byte = 1073741824
+    
 
     def __init__(self, ipAddr, port):
         '''
@@ -912,7 +912,7 @@ class Fileshare(object):
         fileshare_detail = self.show(name)
         current_size = float(fileshare_detail["capacity_gb"])
 
-        if( (new_size * gb_to_byte) >= (current_size * gb_to_byte) ):
+        if(new_size >= current_size ):
             raise SOSError(
                 SOSError.VALUE_ERR,
                 "error: Incorrect value of new size: " + str(new_size) +

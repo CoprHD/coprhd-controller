bdaeca38881a2598396fd05828636ee73ee08e13
Tejaswini <tejaswini.krishnamurthy@dell.com>
2017-05-10 17:26:16 +0000

    Add more failure scenarios for vplex change vpool test - test 11

--- tools/tests/export-tests/wftests.sh
+++ tools/tests/export-tests/wftests.sh
@@ -2462,7 +2462,8 @@ test_11() {
     storage_failure_injections=""
     if [ "${SS}" = "vplex" ]
     then
-	storage_failure_injections=""
+	storage_failure_injections="failure_003_late_in_add_initiator_to_mask \
+                                    failure_082_VPlexDeviceController_late_in_add_targets_to_view"
     fi
 
     if [ "${SS}" = "unity" -o "${SS}" = "xio" ]
@@ -2478,7 +2479,7 @@ test_11() {
     failure_injections="${common_failure_injections} ${storage_failure_injections}"
 
     # Placeholder when a specific failure case is being worked...
-    # failure_injections="failure_004_final_step_in_workflow_complete"
+    # failure_injections="failure_082_VPlexDeviceController_late_in_add_targets_to_view"
 
     for failure in ${failure_injections}
     do

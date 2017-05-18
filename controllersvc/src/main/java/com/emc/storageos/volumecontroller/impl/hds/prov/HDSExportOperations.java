f3964031c4e0ae9413d208eb01c34e4717497e98
Selvamanickam Anbalagan <selvamanickam.anbalagan@emc.com>
2017-05-18 17:09:52 -0400

    Execute add lun path only if the volume is not part of hsd.

--- controllersvc/src/main/java/com/emc/storageos/volumecontroller/impl/hds/prov/HDSExportOperations.java
+++ controllersvc/src/main/java/com/emc/storageos/volumecontroller/impl/hds/prov/HDSExportOperations.java
@@ -936,28 +936,29 @@ public class HDSExportOperations implements ExportMaskOperations {
                         }
                     }
                 }
-
-                List<Path> pathResponseList = hdsApiClient
-                        .getHDSBatchApiExportManager().addLUNPathsToHSDs(
-                                systemObjectID, pathList, storage.getModel());
-                if (null != pathResponseList && !pathResponseList.isEmpty()) {
-                    // update volume-lun relationship to exportmask.
-                    updateVolumeHLUInfo(volumeURIHLUs, pathResponseList,
-                            exportMask);
-                    dbClient.updateObject(exportMask);
-                } else {
-                    log.error(
-                            String.format("addVolumes failed - maskURI: %s",
-                                    exportMaskURI.toString()),
-                            new Exception(
-                                    "Not able to parse the response of addLUN from server"));
-                    ServiceError serviceError = DeviceControllerException.errors
-                            .jobFailedOpMsg(
-                                    ResourceOperationTypeEnum.ADD_EXPORT_VOLUME
-                                            .getName(),
-                                    "Not able to parse the response of addLUN from server");
-                    taskCompleter.error(dbClient, serviceError);
-                    return;
+                if (!pathList.isEmpty()) {
+                    List<Path> pathResponseList = hdsApiClient
+                            .getHDSBatchApiExportManager().addLUNPathsToHSDs(
+                                    systemObjectID, pathList, storage.getModel());
+                    if (null != pathResponseList && !pathResponseList.isEmpty()) {
+                        // update volume-lun relationship to exportmask.
+                        updateVolumeHLUInfo(volumeURIHLUs, pathResponseList,
+                                exportMask);
+                        dbClient.updateObject(exportMask);
+                    } else {
+                        log.error(
+                                String.format("addVolumes failed - maskURI: %s",
+                                        exportMaskURI.toString()),
+                                new Exception(
+                                        "Not able to parse the response of addLUN from server"));
+                        ServiceError serviceError = DeviceControllerException.errors
+                                .jobFailedOpMsg(
+                                        ResourceOperationTypeEnum.ADD_EXPORT_VOLUME
+                                                .getName(),
+                                        "Not able to parse the response of addLUN from server");
+                        taskCompleter.error(dbClient, serviceError);
+                        return;
+                    }
                 }
                 taskCompleter.ready(dbClient);
             }

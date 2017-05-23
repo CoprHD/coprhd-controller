7890f8586b786b64bd3cc69ba119e7cd4eedd78d
Yao <Yuan.Yao@emc.com>
2017-05-23 11:00:26 +0800

    init check in for migration handler

---
+++ dbclient/src/main/java/com/emc/storageos/db/client/upgrade/callbacks/StatsCleanupMigration.java
@@ -0,0 +1,40 @@
+package com.emc.storageos.db.client.upgrade.callbacks;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.emc.storageos.db.client.impl.DbClientImpl;
+import com.emc.storageos.db.client.impl.TimeSeriesType;
+import com.emc.storageos.db.client.impl.TypeMap;
+import com.emc.storageos.db.client.model.Stat;
+import com.emc.storageos.db.client.model.StatTimeSeries;
+import com.emc.storageos.db.client.upgrade.BaseCustomMigrationCallback;
+import com.emc.storageos.svcs.errorhandling.resources.MigrationCallbackException;
+import com.netflix.astyanax.model.ConsistencyLevel;
+
+public class StatsCleanupMigration extends BaseCustomMigrationCallback {
+	private static final Logger log = LoggerFactory.getLogger(StatsCleanupMigration.class);
+	
+	@Override
+	public void process() throws MigrationCallbackException {
+		log.info("begin to cleanup stats CF and change it compaction strategy");
+		
+		DbClientImpl dbClient = (DbClientImpl)getDbClient();
+		TimeSeriesType<Stat> doType = TypeMap.getTimeSeriesType(StatTimeSeries.class);
+		
+		try {
+			dbClient.getLocalContext()
+			        .getKeyspace().prepareQuery(doType.getCf()).setConsistencyLevel(ConsistencyLevel.CL_ALL)
+			        .withCql(String.format("TRUNCATE TABLE %s", doType.getCf().getName()))
+			        .execute();
+			dbClient.getLocalContext()
+					.getKeyspace().prepareQuery(doType.getCf())
+			        .withCql(String.format("ALTER TABLE %s WITH compaction = {'class': 'SizeTieredCompactionStrategy'}", doType.getCf().getName()))
+			        .execute();
+		} catch (Exception e) {
+			log.error("Failed to cleanup stats CF {}", e);
+			throw new MigrationCallbackException("Failed to cleanup stats CF", e);
+		}
+	}
+
+}

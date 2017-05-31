d85872cc48db17bfe9449399f4e978a1d6905979
Nathan Beach <nathan.beach@emc.com>
2017-05-31 10:44:26 -0500

    fixing small typo

--- exportLibraries/vplex/src/main/java/com/emc/storageos/vplex/api/VPlexApiClient.java
+++ exportLibraries/vplex/src/main/java/com/emc/storageos/vplex/api/VPlexApiClient.java
@@ -1709,7 +1709,7 @@ public class VPlexApiClient {
             vvinfo = findVirtualVolumeAndUpdateInfo(virtualVolumeName);
         }
 
-        s_logger.info("returning virtual volume: ");
+        s_logger.info("returning virtual volume: " + vvinfo);
         return vvinfo;
     }
 

9f4c8d104dfd94b6b196d1377ae46c364954743a
Ian Bibby <ian.bibby@emc.com>
2017-06-07 15:57:49 +0000

    COP-30898 Remove obsolete failure scenario for VMAX3 test_13

--- tools/tests/export-tests/wftests.sh
+++ tools/tests/export-tests/wftests.sh
@@ -3213,8 +3213,7 @@ test_13() {
 
     if [ "${SS}" = "vnx" -o "${SS}" = "vmax2" -o "${SS}" = "vmax3" ]
     then
-	storage_failure_injections="failure_015_SmisCommandHelper.invokeMethod_* \
-                                    failure_018_Export_doRollbackExportCreate_before_delete"
+	storage_failure_injections="failure_015_SmisCommandHelper.invokeMethod_*"
     fi
 
     failure_injections="${common_failure_injections} ${storage_failure_injections}"

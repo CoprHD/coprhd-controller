5794ec7edab0a8579f5d8c1a93ac340a7214674e
Bill Elliott IV <bill.elliott@emc.com>
2017-06-09 09:22:34 -0400

    Serialize the steps totally, don't rely on step groups.

--- controllersvc/src/main/java/com/emc/storageos/volumecontroller/impl/block/XtremIOMaskingOrchestrator.java
+++ controllersvc/src/main/java/com/emc/storageos/volumecontroller/impl/block/XtremIOMaskingOrchestrator.java
@@ -337,9 +337,7 @@ public class XtremIOMaskingOrchestrator extends AbstractBasicMaskingOrchestrator
                 }
                 // volumes
                 List<URI> initiators = StringSetUtil.stringSetToUriList(exportGroup.getInitiators());
-                generateExportGroupRemoveVolumesCleanup(workflow,
-                        // If we've generated any steps, use this step group name.
-                        previousStep != null ? EXPORT_GROUP_MASKING_TASK : null, 
+                generateExportGroupRemoveVolumesCleanup(workflow, previousStep,
                         storage, exportGroup, volumes, initiators);
 
                 String successMessage = String.format(

7709476712db1f64436e093a37082ece076f5e9d
Priya Periaswamy <priya.periaswamy@emc.com>
2017-06-13 11:42:09 -0400

    Changes for bug fix 30798 and 31091

--- vipr-portal/com.iwave.isa.content/src/java/com/emc/sa/service/vipr/customservices/CustomServicesService.java
+++ vipr-portal/com.iwave.isa.content/src/java/com/emc/sa/service/vipr/customservices/CustomServicesService.java
@@ -306,9 +306,9 @@ public class CustomServicesService extends ViPRService {
         for (final CustomServicesWorkflowDocument.InputGroup inputGroup : step.getInputGroups().values()) {
             for (final Input value : inputGroup.getInputGroup()) {
                 final String name = value.getName();
-                final String friendlyName = value.getFriendlyName();
+                String friendlyName = value.getFriendlyName();
                 if(friendlyName != null){
-                    friendlyName.replaceAll(CustomServicesConstants.SPACES_REGEX,StringUtils.EMPTY);
+                    friendlyName = friendlyName.replaceAll(CustomServicesConstants.SPACES_REGEX,StringUtils.EMPTY);
                 }
 
                 if (StringUtils.isEmpty(value.getType())) {

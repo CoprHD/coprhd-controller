c9d02f667389ec835d594c8ba49af53172491a1d
lakhiv <vasudevu.lakhinana@emc.com>
2017-06-20 19:22:39 +0530

    Modified the error and fixed cli command parser error

--- vipr-portal/com.iwave.isa.content/src/java/com/emc/sa/service/vmware/file/DeleteNfsDatastoreAndExportService.java
+++ vipr-portal/com.iwave.isa.content/src/java/com/emc/sa/service/vmware/file/DeleteNfsDatastoreAndExportService.java
@@ -12,10 +12,10 @@ public class DeleteNfsDatastoreAndExportService extends DeleteNfsDatastoreServic
     public void execute() throws Exception {
         StringBuffer errorMsg = new StringBuffer();
         errorMsg.append("This operation is currently not supported. Please do the below steps:\r\n")
-                .append("1. Remove all Virtual Machines and data from data store using Vmware vCenter\r\n")
-                .append("2. Use ViPR Controller Catalog Services \"File Services for VMware vCenter->Delete VMware NFS Datastore\", \r\n")
-                .append("   \"File Storage Services ->Remove NFS Export for File System\" \r\n")
-                .append(" and \"File Storage Services ->Remove File System\"");
+                .append("1. Remove all Virtual Machines and data from Datastore using VMware vCenter\r\n")
+                .append("2. Use ViPR Controller Catalog Services \"File Services for VMware vCenter->Delete VMware NFS Datastore\", ")
+                .append("\"File Storage Services ->Remove NFS Export for File System\" ")
+                .append("and \"File Storage Services ->Remove File System\"");
         Exception deleteDataStoreNotSupported = new Exception(errorMsg.toString());
         throw deleteDataStoreNotSupported;
         // For this patch commenting this code

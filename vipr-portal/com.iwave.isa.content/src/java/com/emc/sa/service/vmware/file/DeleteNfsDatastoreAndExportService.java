f38f13567ae7ae1d56358058545a740db2887b93
lakhiv <vasudevu.lakhinana@emc.com>
2017-06-20 17:32:41 +0530

    Added unsupported exception while deleting datastore

--- vipr-portal/com.iwave.isa.content/src/java/com/emc/sa/service/vmware/file/DeleteNfsDatastoreAndExportService.java
+++ vipr-portal/com.iwave.isa.content/src/java/com/emc/sa/service/vmware/file/DeleteNfsDatastoreAndExportService.java
@@ -5,14 +5,22 @@
 package com.emc.sa.service.vmware.file;
 
 import com.emc.sa.engine.service.Service;
-import com.emc.sa.service.vipr.file.FileStorageUtils;
-import com.emc.storageos.volumecontroller.FileControllerConstants;
 
 @Service("VMware-DeleteNfsDatastoreAndExport")
 public class DeleteNfsDatastoreAndExportService extends DeleteNfsDatastoreService {
     @Override
     public void execute() throws Exception {
-        super.execute();
-        FileStorageUtils.deleteFileSystem(fileSystem.getId(), FileControllerConstants.DeleteTypeEnum.FULL);
+        StringBuffer errorMsg = new StringBuffer();
+        errorMsg.append("This operation is currently not supported. Please do the below steps:\r\n")
+                .append("1. Remove all Virtual Machines and data from data store using Vmware vCenter\r\n")
+                .append("2. Use ViPR Controller Catalog Services \"File Services for VMware vCenter->Delete VMware NFS Datastore\", \r\n")
+                .append("   \"File Storage Services ->Remove NFS Export for File System\" \r\n")
+                .append(" and \"File Storage Services ->Remove File System\"");
+        Exception deleteDataStoreNotSupported = new Exception(errorMsg.toString());
+        throw deleteDataStoreNotSupported;
+        // For this patch commenting this code
+        // COP-31252
+        // super.execute();
+        // FileStorageUtils.deleteFileSystem(fileSystem.getId(), FileControllerConstants.DeleteTypeEnum.FULL);
     }
 }

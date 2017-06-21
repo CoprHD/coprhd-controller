b58b3e1f7e9e6a3fb0ecb8de4b15dfdecfd335a3
Simon Wang <simon.wang@gmail.com>
2017-06-21 14:30:00 -0400

    remove useless import

--- authsvc/src/main/java/com/emc/storageos/auth/service/impl/resource/AuthenticationResource.java
+++ authsvc/src/main/java/com/emc/storageos/auth/service/impl/resource/AuthenticationResource.java
@@ -25,7 +25,6 @@ import javax.ws.rs.core.Response.Status;
 import javax.xml.bind.annotation.XmlRootElement;
 
 import com.emc.storageos.model.password.PasswordChangeParam;
-import com.emc.storageos.security.helpers.SecurityUtil;
 import com.emc.storageos.security.password.Password;
 import com.emc.storageos.security.password.PasswordUtils;
 import com.emc.storageos.security.password.PasswordValidator;

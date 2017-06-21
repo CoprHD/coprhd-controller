985e7cc8482a5e381bae083d04157fc03baf122b
bonduj <joseph.bondu@emc.com>
2017-06-21 16:44:41 +0530

    updated quota file for netapp

--- controllersvc/src/main/java/com/emc/storageos/volumecontroller/impl/netappc/NetAppClusterModeDevice.java
+++ controllersvc/src/main/java/com/emc/storageos/volumecontroller/impl/netappc/NetAppClusterModeDevice.java
@@ -40,6 +40,7 @@ import com.emc.storageos.svcs.errorhandling.model.ServiceError;
 import com.emc.storageos.util.FileSystemConstants;
 import com.emc.storageos.volumecontroller.ControllerException;
 import com.emc.storageos.volumecontroller.FileDeviceInputOutput;
+import com.emc.storageos.volumecontroller.FileShareQuotaDirectory;
 import com.emc.storageos.volumecontroller.impl.BiosCommandResult;
 import com.emc.storageos.volumecontroller.impl.NativeGUIDGenerator;
 import com.emc.storageos.volumecontroller.impl.file.AbstractFileStorageDevice;
@@ -1118,7 +1119,7 @@ public class NetAppClusterModeDevice extends AbstractFileStorageDevice {
 
     @Override
     public BiosCommandResult doUpdateQuotaDirectory(StorageSystem storage,
-            FileDeviceInputOutput args, QuotaDirectory qtree)
+            FileDeviceInputOutput args, FileShareQuotaDirectory qtree)
                     throws ControllerException {
         BiosCommandResult result = new BiosCommandResult();
 

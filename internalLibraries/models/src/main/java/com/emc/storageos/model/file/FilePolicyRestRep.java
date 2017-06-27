269b3e4aa16250fb63430ab200178c6e88dbd704
lakhiv <vasudevu.lakhinana@emc.com>
2017-06-27 15:52:56 +0530

    Renamed the xml tag names for schedule policy

--- internalLibraries/models/src/main/java/com/emc/storageos/model/file/FilePolicyRestRep.java
+++ internalLibraries/models/src/main/java/com/emc/storageos/model/file/FilePolicyRestRep.java
@@ -6,6 +6,7 @@ package com.emc.storageos.model.file;
 
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
 
 import com.emc.storageos.model.schedulepolicy.SchedulePolicyRestRep;
 
@@ -17,6 +18,7 @@ import com.emc.storageos.model.schedulepolicy.SchedulePolicyRestRep;
  */
 
 @XmlRootElement(name = "file_snapshot_schedule_policy")
+@XmlType(name = "file_snapshot_schedule_policy")
 public class FilePolicyRestRep extends SchedulePolicyRestRep {
 
     // Type of the policy

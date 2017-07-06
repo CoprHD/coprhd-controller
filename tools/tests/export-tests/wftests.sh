d7b9421a64c75df31e6d9aa4b931bc06c3bd429a
Ian Bibby <ian.bibby@emc.com>
2017-07-06 19:41:04 +0000

    Fix/update wftests usage output to include test names (and others)

--- tools/tests/export-tests/wftests.sh
+++ tools/tests/export-tests/wftests.sh
@@ -33,16 +33,25 @@ source $(dirname $0)/common_subs.sh
 
 Usage()
 {
-    echo 'Usage: wftests.sh <sanity conf file path> [vmax2 | vmax3 | vnx | vplex [local | distributed] | xio | unity | vblock | srdf [sync | async]] [-setup(hw) | -setupsim] [-report] [-cleanup] [-resetsim]  [test1 test2 ...]'
-    echo ' (vmax 2 | vmax3 ...: Storage platform to run on.'
+    echo 'Usage: wftests.sh <sanity conf file path> [vmax2 | vmax3 | vnx | vplex [local | distributed] | xio | unity | vblock | srdf [sync | async]] [-setup(hw) | -setupsim] [-report] [-cleanup] [-resetsim]  [test_1 test_2 ...]'
+    echo ' (vmax2 | vmax3 ...: Storage platform to run on.'
     echo ' [-setup(hw) | setupsim]: Run on a new ViPR database, creates SMIS, host, initiators, vpools, varray, volumes (Required to run first, can be used with tests'
     echo ' [-report]: Report results to reporting server: http://lglw1046.lss.emc.com:8081/index.html (Optional)'
     echo ' [-cleanup]: Clean up the pre-created volumes and exports associated with -setup operation (Optional)'
     echo ' [-resetsim]: Resets the simulator as part of setup (Optional)'
     echo ' test names: Space-delimited list of tests to run.  Use + to start at a specific test.  (Optional, default will run all tests in suite)'
+    print_test_names
     echo ' Example:  ./wftests.sh sanity.conf vmax3 -setupsim -report -cleanup test_7+'
     echo '           Will start from clean DB, report results to reporting server, clean-up when done, and start on test_7 (and run all tests after test_7'
     exit 2
+}
+
+# Print functions in this file that start with test_
+print_test_names() {
+    for test_name in `grep -E "^test_.+\(\)" $0 | sed -E "s/\(.*$//"`
+    do
+      echo "  $test_name"
+    done
 }
 
 cd $(dirname $0)

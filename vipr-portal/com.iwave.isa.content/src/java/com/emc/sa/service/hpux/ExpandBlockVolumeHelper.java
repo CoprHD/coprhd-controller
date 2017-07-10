3d2143dd276cb2f627388ce42f6c030bfc6d9ed6
Trevor Dawe <trevor.dawe@emc.com>
2017-07-10 11:42:44 -0300

    COP-29983: Skip volume expand if already expanded

--- vipr-portal/com.iwave.isa.content/src/java/com/emc/sa/service/hpux/ExpandBlockVolumeHelper.java
+++ vipr-portal/com.iwave.isa.content/src/java/com/emc/sa/service/hpux/ExpandBlockVolumeHelper.java
@@ -5,6 +5,7 @@
 package com.emc.sa.service.hpux;
 
 import static com.emc.sa.service.vipr.ViPRExecutionUtils.logInfo;
+import static com.emc.sa.service.vipr.ViPRExecutionUtils.logWarn;
 
 import java.util.List;
 
@@ -15,6 +16,7 @@ import com.emc.sa.engine.ExecutionUtils;
 import com.emc.sa.engine.bind.BindingUtils;
 import com.emc.sa.service.ArtificialFailures;
 import com.emc.sa.service.vipr.ViPRService;
+import com.emc.sa.service.vipr.block.BlockStorageUtils;
 import com.emc.storageos.db.client.model.Initiator;
 import com.emc.storageos.model.block.BlockObjectRestRep;
 
@@ -48,9 +50,15 @@ public class ExpandBlockVolumeHelper {
         hpuxSupport.removeVolumeMountPointTag(volume);
         ViPRService.artificialFailure(ArtificialFailures.ARTIFICIAL_FAILURE_HPUX_EXPAND_VOLUME_AFTER_REMOVE_TAG);
 
-        logInfo("expand.block.volume.resize.volume", volume.getName(), newSizeInGB.toString());
-        hpuxSupport.resizeVolume(volume, newSizeInGB);
-        ViPRService.artificialFailure(ArtificialFailures.ARTIFICIAL_FAILURE_HPUX_EXPAND_VOLUME_AFTER_VOLUME_RESIZE);
+        // Skip the expand if the current volume capacity is larger than the requested expand size
+        if (BlockStorageUtils.isVolumeExpanded(volume, newSizeInGB)) {
+            logWarn("hpux.expand.skip", volume.getId(), BlockStorageUtils.getCapacity(volume));
+        } else {
+            logInfo("expand.block.volume.resize.volume", volume.getName(), newSizeInGB.toString());
+            hpuxSupport.resizeVolume(volume, newSizeInGB);
+            ViPRService.artificialFailure(ArtificialFailures.ARTIFICIAL_FAILURE_HPUX_EXPAND_VOLUME_AFTER_VOLUME_RESIZE);
+        }
+
         hpuxSupport.rescan();
 
         logInfo("expand.block.volume.remounting", hpuxSupport.getHostName(), mountPoint.getPath());

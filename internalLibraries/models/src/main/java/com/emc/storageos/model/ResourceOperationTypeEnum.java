8e4cea9d9ff4a1c26b0d3e7b23497a7f810d3c0b
sanjes <sanjusha.sanjeev@emc.com>
2017-07-20 16:56:47 +0530

    fixing build error

--- internalLibraries/models/src/main/java/com/emc/storageos/model/ResourceOperationTypeEnum.java
+++ internalLibraries/models/src/main/java/com/emc/storageos/model/ResourceOperationTypeEnum.java
@@ -364,7 +364,6 @@ public enum ResourceOperationTypeEnum {
     ASSIGN_FILE_POLICY("ASSIGN FILE POLICY", "assign file policy to resource"),
     UPDATE_FILE_PROTECTION_POLICY("UPDATE FILE PROTECTION POLICY", "Update file protection policy"),
     GET_EXISTING_FILE_SYSTEM_POLICY("GET EXISTING FILE POLICY ", "get existing file policy"),
-    ASSIGN_FILE_POLICY_TO_FILE_SYSTEM("ASSIGN FILE POLICY TO FILE SYSTEM", "assign file policy to existing file system");
     ASSIGN_FILE_POLICY_TO_FILE_SYSTEM("ASSIGN FILE POLICY TO FILE SYSTEM", "assign file policy to existing file system"),
     CREATE_STORAGE_PORT_GROUP("CREATE STORAGE PORT GROUP", "create storage port group"),
     DELETE_STORAGE_PORT_GROUP("DELETE STORAGE PORT GROUP", "delete storage port group");

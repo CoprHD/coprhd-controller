318b4f9fada99853173ea08de315ffe91a021fe4
Maheshchandra churi <Maheshchandra.churi@emc.com>
2017-07-25 14:08:15 +0530

    Addressed review comments

--- vipr-portal/com.iwave.isa.content/src/java/com/emc/sa/service/vipr/compute/AddHostToClusterService.java
+++ vipr-portal/com.iwave.isa.content/src/java/com/emc/sa/service/vipr/compute/AddHostToClusterService.java
@@ -218,7 +218,7 @@ public class AddHostToClusterService extends ViPRService {
             preCheckErrors.append(
                     ExecutionUtils.getMessage("compute.cluster.service.profile.templates.null", cvp.getName()) + "  ");
         }
-        Pair<Boolean, String> status = new Pair<Boolean, String>();
+        Pair<Boolean, Map<URI, String>> status = new Pair<Boolean, Map<URI,String>>();
         if (ComputeUtils.checkComputeSystemsHaveImageServer(getClient(), cvp, status).getFirstElement().equals(false)) {
             preCheckErrors.append(ExecutionUtils.getMessage("compute.cluster.compute.system.image.server.null", status.getSecondElement()));
         }

73e6e42f0e903c8e9b3608b084e1284e29ff7c35
Kaihong Wang <kaihong.wang@emc.com>
2017-07-27 17:42:31 +0800

    Solution2 : 1. Add a new Rest api to query local backup info 2. Add params to bkutils 3. Changed script(not done yet)

---
+++ internalLibraries/backup/src/main/java/com/emc/storageos/management/backup/BackupDetail.java
@@ -0,0 +1,10 @@
+package com.emc.storageos.management.backup;
+
+/**
+ * Created by wangk14 on 2017/7/27.
+ */
+public class BackupFileValidation {
+    private boolean fileValid = false;
+    private boolean versionValid = false;
+    private boolean isGeo
+}

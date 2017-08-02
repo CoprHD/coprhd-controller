fda47bd25191646ccb5e453986571a8505847bd7
Guoliang Yang <guoliang.yang@emc.com>
2017-08-02 16:48:02 +0800

    vipr client support

--- vipr-portal/vipr-client/src/java/com/emc/vipr/client/system/Diagutil.java
+++ vipr-portal/vipr-client/src/java/com/emc/vipr/client/system/Diagutil.java
@@ -9,20 +9,29 @@ import com.emc.vipr.client.system.impl.PathConstants;
 import com.emc.vipr.model.sys.diagutil.DiagutilInfo;
 import com.emc.vipr.model.sys.diagutil.DiagutilParam;
 import com.sun.jersey.api.client.ClientResponse;
+import static com.emc.vipr.client.impl.jersey.ClientUtils.addQueryParam;
 
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.UriBuilder;
 import java.io.InputStream;
+import java.util.List;
 
 public class Diagutil {
     private RestClient client;
+    private static final String OPTIONS = "options";
+    private static final String NODE_ID = "node_id";
+    private static final String FILE_NAME = "file_name";
 
     public Diagutil(RestClient client) {
         this.client = client;
     }
 
-    public void collect(DiagutilParam param) {
-        client.post(String.class, param, PathConstants.DIAGUTIL_URL);
+    public void collect(List<String> options, DiagutilParam param) {
+        UriBuilder builder = client.uriBuilder(PathConstants.DIAGUTIL_URL);
+        if(options != null) {
+            addQueryParam(builder, OPTIONS, options);
+        }
+        client.postURI(String.class, param, builder.build());
     }
 
     public DiagutilInfo getStatus() {
@@ -31,17 +40,24 @@ public class Diagutil {
         return response;
     }
 
-    public InputStream getAsStream() {
-        ClientResponse response = client.getClient().resource(PathConstants.DIAGUTIL_URL).accept(MediaType.APPLICATION_XML)
+    public InputStream getAsStream(String nodeId, String fileName) {
+        UriBuilder builder = client.uriBuilder(PathConstants.DIAGUTIL_URL);
+        if(nodeId != null && (!nodeId.isEmpty())) {
+            addQueryParam(builder, NODE_ID, nodeId);
+        }
+        if(fileName != null && (!fileName.isEmpty())) {
+            addQueryParam(builder, FILE_NAME, fileName);
+        }
+        ClientResponse response = client.getClient().resource(PathConstants.DIAGUTIL_URL).accept(MediaType.APPLICATION_OCTET_STREAM)
                 .get(ClientResponse.class);
         return response.getEntityInputStream();
     }
 
-    public InputStream getAsText() {
+/*    public InputStream getAsText() {
         ClientResponse response = client.getClient().resource(PathConstants.DIAGUTIL_URL).accept(MediaType.TEXT_PLAIN)
                 .get(ClientResponse.class);
         return response.getEntityInputStream();
-    }
+    }*/
 
     public void cancel() {
         client.delete(String.class, PathConstants.DIAGUTIL_URL);

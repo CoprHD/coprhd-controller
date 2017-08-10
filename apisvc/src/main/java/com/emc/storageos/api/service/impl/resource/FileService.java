534eb721267f1c0a40d5ac8e60f20407135b6e6a
Francis <francis.nijay@emc.com>
2017-08-10 16:30:48 +0530

    removed !

--- apisvc/src/main/java/com/emc/storageos/api/service/impl/resource/FileService.java
+++ apisvc/src/main/java/com/emc/storageos/api/service/impl/resource/FileService.java
@@ -2143,8 +2143,8 @@ public class FileService extends TaskResourceService {
         }
 
 
-        if (!checkExportForDatastoreMount(fs, subDir, param)) {
-            _log.info("Filesystem's export has been mounted to NFS Datasource ", id);
+        if (checkExportForDatastoreMount(fs, subDir, param)) {
+            _log.info("Filesystem's export has been mounted to NFS Datasource %s", id);
             throw APIException.badRequests.unableToProcessRequest(
                     "Cannot perform Delete or Modify of NFS Export Rule as this has been mounted on NFS Datastore externally");
         }

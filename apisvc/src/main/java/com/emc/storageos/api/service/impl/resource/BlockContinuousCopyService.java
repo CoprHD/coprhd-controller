b4b3e35282f9d1c45699d36b794a457d486319c0
krisht <tejaswini.krishnamurthy@emc.com>
2017-08-11 10:42:59 -0700

    Added bulk api support for the block mirrors

--- apisvc/src/main/java/com/emc/storageos/api/service/impl/resource/BlockContinuousCopyService.java
+++ apisvc/src/main/java/com/emc/storageos/api/service/impl/resource/BlockContinuousCopyService.java
@@ -1,21 +1,41 @@
+/*
+ * Copyright (c) 2017 Dell EMC
+ * All Rights Reserved
+ */
+
 package com.emc.storageos.api.service.impl.resource;
 
 import static com.emc.storageos.api.mapper.BlockMapper.map;
 
 import java.net.URI;
+import java.util.Iterator;
+import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import com.emc.storageos.api.mapper.functions.MapBlockMirror;
+import com.emc.storageos.api.service.authorization.PermissionsHelper;
+import com.emc.storageos.api.service.impl.response.BulkList;
+import com.emc.storageos.api.service.impl.response.BulkList.PermissionsEnforcingResourceFilter;
+import com.emc.storageos.api.service.impl.response.BulkList.ResourceFilter;
 import com.emc.storageos.db.client.model.BlockMirror;
 import com.emc.storageos.db.client.model.BlockObject;
 import com.emc.storageos.db.client.model.DataObject;
+import com.emc.storageos.db.client.model.NamedURI;
 import com.emc.storageos.db.client.model.Project;
+import com.emc.storageos.model.BulkIdParam;
+import com.emc.storageos.model.BulkRestRep;
 import com.emc.storageos.model.ResourceTypeEnum;
+import com.emc.storageos.model.block.BlockMirrorBulkRep;
 import com.emc.storageos.model.block.BlockMirrorRestRep;
+import com.emc.storageos.model.block.BlockSnapshotSessionBulkRep;
+import com.emc.storageos.security.authentication.StorageOSUser;
 import com.emc.storageos.security.authorization.ACL;
 import com.emc.storageos.security.authorization.CheckPermission;
 import com.emc.storageos.security.authorization.DefaultPermissions;
@@ -67,5 +87,77 @@ public class BlockContinuousCopyService extends TaskResourceService {
         BlockMirror mirror = (BlockMirror) queryResource(id);
         return map(_dbClient, mirror);
     }
+    
+    /**
+     * Gets the details of the BlockSnapshotSession instances with the ids
+     * specified in the passed data.
+     * 
+     * @brief Get details for requested block snapshot sessions.
+     * 
+     * @prereq none
+     * 
+     * @param param The ids of the desired BlockSnapshotSession instances.
+     * 
+     * @return A BlockSnapshotSessionBulkRep with the snapshot session details.
+     */
+    @POST
+    @Path("/bulk")
+    @Consumes({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+    @Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+    @Override
+    public BlockSnapshotSessionBulkRep getBulkResources(BulkIdParam param) {
+        return (BlockSnapshotSessionBulkRep) super.getBulkResources(param);
+    }
 
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public BulkRestRep queryBulkResourceReps(List<URI> ids) {
+
+        Iterator<BlockMirror> _dbIterator = _dbClient.queryIterativeObjects(getResourceClass(), ids);
+        return new BlockMirrorBulkRep(BulkList.wrapping(_dbIterator, MapBlockMirror.getInstance(_dbClient)));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected BulkRestRep queryFilteredBulkResourceReps(List<URI> ids) {
+        Iterator<BlockMirror> _dbIterator = _dbClient.queryIterativeObjects(getResourceClass(), ids);
+        ResourceFilter<BlockMirror> filter = new BlockMirrorFilter(getUserFromContext(), _permissionsHelper);
+        return new BlockMirrorBulkRep(BulkList.wrapping(_dbIterator, MapBlockMirror.getInstance(_dbClient), filter));
+    }
+
+    /**
+     * Private class defined filter for bulk resources.
+     */
+    private class BlockMirrorFilter extends PermissionsEnforcingResourceFilter<BlockMirror> {
+
+        /**
+         * Constructor.
+         * 
+         * @param user A reference to a user.
+         * @param permissionsHelper A reference to a permissions helper.
+         */
+        protected BlockMirrorFilter(StorageOSUser user, PermissionsHelper permissionsHelper) {
+            super(user, permissionsHelper);
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public boolean isAccessible(BlockMirror blockMirror) {
+            boolean isAccesible = false;
+            isAccesible = isTenantAccessible(getTenantOwner(blockMirror.getId()));
+            if (!isAccesible) {
+                NamedURI project = blockMirror.getProject();
+                if (project != null) {
+                    isAccesible = isProjectAccessible(project.getURI());
+                }
+            }
+            return isAccesible;
+        }
+    }
 }
--- vipr-portal/vipr-client/src/java/com/emc/vipr/client/core/BlockContinuousCopies.java
+++ vipr-portal/vipr-client/src/java/com/emc/vipr/client/core/BlockContinuousCopies.java
@@ -1,8 +1,16 @@
+/*
+ * Copyright (c) 2017 Dell EMC
+ * All Rights Reserved
+ */
+
 package com.emc.vipr.client.core;
+
+import static com.emc.vipr.client.core.util.ResourceUtils.defaultList;
 
 import java.util.List;
 
 import com.emc.storageos.model.BulkIdParam;
+import com.emc.storageos.model.block.BlockMirrorBulkRep;
 import com.emc.storageos.model.block.BlockMirrorRestRep;
 import com.emc.vipr.client.ViPRCoreClient;
 import com.emc.vipr.client.core.impl.PathConstants;
@@ -16,7 +24,8 @@ public class BlockContinuousCopies extends ProjectResources<BlockMirrorRestRep> 
 
 	@Override
 	protected List getBulkResources(BulkIdParam input) {
-		return null;
+		BlockMirrorBulkRep response = client.post(BlockMirrorBulkRep.class, input, getBulkUrl());
+	     return defaultList(response.getBlockMirrors());
 	}
 
 }

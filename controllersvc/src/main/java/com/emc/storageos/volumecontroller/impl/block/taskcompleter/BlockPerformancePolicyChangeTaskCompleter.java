0e010a67495efce8a2be41a7b37ceb6c9d2419a2
mhegeric <mike.hegerich@emc.com>
2017-08-17 17:02:06 -0400

    Changes in task completer to restore policy on error to handle VPLEX.

--- controllersvc/src/main/java/com/emc/storageos/volumecontroller/impl/block/taskcompleter/BlockPerformancePolicyChangeTaskCompleter.java
+++ controllersvc/src/main/java/com/emc/storageos/volumecontroller/impl/block/taskcompleter/BlockPerformancePolicyChangeTaskCompleter.java
@@ -24,6 +24,7 @@ import com.emc.storageos.db.client.util.NullColumnValueGetter;
 import com.emc.storageos.security.audit.AuditLogManager;
 import com.emc.storageos.services.OperationTypeEnum;
 import com.emc.storageos.svcs.errorhandling.model.ServiceCoded;
+import com.emc.storageos.util.VPlexUtil;
 import com.emc.storageos.volumecontroller.impl.ControllerUtils;
 
 /**
@@ -64,32 +65,46 @@ public class BlockPerformancePolicyChangeTaskCompleter extends VolumeWorkflowCom
                     // Restore the performance policy.
                     Volume volume = dbClient.queryObject(Volume.class, volumeURI);
                     logger.info("Rolling back performance policy on volume {}({})", volumeURI, volume.getLabel());
-                    URI oldPerfPolicyURI = oldVolumeToPerfPolicyMap.get(volumeURI);
-                    if (oldPerfPolicyURI == null) {
-                        oldPerfPolicyURI = NullColumnValueGetter.getNullURI();
-                    }
-                    volume.setPerformancePolicy(oldPerfPolicyURI);
-                    
-                    // Also restore the auto tiering policy set on the volume
-                    // to the value specified in the old performance policy.
-                    if (!NullColumnValueGetter.isNullURI(oldPerfPolicyURI)) {
-                        URI oldATPURI = ControllerUtils.getAutoTieringPolicyURIFromPerfPolicy(oldPerfPolicyURI, volume, dbClient);
-                        if (oldATPURI == null) {
-                            oldATPURI = NullColumnValueGetter.getNullURI();
-                        }
-                        volume.setAutoTieringPolicyUri(oldATPURI);
+                    if (!VPlexUtil.isVplexVolume(volume, dbClient)) {
+                        URI oldPerfPolicyURI = oldVolumeToPerfPolicyMap.get(volumeURI);
+                        restorePerformancePolicyForVolume(volume, oldPerfPolicyURI, true, dbClient);
+                        volumesToUpdate.add(volume);
                     } else {
-                        // The volume may not have had a performance policy, but still 
-                        // may have had an auto tiering policy from the volume's virtual
-                        // pool.
-                        VirtualPool vpool = dbClient.queryObject(VirtualPool.class, volume.getVirtualPool());
-                        StorageSystem storageSystem = dbClient.queryObject(StorageSystem.class, volume.getStorageController());
-                        URI oldATPURI = ControllerUtils.getAutoTieringPolicyURIFromVirtualPool(vpool, storageSystem, dbClient) ;
-                        if (!NullColumnValueGetter.isNullURI(oldATPURI)) {
-                            volume.setAutoTieringPolicyUri(oldATPURI);
+                        // See if we need to restore the primary side of the volume. We know we
+                        // do if the old policy map contains an entry for the primary backend volume.
+                        // Note that we cannot just get the value from the map and check for null. 
+                        // Originally, the volume may not have had a performance policy in which case
+                        // the value in the map would be null. However, if the map contains an entry
+                        // we did try to assign it a policy in the change performance policy request,
+                        // and the values were updated in the volume to reflect that new policy so we
+                        // have to restore it back to no policy.
+                        Volume primaryBackendVolume = VPlexUtil.getVPLEXBackendVolume(volume, true, dbClient, false);
+                        if (primaryBackendVolume != null &&  oldVolumeToPerfPolicyMap.containsKey(primaryBackendVolume.getId())) {
+                            URI oldPerfPolicyURI = oldVolumeToPerfPolicyMap.get(primaryBackendVolume.getId());
+                            restorePerformancePolicyForVolume(primaryBackendVolume, oldPerfPolicyURI, true, dbClient);
+                            volumesToUpdate.add(primaryBackendVolume);
+                            
+                            // If we tried to change the performance policy of the primary
+                            // side of the volume, then we need to restore the performance
+                            // policy of the VPLEX volume itself. It will have had the same
+                            // old performance policy as the primary backend volume. Note
+                            // that we do not reflect the auto tiering policy in the VPLEX,
+                            // so no need to restore that in the VPLEX volume itself.
+                            restorePerformancePolicyForVolume(volume, oldPerfPolicyURI, false, dbClient);
+                            volumesToUpdate.add(volume);
+                        }
+                        
+                        // Now lets see if we need to restore the HA side of the VPLEX volume if
+                        // the request included a change to the HA side of the VPLEX volume. Again,
+                        // we must check if the map contains and entry for the HA backend volume, rather
+                        // than just getting the value and checking for null, as described above.
+                        Volume haBackendVolume = VPlexUtil.getVPLEXBackendVolume(volume, false, dbClient, false);
+                        if (haBackendVolume != null &&  oldVolumeToPerfPolicyMap.containsKey(haBackendVolume.getId())) {
+                            URI oldPerfPolicyURI = oldVolumeToPerfPolicyMap.get(haBackendVolume.getId());
+                            restorePerformancePolicyForVolume(haBackendVolume, oldPerfPolicyURI, true, dbClient);
+                            volumesToUpdate.add(haBackendVolume);
                         }
                     }
-                    volumesToUpdate.add(volume);                    
                 }
                 dbClient.updateObject(volumesToUpdate);
                 break;
@@ -137,5 +152,44 @@ public class BlockPerformancePolicyChangeTaskCompleter extends VolumeWorkflowCom
 
         // Call super to update the volume tasks and notify the workflow of completion.
         super.complete(dbClient, status, serviceCoded);
-    }    
+    }
+
+    /**
+     * 
+     * @param volume
+     * @param oldPerfPolicyURI
+     * @param restoreAutoTieringPolicy
+     * @param dbClient
+     */
+    private void restorePerformancePolicyForVolume(Volume volume, URI oldPerfPolicyURI, boolean restoreAutoTieringPolicy, DbClient dbClient) {
+        if (oldPerfPolicyURI == null) {
+            oldPerfPolicyURI = NullColumnValueGetter.getNullURI();
+        }
+        volume.setPerformancePolicy(oldPerfPolicyURI);
+        
+        // Also restore the auto tiering policy set on the volume
+        // to the value specified in the old performance policy
+        // unless the flag indicates otherwise. This would be the
+        // case for a VPLEX volume as we never reflect the ATP
+        // in the VPLEX volume.
+        if (restoreAutoTieringPolicy) {
+            if (!NullColumnValueGetter.isNullURI(oldPerfPolicyURI)) {
+                URI oldATPURI = ControllerUtils.getAutoTieringPolicyURIFromPerfPolicy(oldPerfPolicyURI, volume, dbClient);
+                if (oldATPURI == null) {
+                    oldATPURI = NullColumnValueGetter.getNullURI();
+                }
+                volume.setAutoTieringPolicyUri(oldATPURI);
+            } else {
+                // The volume may not have had a performance policy, but still 
+                // may have had an auto tiering policy from the volume's virtual
+                // pool.
+                VirtualPool vpool = dbClient.queryObject(VirtualPool.class, volume.getVirtualPool());
+                StorageSystem storageSystem = dbClient.queryObject(StorageSystem.class, volume.getStorageController());
+                URI oldATPURI = ControllerUtils.getAutoTieringPolicyURIFromVirtualPool(vpool, storageSystem, dbClient) ;
+                if (!NullColumnValueGetter.isNullURI(oldATPURI)) {
+                    volume.setAutoTieringPolicyUri(oldATPURI);
+                }
+            }
+        }
+    }
 }

b20fa01ef15c2d1577f6615cdf0f252b21674c36
Francis <francis.nijay@emc.com>
2017-08-17 18:41:54 +0530

    add all endpoints

--- apisvc/src/main/java/com/emc/storageos/api/service/impl/resource/FileService.java
+++ apisvc/src/main/java/com/emc/storageos/api/service/impl/resource/FileService.java
@@ -4559,6 +4559,8 @@ public class FileService extends TaskResourceService {
                         if (mountpointIp.equals(datastoreMount.getRemoteHost()) || FileServiceUtils.getIpFromFqdn(mountpointIp)
                                 .equals(FileServiceUtils.getIpFromFqdn(datastoreMount.getRemoteHost()))) {
                             newEndpointList.addAll(modifyRule.getRootHosts());
+                            newEndpointList.addAll(modifyRule.getReadWriteHosts());
+                            newEndpointList.addAll(modifyRule.getReadOnlyHosts());
                             // Add all root hosts matching the mountpoint of datastore to new endpoint list
                         }
                     }

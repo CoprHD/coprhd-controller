fa5109d201673d736594c508161caa8acf2a230b
Francis <francis.nijay@emc.com>
2017-08-22 02:11:55 +0530

    Check added to apisvc

--- apisvc/src/main/java/com/emc/storageos/api/service/impl/resource/UnManagedFilesystemService.java
+++ apisvc/src/main/java/com/emc/storageos/api/service/impl/resource/UnManagedFilesystemService.java
@@ -16,6 +16,7 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -29,12 +30,15 @@ import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import com.emc.storageos.api.mapper.functions.MapUnmanagedFileSystem;
 import com.emc.storageos.api.service.impl.resource.utils.CapacityUtils;
 import com.emc.storageos.api.service.impl.resource.utils.FileSystemIngestionUtil;
 import com.emc.storageos.api.service.impl.resource.utils.PropertySetterUtil;
 import com.emc.storageos.api.service.impl.response.BulkList;
+import com.emc.storageos.customconfigcontroller.CustomConfigConstants;
+import com.emc.storageos.customconfigcontroller.impl.CustomConfigHandler;
 import com.emc.storageos.db.client.DbClient;
 import com.emc.storageos.db.client.URIUtil;
 import com.emc.storageos.db.client.constraint.AlternateIdConstraint;
@@ -107,6 +111,13 @@ public class UnManagedFilesystemService extends TaggedResource {
      */
     private static final Logger _logger = LoggerFactory
             .getLogger(UnManagedFilesystemService.class);
+
+    @Autowired
+    private CustomConfigHandler customConfigHandler;
+
+    public CustomConfigHandler getCustomConfigHandler() {
+        return customConfigHandler;
+    }
 
     @Override
     protected DataObject queryResource(URI id) {
@@ -275,6 +286,13 @@ public class UnManagedFilesystemService extends TaggedResource {
             FileSystemIngestionUtil.isIngestionRequestValidForUnManagedFileSystems(
                     param.getUnManagedFileSystems(), cos, _dbClient);
 
+            boolean shareVNASWithMultipleProjects = Boolean.valueOf(customConfigHandler.getComputedCustomConfigValue(
+                    CustomConfigConstants.SHARE_VNAS_WITH_MULTIPLE_PROJECTS, "global", null));
+            Set<String> projectVnas = null;
+            if (!shareVNASWithMultipleProjects) {
+                projectVnas = project.getAssignedVNasServers();
+            }
+
             List<FileShare> filesystems = new ArrayList<FileShare>();
             Map<URI, FileShare> unManagedFSURIToFSMap = new HashMap<>();
             List<FileExportRule> fsExportRules = new ArrayList<FileExportRule>();
@@ -313,6 +331,12 @@ public class UnManagedFilesystemService extends TaggedResource {
                 }
 
                 if (!FileSystemIngestionUtil.checkVirtualPoolValidForUnManagedFileSystem(_dbClient, cos, unManagedFileSystemUri)) {
+                    continue;
+                }
+
+                if (!shareVNASWithMultipleProjects && checkProjectVnas(projectVnas, unManagedFileSystem)) {
+                    _logger.warn("UnManaged FileSystem {} does not belong to the Project's vnas. Skipping Ingestion..",
+                            unManagedFileSystemUri);
                     continue;
                 }
 
@@ -1298,5 +1322,25 @@ public class UnManagedFilesystemService extends TaggedResource {
                 operationalStatus ? AuditLogManager.AUDITLOG_SUCCESS : AuditLogManager.AUDITLOG_FAILURE,
                 description,
                 descparams);
+    }
+
+    private boolean checkProjectVnas(Set<String> projectVnas, UnManagedFileSystem unManagedFileSystem) {
+
+        if (projectVnas != null) {
+            String umfsNas = PropertySetterUtil.extractValueFromStringSet(
+                    SupportedFileSystemInformation.NAS.toString(),
+                    unManagedFileSystem.getFileSystemInformation());
+            // If share vnas on multiple projects is false and nas of umfs is virtual then compare the vnas else add the umfs
+            if (umfsNas != null && umfsNas.contains("VirtualNAS")) {
+                // Only if vnas of project doesnt match with available vnas of umfs continue to add
+                if (projectVnas.contains(umfsNas)) {
+                    return false;
+                } else {
+                    // Case when project vnas is different from FS vnas
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }

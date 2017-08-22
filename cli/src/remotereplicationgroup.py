7f7f9c15e09bce3da9627fe4b163038d84181a6f
Cao, Shiliang <shiliang.cao@emc.com>
2017-08-22 16:37:56 +0800

    add remote repliation support for viprcli volume creating operation

---
+++ cli/src/remotereplicationgroup.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python
+
+# Copyright (c) 2017 DellEMC Corporation
+# All Rights Reserved
+
+import common
+from common import SOSError
+
+class RemoteReplicationGroup(object):
+    '''
+    Encapsulate operations around remote replication group
+    '''
+
+    # Remote replication group URIs
+    URI_REMOTEREPLICATIONGROUP_LIST = '/vdc/block/remote-replication-groups'
+    URI_REMOTEREPLICATIONGROUP_DETAILS = '/vdc/block/remote-replication-groups/{0}'
+
+    def __init__(self, ipAddr, port):
+        '''
+        Constructor: takes IP address and port of the ViPR instance. These are
+        needed to make http requests for REST API
+        '''
+        self.__ipAddr = ipAddr
+        self.__port = port
+
+    def show_by_uri(self, uri):
+        '''
+        Makes a REST API call to retrieve details of a
+        remote replication group based on its UUID
+        '''
+        (s, h) = common.service_json_request(
+            self.__ipAddr, self.__port, "GET",
+            RemoteReplicationGroup.URI_REMOTEREPLICATIONGROUP_DETAILS.format(uri), None)
+        o = common.json_decode(s)
+        inactive = common.get_node_value(o, 'inactive')
+
+        if(inactive):
+            return None
+
+        return o
+
+    def list(self):
+        '''
+        Makes a REST API call to retrieve list of all remote replication groups
+        '''
+        (s, h) = common.service_json_request(
+            self.__ipAddr, self.__port, "GET",
+            RemoteReplicationGroup.URI_REMOTEREPLICATIONGROUP_LIST,
+            None)
+        o = common.json_decode(s)
+
+        if (not o or "remote_replication_group" not in o):
+            return []
+
+        return common.get_node_value(o, 'remote_replication_group')
+
+    def query_by_name(self, name):
+        '''
+        Return URI of remote repliation group of specified name
+        '''
+        rrgroups = self.list()
+        for rrgroup in rrgroups:
+            rrgroup = self.show_by_uri(rrgroup['id'])
+            if (name == rrgroup['name'])
+                return rrgroup['id']
+
+        raise SOSError(SOSError.NOT_FOUND_ERR,
+                       'Remote repliation group not found with name (device label): '
+                       + name)
---
+++ cli/src/remotereplicationset.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python
+
+# Copyright (c) 2017 DellEMC Corporation
+# All Rights Reserved
+
+import common
+from common import SOSError
+
+class RemoteReplicationSet(object):
+    '''
+    Encapsulate operations around remote replication set
+    '''
+
+    # Remote replication set URIs
+    URI_REMOTEREPLICATIONSET_LIST = '/vdc/block/remote-replication-sets'
+    URI_REMOTEREPLICATIONSET_DETAILS = '/vdc/block/remote-replication-sets/{0}'
+
+    def __init__(self, ipAddr, port):
+        '''
+        Constructor: takes IP address and port of the ViPR instance. These are
+        needed to make http requests for REST API
+        '''
+        self.__ipAddr = ipAddr
+        self.__port = port
+
+    def show_by_uri(self, uri):
+        '''
+        Makes a REST API call to retrieve details of a
+        remote replication set based on its UUID
+        '''
+        (s, h) = common.service_json_request(
+            self.__ipAddr, self.__port, "GET",
+            RemoteReplicationSet.URI_REMOTEREPLICATIONSET_DETAILS.format(uri), None)
+        o = common.json_decode(s)
+        inactive = common.get_node_value(o, 'inactive')
+
+        if(inactive):
+            return None
+
+        return o
+
+    def list(self):
+        '''
+        Makes a REST API call to retrieve list of all remote replication sets
+        '''
+        (s, h) = common.service_json_request(
+            self.__ipAddr, self.__port, "GET",
+            RemoteReplicationSet.URI_REMOTEREPLICATIONSET_LIST,
+            None)
+        o = common.json_decode(s)
+
+        if (not o or "remote_replication_set" not in o):
+            return []
+
+        return common.get_node_value(o, 'remote_replication_set')
+
+    def query_by_name(self, name):
+        '''
+        Return URI of remote repliation set of specified name
+        '''
+        rrsets = self.list()
+        for rrset in rrsets:
+            rrset = self.show_by_uri(rrset['id'])
+            if (name == rrset['name'])
+                return rrset['id']
+
+        raise SOSError(SOSError.NOT_FOUND_ERR,
+                       'Remote repliation set not found with name (device label): '
+                       + name)

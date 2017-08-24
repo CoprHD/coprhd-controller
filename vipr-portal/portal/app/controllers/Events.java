da59ee2becb1b3ecc30c193f9543532f213e07c8
Sean Gallacher <sean.gallacher@emc.com>
2017-08-24 11:42:51 -0300

    COP-34135: updated Event portal logic to fetch event details if they do not exist

--- vipr-portal/portal/app/controllers/Events.java
+++ vipr-portal/portal/app/controllers/Events.java
@@ -294,14 +294,14 @@ public class Events extends Controller {
         List<String> approveDetails = Lists.newArrayList();
         List<String> declineDetails = Lists.newArrayList();
 
-        if (event.getEventStatus().equalsIgnoreCase(ActionableEvent.Status.pending.name().toString())
-                || event.getEventStatus().equalsIgnoreCase(ActionableEvent.Status.failed.name().toString())) {
-            EventDetailsRestRep details = getViprClient().events().getDetails(uri(id));
+        if (event.getApproveDetails() != null && !event.getApproveDetails().isEmpty()
+        		&& event.getDeclineDetails() != null && !event.getDeclineDetails().isEmpty()) {
+        	approveDetails = event.getApproveDetails();
+            declineDetails = event.getDeclineDetails();
+        } else {
+        	EventDetailsRestRep details = getViprClient().events().getDetails(uri(id));
             approveDetails = details.getApproveDetails();
             declineDetails = details.getDeclineDetails();
-        } else {
-            approveDetails = event.getApproveDetails();
-            declineDetails = event.getDeclineDetails();
         }
 
         List<TaskResourceRep> tasks = Lists.newArrayList();

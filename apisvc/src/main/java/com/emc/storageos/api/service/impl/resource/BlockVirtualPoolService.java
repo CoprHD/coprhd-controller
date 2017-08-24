9f54ceb47b74116cf89ebdb46c3de70621f759c1
krisht <tejaswini.krishnamurthy@emc.com>
2017-08-24 01:16:24 -0700

    Fix NPE and remove rdf group field in Create VMware datastore catalog service.

--- apisvc/src/main/java/com/emc/storageos/api/service/impl/resource/BlockVirtualPoolService.java
+++ apisvc/src/main/java/com/emc/storageos/api/service/impl/resource/BlockVirtualPoolService.java
@@ -670,7 +670,7 @@ public class BlockVirtualPoolService extends VirtualPoolService {
             if (vpool == null) {
                 rdfGroupList.getRdfGroups().add(toRDFGroupRep(rdg, _dbClient));
             } else if (vpoolStorageSystemIds.contains(rdg.getSourceStorageSystemUri())) {
-                if (rdg.getSupportedCopyMode().equalsIgnoreCase(RemoteDirectorGroup.SupportedCopyModes.ALL.name())) {
+                if (RemoteDirectorGroup.SupportedCopyModes.ALL.name().equalsIgnoreCase(rdg.getSupportedCopyMode())) {
                     // Check to see if the RDG supports all copy modes
                     rdfGroupList.getRdfGroups().add(toRDFGroupRep(rdg, _dbClient));
                 } else if (vpoolCopyMode.equalsIgnoreCase(rdg.getSupportedCopyMode())) {

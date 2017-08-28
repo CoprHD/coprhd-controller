8985d49dd4a339189834d8de4b9d6f99160e1bab
Selvamanickam Anbalagan <selvamanickam.anbalagan@emc.com>
2017-08-28 12:15:25 -0400

    COP-34153 Calculate percentage done and updated in migration object to show the progress.

--- controllersvc/src/main/java/com/emc/storageos/volumecontroller/impl/vmax/rest/VMAXMigrationJob.java
+++ controllersvc/src/main/java/com/emc/storageos/volumecontroller/impl/vmax/rest/VMAXMigrationJob.java
@@ -9,6 +9,7 @@ import java.net.URI;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.emc.storageos.db.client.model.Migration;
 import com.emc.storageos.db.client.model.StorageProvider;
 import com.emc.storageos.vmax.restapi.VMAXApiClient;
 import com.emc.storageos.vmax.restapi.model.response.migration.MigrationStorageGroupResponse;
@@ -45,6 +46,14 @@ public class VMAXMigrationJob extends VMAXJob {
             MigrationStorageGroupResponse sgResponse = vmaxApiClient.getMigrationStorageGroup(sourceSerialNumber, sgName);
             String migrationStatus = sgResponse.getState();
             logger.info("Migration status {}", migrationStatus);
+            Migration migration = jobContext.getDbClient().queryObject(Migration.class, migrationURI);
+            int percent = 0;
+            if (sgResponse.getTotalCapacity() != 0) { // To avoid dive by zero error
+                percent = (int) ((sgResponse.getTotalCapacity() - sgResponse.getRemainingCapacity()) / sgResponse.getTotalCapacity()) * 100;
+            }
+            logger.info("Percent done :{}%", percent);
+            migration.setPercentDone(String.valueOf(percent));
+            jobContext.getDbClient().updateObject(migration);
             ((MigrationOperationTaskCompleter) getTaskCompleter()).setMigrationStatus(migrationStatus);
         } catch (Exception e) {
             logger.error("Exception occurred", e);

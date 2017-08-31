5c10f138f5cb9618250c6d4084e51bf15be8919d
Mudit Jain <mudit.jain@emc.com>
2017-08-31 20:42:50 +0530

    helper class

--- exportLibraries/isilon/src/main/java/com/emc/storageos/isilon/restapi/IsilonHelper.java
+++ exportLibraries/isilon/src/main/java/com/emc/storageos/isilon/restapi/IsilonHelper.java
@@ -14,6 +14,8 @@ import java.util.concurrent.ConcurrentHashMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.emc.storageos.isilon.restapi.IsilonApi.IsilonList;
+
 public class IsilonHelper {
 	private static volatile IsilonApi _client;
 	private static volatile IsilonApiFactory _factory = new IsilonApiFactory();
@@ -93,12 +95,13 @@ public class IsilonHelper {
 	public static void main(String args[]) throws Exception {
 		try {
 			String fspath;
+			String shareName;
+			IsilonSMBShare smbshare;
 			properties_file = args[0];
 			setup();
-			// Operations to be performed..
 			String op = args[1];
-
 			switch (op) {
+
 			case "check_for_dir":
 				fspath = args[2];
 				if (_client.existsDir(fspath)) {
@@ -109,19 +112,114 @@ public class IsilonHelper {
 					System.exit(1);
 				}
 				break;
+
+			case "create_snapshot":
+				fspath = args[2];
+				String snapName = args[3];
+				String snapId = _client.createSnapshot(snapName, fspath);
+				if (snapId != null && !snapId.isEmpty()) {
+					System.exit(0);
+				} else {
+					System.exit(1);
+				}
+				break;
+			case "check_for_snapshot":
+				fspath = args[2];
+				String snapNam = args[3];
+				IsilonSnapshot snap = _client.getSnapshot(snapNam);
+				if (snap != null && snap.getPath().equals(fspath)) {
+					System.out.println("snapshot with name" + snapNam + " exists on Array");
+					System.exit(0);
+				} else {
+					System.out.println("snapshot with name" + snapNam + " doesn't exists on Array");
+					System.exit(1);
+				}
+				break;
+
 			case "create_directory":
 				fspath = args[2];
 				_client.createDir(fspath);
 				break;
-			case "ku":
+			case "create_cifs_share":
+				fspath = args[2];
+				shareName = args[3];
+				smbshare = new IsilonSMBShare(shareName, fspath, "New_SMB_Share");
+				String shareId = _client.createShare(smbshare);
+				if (shareId != null && !shareId.isEmpty()) {
+					System.exit(0);
+				} else {
+					System.exit(1);
+				}
+				break;
 
+			case "check_for_share":
+				fspath = args[2];
+				shareName = args[3];
+				smbshare = _client.getShare(shareName);
+				if (smbshare != null && smbshare.getPath().equals(fspath)) {
+					System.out.println("share with path" + fspath + " exists on Array");
+					System.exit(0);
+				} else {
+					System.out.println("share with path" + fspath + " doesn't exists on Array");
+					System.exit(1);
+				}
+				break;
+
+			case "update_cifs_share_name":
+				fspath = args[2];
+				shareName = args[3];
+				String newshareName = args[4];
+				smbshare = new IsilonSMBShare(newshareName, fspath, "updated_the_name");
+				_client.modifyShare(shareName, smbshare);
+				break;
+			case "create_nfs_export":
+				fspath = args[2];
+				IsilonExport e1 = new IsilonExport();
+				e1.addPath(fspath);
+				e1.addClient("www.amazon.com");
+				e1.addClient("www.ford.com");
+				ArrayList<String> securityFlavors1 = new ArrayList<>();
+				securityFlavors1.add("unix");
+				e1.setSecurityFlavors(securityFlavors1);
+				e1.setReadOnly();
+				e1.setComment("New export: unix.rw.nobody");
+				String id = _client.createExport(e1, false);
+				if (id != null && !id.isEmpty()) {
+					System.exit(0);
+				} else {
+					System.exit(1);
+				}
+				break;
+			case "check_for_export":
+				fspath = args[2];
+				String resumetoken = "";
+
+				while (resumetoken != null) {
+					IsilonList<IsilonExport> exports = _client.listExports(resumetoken);
+					resumetoken = exports.getToken();
+					for (IsilonExport exp : exports.getList()) {
+						if (exp.getPaths().contains(fspath)) {
+							System.out.println(
+									"export with path and id " + fspath + " " + exp.getId() + " exists on Array");
+							System.exit(0);
+						}
+					}
+				}
+				System.out.println("export with path" + fspath + " doesn't exists on Array");
+				System.exit(1);
 				break;
 			default:
 				System.out.println("INVALID COMMAND!!" + "Valid Commands are:" + getAllCommands());
 				System.exit(1);
 			}
 		} catch (ArrayIndexOutOfBoundsException e) {
-			System.out.println(e.getLocalizedMessage());
+			System.out.println(e);
+			System.exit(1);
+		} catch (IsilonException ex) {
+			System.out.println(ex);
+			System.exit(1);
+		} catch (Exception e) {
+			System.out.println(e);
 			System.exit(1);
 		}
 	}
@@ -130,6 +228,13 @@ public class IsilonHelper {
 		ArrayList<String> commands = new ArrayList<>();
 		commands.add("check_for_dir");
 		commands.add("create_directory");
+		commands.add("create_cifs_share");
+		commands.add("check_for_share");
+		commands.add("update_cifs_share_name");
+		commands.add("create_nfs_export");
+		commands.add("check_for_export");
+		commands.add("create_snapshot");
+		commands.add("check_for_snapshot");
 		return commands;
 	}
 }

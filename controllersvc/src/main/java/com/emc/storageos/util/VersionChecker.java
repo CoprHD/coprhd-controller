857c5d0dabcf05bbff5d24b2b4303e94ed277893
lakhiv <vasudevu.lakhinana@emc.com>
2017-10-17 12:04:01 +0530

    Added prefix V instead of processing verion in verion checker method

--- controllersvc/src/main/java/com/emc/storageos/util/VersionChecker.java
+++ controllersvc/src/main/java/com/emc/storageos/util/VersionChecker.java
@@ -32,10 +32,6 @@ public class VersionChecker {
         }
         version = version.trim();
 
-        // Matches as many consecutive non-digits as possible
-        // at the beginning of the version string and remove them from it.
-        version = Pattern.compile("^\\D*").matcher(version).replaceFirst("");
-
         // split by dots, parentheses, and adjoining letters and numbers
         String[] versionToVerifyWith = Pattern.compile("[\\.|\\)|\\(| ]|(?<=\\D)(?=\\d)|(?<=\\d)(?=\\D)", Pattern.DOTALL).split(
                 minimumSupportedVersion);

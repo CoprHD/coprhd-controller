3e8ee1e5469343798a66542f16e24646bda83970
Harsha Yalamanchili <harsha.yalamanchili@emc.com>
2017-10-23 15:25:11 -0400

    Fixed null pointer exceptions associated with MetaVolumeMembersProcessor and MetaVolumeTypeProcessor. As part of block-discovery and volume-discovery beans, supportedVersion was introduced to differentiate between operations run against VMAX (8.x) and VNX (4.x). I have to make changes to include SMI-S 9.x for VMAX.

--- controllersvc/src/main/java/com/emc/storageos/volumecontroller/impl/plugins/discovery/smis/processor/detailedDiscovery/MetaVolumeMembersProcessor.java
+++ controllersvc/src/main/java/com/emc/storageos/volumecontroller/impl/plugins/discovery/smis/processor/detailedDiscovery/MetaVolumeMembersProcessor.java
@@ -48,81 +48,86 @@ public class MetaVolumeMembersProcessor extends StorageProcessor {
             WBEMClient client = SMICommunicationInterface.getCIMClient(keyMap);
 
             CIMObjectPath[] metaMembersPaths = (CIMObjectPath[]) getFromOutputArgs((CIMArgument[]) resultObj, "OutElements");
-            _logger.debug(String.format("Processing meta members: %s", Arrays.toString(metaMembersPaths)));
+            if (metaMembersPaths == null || (metaMembersPaths.length == 0)) {
+                _logger.info(String.format("The are no meta members to process"));
+            } else {
+                _logger.debug(String.format("Processing meta members: %s", Arrays.toString(metaMembersPaths)));
 
-            // Get volume from db
-            _logger.debug(String.format("Args size: %s", _args.size()));
-            Object[] arguments = (Object[]) _args.get(0);
-            CIMArgument theElement = ((CIMArgument[]) arguments[2])[1];
-            _logger.info(String.format("TheElement: %s, type %s", theElement.getValue().toString(), theElement.getValue().getClass()
-                    .toString()));
-            CIMObjectPath theElementPath = (CIMObjectPath) theElement.getValue();
+                // Get volume from db
+                _logger.debug(String.format("Args size: %s", _args.size()));
+                Object[] arguments = (Object[]) _args.get(0);
+                CIMArgument theElement = ((CIMArgument[]) arguments[2])[1];
+                _logger.info(String.format("TheElement: %s, type %s", theElement.getValue().toString(), theElement.getValue().getClass()
+                        .toString()));
+                CIMObjectPath theElementPath = (CIMObjectPath) theElement.getValue();
 
-            UnManagedVolume preExistingVolume = null;
-            String isMetaVolume = "true";
-            String nativeGuid;
+                UnManagedVolume preExistingVolume = null;
+                String isMetaVolume = "true";
+                String nativeGuid;
 
-            // Check if storage volume exists in db (the method is called from re-discovery context).
-            nativeGuid = getVolumeNativeGuid(theElementPath);
-            Volume storageVolume = checkStorageVolumeExistsInDB(nativeGuid, dbClient);
-            if (null == storageVolume || storageVolume.getInactive()) {
-                // Check if unmanaged volume exists in db (the method is called from unmanaged volumes discovery context).
-                nativeGuid = getUnManagedVolumeNativeGuidFromVolumePath(theElementPath);
-                _logger.debug("Volume nativeguid :" + nativeGuid);
-                preExistingVolume = checkUnManagedVolumeExistsInDB(nativeGuid, dbClient);
-                if (null == preExistingVolume) {
-                    _logger.debug("Volume Info Object not found :" + nativeGuid);
+                // Check if storage volume exists in db (the method is called from re-discovery context).
+                nativeGuid = getVolumeNativeGuid(theElementPath);
+                Volume storageVolume = checkStorageVolumeExistsInDB(nativeGuid, dbClient);
+                if (null == storageVolume || storageVolume.getInactive()) {
+                    // Check if unmanaged volume exists in db (the method is called from unmanaged volumes discovery context).
+                    nativeGuid = getUnManagedVolumeNativeGuidFromVolumePath(theElementPath);
+                    _logger.debug("Volume nativeguid :" + nativeGuid);
+                    preExistingVolume = checkUnManagedVolumeExistsInDB(nativeGuid, dbClient);
+                    if (null == preExistingVolume) {
+                        _logger.debug("Volume Info Object not found :" + nativeGuid);
+                        return;
+                    }
+                    isMetaVolume = preExistingVolume.getVolumeCharacterstics()
+                            .get(UnManagedVolume.SupportedVolumeCharacterstics.IS_METAVOLUME.toString());
+                } else {
+                    _logger.debug("Volume managed by Bourne :" + storageVolume.getNativeGuid());
+                    isMetaVolume = storageVolume.getIsComposite().toString();
+                }
+
+                if (isMetaVolume.equalsIgnoreCase("false")) {
+                    _logger.error(String.format("MetaVolumeMembersProcessor called for regular volume: %s", nativeGuid));
                     return;
                 }
-                isMetaVolume = preExistingVolume.getVolumeCharacterstics().
-                        get(UnManagedVolume.SupportedVolumeCharacterstics.IS_METAVOLUME.toString());
-            } else {
-                _logger.debug("Volume managed by Bourne :" + storageVolume.getNativeGuid());
-                isMetaVolume = storageVolume.getIsComposite().toString();
+
+                Integer membersCount = metaMembersPaths.length;
+
+                // get meta member size. use second member --- the first member will show size of meta volume itself.
+                CIMObjectPath metaMemberPath = metaMembersPaths[1];
+                CIMInstance cimVolume = client.getInstance(metaMemberPath, false,
+                        false, META_MEMBER_SIZE_INFO);
+
+                CIMProperty consumableBlocks = cimVolume.getProperty(SmisConstants.CP_CONSUMABLE_BLOCKS);
+                CIMProperty blockSize = cimVolume.getProperty(SmisConstants.CP_BLOCK_SIZE);
+                // calculate size = consumableBlocks * block size
+                Long size = Long.valueOf(consumableBlocks.getValue().toString()) * Long.valueOf(blockSize.getValue().toString());
+
+                // set meta member count and meta members size for meta volume (required for volume expansion)
+                if (null != preExistingVolume) {
+                    StringSet metaMembersCount = new StringSet();
+                    metaMembersCount.add(membersCount.toString());
+                    preExistingVolume.putVolumeInfo(UnManagedVolume.SupportedVolumeInformation.META_MEMBER_COUNT.toString(),
+                            metaMembersCount);
+
+                    StringSet metaMemberSize = new StringSet();
+                    metaMemberSize.add(size.toString());
+                    preExistingVolume.putVolumeInfo(UnManagedVolume.SupportedVolumeInformation.META_MEMBER_SIZE.toString(), metaMemberSize);
+
+                    // persist unmanaged volume in db
+                    dbClient.persistObject(preExistingVolume);
+                } else {
+                    storageVolume.setMetaMemberCount(membersCount);
+                    storageVolume.setMetaMemberSize(size);
+                    storageVolume.setTotalMetaMemberCapacity(membersCount * size);
+                    // persist volume in db
+                    dbClient.persistObject(storageVolume);
+                }
+
+                _logger.info(String.format("Meta member info: meta member count --- %s, blocks --- %s, block size --- %s, size --- %s .",
+                        membersCount,
+                        consumableBlocks.getValue().toString(),
+                        blockSize.getValue().toString(), size));
+
             }
-
-            if (isMetaVolume.equalsIgnoreCase("false")) {
-                _logger.error(String.format("MetaVolumeMembersProcessor called for regular volume: %s", nativeGuid));
-                return;
-            }
-
-            Integer membersCount = metaMembersPaths.length;
-
-            // get meta member size. use second member --- the first member will show size of meta volume itself.
-            CIMObjectPath metaMemberPath = metaMembersPaths[1];
-            CIMInstance cimVolume = client.getInstance(metaMemberPath, false,
-                    false, META_MEMBER_SIZE_INFO);
-
-            CIMProperty consumableBlocks = cimVolume.getProperty(SmisConstants.CP_CONSUMABLE_BLOCKS);
-            CIMProperty blockSize = cimVolume.getProperty(SmisConstants.CP_BLOCK_SIZE);
-            // calculate size = consumableBlocks * block size
-            Long size =
-                    Long.valueOf(consumableBlocks.getValue().toString()) * Long.valueOf(blockSize.getValue().toString());
-
-            // set meta member count and meta members size for meta volume (required for volume expansion)
-            if (null != preExistingVolume) {
-                StringSet metaMembersCount = new StringSet();
-                metaMembersCount.add(membersCount.toString());
-                preExistingVolume.putVolumeInfo(UnManagedVolume.SupportedVolumeInformation.META_MEMBER_COUNT.toString(), metaMembersCount);
-
-                StringSet metaMemberSize = new StringSet();
-                metaMemberSize.add(size.toString());
-                preExistingVolume.putVolumeInfo(UnManagedVolume.SupportedVolumeInformation.META_MEMBER_SIZE.toString(), metaMemberSize);
-
-                // persist unmanaged volume in db
-                dbClient.persistObject(preExistingVolume);
-            } else {
-                storageVolume.setMetaMemberCount(membersCount);
-                storageVolume.setMetaMemberSize(size);
-                storageVolume.setTotalMetaMemberCapacity(membersCount * size);
-                // persist volume in db
-                dbClient.persistObject(storageVolume);
-            }
-
-            _logger.info(String.format("Meta member info: meta member count --- %s, blocks --- %s, block size --- %s, size --- %s .",
-                    membersCount,
-                    consumableBlocks.getValue().toString(),
-                    blockSize.getValue().toString(), size));
 
         } catch (Exception e) {
             _logger.error("Processing meta volume  information failed :", e);

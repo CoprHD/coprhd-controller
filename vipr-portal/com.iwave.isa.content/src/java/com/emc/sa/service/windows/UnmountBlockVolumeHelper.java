c9a0708596419042abbcb6c61759b7c208912ed5
Pooja Kumar <Pooj.Kumar@dell.com>
2017-12-20 07:15:34 +0000

    COP-31747: merging to patch3 branch

--- vipr-portal/com.iwave.isa.content/src/java/com/emc/sa/service/windows/UnmountBlockVolumeHelper.java
+++ vipr-portal/com.iwave.isa.content/src/java/com/emc/sa/service/windows/UnmountBlockVolumeHelper.java
@@ -4,6 +4,8 @@
  */
 package com.emc.sa.service.windows;
 
+import static com.emc.sa.service.vipr.ViPRExecutionUtils.logInfo;
+
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
@@ -86,7 +88,11 @@ public class UnmountBlockVolumeHelper {
                 }
             }
 
-            windows.offlineDisk(disk);
+            if (diskDetail.isOnline()) {
+                windows.offlineDisk(disk);
+            } else {
+                logInfo("win.unmount.block.volume.disk.offline", disk.getNumber(), volume.getWwn());
+            }
             windows.removeVolumeMountPoint(volume);
         }
     }

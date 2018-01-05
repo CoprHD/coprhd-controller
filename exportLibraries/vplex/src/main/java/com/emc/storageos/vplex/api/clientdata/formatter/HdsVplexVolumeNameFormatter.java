b2a7f5dee7f174a37b700d98326f1fce07f72bab
Manisha Agrawal <Manisha.Agrawal@emc.com>
2018-01-05 12:46:10 +0530

    removing unnecessary code

--- exportLibraries/vplex/src/main/java/com/emc/storageos/vplex/api/clientdata/formatter/HdsVplexVolumeNameFormatter.java
+++ exportLibraries/vplex/src/main/java/com/emc/storageos/vplex/api/clientdata/formatter/HdsVplexVolumeNameFormatter.java
@@ -7,17 +7,9 @@ public class HdsVplexVolumeNameFormatter extends DefaultVplexVolumeNameFormatter
 
 	public HdsVplexVolumeNameFormatter(VolumeInfo volumeInfo) {
 		super(volumeInfo);
-		this._volumeInfo = volumeInfo;
-		String storageSystemNativeGuid = _volumeInfo.getStorageSystemNativeGuid();
-		//replacing dot operator with hyphen(instead of underscore as done by
-		//DefaultVplexVolumeNameFormatter) - check COP-33663
-		storageSystemNativeGuid = storageSystemNativeGuid
-				.replace(VPlexApiConstants.DOT_OPERATOR,
-						VPlexApiConstants.HYPHEN_OPERATOR);
-		_storageSystemSerialNumber = storageSystemNativeGuid.substring(
-				storageSystemNativeGuid.indexOf(VPlexApiConstants.PLUS_OPERATOR) + 1);
-		_volumeNativeId = _volumeInfo.getVolumeNativeId();
-
+		//replacing underscores with hyphen (check COP-33663)
+		_storageSystemSerialNumber = _storageSystemSerialNumber.replace(VPlexApiConstants.UNDERSCORE_OPERATOR,
+				VPlexApiConstants.HYPHEN_OPERATOR);				
 	}
 
 }

541353083a0e532734474f9b9113f6d0b1611f49
Manisha Agrawal <Manisha.Agrawal@emc.com>
2018-01-05 12:15:15 +0530

    removing unnecessary code

--- exportLibraries/vplex/src/main/java/com/emc/storageos/vplex/api/clientdata/formatter/HdsVplexVolumeNameFormatter.java
+++ exportLibraries/vplex/src/main/java/com/emc/storageos/vplex/api/clientdata/formatter/HdsVplexVolumeNameFormatter.java
@@ -8,14 +8,16 @@ public class HdsVplexVolumeNameFormatter extends DefaultVplexVolumeNameFormatter
 	public HdsVplexVolumeNameFormatter(VolumeInfo volumeInfo) {
 		super(volumeInfo);
 		this._volumeInfo = volumeInfo;
-        String storageSystemNativeGuid = _volumeInfo.getStorageSystemNativeGuid();
-        storageSystemNativeGuid = storageSystemNativeGuid
-                .replace(VPlexApiConstants.DOT_OPERATOR,
-                        VPlexApiConstants.HYPHEN_OPERATOR);
-        _storageSystemSerialNumber = storageSystemNativeGuid.substring(
-                storageSystemNativeGuid.indexOf(VPlexApiConstants.PLUS_OPERATOR) + 1);
-        _volumeNativeId = _volumeInfo.getVolumeNativeId();
-		
+		String storageSystemNativeGuid = _volumeInfo.getStorageSystemNativeGuid();
+		//replacing dot operator with hyphen(instead of underscore as done by
+		//DefaultVplexVolumeNameFormatter) - check COP-33663
+		storageSystemNativeGuid = storageSystemNativeGuid
+				.replace(VPlexApiConstants.DOT_OPERATOR,
+						VPlexApiConstants.HYPHEN_OPERATOR);
+		_storageSystemSerialNumber = storageSystemNativeGuid.substring(
+				storageSystemNativeGuid.indexOf(VPlexApiConstants.PLUS_OPERATOR) + 1);
+		_volumeNativeId = _volumeInfo.getVolumeNativeId();
+
 	}
 
 }

67f2bdd5a3857ebbe68d6fada75decc3beeb9633
Manisha Agrawal <Manisha.Agrawal@emc.com>
2018-01-09 11:10:11 +0530

    Adding copyright statement and inheriting documnetation comment

--- exportLibraries/vplex/src/main/java/com/emc/storageos/vplex/api/clientdata/formatter/HdsVplexVolumeNameFormatter.java
+++ exportLibraries/vplex/src/main/java/com/emc/storageos/vplex/api/clientdata/formatter/HdsVplexVolumeNameFormatter.java
@@ -1,13 +1,21 @@
+/*
+ * Copyright (c) 2018 EMC Corporation
+ * All Rights Reserved
+ */
 package com.emc.storageos.vplex.api.clientdata.formatter;
 
 import com.emc.storageos.vplex.api.VPlexApiConstants;
 import com.emc.storageos.vplex.api.clientdata.VolumeInfo;
 
+/**
+ * @{inheritDoc
+ */
 public class HdsVplexVolumeNameFormatter extends DefaultVplexVolumeNameFormatter {
 
 	public HdsVplexVolumeNameFormatter(VolumeInfo volumeInfo) {
 		super(volumeInfo);
-		//replacing underscores with hyphen (check COP-33663)
+		//replacing underscores with hyphen as underscores are being used as separator between storage system serial 
+		//number and the volume native id when volume name is created. (check COP-33663)
 		_storageSystemSerialNumber = _storageSystemSerialNumber.replace(VPlexApiConstants.UNDERSCORE_OPERATOR,
 				VPlexApiConstants.HYPHEN_OPERATOR);				
 	}

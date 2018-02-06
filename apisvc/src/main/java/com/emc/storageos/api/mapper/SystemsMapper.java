5daba66ca5ecd9f30d7c8e6b848973412d3bb701
sanjes <sanjusha.sanjeev@emc.com>
2018-02-06 12:25:02 +0530

    Fix for COP-32237

--- apisvc/src/main/java/com/emc/storageos/api/mapper/SystemsMapper.java
+++ apisvc/src/main/java/com/emc/storageos/api/mapper/SystemsMapper.java
@@ -516,6 +516,7 @@ public class SystemsMapper {
         to.setStorageDevice(toRelatedResource(ResourceTypeEnum.STORAGE_SYSTEM, from.getStorageDevice()));
         to.setRegistrationStatus(from.getRegistrationStatus());
         to.setNativeGuid(from.getNativeGuid());
+        to.setLink(new RestLinkRep("self", RestLinkFactory.newLink(from)));
         StringMap metrics = from.getMetrics();
         if (metrics != null && !metrics.isEmpty()) {
             Double portMetric = MetricsKeys.getDoubleOrNull(MetricsKeys.portMetric, metrics);

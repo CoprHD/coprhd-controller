f8e8bffb4a81e7785c2bc5cf657683c29b9bb844
Amit Saurabh <amit.saurabh@emc.com>
2018-02-26 00:47:58 -0500

    we should be deleting FC zone directly without removing from switch.

--- controllersvc/src/main/java/com/emc/storageos/networkcontroller/impl/NetworkDeviceController.java
+++ controllersvc/src/main/java/com/emc/storageos/networkcontroller/impl/NetworkDeviceController.java
@@ -2525,10 +2525,18 @@ public class NetworkDeviceController implements NetworkController {
                                     "the port and/or the initiator were removed from the mask", key, val);
                         } else if (removedInitiators.contains(key) || removedPorts.contains(val)) {
                             // the port or initiator were removed, remove the zone map entry
+                            // add a check to see initiator and port has active FCZoneReference or not before removing it.
+                            List<FCZoneReference> fcs = getFcZoneRefrence(exportMask);
+                            if (fcs != null) {
+                                _log.info("Found active FCZoneReference for initiator {} and port {} " +
+                                        "the port and/or initator were not removed from Export mask zoningMap ",
+                                        initiator.getInitiatorPort(), port.getPortNetworkId());
+                            } else {
                             exportMask.getZoningMap().remove(key, val);
                             _log.info("Removed zoningMap entry between initiator {} and port {} because " +
                                     "the port and/or the initiator were removed from the mask",
                                     initiator.getInitiatorPort(), port.getPortNetworkId());
+                            }
                         } else if (exportMask.hasExistingInitiator(
                                 WWNUtility.getUpperWWNWithNoColons(initiator.getInitiatorPort()))) {
                             exportMask.getZoningMap().remove(key, val);
@@ -2541,6 +2549,7 @@ public class NetworkDeviceController implements NetworkController {
                                     " a ViPR initiator-port assignment", initiator.getInitiatorPort(), port.getPortNetworkId());
                         }
                         if (port != null && initiator != null) {
+                            // ideally we should have consider ITL (initiator ,port and volume to determine it.)
                             removedZonesKeys.add(FCZoneReference.makeEndpointsKey(initiator.getInitiatorPort(), port.getPortNetworkId()));
                         }
                     }
@@ -2548,8 +2557,8 @@ public class NetworkDeviceController implements NetworkController {
             }
 
             // get all the existing zone references from the database, these are
-            refreshFCZoneReferences(exportMask,
-                    existingRefs, addedZoneInfos, updatedZoneInfos, removedZonesKeys);
+           //refreshFCZoneReferences(exportMask,
+                   // existingRefs, addedZoneInfos, updatedZoneInfos, removedZonesKeys);
             if (persist) {
                 _dbClient.updateAndReindexObject(exportMask);
             }
@@ -2560,6 +2569,19 @@ public class NetworkDeviceController implements NetworkController {
             _log.error("An exception occurred while updating zoning map for export mask {} with message {}",
                     new Object[] { exportMask.getMaskName(), ex.getMessage() }, ex);
         }
+    }
+
+    private List<FCZoneReference> getFcZoneRefrence(ExportMask exportMask) {
+        List<ExportGroup> exportGroups = ExportUtils.getExportGroupsForMask(exportMask.getId(), _dbClient);
+        for (ExportGroup exportGroup : exportGroups) {
+
+            List<FCZoneReference> fcs = NetworkUtil.getFCZoneReferencesFromExportGroup(_dbClient, exportGroup);
+            if (!CollectionUtils.isEmpty(fcs)) {
+                return fcs;
+            }
+        }
+
+        return null;
     }
 
     /**

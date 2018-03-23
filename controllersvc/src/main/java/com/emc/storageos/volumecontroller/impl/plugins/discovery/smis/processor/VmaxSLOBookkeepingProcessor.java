fb17eaa87b07e4cbfc1702c72c81882459e2a374
lakhiv <vasudevu.lakhinana@emc.com>
2018-03-23 14:54:07 +0530

    Changes to support ELM policy change

--- controllersvc/src/main/java/com/emc/storageos/volumecontroller/impl/plugins/discovery/smis/processor/VmaxSLOBookkeepingProcessor.java
+++ controllersvc/src/main/java/com/emc/storageos/volumecontroller/impl/plugins/discovery/smis/processor/VmaxSLOBookkeepingProcessor.java
@@ -8,9 +8,12 @@ package com.emc.storageos.volumecontroller.impl.plugins.discovery.smis.processor
 import java.io.IOException;
 import java.net.URI;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+
+import javax.cim.CIMInstance;
 
 import com.google.common.base.Joiner;
 import org.slf4j.Logger;
@@ -20,7 +23,9 @@ import com.emc.storageos.db.client.DbClient;
 import com.emc.storageos.db.client.constraint.ContainmentConstraint;
 import com.emc.storageos.db.client.constraint.URIQueryResultList;
 import com.emc.storageos.db.client.model.AutoTieringPolicy;
+import com.emc.storageos.db.client.model.BlockMirror;
 import com.emc.storageos.db.client.model.StorageSystem;
+import com.emc.storageos.db.client.model.Volume;
 import com.emc.storageos.plugins.AccessProfile;
 import com.emc.storageos.plugins.BaseCollectionException;
 import com.emc.storageos.plugins.common.Constants;
@@ -46,6 +51,9 @@ public class VmaxSLOBookkeepingProcessor extends Processor {
         boolean isVmax3 = storageSystem.checkIfVmax3();
         if (isVmax3 && policyNames != null && !policyNames.isEmpty()) {
             try {
+                if(storageSystem.isV3ElmCodeOrMore()) {
+                    updateBlockObjectsWithPolicyBookKeeping(dbClient, policyNames, storageSystem);
+                }
                 performPolicyBookKeeping(dbClient, policyNames, storageSystem);
             } catch (IOException e) {
                 log.error("Exception caught while trying to run bookkeeping on VMAX3 SLO AutoTieringPolicies", e);
@@ -64,7 +72,15 @@ public class VmaxSLOBookkeepingProcessor extends Processor {
     private void performPolicyBookKeeping(DbClient dbClient, Set<String> policyNames, StorageSystem storageSystem) throws IOException {
         log.debug(String.format("SLO policyNames found by discovery for array %s:%n%s", storageSystem.getNativeGuid(),
                 Joiner.on(',').join(policyNames)));
-        List<AutoTieringPolicy> policiesToUpdate = new ArrayList<>();
+        List<AutoTieringPolicy> policiesToUpdate = getPoliciesToBeDeleted(dbClient, policyNames, storageSystem);
+        if (!policiesToUpdate.isEmpty()) {
+            dbClient.updateAndReindexObject(policiesToUpdate);
+        }
+    }
+    
+    private List<AutoTieringPolicy> getPoliciesToBeDeleted(DbClient dbClient, Set<String> policyNames, StorageSystem storageSystem) throws IOException {
+        
+        List<AutoTieringPolicy> policiesToDelete = new ArrayList<>();
         URIQueryResultList policiesInDB = new URIQueryResultList();
         dbClient.queryByConstraint(ContainmentConstraint.Factory.getStorageDeviceFASTPolicyConstraint(storageSystem.getId()), policiesInDB);
         for (URI policy : policiesInDB) {
@@ -80,11 +96,63 @@ public class VmaxSLOBookkeepingProcessor extends Processor {
                 policyObject.setPolicyEnabled(false);
                 policyObject.getPools().clear();
                 policyObject.setInactive(true);
-                policiesToUpdate.add(policyObject);
+                policiesToDelete.add(policyObject);
             }
         }
-        if (!policiesToUpdate.isEmpty()) {
-            dbClient.updateAndReindexObject(policiesToUpdate);
-        }
+        return policiesToDelete; 
     }
+    
+    private void updateBlockObjectsWithPolicyBookKeeping(DbClient dbClient, Set<String> policyNames, StorageSystem storageSystem) throws IOException {
+        log.debug(String.format("SLO policyNames found by discovery for array %s:%n%s", storageSystem.getNativeGuid(),
+                Joiner.on(',').join(policyNames)));
+        List<AutoTieringPolicy> policiesToDelete = getPoliciesToBeDeleted(dbClient, policyNames, storageSystem);
+        
+        for(AutoTieringPolicy policyToDelete: policiesToDelete){
+            // Find any Volume or Mirror having reference to this 
+        }
+        
+    }
+    
+    private List<Volume> getVolumesHasPolicy(DbClient dbClient, AutoTieringPolicy policyObject) {
+        // Look for volumes through pools as there is no relation index between Policy and Volume
+        List<Volume> volumesWithPolicy = new ArrayList<Volume>();
+        if (policyObject.getPools() != null) {
+            for (String pool : policyObject.getPools()) {
+                URIQueryResultList volumeList = new URIQueryResultList();
+                dbClient.queryByConstraint(ContainmentConstraint.Factory
+                        .getStoragePoolVolumeConstraint(URI.create(pool)), volumeList);
+                Iterator<Volume> volumeIterator = dbClient.queryIterativeObjects(Volume.class,
+                        volumeList, true);
+                while (volumeIterator.hasNext()) {
+                    Volume vol = volumeIterator.next();
+                    if(vol != null && !vol.getInactive()){
+                        volumesWithPolicy.add(vol);
+                    }
+                }
+            }
+        }
+        return volumesWithPolicy;
+    }
+    
+    private List<BlockMirror> getMirrorsHasPolicy(DbClient dbClient, AutoTieringPolicy policyObject) {
+        // Look for volumes through pools as there is no relation index between Policy and Volume
+        List<BlockMirror> mirrorsWithPolicy = new ArrayList<BlockMirror>();
+        if (policyObject.getPools() != null) {
+            for (String pool : policyObject.getPools()) {
+                URIQueryResultList mirrorsList = new URIQueryResultList();
+                dbClient.queryByConstraint(ContainmentConstraint.Factory
+                        .getStoragePoolBlockMirrorConstraint(URI.create(pool)), mirrorsList);
+                Iterator<BlockMirror> mirrorIterator = dbClient.queryIterativeObjects(BlockMirror.class,
+                        mirrorsList, true);
+                while (mirrorIterator.hasNext()) {
+                    BlockMirror mirror = mirrorIterator.next();
+                    if(mirror != null && !mirror.getInactive()){
+                        mirrorsWithPolicy.add(mirror);
+                    }
+                }
+            }
+        }
+        return mirrorsWithPolicy;
+    }
+    
 }

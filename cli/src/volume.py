e9aab615ebbb6901add9820ffc07c9a7ef7048c7
Ananya Garg <ananya.garg@emc.com>
2018-03-23 11:50:28 +0530

    comment addressed

--- cli/src/volume.py
+++ cli/src/volume.py
@@ -2452,7 +2452,7 @@ def volume_create(args):
         if(not args.tenant):
             args.tenant = ""
         if(args.portgroupname):
-            if(not args.storagesystem and not args.serialnumber):
+            if((not args.storagesystem and not args.type)  and not args.serialnumber):
                 raise SOSError(SOSError.CMD_LINE_ERR, 'error: Please enter either Serial Number or Storage Device Name for PortGroupName. If Storage Device Name is being given, also give Type of device')
         res = obj.create(
             args.tenant + "/" + args.project, args.name, size,

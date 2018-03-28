3f612222894a013731a3fb2db154dac275b5e887
Prashant Rakheja <Prashant.Rakheja@emc.com>
2018-03-28 20:37:11 +0000

    Remove logging the command as it results in logging of private key
    Signed-off-by: Prashant Rakheja <Prashant.Rakheja@emc.com>

--- internalLibraries/serviceutils/src/main/java/com/emc/storageos/services/util/Exec.java
+++ internalLibraries/serviceutils/src/main/java/com/emc/storageos/services/util/Exec.java
@@ -254,7 +254,6 @@ public class Exec {
                     p.exitValue(), stdOutput.toString(), stdError.toString(),
                     (destroyed && exitValue != 0) ? Termination._TIMEOUT : Termination._NORMAL, maskFilter);
 
-            _log.error("exec(): " + result);
             return result;
         } catch (Exception e) {
             Result result = new Result(cmd, timeout,
